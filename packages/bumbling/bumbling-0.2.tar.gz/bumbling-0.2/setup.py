6b05ee45f1c0fcacf348c087a9220fe5eedfcaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:30 +0000

    Add package bumbling/0.2/bumbling-0.2.tar.gz

---
+++ packages/bumbling/bumbling-0.2.tar.gz/bumbling-0.2/setup.py
@@ -0,0 +1,18 @@
+#   Jonathan Roth
+#   jonathanroth@protonmail.com
+#   https://github.com/JonathanRoth13
+#   2022-03-03
+
+from setuptools import setup, find_packages
+
+setup(
+    name="bumbling",
+    version="0.2",
+    license="MIT",
+    author="Jonathan Roth",
+    author_email="JonathanRoth@protonmail.com",
+    py_modules=["src.bumbling"],
+    url="https://github.com/JonathanRoth13/bumbling",
+    install_requires=["PyExifTool"],
+    entry_points={"console_scripts": ["bumbling=bumbling:main"]},
+)
