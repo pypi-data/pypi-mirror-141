09c14bd426426c814c8fefb351b3595554eaa20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:43 +0000

    Add package openmetadata-ingestion/0.8.12/openmetadata_ingestion-0.8.12-py3-none-any.whl

---
+++ packages/openmetadata-ingestion/openmetadata_ingestion-0.8.12-py3-none-any.whl/metadata/orm_profiler/validations/core.py
@@ -0,0 +1,57 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""
+Core Validation definitions.
+
+In this module we define how to check specific test case
+behavior based on the computed metrics of the profiler.
+
+These functions should not raise an error, but rather
+mark the test as Failure/Aborted and pass a proper
+result string. The ORM Processor will be the one in charge
+of logging these issues.
+"""
+from functools import singledispatch
+
+from metadata.generated.schema.tests.basic import TestCaseResult
+from metadata.orm_profiler.utils import logger
+from metadata.orm_profiler.validations.column.column_values_to_be_between import (
+    column_values_to_be_between,
+)
+from metadata.orm_profiler.validations.column.column_values_to_be_unique import (
+    column_values_to_be_unique,
+)
+from metadata.orm_profiler.validations.table.table_row_count_to_equal import (
+    table_row_count_to_equal,
+)
+
+logger = logger()
+
+
+@singledispatch
+def validate(test_case, *args, **kwargs) -> TestCaseResult:
+    """
+    Default function to validate test cases.
+
+    Note that the first argument should be a positional argument.
+    """
+    raise NotImplementedError(
+        f"Missing test case validation implementation for {type(test_case)}."
+    )
+
+
+# Table Tests
+validate.register(table_row_count_to_equal)
+
+# Column Tests
+validate.register(column_values_to_be_between)
+validate.register(column_values_to_be_unique)
