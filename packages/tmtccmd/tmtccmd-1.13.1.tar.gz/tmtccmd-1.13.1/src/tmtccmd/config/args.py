78eb351f201271174b72cdc00a68165e6b95a21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:23 +0000

    Add package tmtccmd/1.13.1/tmtccmd-1.13.1.tar.gz

---
+++ packages/tmtccmd/tmtccmd-1.13.1.tar.gz/tmtccmd-1.13.1/src/tmtccmd/config/args.py
@@ -0,0 +1,396 @@
+"""
+Argument parser modules for the TMTC commander core
+"""
+import argparse
+import sys
+
+from prompt_toolkit.completion import WordCompleter
+import prompt_toolkit
+from tmtccmd.config.definitions import (
+    CoreModeList,
+    ServiceOpCodeDictT,
+    OpCodeEntryT,
+    OpCodeDictKeys,
+)
+from tmtccmd.utility.logger import get_console_logger
+
+
+LOGGER = get_console_logger()
+
+
+def parse_input_arguments(
+    print_known_args: bool = False, print_unknown_args: bool = False
+) -> argparse.Namespace:
+    try:
+        from tmtccmd.config.hook import get_global_hook_obj
+
+        hook_obj = get_global_hook_obj()
+        args = hook_obj.custom_args_parsing()
+        if args is None:
+            return parse_default_input_arguments(print_known_args, print_unknown_args)
+    except ImportError:
+        return parse_default_input_arguments(print_known_args, print_unknown_args)
+
+
+def parse_default_input_arguments(
+    print_known_args: bool = False, print_unknown_args: bool = False
+):
+    """Parses all input arguments
+    :return: Input arguments contained in a special namespace and accessable by args.<variable>
+    """
+    from tmtccmd.utility.conf_util import AnsiColors
+
+    descrip_text = (
+        f"{AnsiColors.GREEN}TMTC Client Command Line Interface\n"
+        f"{AnsiColors.RESET}This application provides generic components to execute "
+        f"TMTC commanding.\n"
+        f"The developer is expected to specify the packaged telecommands for a given\n"
+        "service and operation code combination. The developer is also expected\n"
+        "to implement the handling of telemetry. All these tasks can be done by implementing\n"
+        "a hook object and passing it to the core."
+    )
+    arg_parser = argparse.ArgumentParser(
+        description=descrip_text, formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    add_default_mode_arguments(arg_parser)
+    add_default_com_if_arguments(arg_parser)
+    add_generic_arguments(arg_parser)
+
+    # TODO: Only add this if TMTC commander is configured for Ethernet?
+    add_ethernet_arguments(arg_parser)
+
+    arg_parser.add_argument(
+        "--tc_timeout_factor",
+        type=float,
+        help="TC Timeout Factor. Multiplied with "
+        "TM Timeout, TC sent again after this time period. Default: 3.5",
+        default=3.5,
+    )
+    arg_parser.add_argument(
+        "-r",
+        "--raw_data_print",
+        help="Supply -r to print all raw TM data directly",
+        action="store_true",
+    )
+    arg_parser.add_argument(
+        "-d",
+        "--short_display_mode",
+        help="Supply -d to print short output",
+        action="store_true",
+    )
+    arg_parser.add_argument(
+        "--hk",
+        dest="print_hk",
+        help="Supply -k or --hk to print HK data",
+        action="store_true",
+    )
+    arg_parser.add_argument(
+        "--rs",
+        dest="resend_tc",
+        help="Specify whether TCs are sent again after timeout",
+        action="store_true",
+    )
+
+    if len(sys.argv) == 1:
+        LOGGER.info(
+            "No input arguments specified. Run with -h to get list of arguments"
+        )
+
+    args, unknown = arg_parser.parse_known_args()
+
+    if print_known_args:
+        LOGGER.info("Printing known arguments:")
+        for argument in vars(args):
+            LOGGER.debug(argument + ": " + str(getattr(args, argument)))
+    if print_unknown_args:
+        LOGGER.info("Printing unknown arguments:")
+        for argument in unknown:
+            LOGGER.info(argument)
+
+    args_post_processing(args, unknown)
+    return args
+
+
+def add_generic_arguments(arg_parser: argparse.ArgumentParser):
+    arg_parser.add_argument(
+        "-s", "--service", type=str, help="Service to test", default=None
+    )
+    arg_parser.add_argument(
+        "-o",
+        "--op_code",
+        help="Operation code, which is passed to the TC packer functions",
+        default=None,
+    )
+    arg_parser.add_argument(
+        "-l",
+        "--listener",
+        help="Determine whether the listener mode will be active after performing the operation",
+        action="store_true",
+        default=None,
+    )
+    arg_parser.add_argument(
+        "-t",
+        "--tm_timeout",
+        type=float,
+        help="TM Timeout when listening to verification sequence."
+        " Default: 5 seconds",
+        default=5.0,
+    )
+    arg_parser.add_argument(
+        "--nl",
+        dest="print_log",
+        help="Supply --nl to suppress print output to log files.",
+        action="store_false",
+    )
+    arg_parser.add_argument(
+        "--np",
+        dest="print_tm",
+        help="Supply --np to suppress print output to console.",
+        action="store_false",
+    )
+
+
+def add_default_mode_arguments(arg_parser: argparse.ArgumentParser):
+    from tmtccmd.config.definitions import CoreModeList, CoreModeStrings
+
+    help_text = "Core Modes. Default: seqcmd\n"
+    seq_help = (
+        f"{CoreModeList.SEQUENTIAL_CMD_MODE} or "
+        f"{CoreModeStrings[CoreModeList.SEQUENTIAL_CMD_MODE]}: "
+        f"Sequential Command Mode\n"
+    )
+    listener_help = (
+        f"{CoreModeList.LISTENER_MODE} or {CoreModeStrings[CoreModeList.LISTENER_MODE]}: "
+        f"Listener Mode\n"
+    )
+    gui_help = (
+        f"{CoreModeList.GUI_MODE} or "
+        f"{CoreModeStrings[CoreModeList.GUI_MODE]}: "
+        f"GUI mode\n"
+    )
+    help_text += seq_help + listener_help + gui_help
+    arg_parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        help=help_text,
+        default=CoreModeStrings[CoreModeList.SEQUENTIAL_CMD_MODE],
+    )
+
+
+def add_default_com_if_arguments(arg_parser: argparse.ArgumentParser):
+    from tmtccmd.config.definitions import CoreComInterfacesDict, CoreComInterfaces
+
+    help_text = (
+        "Core Communication Interface. If this is not specified, the commander core\n"
+        "will try to extract it from the JSON or prompt it from the user.\n"
+    )
+    dummy_line = (
+        f"{CoreComInterfacesDict[CoreComInterfaces.DUMMY.value]}: Dummy Interface\n"
+    )
+    udp_line = (
+        f"{CoreComInterfacesDict[CoreComInterfaces.TCPIP_UDP.value]}: " f"UDP client\n"
+    )
+    ser_dle_line = (
+        f"{CoreComInterfacesDict[CoreComInterfaces.SERIAL_DLE.value]}: "
+        f"Serial with DLE transport layer\n"
+    )
+    ser_fixed_line = (
+        f"{CoreComInterfacesDict[CoreComInterfaces.SERIAL_FIXED_FRAME.value]}: "
+        f"Serial with fixed frames\n"
+    )
+    ser_qemu_line = (
+        f"{CoreComInterfacesDict[CoreComInterfaces.SERIAL_QEMU.value]}: "
+        f"QEMU serial interface\n"
+    )
+    help_text += dummy_line + ser_dle_line + udp_line + ser_fixed_line + ser_qemu_line
+    arg_parser.add_argument(
+        "-c",
+        "--com_if",
+        type=str,
+        help=help_text,
+        default=CoreComInterfaces.UNSPECIFIED.value,
+    )
+
+
+def add_ethernet_arguments(arg_parser: argparse.ArgumentParser):
+    arg_parser.add_argument(
+        "--clientIP", help="Client(Computer) IP. Default:''", default=""
+    )
+    arg_parser.add_argument(
+        "--boardIP", help="Board IP. Default: Localhost 127.0.0.1", default="127.0.0.1"
+    )
+
+
+def args_post_processing(args, unknown: list) -> None:
+    """Handles the parsed arguments.
+    :param args: Namespace objects (see https://docs.python.org/dev/library/argparse.html#argparse.Namespace)
+    :param unknown: List of unknown parameters.
+    :return: None
+    """
+    if len(unknown) > 0:
+        print("Unknown arguments detected: " + str(unknown))
+    if len(sys.argv) > 1:
+        handle_unspecified_args(args)
+    if len(sys.argv) == 1:
+        handle_empty_args(args)
+
+
+def handle_unspecified_args(args) -> None:
+    """If some arguments are unspecified, they are set here with (variable) default values.
+    :param args:
+    :return: None
+    """
+    from tmtccmd.config.hook import get_global_hook_obj
+    from tmtccmd.config.definitions import CoreModeStrings
+
+    if args.tm_timeout is None:
+        args.tm_timeout = 5.0
+    if args.mode is None:
+        args.mode = CoreModeStrings[CoreModeList.SEQUENTIAL_CMD_MODE]
+    service_op_code_dict = dict()
+    if args.service is None or args.op_code is None:
+        hook_obj = get_global_hook_obj()
+        service_op_code_dict = hook_obj.get_service_op_code_dictionary()
+    if args.service is None:
+        if args.mode == CoreModeStrings[CoreModeList.SEQUENTIAL_CMD_MODE]:
+            LOGGER.info("No service argument (-s) specified, prompting from user..")
+            # Try to get the service list from the hook base and prompt service from user
+            args.service = prompt_service(service_op_code_dict)
+            if args.op_code is None:
+                current_service = args.service
+                args.op_code = prompt_op_code(
+                    service_op_code_dict=service_op_code_dict, service=current_service
+                )
+    elif args.op_code is None:
+        current_service = args.service
+        args.op_code = prompt_op_code(
+            service_op_code_dict=service_op_code_dict, service=current_service
+        )
+    op_code_value = service_op_code_dict[args.service][1]
+    op_code_options = op_code_value[args.op_code][1]
+    if op_code_options is not None and isinstance(op_code_options, dict):
+        if op_code_options.get(OpCodeDictKeys.ENTER_LISTENER_MODE):
+            if args.listener is None:
+                LOGGER.info(
+                    "Detected op code configuration: Enter listener mode after command"
+                )
+                args.listener = True
+            else:
+                LOGGER.warning(
+                    "Detected op code listerner mode configuration but is overriden by CLI argument"
+                )
+        timeout = op_code_options.get(OpCodeDictKeys.TIMEOUT)
+        if timeout is not None:
+            if args.tm_timeout is None:
+                LOGGER.info(
+                    f"Detected op code configuration: Set custom timeout {timeout}"
+                )
+                args.tm_timeout = timeout
+            else:
+                LOGGER.warning(
+                    "Detected op code timeout configuration but is overriden by CLI argument"
+                )
+    if args.listener is None:
+        args.listener = False
+
+
+def handle_empty_args(args) -> None:
+    """If no args were supplied, request input from user directly.
+    :param args:
+    :return:
+    """
+    LOGGER.info("No arguments specified..")
+    handle_unspecified_args(args=args)
+
+
+def prompt_service(service_op_code_dict: ServiceOpCodeDictT) -> str:
+    service_adjustment = 20
+    info_adjustment = 30
+    horiz_line_num = service_adjustment + info_adjustment + 3
+    horiz_line = horiz_line_num * "-"
+    service_string = "Service".ljust(service_adjustment)
+    info_string = "Information".ljust(info_adjustment)
+    while True:
+        print(f" {horiz_line}")
+        print(f"|{service_string} | {info_string}|")
+        print(f" {horiz_line}")
+        srv_completer = build_service_word_completer(
+            service_op_code_dict=service_op_code_dict
+        )
+        for service_entry in service_op_code_dict.items():
+            try:
+                adjusted_service_entry = service_entry[0].ljust(service_adjustment)
+                adjusted_service_info = service_entry[1][0].ljust(info_adjustment)
+                print(f"|{adjusted_service_entry} | {adjusted_service_info}|")
+            except AttributeError:
+                LOGGER.warning(
+                    f"Error handling service entry {service_entry[0]}. Skipping.."
+                )
+        print(f" {horiz_line}")
+        service_string = prompt_toolkit.prompt(
+            "Please select a service by specifying the key: ", completer=srv_completer
+        )
+        if service_string in service_op_code_dict:
+            LOGGER.info(f"Selected service: {service_string}")
+            return service_string
+        else:
+            LOGGER.warning("Invalid key, try again")
+
+
+def build_service_word_completer(
+    service_op_code_dict: ServiceOpCodeDictT,
+) -> WordCompleter:
+    srv_list = []
+    for service_entry in service_op_code_dict.items():
+        srv_list.append(service_entry[0])
+    srv_completer = WordCompleter(words=srv_list, ignore_case=True)
+    return srv_completer
+
+
+def prompt_op_code(service_op_code_dict: ServiceOpCodeDictT, service: str) -> str:
+    op_code_adjustment = 24
+    info_adjustment = 56
+    horz_line_num = op_code_adjustment + info_adjustment + 3
+    horiz_line = horz_line_num * "-"
+    op_code_info_str = "Operation Code".ljust(op_code_adjustment)
+    info_string = "Information".ljust(info_adjustment)
+    while True:
+        print(f" {horiz_line}")
+        print(f"|{op_code_info_str} | {info_string}|")
+        print(f" {horiz_line}")
+        if service in service_op_code_dict:
+            op_code_dict = service_op_code_dict[service][1]
+            completer = build_op_code_word_completer(
+                service=service, op_code_dict=op_code_dict
+            )
+            for op_code_entry in op_code_dict.items():
+                adjusted_op_code_entry = op_code_entry[0].ljust(op_code_adjustment)
+                adjusted_op_code_info = op_code_entry[1][0].ljust(info_adjustment)
+                print(f"|{adjusted_op_code_entry} | {adjusted_op_code_info}|")
+            print(f" {horiz_line}")
+            op_code_string = prompt_toolkit.prompt(
+                "Please select an operation code by specifying the key: ",
+                completer=completer,
+            )
+            if op_code_string in op_code_dict:
+                LOGGER.info(f"Selected op code: {op_code_string}")
+                return op_code_string
+            else:
+                LOGGER.warning("Invalid key, try again")
+        else:
+            LOGGER.warning(
+                "Service not in dictionary. Setting default operation code 0"
+            )
+            return "0"
+
+
+def build_op_code_word_completer(
+    service: str, op_code_dict: OpCodeEntryT
+) -> WordCompleter:
+    op_code_list = []
+    for op_code_entry in op_code_dict.items():
+        op_code_list.append(op_code_entry[0])
+    op_code_completer = WordCompleter(words=op_code_list, ignore_case=True)
+    return op_code_completer
---
+++ packages/tmtccmd/tmtccmd-1.13.1.tar.gz/tmtccmd-1.13.1/src/tmtccmd/runner.py
@@ -0,0 +1,299 @@
+"""Contains core methods called by entry point files to initiate the TMTC commander.
+
+The commander is started by first running :py:func:`initialize_tmtc_commander` and then
+running :py:func:`run_tmtc_commander`
+"""
+import sys
+import os
+from typing import Union
+
+from spacepackets.ecss.conf import get_default_tc_apid
+
+from tmtccmd import __version__
+from tmtccmd.config.hook import TmTcHookBase
+from tmtccmd.core.backend import BackendBase
+from tmtccmd.core.frontend_base import FrontendBase
+from tmtccmd.config.definitions import CoreGlobalIds
+from tmtccmd.tm.definitions import TmTypes
+from tmtccmd.tm.handler import TmHandler
+from tmtccmd.ccsds.handler import CcsdsTmHandler
+from tmtccmd.core.globals_manager import (
+    update_global,
+    get_global,
+    lock_global_pool,
+    unlock_global_pool,
+)
+from tmtccmd.core.object_id_manager import insert_object_ids
+from tmtccmd.config.args import parse_input_arguments
+from tmtccmd.config.objects import get_core_object_ids
+from tmtccmd.utility.logger import set_tmtc_console_logger, get_console_logger
+from tmtccmd.utility.conf_util import AnsiColors
+
+
+LOGGER = get_console_logger()
+
+
+def get_tmtccmd_version() -> str:
+    return __version__
+
+
+def initialize_tmtc_commander(hook_object: TmTcHookBase):
+    """This function needs to be called first before running the TMTC commander core. A hook
+    object handle needs to be passed to this function. The user should implement an own hook class
+    instance which in turn implemented TmTcHookBase. An instantiation of the hook object is then
+    passed to the core. The hook object ecncapsulates the control of the user over the TMTC
+    commander core.
+
+    Example for a simple main function content to use the command line mode:
+
+        hook_obj = MyCustomHookClass()
+        initialize_tmtccmd(hook_obj)
+        run_tmtc_client(False)
+
+    :param hook_object:     Instantiation of a custom hook object. The TMTC core will call the
+                            various hook functions during program run-time.
+    :raises: ValueError for an invalid hook object.
+    """
+    if os.name == "nt":
+        import colorama
+
+        colorama.init()
+
+    __assign_tmtc_commander_hooks(hook_object=hook_object)
+
+
+def add_ccsds_handler(ccsds_handler: CcsdsTmHandler):
+    """Add a handler for CCSDS space packets, for example PUS packets
+    :param pus_handler:
+    :return:
+    """
+    lock_global_pool()
+    tm_handler = get_global(CoreGlobalIds.TM_HANDLER_HANDLE)
+    if tm_handler is None:
+        update_global(CoreGlobalIds.TM_HANDLER_HANDLE, ccsds_handler)
+    unlock_global_pool()
+
+
+def run_tmtc_commander(
+    use_gui: bool,
+    reduced_printout: bool = False,
+    ansi_colors: bool = True,
+    tmtc_backend: Union[BackendBase, None] = None,
+    tmtc_frontend: Union[FrontendBase, None] = None,
+    app_name: str = "TMTC Commander",
+):
+    """This is the primary function to run the TMTC commander. Users should call this function to
+    start the TMTC commander. Please note that :py:func:`initialize_tmtc_commander` needs to be
+    called before this function. Raises RuntimeError if :py:func:`initialize_tmtc_commander`
+    has not been called before calling this function.
+
+    :param use_gui:             Specify whether the GUI is used or not
+    :param reduced_printout:    It is possible to reduce the initial printout with this flag
+    :param ansi_colors:         Enable ANSI color output for terminal
+    :param tmtc_backend:        Custom backend can be passed here. Otherwise, a default backend
+                                will be created
+    :param tmtc_frontend:       Custom frontend can be passed here. Otherwise, a default backend
+                                will be created
+    :param app_name:            Name of application. Will be displayed in GUI
+    :raises RunTimeError:  if :py:func:`initialize_tmtc_commander` was not called before
+    :return:
+    """
+    try:
+        __set_up_tmtc_commander(
+            use_gui=use_gui, reduced_printout=reduced_printout, ansi_colors=ansi_colors
+        )
+    except ValueError:
+        raise RuntimeError
+
+    if use_gui:
+        __start_tmtc_commander_qt_gui(tmtc_frontend=tmtc_frontend, app_name=app_name)
+    else:
+        if tmtc_backend is None:
+            from tmtccmd.config.hook import get_global_hook_obj
+
+            hook_obj = get_global_hook_obj()
+            json_cfg_path = hook_obj.get_json_config_file_path()
+            tm_handler = get_global(CoreGlobalIds.TM_HANDLER_HANDLE)
+            tmtc_backend = get_default_tmtc_backend(
+                hook_obj=hook_obj, tm_handler=tm_handler, json_cfg_path=json_cfg_path
+            )
+        __start_tmtc_commander_cli(tmtc_backend=tmtc_backend)
+
+
+def __assign_tmtc_commander_hooks(hook_object: TmTcHookBase):
+    if hook_object is None:
+        raise ValueError
+
+    # Check whether all required hook functions have bee implemented properly, Python
+    # does not enforce this.
+    if (
+        hook_object.add_globals_pre_args_parsing is None
+        or hook_object.add_globals_post_args_parsing is None
+    ):
+        LOGGER.error(
+            "Passed hook base object handle is invalid. Abstract functions have to be implemented!"
+        )
+        raise ValueError
+    # Insert hook object handle into global dictionary so it can be used by the TMTC commander
+    update_global(CoreGlobalIds.TMTC_HOOK, hook_object)
+    # Set core object IDs
+    insert_object_ids(get_core_object_ids())
+    # Set object IDs specified by the user.
+    insert_object_ids(hook_object.get_object_ids())
+
+
+def __set_up_tmtc_commander(
+    use_gui: bool,
+    reduced_printout: bool,
+    ansi_colors: bool = True,
+    tmtc_backend: Union[BackendBase, None] = None,
+):
+    """Set up the TMTC commander. Raise ValueError if a passed parameter is invalid.
+    :param use_gui:
+    :param reduced_printout:
+    :param ansi_colors:
+    :param tmtc_backend:
+    :return:
+    """
+    from tmtccmd.config.hook import TmTcHookBase
+    from typing import cast
+
+    set_tmtc_console_logger()
+
+    # First, we check whether a hook object was passed to the TMTC commander. This hook object
+    # encapsulates control of the commnader core so it is required for proper functioning
+    # of the commander core.
+    hook_obj_raw = get_global(CoreGlobalIds.TMTC_HOOK)
+    if hook_obj_raw is None:
+        LOGGER.warning(
+            "No valid hook object found. initialize_tmtc_commander needs to be called first. "
+            "Terminating.."
+        )
+        raise ValueError
+    hook_obj = cast(TmTcHookBase, hook_obj_raw)
+
+    if not reduced_printout:
+        __handle_init_printout(use_gui, ansi_colors)
+
+    LOGGER.info("Starting TMTC Commander..")
+
+    if use_gui:
+        hook_obj.add_globals_pre_args_parsing(True)
+    else:
+        __handle_cli_args_and_globals()
+
+
+def __handle_init_printout(use_gui: bool, ansi_colors: bool):
+    if ansi_colors:
+        print(f"{AnsiColors.CYAN}-- Python TMTC Commander --{AnsiColors.RESET}")
+    if use_gui:
+        print("-- GUI mode --")
+    else:
+        print("-- Command line mode --")
+
+    print(f"-- tmtccmd version v{get_tmtccmd_version()} --")
+
+
+def __handle_cli_args_and_globals():
+    from typing import cast
+    from tmtccmd.core.globals_manager import get_global
+
+    hook_obj = cast(TmTcHookBase, get_global(CoreGlobalIds.TMTC_HOOK))
+    LOGGER.info("Setting up pre-globals..")
+    hook_obj.add_globals_pre_args_parsing(False)
+
+    LOGGER.info("Parsing input arguments..")
+    args = parse_input_arguments()
+    LOGGER.info("Setting up post-globals..")
+    hook_obj.add_globals_post_args_parsing(args=args)
+
+
+def __start_tmtc_commander_cli(tmtc_backend: BackendBase):
+    __get_backend_init_variables()
+    tmtc_backend.initialize()
+    tmtc_backend.start_listener()
+
+
+def __start_tmtc_commander_qt_gui(
+    tmtc_frontend: Union[None, FrontendBase] = None, app_name: str = "TMTC Commander"
+):
+    app = None
+    if tmtc_frontend is None:
+        from tmtccmd.core.frontend import TmTcFrontend
+        from tmtccmd.config.hook import get_global_hook_obj
+
+        try:
+            from PyQt5.QtWidgets import QApplication
+        except ImportError:
+            LOGGER.error("PyQt5 module not installed, can't run GUI mode!")
+            sys.exit(1)
+        app = QApplication([app_name])
+        hook_obj = get_global_hook_obj()
+        json_cfg_path = hook_obj.get_json_config_file_path()
+        tm_handler = get_global(CoreGlobalIds.TM_HANDLER_HANDLE)
+        # The global variables are set by the argument parser.
+        tmtc_backend = get_default_tmtc_backend(
+            hook_obj=hook_obj, tm_handler=tm_handler, json_cfg_path=json_cfg_path
+        )
+        tmtc_frontend = TmTcFrontend(
+            hook_obj=hook_obj, tmtc_backend=tmtc_backend, app_name=app_name
+        )
+    tmtc_frontend.start(app)
+
+
+def __get_backend_init_variables():
+    service = get_global(CoreGlobalIds.CURRENT_SERVICE)
+    op_code = get_global(CoreGlobalIds.OP_CODE)
+    com_if = get_global(CoreGlobalIds.COM_IF)
+    mode = get_global(CoreGlobalIds.MODE)
+    return service, op_code, com_if, mode
+
+
+def get_default_tmtc_backend(
+    hook_obj: TmTcHookBase, tm_handler: TmHandler, json_cfg_path: str
+):
+    from tmtccmd.core.backend import TmTcHandler
+    from tmtccmd.utility.tmtc_printer import TmTcPrinter
+    from tmtccmd.sendreceive.tm_listener import TmListener
+    from typing import cast
+
+    service, op_code, com_if_id, mode = __get_backend_init_variables()
+    display_mode = get_global(CoreGlobalIds.DISPLAY_MODE)
+    print_to_file = get_global(CoreGlobalIds.PRINT_TO_FILE)
+    tmtc_printer = TmTcPrinter(display_mode, print_to_file, True)
+    if tm_handler is None:
+        LOGGER.warning(
+            "No TM Handler specified! Make sure to specify at least one TM handler"
+        )
+        sys.exit(1)
+    else:
+        if tm_handler.get_type() == TmTypes.CCSDS_SPACE_PACKETS:
+            tm_handler = cast(CcsdsTmHandler, tm_handler)
+            tm_handler.initialize(tmtc_printer=tmtc_printer)
+    apid = get_default_tc_apid()
+
+    if json_cfg_path:
+        pass
+    com_if = hook_obj.assign_communication_interface(
+        com_if_key=get_global(CoreGlobalIds.COM_IF), tmtc_printer=tmtc_printer
+    )
+    tc_send_timeout_factor = get_global(CoreGlobalIds.TC_SEND_TIMEOUT_FACTOR)
+    tm_timeout = get_global(CoreGlobalIds.TM_TIMEOUT)
+    tm_listener = TmListener(
+        com_if=com_if, tm_timeout=tm_timeout, tc_timeout_factor=tc_send_timeout_factor
+    )
+    # The global variables are set by the argument parser.
+    tmtc_backend = TmTcHandler(
+        com_if=com_if,
+        tmtc_printer=tmtc_printer,
+        tm_listener=tm_listener,
+        init_mode=mode,
+        init_service=service,
+        init_opcode=op_code,
+        tm_handler=tm_handler,
+    )
+    tmtc_backend.set_current_apid(apid=apid)
+    tmtc_backend.set_one_shot_or_loop_handling(
+        not get_global(CoreGlobalIds.USE_LISTENER_AFTER_OP)
+    )
+    return tmtc_backend
