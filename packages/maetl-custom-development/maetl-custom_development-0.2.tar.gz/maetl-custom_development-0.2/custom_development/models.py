06c3c31b68846bc29fcdc47b7184f422e52398c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:44 +0000

    Add package maetl-custom-development/0.2/maetl-custom_development-0.2.tar.gz

---
+++ packages/maetl-custom-development/maetl-custom_development-0.2.tar.gz/maetl-custom_development-0.2/custom_development/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from custom.models import *
+
+# Create your models here.
+class National(models.Model):
+	name = models.CharField(max_length=200, null=True, verbose_name="Nasional")
+	code = models.CharField(max_length=10, null=True, blank=True)
+	def __str__(self):
+		template = '{0.name}'
+		return template.format(self)
+
+	class Meta:
+		verbose_name_plural='Dadus Custom Ba Nasional'
+
+class ONG(models.Model):
+	name = models.CharField(max_length=30, null=True,verbose_name="ONG")
+
+	def __str__(self):
+		template = '{0.name}'
+		return template.format(self)
+	class Meta:
+		verbose_name_plural='Dadus Custom Ba ONG'
+
+class Agency(models.Model):
+	name = models.CharField(max_length=30, null=True, verbose_name="Ajénsia")
+
+	def __str__(self):
+		template = '{0.name}'
+		return template.format(self)
+
+	class Meta:
+		verbose_name_plural='Dadus Custom Ba Ajénsia'
+
+class Company(models.Model):
+	name = models.CharField(max_length=300, null=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural=' Dadus Custom Ba Kompañia' 
+
