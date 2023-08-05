b0247a9c4a290fc97863215deaa3c84aa8a0ae67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:24 +0000

    Add package invenio-drafts-resources/0.16.0/invenio-drafts-resources-0.16.0.tar.gz

---
+++ packages/invenio-drafts-resources/invenio-drafts-resources-0.16.0.tar.gz/invenio-drafts-resources-0.16.0/tests/services/test_record_service_search.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+
+from .utils import create_and_publish
+
+
+def create_two_versions(service, identity_simple, input_data):
+    """Create 2 versions."""
+    record = create_and_publish(service, identity_simple, input_data)
+    draft = service.new_version(identity_simple, record.id)
+    # new_version resets files.enabled to True
+    service.update_draft(identity_simple, draft.id, input_data)
+    record_2 = service.publish(identity_simple, draft.id)
+    service.record_cls.index.refresh()
+    return [record, record_2]
+
+
+def test_search_versions(app, identity_simple, input_data, service):
+    v1, v2 = create_two_versions(service, identity_simple, input_data)
+
+    # test search defaults to latest version
+    results = service.search(identity_simple)
+    assert set([v2.id]) == set([r["id"] for r in results])
+
+    # test search all versions
+    results = service.search(identity_simple, params={"allversions": True})
+    assert set([v1.id, v2.id]) == set([r["id"] for r in results])
