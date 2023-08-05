ee3d7ec372a063227952fd9cf948a56227e76e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package QScreenCast/0.1.41/QScreenCast-0.1.41.tar.gz

---
+++ packages/QScreenCast/QScreenCast-0.1.41.tar.gz/QScreenCast-0.1.41/QScreenCast/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © Semi-ATE
+# Licensed under the terms of the MIT License
+# ----------------------------------------------------------------------------
+
+from QScreenCast.QScreenCast import ScreenCastToolButton
+
+__version__ = "0.0.0"
+__version__='0.1.41'
