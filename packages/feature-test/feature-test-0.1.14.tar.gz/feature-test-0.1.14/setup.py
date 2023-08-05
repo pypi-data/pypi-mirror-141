0bcb7c36e3b917682325a55ab839f5c39155a138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:04 +0000

    Add package feature-test/0.1.14/feature-test-0.1.14.tar.gz

---
+++ packages/feature-test/feature-test-0.1.14.tar.gz/feature-test-0.1.14/setup.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from setuptools import setup
+
+here = Path(__file__).parent
+README = here.joinpath("README.md").read_text()
+
+setup(
+    name="feature-test",
+    version="0.1.14",
+    description="examines the relationships between a new feature and everyother feature in a dataset",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cee.redhat.com/twileman/feature_test",
+    author="Thomas Wilmean",
+    author_email="twileman@redhat.com",
+    license="MIT License",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["scipy"],
+)
