26dbc1cf2d1961ce13311dd22016281e2b0068cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package dagster/0.14.3rc0/dagster-0.14.3rc0-py3-none-any.whl

---
+++ packages/dagster/dagster-0.14.3rc0-py3-none-any.whl/dagster_tests/core_tests/storage_tests/test_run_storage.py
@@ -0,0 +1,59 @@
+import tempfile
+from contextlib import contextmanager
+
+import pytest
+from dagster_tests.core_tests.storage_tests.utils.run_storage import TestRunStorage
+
+from dagster.core.storage.runs import InMemoryRunStorage, SqliteRunStorage
+
+
+@contextmanager
+def create_sqlite_run_storage():
+    with tempfile.TemporaryDirectory() as tempdir:
+        yield SqliteRunStorage.from_local(tempdir)
+
+
+@contextmanager
+def create_non_bucket_sqlite_run_storage():
+    with tempfile.TemporaryDirectory() as tempdir:
+        yield NonBucketQuerySqliteRunStorage.from_local(tempdir)
+
+
+class NonBucketQuerySqliteRunStorage(SqliteRunStorage):
+    def supports_bucket_query(self):
+        return False
+
+
+@contextmanager
+def create_in_memory_storage():
+    yield InMemoryRunStorage()
+
+
+class TestSqliteImplementation(TestRunStorage):
+    __test__ = True
+
+    @pytest.fixture(name="storage", params=[create_sqlite_run_storage])
+    def run_storage(self, request):
+        with request.param() as s:
+            yield s
+
+
+class TestNonBucketQuerySqliteImplementation(TestRunStorage):
+    __test__ = True
+
+    @pytest.fixture(name="storage", params=[create_non_bucket_sqlite_run_storage])
+    def run_storage(self, request):
+        with request.param() as s:
+            yield s
+
+
+class TestInMemoryImplementation(TestRunStorage):
+    __test__ = True
+
+    @pytest.fixture(name="storage", params=[create_in_memory_storage])
+    def run_storage(self, request):
+        with request.param() as s:
+            yield s
+
+    def test_storage_telemetry(self, storage):
+        pass
