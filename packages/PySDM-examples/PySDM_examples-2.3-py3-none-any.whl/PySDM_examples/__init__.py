75ffb7b319ad197d98a5d02258c146f66240df21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:11 +0000

    Add package PySDM-examples/2.3/PySDM_examples-2.3-py3-none-any.whl

---
+++ packages/PySDM-examples/PySDM_examples-2.3-py3-none-any.whl/PySDM_examples/__init__.py
@@ -0,0 +1,7 @@
+# pylint disable=fixme
+from pkg_resources import get_distribution, DistributionNotFound, VersionConflict
+try:
+    __version__ = get_distribution(__name__).version
+except (DistributionNotFound, VersionConflict):
+    # package is not installed
+    pass
