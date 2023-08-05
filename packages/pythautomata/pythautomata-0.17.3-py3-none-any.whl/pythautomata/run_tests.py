d989173213a1b4f49d4c4f14949c79aa997ea6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:54 +0000

    Add package pythautomata/0.17.3/pythautomata-0.17.3-py3-none-any.whl

---
+++ packages/pythautomata/pythautomata-0.17.3-py3-none-any.whl/pythautomata/run_tests.py
@@ -0,0 +1,34 @@
+from unittest import TestLoader, TestSuite, TextTestRunner
+
+from pythautomata.tests.test_minimizer import TestMinimizer
+from pythautomata.tests.test_automata_convertor import TestAutomataConvertor
+from pythautomata.tests.test_pdfa_generator import TestPDFAGenerator
+from pythautomata.tests.test_pdfa_serialization import TestPDFASerialization
+from pythautomata.tests.test_DFA_generators import TestDFAGenerators
+from pythautomata.tests.test_automata_comparison import TestAutomataComparison
+from pythautomata.tests.test_sequence import TestSequence
+from pythautomata.tests.test_dfa_operations import TestDFAOperations
+from pythautomata.tests.test_automata_definitions import TestAutomataDefinitions
+from pythautomata.tests.test_regex_generator import TestRegexGenerator
+
+
+def get_all_tests():
+    return [TestPDFAGenerator, TestMinimizer, TestAutomataConvertor,
+            TestDFAGenerators, TestAutomataComparison, TestSequence,
+            TestDFAOperations, TestAutomataDefinitions, TestPDFAGenerator,
+            TestRegexGenerator, TestPDFASerialization]
+
+
+def run():
+    test_classes_to_run = get_all_tests()
+    loader = TestLoader()
+    suites_list = []
+    for test_class in test_classes_to_run:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites_list.append(suite)
+    meta_suite = TestSuite(suites_list)
+    TextTestRunner().run(meta_suite)
+
+
+if __name__ == '__main__':
+    run()
