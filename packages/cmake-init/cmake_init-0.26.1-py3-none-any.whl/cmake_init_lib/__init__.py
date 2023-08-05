e71b8860870e51b6fa1a91424cd7752d05ac793c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:52 +0000

    Add package cmake-init/0.26.1/cmake_init-0.26.1-py3-none-any.whl

---
+++ packages/cmake-init/cmake_init-0.26.1-py3-none-any.whl/cmake_init_lib/__init__.py
@@ -0,0 +1,16 @@
+import os
+import zipfile
+
+from .cmake_init import main
+
+def pypi_main():
+    zip = zipfile.ZipFile(
+        os.path.join(os.path.dirname(__file__), "cmake-init.zip"),
+        "r",
+    )
+    try:
+        # open a dummy fd to keep the zip from being closed
+        with zip.open("templates/common/.gitignore") as dummy_fp:
+            main(zip)
+    except KeyboardInterrupt:
+        pass
