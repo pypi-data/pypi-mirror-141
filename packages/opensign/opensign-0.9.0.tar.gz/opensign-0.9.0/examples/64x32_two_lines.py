edef945d892d9345ba7348c4ed8de31d252ab1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package opensign/0.9.0/opensign-0.9.0.tar.gz

---
+++ packages/opensign/opensign-0.9.0.tar.gz/opensign-0.9.0/examples/64x32_two_lines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from opensign import OpenSign
+from opensign.canvas import OpenSignCanvas
+
+
+def main():
+    message = OpenSignCanvas()
+    message.add_font("dejavu", "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+    message.set_stroke(1, (255, 255, 255))
+    message.add_text("Hello\nWorld!", color=(0, 255, 0))
+
+    sign = OpenSign(columns=64, rows=32, slowdown_gpio=2)
+    while True:
+        sign.scroll_in_from_left(message)
+        sign.sleep(1)
+        sign.scroll_out_to_right(message)
+        sign.sleep(1)
+
+
+# Main function
+if __name__ == "__main__":
+    main()
