e78999a4ac48e5d9eb24e2d56c5c78c922794d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package lexref/1.0/lexref-1.0.tar.gz

---
+++ packages/lexref/lexref-1.0.tar.gz/lexref-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="lexref",
+    author="Martin Heimsoth",
+    author_email="mail@lexparency.org",
+    url="https://github.com/Lexparency/lexref",
+    description="",
+    version="1.0",
+    packages=['lexref'],
+    package_data={
+        '': ['static/named_entity.csv'],
+    },
+    install_requires=['anytree', 'lxml'],
+)
