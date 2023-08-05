e099387e5b4f4d11d62c6034a5937f98ba0ad772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:30 +0000

    Add package cellworld-cotroller-service/0.0.1/cellworld_cotroller_service-0.0.1.tar.gz

---
+++ packages/cellworld-cotroller-service/cellworld_cotroller_service-0.0.1.tar.gz/cellworld_cotroller_service-0.0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='cellworld_cotroller_service',author='German Espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld_controller_service'],version='0.0.1',zip_safe=False)
