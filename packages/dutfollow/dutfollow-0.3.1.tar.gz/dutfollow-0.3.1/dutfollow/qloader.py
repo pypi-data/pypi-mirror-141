a9d39522ff11826c9c0e328b3dc9b1191a9879ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package dutfollow/0.3.1/dutfollow-0.3.1.tar.gz

---
+++ packages/dutfollow/dutfollow-0.3.1.tar.gz/dutfollow-0.3.1/dutfollow/qloader.py
@@ -0,0 +1,13 @@
+import json
+
+qloaded = {}
+
+def qLoad(s: str):
+    if s in qloaded:
+        return qloaded[s]
+    else:
+        f = open(f"{s}.txt", "r")
+        t = json.loads(f.read())
+        f.close()
+        qloaded[s] = t
+        return t
