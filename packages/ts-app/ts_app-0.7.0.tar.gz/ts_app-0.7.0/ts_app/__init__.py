71e03647e87d1da1b15c5999fe87ee4a2eb7d5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:44 +0000

    Add package ts-app/0.7.0/ts_app-0.7.0.tar.gz

---
+++ packages/ts-app/ts_app-0.7.0.tar.gz/ts_app-0.7.0/ts_app/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import webbrowser
+
+from waitress import serve
+
+from ts_app.cli import process_cli_args
+from ts_app.dashboard import server
+
+__version__ = "0.7.0"
+
+logging.basicConfig(level="INFO")
+
+# Suppress benign queue warnings
+logging.getLogger("waitress.queue").setLevel("ERROR")
+
+
+def run_app(
+    host: str = "0.0.0.0", port: int = 8000, launch_browser: bool = True
+) -> None:
+    """Start the app server, and launch a web browser to it.
+
+    Parameters
+    ----------
+    host : str, optional
+        A host-name or IP address, default "0.0.0.0".
+    port : int, optional
+        The TCP port on which to listen, default 8000.
+    launch_browser : bool
+        Whether or not to launch a web browser to view the app.
+    """
+    if launch_browser is True:
+        webbrowser.open(f"{host}:{port}")
+
+    serve(server, host=host, port=port)
+
+
+def _run_in_cli() -> None:
+    """Start the app server using input collected from the command line
+    interface.
+    """
+    args = process_cli_args()
+
+    if not args.no_browser:
+        webbrowser.open(f"{args.host}:{args.port}")
+
+    serve(server, host=args.host.split("://")[-1], port=args.port)
