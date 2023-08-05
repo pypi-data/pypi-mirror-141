ada8443a160dbe40805a773f73ddb28cfc1742e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:57 +0000

    Add package bda-service-template/0.0.4/bda_service_template-0.0.4-py3-none-any.whl

---
+++ packages/bda-service-template/bda_service_template-0.0.4-py3-none-any.whl/bdaservicetemplate/pandas_service.py
@@ -0,0 +1,22 @@
+from .spark_service import SparkService
+import os
+import glob
+import pandas as pd
+
+class PandasService(SparkService):
+    def __init__(self):
+        super().__init__()
+
+    def get_dataset(self):
+        spark_df = super().get_dataset().toPandas()
+
+        # Save dataset to tmp file and read it using pandas
+        path = "/tmp"
+        spark_df.repartition(1).write.format("com.databricks.spark.csv").option("header", "false").save(path)
+        os.chdir(path)
+        result = glob.glob('*.{}'.format("csv"))
+        return pd.read_csv(result[0])
+
+
+    def save_dataset(self, dataset):
+        super().save_dataset(self.spark.createDataFrame(dataset))
