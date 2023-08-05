f5b391a9bea065049559d7525da93510500e913f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:21 +0000

    Add package quara-poetry-core-next/1.1.0a6/quara-poetry-core-next-1.1.0a6.tar.gz

---
+++ packages/quara-poetry-core-next/quara-poetry-core-next-1.1.0a6.tar.gz/quara-poetry-core-next-1.1.0a6/tests/pyproject/conftest.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def pyproject_toml(tmp_path: Path) -> Path:
+    path = tmp_path / "pyproject.toml"
+    with path.open(mode="w"):
+        pass
+    return path
+
+
+@pytest.fixture
+def build_system_section(pyproject_toml: Path) -> str:
+    content = """
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
+"""
+    with pyproject_toml.open(mode="a") as f:
+        f.write(content)
+    return content
+
+
+@pytest.fixture
+def poetry_section(pyproject_toml: Path) -> str:
+    content = """
+[tool.poetry]
+name = "poetry"
+
+[tool.poetry.dependencies]
+python = "^3.5"
+"""
+    with pyproject_toml.open(mode="a") as f:
+        f.write(content)
+    return content
