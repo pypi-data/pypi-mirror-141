4577f3c092e399c9e5bede5ad9030b31e759aebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:05 +0000

    Add package fkeycapture/1.0.7/fkeycapture-1.0.7.tar.gz

---
+++ packages/fkeycapture/fkeycapture-1.0.7.tar.gz/fkeycapture-1.0.7/README.md
@@ -0,0 +1,12 @@
+# fkeycapture
+This is a simple and easy to use package that allows you to capture individual keystrokes from the user.
+#### Forms:
+1. (Default) Recive key as a string
+2. Recive key as bytes
+#### How to Use:
+1. from fkeycapture import get, getnum
+2. Use it like this 
+: get([number of keys to capture],[if you want bytes output, make this 'True'])
+3. To use the getnum method use like this: getnum([number of key to capture])
+###### v.1.0.7:
+Added the getnum method
---
+++ packages/fkeycapture/fkeycapture-1.0.7.tar.gz/fkeycapture-1.0.7/src/fkeycapture/__init__.py
@@ -0,0 +1,48 @@
+import termios, fcntl, sys
+global fd,flags_save,attrs_save
+fd = sys.stdin.fileno()
+flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+attrs_save = termios.tcgetattr(fd)
+def getp1():
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+    # save old state
+    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+    attrs_save = termios.tcgetattr(fd)
+    # make raw - the way to do this comes from the termios(3) man page.
+    attrs = list(attrs_save) # copy the stored version to update
+    # iflag
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK
+                  | termios.ISTRIP | termios.INLCR | termios. IGNCR
+                  | termios.ICRNL | termios.IXON )
+    # oflag
+    attrs[1] &= ~termios.OPOST
+    # cflag
+    attrs[2] &= ~(termios.CSIZE | termios. PARENB)
+    attrs[2] |= termios.CS8
+    # lflag
+    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
+                  | termios.ISIG | termios.IEXTEN)
+    termios.tcsetattr(fd, termios.TCSANOW, attrs)
+    # turn off non-blocking
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
+def getp2():
+    termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
+def get(keycount=1,bytes=False):
+  getp1()
+  key = sys.stdin.read(keycount)
+  getp2()
+  if bytes == True:
+    key = key.encode()
+  return key
+def getnum(keycount=1):
+  internalcounter=0
+  keys = []
+  while internalcounter != keycount:
+    key = get()
+    if key == "0" or key == "1" or key == "2" or key == "3" or key == "4" or key == "5" or key == "6" or key == "7" or key == "8" or key == "9":
+      keys.append(key)
+      internalcounter += 1
+  key = "".join(keys)
+  return key
