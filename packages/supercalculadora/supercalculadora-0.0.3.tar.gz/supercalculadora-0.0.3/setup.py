34a0347a0ee5c2e6680f4f6267cd8becdeeef15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:03 +0000

    Add package supercalculadora/0.0.3/supercalculadora-0.0.3.tar.gz

---
+++ packages/supercalculadora/supercalculadora-0.0.3.tar.gz/supercalculadora-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.3'
+DESCRIPTION = 'supercalc'
+LONG_DESCRIPTION = 'supercalc'
+
+# Setting up
+setup( 
+    name="supercalculadora",
+    version=VERSION,
+    author="Gustavo",
+    author_email="<gustavodanieldetoledo@gmail.com.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
