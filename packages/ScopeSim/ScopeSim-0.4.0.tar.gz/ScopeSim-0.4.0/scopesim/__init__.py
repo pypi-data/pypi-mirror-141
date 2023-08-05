84a4b8feeda9e583db4b19b64b51f61f3db98003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:51 +0000

    Add package ScopeSim/0.4.0/ScopeSim-0.4.0.tar.gz

---
+++ packages/ScopeSim/ScopeSim-0.4.0.tar.gz/ScopeSim-0.4.0/scopesim/__init__.py
@@ -0,0 +1,65 @@
+"""The Instrument data simulator for MICADO on the E-ELT"""
+
+################################################################################
+#                            TURN OFF WARNINGS                                 #
+################################################################################
+import sys
+import logging
+import warnings
+import yaml
+from astropy.utils.exceptions import AstropyWarning
+
+warnings.simplefilter('ignore', UserWarning)
+warnings.simplefilter('ignore', FutureWarning)
+warnings.simplefilter('ignore', RuntimeWarning)     # warnings for the developer
+warnings.simplefilter('ignore', category=AstropyWarning)
+yaml.warnings({'YAMLLoadWarning': False})
+
+################################################################################
+#                         PACKAGE GLOBAL VARIABLES                             #
+################################################################################
+
+from . import rc
+
+
+
+################################################################################
+#                         SET BASIC LOGGING LEVEL                              #
+################################################################################
+
+root = logging.getLogger()
+root.setLevel("CRITICAL")
+
+################################################################################
+#                         IMPORT PACKAGE MODULES                               #
+################################################################################
+
+# Import all the modules to go under ScopeSim
+
+from . import commands
+from . import source
+from . import optics
+from . import detector
+from . import effects
+from . import server
+from . import utils
+
+# import specific classes from the modules to included in the global namespace
+
+from .utils import bug_report
+from .optics.optical_train import OpticalTrain
+from .commands.user_commands import UserCommands
+from .source.source import Source
+
+from .server.database import (list_packages, download_package,
+                              list_example_data,
+                              download_example_data)
+
+################################################################################
+#                          VERSION INFORMATION                                 #
+################################################################################
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = "Version number is not available"
