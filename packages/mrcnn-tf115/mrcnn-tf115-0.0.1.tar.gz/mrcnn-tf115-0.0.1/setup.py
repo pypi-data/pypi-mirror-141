94d4181e39e0932b2395b2a1c7407f060c0d9038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:56 +0000

    Add package mrcnn-tf115/0.0.1/mrcnn-tf115-0.0.1.tar.gz

---
+++ packages/mrcnn-tf115/mrcnn-tf115-0.0.1.tar.gz/mrcnn-tf115-0.0.1/setup.py
@@ -0,0 +1,65 @@
+"""
+The build/compilations setup
+
+>> pip install -r requirements.txt
+>> python setup.py install
+"""
+import pip
+import logging
+import pkg_resources
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+def _parse_requirements(file_path):
+    pip_ver = pkg_resources.get_distribution('pip').version
+    pip_version = list(map(int, pip_ver.split('.')[:2]))
+    if pip_version >= [6, 0]:
+        raw = pip.req.parse_requirements(file_path,
+                                         session=pip.download.PipSession())
+    else:
+        raw = pip.req.parse_requirements(file_path)
+    return [str(i.req) for i in raw]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning('Fail load requirements file, so using default ones.')
+    install_reqs = []
+
+setup(
+    name='mrcnn-tf115',
+    version='0.0.1',
+    url='https://github.com/matterport/Mask_RCNN',
+    author='Matterport',
+    author_email='f416720001@gmail.com',
+    license='MIT',
+    description='Mask R-CNN for object detection and instance segmentation',
+    packages=["mrcnn"],
+    install_requires=install_reqs,
+    include_package_data=True,
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Visualization",
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords="image instance segmentation object detection mask rcnn r-cnn tensorflow keras",
+)
