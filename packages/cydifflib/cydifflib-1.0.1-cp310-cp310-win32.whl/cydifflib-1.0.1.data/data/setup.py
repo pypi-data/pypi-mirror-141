4dab193d7fa3880029b741a57f1a0b22863533ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package cydifflib/1.0.1/cydifflib-1.0.1-cp310-cp310-win32.whl

---
+++ packages/cydifflib/cydifflib-1.0.1-cp310-cp310-win32.whl/cydifflib-1.0.1.data/data/setup.py
@@ -0,0 +1,32 @@
+from skbuild import setup
+
+with open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="cydifflib",
+    version="1.0.1",
+    url="https://github.com/maxbachmann/cydifflib",
+    author="Max Bachmann",
+    author_email="pypi@maxbachmann.de",
+    description="Fast implementation of difflib's algorithms",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License"
+    ],
+
+    packages=["cydifflib"],
+    package_dir={'':'src'},
+    zip_safe=True,
+    include_package_data=True,
+    python_requires=">=3.6",
+)
