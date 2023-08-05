f5cf64ea3364b01e385d74b4fd9a1f161659278a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package shiftyface/0.1/shiftyface-0.1.tar.gz

---
+++ packages/shiftyface/shiftyface-0.1.tar.gz/shiftyface-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='shiftyface',
+    long_description='A simple python module for generating random images with rarity level you dictate and assets you '
+                     'provide. The primary use case being the generation of images which can be turned into NFTs.',
+    version='0.1',
+    license='MIT',
+    author="Paul Thompson",
+    author_email='pt@sevenelm.com',
+    packages=find_packages('shifty_face'),
+    package_dir={'': 'shifty_face'},
+    url='https://github.com/rpupkin77/shiftyface',
+    keywords='nft, rarity, image generation, eth, collection generator',
+    install_requires=[
+          'Pillow',
+      ],
+
+)
