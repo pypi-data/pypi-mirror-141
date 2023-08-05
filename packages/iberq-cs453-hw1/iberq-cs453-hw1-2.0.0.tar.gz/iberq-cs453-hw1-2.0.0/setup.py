4681bef57680c8003d9f84e64ba1da77fe1d39fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:14 +0000

    Add package iberq-cs453-hw1/2.0.0/iberq-cs453-hw1-2.0.0.tar.gz

---
+++ packages/iberq-cs453-hw1/iberq-cs453-hw1-2.0.0.tar.gz/iberq-cs453-hw1-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="iberq-cs453-hw1",
+    author="Ahmet Ayberk Yilmaz",  # TODO: Write your name
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+)
