da273b20994302742667e688d7769eaed6687e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:41 +0000

    Add package siliconcompiler/0.7.0/siliconcompiler-0.7.0-cp37-cp37m-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.7.0-cp37-cp37m-win_amd64.whl/siliconcompiler/tools/klayout/klayout.py
@@ -0,0 +1,198 @@
+import os
+import platform
+import re
+import shutil
+import siliconcompiler
+from pathlib import Path
+
+####################################################################
+# Make Docs
+####################################################################
+
+def make_docs():
+    '''
+    Klayout is a production grade viewer and editor of GDSII and
+    Oasis data with customizable Python and Ruby interfaces.
+
+    Documentation: https://www.klayout.de
+
+    Sources: https://github.com/KLayout/klayout
+
+    Installation: https://www.klayout.de/build.html
+
+    '''
+
+    chip = siliconcompiler.Chip()
+    chip.load_target('freepdk45_demo')
+    chip.set('arg','step','export')
+    chip.set('arg','index','<index>')
+    chip.set('design', '<design>')
+    setup(chip)
+
+    return chip
+
+####################################################################
+# Setup tool
+####################################################################
+
+def setup(chip, mode="batch"):
+    '''
+    Setup function for Klayout
+    '''
+
+    tool = 'klayout'
+    refdir = 'tools/'+tool
+    step = chip.get('arg','step')
+    index = chip.get('arg','index')
+
+    if platform.system() == 'Windows':
+        klayout_exe = 'klayout_app.exe'
+        if not shutil.which(klayout_exe):
+            loc_dir = os.path.join(Path.home(), 'AppData', 'Roaming', 'KLayout')
+            global_dir = os.path.join(os.path.splitdrive(Path.home())[0],
+                                      os.path.sep,
+                                      'Program Files (x86)',
+                                      'KLayout')
+            if os.path.isdir(loc_dir):
+                chip.set('eda', tool, 'path', loc_dir)
+            elif os.path.isdir(global_dir):
+                chip.set('eda', tool, 'path', global_dir)
+    elif platform.system() == 'Darwin':
+        klayout_exe = 'klayout'
+        if not shutil.which(klayout_exe):
+            klayout_dir = os.path.join(os.path.sep,
+                                       'Applications',
+                                       'klayout.app',
+                                       'Contents',
+                                       'MacOS')
+            # different install directory when installed using Homebrew
+            klayout_brew_dir = os.path.join(os.path.sep,
+                                            'Applications',
+                                            'KLayout',
+                                            'klayout.app',
+                                            'Contents',
+                                            'MacOS')
+            if os.path.isdir(klayout_dir):
+                chip.set('eda', tool, 'path', klayout_dir)
+            elif os.path.isdir(klayout_brew_dir):
+                chip.set('eda', tool, 'path', klayout_brew_dir)
+    else:
+        klayout_exe = 'klayout'
+
+    if mode == 'show':
+        clobber = False
+        script = '/klayout_show.py'
+        option = ['-nc', '-rm']
+    else:
+        clobber = False
+        script = '/klayout_export.py'
+        option = ['-zz', '-r']
+
+    chip.set('eda', tool, 'exe', klayout_exe, clobber=True)
+    chip.set('eda', tool, 'vswitch', ['-zz', '-v'], clobber=clobber)
+    chip.set('eda', tool, 'version', '0.26.11', clobber=clobber)
+    chip.set('eda', tool, 'format', 'json', clobber=clobber)
+    chip.set('eda', tool, 'copy', 'true', clobber=clobber)
+    chip.set('eda', tool, 'refdir', step, index, refdir, clobber=clobber)
+    chip.set('eda', tool, 'script', step, index, refdir + script, clobber=clobber)
+    chip.set('eda', tool, 'option', step, index, option, clobber=clobber)
+
+    # Export GDS with timestamps by default.
+    chip.set('eda', tool, 'variable', step, index, 'timestamps', 'true', clobber=False)
+
+    # Input/Output requirements
+    if (not chip.valid('read', 'def', step, index) or
+        not chip.get('read', 'def', step, index)):
+        chip.add('eda', tool, 'input', step, index, chip.get('design') + '.def')
+    chip.add('eda', tool, 'output', step, index, chip.get('design') + '.gds')
+
+    # Adding requirements
+    if mode != 'show':
+        targetlibs = chip.get('asic', 'logiclib')
+        stackup = chip.get('asic', 'stackup')
+        if bool(stackup) & bool(targetlibs):
+            macrolibs = chip.get('asic', 'macrolib')
+
+            chip.add('eda', tool, 'require', step, index, ",".join(['asic', 'logiclib']))
+            chip.add('eda', tool, 'require', step, index, ",".join(['asic', 'stackup']))
+            chip.add('eda', tool, 'require', step, index,  ",".join(['pdk', 'layermap', 'klayout', stackup, 'def','gds']))
+
+            for lib in (targetlibs + macrolibs):
+                chip.add('eda', tool, 'require', step, index, ",".join(['library', lib, 'gds', stackup]))
+                chip.add('eda', tool, 'require', step, index, ",".join(['library', lib, 'lef', stackup]))
+        else:
+            chip.error = 1
+            chip.logger.error(f'Stackup and targetlib paremeters required for Klayout.')
+
+
+
+
+################################
+#  Environment setup
+################################
+
+def setup_env(chip):
+    '''
+    Creates environment setup files in the current directory.
+
+    Setup is based on the parameters passed in through the chip object.
+
+    '''
+
+    return 0
+
+################################
+#  Custom runtime options
+################################
+
+def runtime_options(chip):
+    '''
+    Custom runtime options, returns list of command line options.
+    '''
+    return []
+
+################################
+# Version Check
+################################
+
+def parse_version(stdout):
+    # KLayout 0.26.11
+    return stdout.split()[1]
+
+
+################################
+# Post_process (post executable)
+################################
+
+def post_process(chip):
+    ''' Tool specific function to run after step execution
+    '''
+
+    tool = 'klayout'
+    step = chip.get('arg', 'step')
+    index = chip.get('arg','index')
+    logfile = f"{step}.log"
+
+    # Log file parsing
+    chip.set('eda', tool, 'regex', step, index, 'warnings', "WARNING", clobber=False)
+    chip.set('eda', tool, 'regex', step, index, 'errors', "ERROR", clobber=False)
+
+    # Reports
+    for metric in ('errors', 'warnings'):
+        chip.set('eda', tool, 'report', step, index, metric, logfile)
+
+    return 0
+
+##################################################
+if __name__ == "__main__":
+    # File being executed
+    prefix = os.path.splitext(os.path.basename(__file__))[0]
+    output = prefix + '.json'
+
+    # create a chip instance
+    chip = siliconcompiler.Chip()
+    chip.load_target("freepdk45_demo")
+    # load configuration
+    setup(chip, step='export', index='0')
+    # write out results
+    chip.writecfg(output)
---
+++ packages/siliconcompiler/siliconcompiler-0.7.0-cp37-cp37m-win_amd64.whl/siliconcompiler/tools/openroad/sc_floorplan.tcl
@@ -0,0 +1,122 @@
+########################################################
+# FLOORPLANNING
+########################################################
+
+# Functon adapted from OpenROAD:
+# https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts/blob/ca3004b85e0d4fbee3470115e63b83c498cfed85/flow/scripts/macro_place.tcl#L26
+proc design_has_macros {} {
+  set db [::ord::get_db]
+  set block [[$db getChip] getBlock]
+  foreach inst [$block getInsts] {
+    set inst_master [$inst getMaster]
+
+    # BLOCK means MACRO cells
+    if { [string match [$inst_master getType] "BLOCK"] } {
+        return true
+    }
+  }
+  return false
+}
+
+# Auto-generate floorplan if not defined yet
+if {[expr ! [dict exists $sc_cfg "read" def $sc_step $sc_index]]} {
+
+    #########################
+    #Init Floorplan
+    #########################
+    #NOTE: assuming a two tuple value as lower left, upper right
+    set sc_diearea   [dict get $sc_cfg asic diearea]
+    set sc_corearea  [dict get $sc_cfg asic corearea]
+    set sc_diesize   [regsub -all {[\,\)\(]} $sc_diearea " "]
+    set sc_coresize  [regsub -all {[\,\)\(]} $sc_corearea " "]
+    puts $sc_diearea
+    puts $sc_corearea
+    puts $sc_diesize
+    puts $sc_coresize
+
+    #TODO: if there is more than one site, pick the first one?
+    initialize_floorplan -die_area $sc_diesize \
+	-core_area $sc_coresize \
+	-site $sc_site
+
+    ###########################
+    # Track Creation
+    ###########################
+
+    set metal_list ""
+    dict for {key value} [dict get $sc_cfg pdk grid $sc_stackup] {
+	lappend metal_list $key
+    }
+
+    # source tracks from file if found, else else use schema entries
+    if [dict exists $sc_cfg pdk aprtech openroad $sc_stackup $sc_libtype tracks] {
+	source [lindex [dict get $sc_cfg pdk aprtech openroad $sc_stackup $sc_libtype tracks] 0]
+    } else {
+	foreach metal $metal_list {
+	    #extracting values from dictionary
+	    set xpitch [dict get $sc_cfg pdk grid $sc_stackup $metal xpitch]
+	    set xoffset [dict get $sc_cfg pdk grid $sc_stackup $metal xoffset]
+	    set ypitch [dict get $sc_cfg pdk grid $sc_stackup $metal ypitch]
+	    set yoffset [dict get $sc_cfg pdk grid $sc_stackup $metal yoffset]
+
+	    make_tracks $metal \
+		-x_offset $xoffset \
+		-x_pitch $xpitch \
+		-y_offset $yoffset \
+		-y_pitch $ypitch
+	}
+    }
+
+    ###########################
+    # Automatic Pin Placement
+    ###########################
+
+    place_pins -hor_layers $sc_hpinmetal \
+	-ver_layers $sc_vpinmetal \
+	-random \
+
+    # Need to check if we have any macros before performing macro placement,
+    # since we get an error otherwise.
+    if {[design_has_macros]} {
+        ###########################
+        # TDMS Placement
+        ###########################
+
+        global_placement -density $openroad_place_density \
+            -pad_left $openroad_pad_global_place \
+            -pad_right $openroad_pad_global_place
+
+        ###########################
+        # Macro placement
+        ###########################
+
+        macro_placement \
+            -halo $openroad_macro_place_halo \
+            -channel $openroad_macro_place_channel
+
+        # Note: some platforms set a "macro blockage halo" at this point, but the
+        # technologies we support do not, so we don't include that step for now.
+    }
+
+    ###########################
+    # Power Network (not good)
+    ###########################
+    #pdngen $::env(PDN_CFG) -verbose
+
+}
+
+###########################
+# Tap Cells
+###########################
+
+if [dict exists $sc_cfg pdk aprtech openroad $sc_stackup $sc_libtype tapcells] {
+    source [lindex [dict get $sc_cfg pdk aprtech openroad $sc_stackup $sc_libtype tapcells] 0]
+} else {
+    tapcell \
+	-endcap_cpp $sc_tapoffset \
+	-distance $sc_tapmax \
+	-tapcell_master $sc_tapcell \
+	-endcap_master $sc_endcap
+}
+
+remove_buffers
