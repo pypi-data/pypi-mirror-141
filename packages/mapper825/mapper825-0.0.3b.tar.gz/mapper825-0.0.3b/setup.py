89b5be6548ba56f03732d6945454dd1a136aa44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:23 +0000

    Add package mapper825/0.0.3b0/mapper825-0.0.3b.tar.gz

---
+++ packages/mapper825/mapper825-0.0.3b.tar.gz/mapper825-0.0.3b/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'mapper825',         # How you named your package folder (MyLib)
+  packages = ['mapper825'],   # Chose the same as "name"
+  version = '0.0.3b',   # Start with a small number and increase it with every change you make
+  install_requires=[
+    'numpy',
+    'pandas',
+    'fiona',
+    'shapely',
+    'geopandas',
+    'plotly',
+    'kaleido',
+    "setuptools>=42",
+    "wheel"
+  ]
+)
