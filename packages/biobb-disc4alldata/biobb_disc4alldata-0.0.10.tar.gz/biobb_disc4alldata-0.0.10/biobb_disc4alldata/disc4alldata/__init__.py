b455a5b445686a5010196c9a969c542284529e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:06 +0000

    Add package biobb-disc4alldata/0.0.10/biobb_disc4alldata-0.0.10.tar.gz

---
+++ packages/biobb-disc4alldata/biobb_disc4alldata-0.0.10.tar.gz/biobb_disc4alldata-0.0.10/biobb_disc4alldata/disc4alldata/__init__.py
@@ -0,0 +1,2 @@
+name = "disc4alldata"
+__all__ = ["gda_disgenet", "vda_disgenet", "dda_disgenet", "ga_disgenet", "va_disgenet", "da_disgenet"]
---
+++ packages/biobb-disc4alldata/biobb_disc4alldata-0.0.10.tar.gz/biobb_disc4alldata-0.0.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_disc4alldata",
+    version="0.0.10",
+    author="Biobb developers - Maria Paola Ferri",
+    author_email="maria.ferri@bsc.es",
+    description="Biobb_disc4alldata is a complete code wrapper to interrogate the Disc4All pre-process database REST API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/mapoferri/Biobb_disgenet",
+    project_urls={
+        "Documentation": "http://biobb_template.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['adapters', 'docs', 'test']),
+    install_requires=['biobb_common>=3.5.1'],
+    python_requires='==3.7.*',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ),
+)
