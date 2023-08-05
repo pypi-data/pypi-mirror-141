9131d898fe1cf6b208fbad79af34f8ee4730d2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:56 +0000

    Add package cellworld-cotroller-service/0.0.4/cellworld_cotroller_service-0.0.4.tar.gz

---
+++ packages/cellworld-cotroller-service/cellworld_cotroller_service-0.0.4.tar.gz/cellworld_cotroller_service-0.0.4/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='cellworld_cotroller_service',author='German Espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld_controller_service'],install_requires=['cellworld_experiment_service'],version='0.0.4',zip_safe=False)
