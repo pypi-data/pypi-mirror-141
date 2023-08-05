51274086b557ce4c5172177903c1462ab5494819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:52 +0000

    Add package pymmcore-plus/0.3.0/pymmcore_plus-0.3.0-py3-none-any.whl

---
+++ packages/pymmcore-plus/pymmcore_plus-0.3.0-py3-none-any.whl/pymmcore_plus/__init__.py
@@ -0,0 +1,62 @@
+try:
+    from ._version import version as __version__
+except ImportError:  # pragma: no cover
+    __version__ = "unknown"
+
+from typing import TYPE_CHECKING
+
+from ._util import find_micromanager
+from .core import (
+    ActionType,
+    CMMCorePlus,
+    Configuration,
+    DeviceDetectionStatus,
+    DeviceNotification,
+    DeviceType,
+    FocusDirection,
+    Metadata,
+    PortType,
+    PropertyType,
+)
+from .core.events import CMMCoreSignaler, PCoreSignaler
+
+if TYPE_CHECKING:
+    from .remote import RemoteMMCore, server
+
+__all__ = [
+    "ActionType",
+    "CMMCorePlus",
+    "CMMCorePlus",
+    "CMMCoreSignaler",
+    "Configuration",
+    "PCoreSignaler",
+    "DeviceDetectionStatus",
+    "DeviceNotification",
+    "DeviceType",
+    "find_micromanager",
+    "FocusDirection",
+    "Metadata",
+    "PortType",
+    "PropertyType",
+    "RemoteMMCore",
+    "server",
+]
+
+
+def __dir__():
+    return list(globals()) + ["RemoteMMCore", "server"]
+
+
+def __getattr__(name: str):
+    if name in {"RemoteMMCore", "server"}:
+        try:
+            from . import remote
+
+            return getattr(remote, name)
+        except ImportError as e:
+            raise ImportError(
+                f"{e}.\nTo use the interprocess features of pymmcore-plus, "
+                "please install with `pip install pymmcore-plus[remote]`"
+            ) from e
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
