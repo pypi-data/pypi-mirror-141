ff99aeaef2d16d164d3fd82adc51a10e5b0c6854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:25 +0000

    Add package civis-jupyter-extensions/1.1.0/civis_jupyter_extensions-1.1.0-py3-none-any.whl

---
+++ packages/civis-jupyter-extensions/civis_jupyter_extensions-1.1.0-py3-none-any.whl/civis_jupyter_ext/__init__.py
@@ -0,0 +1,19 @@
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    # For Python 3.7
+    from importlib_metadata import version
+
+from .magics import load_ipython_extension  # noqa: F401
+
+
+__version__ = version("civis-jupyter-extensions")
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        "section": "notebook",
+        "src": "static",
+        "dest": "civis_jupyter_ext",
+        "require": "civis_jupyter_ext/index"
+    }]
