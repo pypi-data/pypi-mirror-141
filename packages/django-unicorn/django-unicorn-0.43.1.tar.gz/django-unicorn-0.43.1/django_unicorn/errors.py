6e342d9c3b1441c460edad3f2959b7b40ef47670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:13 +0000

    Add package django-unicorn/0.43.1/django-unicorn-0.43.1.tar.gz

---
+++ packages/django-unicorn/django-unicorn-0.43.1.tar.gz/django-unicorn-0.43.1/django_unicorn/errors.py
@@ -0,0 +1,26 @@
+class UnicornCacheError(Exception):
+    pass
+
+
+class UnicornViewError(Exception):
+    pass
+
+
+class ComponentLoadError(Exception):
+    pass
+
+
+class RenderNotModified(Exception):
+    pass
+
+
+class MissingComponentElement(Exception):
+    pass
+
+
+class MissingComponentViewElement(Exception):
+    pass
+
+
+class ComponentNotValid(Exception):
+    pass
