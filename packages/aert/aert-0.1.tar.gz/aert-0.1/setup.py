daaa1d4f755f7b660a94887b24fe567ad4f7c133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package aert/0.1/aert-0.1.tar.gz

---
+++ packages/aert/aert-0.1.tar.gz/aert-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='aert',  
+     version='0.1',
+     scripts=['aert'] ,
+     author="fwang",
+     author_email="fwang@mowentech.com",
+     description="Place holder!",
+     long_description="Place Holder!",
+     url="https://aert.aerouter.com",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
