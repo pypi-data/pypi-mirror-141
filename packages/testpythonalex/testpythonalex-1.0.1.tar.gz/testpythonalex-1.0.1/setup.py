160101254f9df97564364d8d728788b60f279613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:03 +0000

    Add package testpythonalex/1.0.1/testpythonalex-1.0.1.tar.gz

---
+++ packages/testpythonalex/testpythonalex-1.0.1.tar.gz/testpythonalex-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='testpythonalex',
+    version='1.0.1',
+    author='alexguo247',
+    author_email='alexguo247@gmail.com',
+    description='Test Python Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexguo247/cohere-python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
