ae5b8b1ceff5aeb1195d45a0b1f4185f7bf02714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package nonebot-plugin-anime-news/1.1.0/nonebot_plugin_anime_news-1.1.0.tar.gz

---
+++ packages/nonebot-plugin-anime-news/nonebot_plugin_anime_news-1.1.0.tar.gz/nonebot_plugin_anime_news-1.1.0/setup.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nonebot_plugin_anime_news',
+    version='1.1.0',
+    description=(
+        '从bangumi.moe定时获取番剧信息'
+    ),
+    license='TIM License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/5656565566/anime_news',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+)
