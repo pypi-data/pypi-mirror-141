f00f104eb379fb613f0c2976f5da1b1297d8ca66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:48 +0000

    Add package erdpy/1.2.1/erdpy-1.2.1-py3-none-any.whl

---
+++ packages/erdpy/erdpy-1.2.1-py3-none-any.whl/erdpy/projects/project_sol.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+from typing import List
+
+from erdpy.projects.project_base import Project
+
+logger = logging.getLogger("ProjectSol")
+
+
+class ProjectSol(Project):
+    def __init__(self, directory: Path):
+        super().__init__(directory)
+
+    def perform_build(self):
+        pass
+
+    def get_dependencies(self) -> List[str]:
+        return []
