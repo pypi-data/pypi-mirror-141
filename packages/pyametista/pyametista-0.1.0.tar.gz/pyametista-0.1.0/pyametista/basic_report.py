7a8db122d20fa95f6828bff658a1e52281cf8e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:42 +0000

    Add package pyametista/0.1.0/pyametista-0.1.0.tar.gz

---
+++ packages/pyametista/pyametista-0.1.0.tar.gz/pyametista-0.1.0/pyametista/basic_report.py
@@ -0,0 +1,26 @@
+from pyametista.engine import reportGenerator
+
+
+class BasicReport:
+    """ "
+    Base wrapper class to generate reports.
+    """
+
+    def __init__(self, jrxml_filename, output_filename, data_config=None, fonts=None, report_type="pdf"):
+        self.jrxml_filename = jrxml_filename
+        self.output_filename = output_filename
+        self.data_config = data_config
+        self.fonts = fonts
+        self.report_type = report_type
+
+    def generate_report(self):
+        reportGenerator.generate_report(
+            jrxml_filename=self.jrxml_filename,
+            output_filename=self.output_filename,
+            data_config=self.data_config,
+            fonts=self.fonts,
+            report_type=self.report_type,
+        )
+
+    def set_report_type(self, report_type):
+        self.report_type = report_type
---
+++ packages/pyametista/pyametista-0.1.0.tar.gz/pyametista-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyametista',
+ 'pyametista.adapters',
+ 'pyametista.engine',
+ 'pyametista.engine.bands',
+ 'pyametista.engine.components',
+ 'pyametista.exports']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mysql-connector-python>=8.0.28,<9.0.0',
+ 'psycopg2-binary>=2.9.3,<3.0.0',
+ 'reportlab>=3.6.8,<4.0.0']
+
+setup_kwargs = {
+    'name': 'pyametista',
+    'version': '0.1.0',
+    'description': 'Módulo Python para geração de PDF utilizando arquivos .jrxml (JasperReports)',
+    'long_description': None,
+    'author': 'Bruno Souza',
+    'author_email': 'bruno@komu.com.br',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
