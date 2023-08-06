35dee1818c675d8f8136885da9c0b57d898c95b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:47 +0000

    Add package idom/0.37.1/idom-0.37.1-py2.py3-none-any.whl

---
+++ packages/idom/idom-0.37.1-py2.py3-none-any.whl/idom/__init__.py
@@ -0,0 +1,57 @@
+from . import config, html, log, web
+from .core import hooks
+from .core.component import Component, component
+from .core.dispatcher import Stop
+from .core.events import EventHandler, event
+from .core.hooks import (
+    create_context,
+    use_callback,
+    use_context,
+    use_effect,
+    use_memo,
+    use_reducer,
+    use_ref,
+    use_state,
+)
+from .core.layout import Layout
+from .core.proto import ComponentType, VdomDict
+from .core.vdom import vdom
+from .sample import run_sample_app
+from .server.prefab import run
+from .utils import Ref, html_to_vdom
+from .widgets import hotswap, multiview
+
+
+__author__ = "idom-team"
+__version__ = "0.37.1"  # DO NOT MODIFY
+
+__all__ = [
+    "component",
+    "Component",
+    "ComponentType",
+    "config",
+    "create_context",
+    "event",
+    "EventHandler",
+    "hooks",
+    "hotswap",
+    "html_to_vdom",
+    "html",
+    "Layout",
+    "log",
+    "multiview",
+    "Ref",
+    "run_sample_app",
+    "run",
+    "Stop",
+    "use_callback",
+    "use_context",
+    "use_effect",
+    "use_memo",
+    "use_reducer",
+    "use_ref",
+    "use_state",
+    "vdom",
+    "VdomDict",
+    "web",
+]
