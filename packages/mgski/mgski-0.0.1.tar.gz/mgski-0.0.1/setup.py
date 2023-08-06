54e103833cbcc7e6b065f3b943ae2142b85f9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:40 +0000

    Add package mgski/0.0.1/mgski-0.0.1.tar.gz

---
+++ packages/mgski/mgski-0.0.1.tar.gz/mgski-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mgski',
+    version='0.0.1',
+    license='MIT',
+    author="Mayank Gupta",
+    author_email='seekmayank@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='mgski project',
+    install_requires=[
+          '',
+      ]
+)
