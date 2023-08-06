4505094d32ce1bd010a6df018be1a50ca1cc17b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package eso-qvalue/0.0.1/eso-qvalue-0.0.1.tar.gz

---
+++ packages/eso-qvalue/eso-qvalue-0.0.1.tar.gz/eso-qvalue-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+setup(
+    name='eso-qvalue',
+    version='0.0.1',
+    author='barwe',
+    author_email='barwechin@163.com',
+    url='',
+    description='',
+    packages=[],
+    install_requires=[]
+)
+
