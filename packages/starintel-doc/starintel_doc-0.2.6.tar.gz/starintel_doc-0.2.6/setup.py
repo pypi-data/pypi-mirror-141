726a9077d2fd48db5b026dc6060f02c708eb5c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:02 +0000

    Add package starintel-doc/0.2.6/starintel_doc-0.2.6.tar.gz

---
+++ packages/starintel-doc/starintel_doc-0.2.6.tar.gz/starintel_doc-0.2.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+import src
+
+setup(name="starintel_doc",
+        version=src.__version__,
+        description="Document Spec for Star intel",
+        long_description=open("README.md", "r").read(),
+        long_description_content_type="text/markdown",
+        url="https://gitlab.com/unseen-giants/starintel_doc",
+        author="Nsaspy",
+        packages=find_packages(where='src'),
+        license="MIT",
+        py_modules=['src.starintel_doc'],
+        classifiers=[
+              "License :: OSI Approved :: MIT License",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.10",
+             "Operating System :: OS Independent",
+            ]
+)
---
+++ packages/starintel-doc/starintel_doc-0.2.6.tar.gz/starintel_doc-0.2.6/src/__init__.py
@@ -0,0 +1,3 @@
+from src.starintel_doc import BookerDocument, make_id, BookerAddress, BookerBreach, BookerCVE, BookerEmail, BookerHost, BookerMember, BookerMesaage, BookerOganizations, BookerPerson, BookerUsername, BookerWebService
+
+__version__ = '0.2.6'
