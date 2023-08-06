71cfd180ee2dfbd8404b5e81878b886f4f539324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:40 +0000

    Add package eric-ide/22.3/eric-ide-22.3.tar.gz

---
+++ packages/eric-ide/eric-ide-22.3.tar.gz/eric-ide-22.3/eric7/UI/Info.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2022 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining some informational strings.
+"""
+
+
+Program = 'eric7'
+Version = '22.3 (rev. 76a32b7b17b2)'
+VersionOnly = '22.3'
+Copyright = (
+    'Copyright (c) 2002 - 2022 Detlev Offenbach'
+    ' <detlev@die-offenbachs.de>'
+)
+CopyrightShort = '(c) 2002 - 2022 Detlev Offenbach'
+BugAddress = 'eric-bugs@eric-ide.python-projects.org'
+FeatureAddress = 'eric-featurerequest@eric-ide.python-projects.org'
+Homepage = "https://eric-ide.python-projects.org/index.html"
