5bc622f6b081f5a3ad0d09a3ab8d7f6e8f114bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:53 +0000

    Add package MJOLNIR/1.1.24/MJOLNIR-1.1.24.tar.gz

---
+++ packages/MJOLNIR/MJOLNIR-1.1.24.tar.gz/MJOLNIR-1.1.24/MJOLNIR/__init__.py
@@ -0,0 +1,17 @@
+"""
+MJOLNIR Module
+^^^^^^^^^^^^^^
+
+"""
+#import Geometry, Statistic, Data
+
+import sys,os
+sys.path.append('.')
+
+__version__ = '1.1.24'
+__author__ = 'Jakob Lass'
+
+__multiFLEXXNormalization__ = os.path.join(os.path.dirname(__file__),'CalibrationMultiFLEXX.csv') 
+__flatConeNormalization__ = os.path.join(os.path.dirname(__file__),'CalibrationFlatCone.csv')
+__CAMEANormalization__ = os.path.join(os.path.dirname(__file__),'Normalization_1.calib')
+__bambusNormalization__ = os.path.join(os.path.dirname(__file__),'CalibrationBambus.csv')
