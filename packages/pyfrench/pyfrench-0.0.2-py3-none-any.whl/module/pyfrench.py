67aa7dfc3558e58eb65cc2bda479d36f4f5bb8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:18 +0000

    Add package pyfrench/0.0.2/pyfrench-0.0.2-py3-none-any.whl

---
+++ packages/pyfrench/pyfrench-0.0.2-py3-none-any.whl/module/pyfrench.py
@@ -0,0 +1,29 @@
+# Console
+def couleur(rouge: int, vert: int, bleu: int, texte: str):
+    return "\033[38;2;{};{};{}m{}".format(rouge, vert, bleu, texte)
+def txt(nombre: str):
+    return int(nombre)
+def num(nombre: int):
+    return str(nombre)
+def afficher(texte: str):
+    return print(texte + couleur(203, 203, 203, ''))
+def demander(question: str):
+    return input('{}\n'.format(question) + couleur(203, 203, 203, ''))
+def erreur(erreur: str):
+    return print(couleur(255, 0, 0, 'ERREUR: {}'.format(erreur)) + couleur(203, 203, 203, ''))
+def attention(texte: str):
+    return print(couleur(235, 192, 52, 'ATTENTION: {}'.format(texte)) + couleur(203, 203, 203, ''))
+def succes(texte: str):
+    return print(couleur(45, 166, 49, 'SUCCÈS: {}'.format(texte)) + couleur(203, 203, 203, ''))
+def maths(calcul: str):
+    return print(int(eval(calcul)))
+
+# Fichier
+def ouvrir(fichier: str):
+    return open(file=fichier)
+
+# Vrai/Faux
+def Vrai():
+    return True
+def Faux():
+    return False
