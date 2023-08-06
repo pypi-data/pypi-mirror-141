27feb07d7e1da438d6e8468c2ca98957e413427a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:55 +0000

    Add package asciii/1.0.2/asciii-1.0.2.tar.gz

---
+++ packages/asciii/asciii-1.0.2.tar.gz/asciii-1.0.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['asciii']
+install_requires = \
+['Pillow>=9.0.1,<10.0.0']
+
+setup_kwargs = {
+    'name': 'asciii',
+    'version': '1.0.2',
+    'description': 'A library that converts images to ascii',
+    'long_description': None,
+    'author': 'acup1',
+    'author_email': 'georgiyegoriy@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
