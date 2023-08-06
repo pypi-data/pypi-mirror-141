2941faeb24cf1deee13842b77b9818242d9b4a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:50 +0000

    Add package yandex-conqueror/1.2/yandex_conqueror-1.2-py3.9.egg

---
+++ packages/yandex-conqueror/yandex_conqueror-1.2-py3.9.egg/conqueror/endpoints/add_business.py
@@ -0,0 +1,56 @@
+from flask import request
+
+from conqueror.app import app
+from conqueror.cassandra import session
+from conqueror.utils import as_json
+
+
+@app.route('/v1/add-businesses', methods=['PUT'])
+@as_json
+def add_business():
+    """
+    Adds a business
+    ---
+    requestBody:
+        content:
+            application/json:
+                schema:
+                    type: array
+                    items:
+                        type: object
+                        properties:
+                            businessId:
+                                type: string
+                                description: Business ID
+                            sector:
+                                type: string
+                                description: Business' sector
+                            lat:
+                                type: number
+                                description: Geographical latitude in degrees. + is North
+                            lon:
+                                type: number
+                                description: Geographical longitude in degrees. + is East
+                            name:
+                                type: string
+                                description: Optional object name
+                        required:
+                            - businessId
+                            - sector
+    responses:
+        '201':
+            description: Business added
+    """
+    data = request.get_json()
+
+    for datum in data:
+        businessId = datum['businessId']
+        sector = datum['sector']
+        lat = datum.get('lat')
+        lon = datum.get('lon')
+        name = datum.get('name')
+
+        session.execute('INSERT INTO businesses (businessId, sector, GeoLat, geoLon, name) VALUES (%s, %s, %s, %s, %s)',
+                        (businessId, sector, lat, lon, name))
+        session.execute('INSERT INTO sectors (sector) VALUES (%s)', (sector,))
+    return {}, 201
---
+++ packages/yandex-conqueror/yandex_conqueror-1.2-py3.9.egg/conqueror/msg_generator/messages.py
@@ -0,0 +1,18 @@
+import random
+
+from .definitions import get_messages, get_messages_backend
+
+
+def get_msg_using_provider(provider):
+    if random.randint(1, 5) <= 3:
+        return str(random.choice(provider('1-A')))
+    else:
+        return str(random.choice(provider('2-A')))+' '+str(random.choice(provider('2-B')))
+
+
+def generate_message() -> str:
+    return get_msg_using_provider(get_messages)
+
+
+def generate_message_client() -> str:
+    return get_msg_using_provider(get_messages_backend)
