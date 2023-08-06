c1ec093064ba566fbc880d8be88013560e11d861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:20 +0000

    Add package owlshell/0.0.3/owlshell-0.0.3-py3-none-any.whl

---
+++ packages/owlshell/owlshell-0.0.3-py3-none-any.whl/owlshell/__init__.py
@@ -0,0 +1,49 @@
+import os, subprocess
+def shell(cmd, encoding='utf-8'):
+    process = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    return out.decode(encoding)
+def linux():
+    try:
+        if shell('uname') == 'Linux\n': 
+            return True
+        else: return False
+    except:
+        return False
+def notify(header='Python App', text='Hello, Sovenok-Hacker!', time=1, icon=None):
+    if not linux():
+        print('Supported only Linux!')
+    else:
+        try:
+            if icon == None:
+                print(f'notify-send "{header}" "{text}" --expire-time={time * 1000}')
+                shell(f'notify-send "{header}" "{text}" --expire-time={time * 1000}')
+            else:
+                print(f'notify-send "{header}" "{text}" --expire-time={time * 1000}')
+                shell(f'notify-send "{header}" "{text}" --expire-time={time * 1000} --icon "{icon}"')
+        except:
+            raise OSError('cannot send notification')
+def owl():
+    print('''
+Hello, Sovenok-Hacker!    ,''    ''.
+                         / `-.  .-' \
+                        /( (O))((O) )
+                       /'-..-'/\`-..|
+                     ,'\   `-.\/.--'|
+                   ,' ( \           |
+                 ,'( (   `._        |
+                /( (  ( ( | `-._ _,-;
+               /( (  ( ( (|     '  ;
+              / ((  (    /        /
+             //         /        /
+             //  / /  ,'        /
+            // /    ,'         /
+            //  / ,'          ;
+            //_,-'          ;
+            // /,,,,..-))-))\    /|
+              /; ; ;\ `.  \  \  / |
+             /; ; ; ;\  \.  . \/./
+            (; ; ;_,_,\  .: \   /
+             `-'-'     | : . |:|
+                       |. | : .|
+''')
