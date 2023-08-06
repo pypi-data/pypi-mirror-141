667a96518bd1110eca3307976338ed8e056af089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:29 +0000

    Add package fast-sql-manager/1.3.8/fast_sql_manager-1.3.8.tar.gz

---
+++ packages/fast-sql-manager/fast_sql_manager-1.3.8.tar.gz/fast_sql_manager-1.3.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fast_sql_manager",
+    version="1.3.8",
+    author="Oscar da Silva",
+    author_email="oscarkaka222@gmail.com",
+    description="Um pacote simples para realizar operações no banco",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OscarSilvaOfficial/fast_sql_manager",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'mysql-connector-python>=8.0.27',
+        'pymongo>=4.0.1',
+        'psycopg2-binary>=2.9.3',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
