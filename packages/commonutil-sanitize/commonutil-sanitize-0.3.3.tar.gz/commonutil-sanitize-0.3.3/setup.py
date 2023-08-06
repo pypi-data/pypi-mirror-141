28d66c258b174501ffd7b50aff4f9143e834988b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:23 +0000

    Add package commonutil-sanitize/0.3.3/commonutil-sanitize-0.3.3.tar.gz

---
+++ packages/commonutil-sanitize/commonutil-sanitize-0.3.3.tar.gz/commonutil-sanitize-0.3.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+		name="commonutil-sanitize",
+		version="0.3.3",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="Routines support text sanitization",
+		py_modules=[
+				"commonutil_sanitize",
+		],
+		classifiers=[
+				"Development Status :: 5 - Production/Stable",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.7",
+		],
+		license="MIT License",
+)
