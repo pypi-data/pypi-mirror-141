48a991219a314cb84e55bd5530965326083fd369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:57 +0000

    Add package semih-demir-cs453-hw1/2.0.0/semih-demir-cs453-hw1-2.0.0.tar.gz

---
+++ packages/semih-demir-cs453-hw1/semih-demir-cs453-hw1-2.0.0.tar.gz/semih-demir-cs453-hw1-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="semih-demir-cs453-hw1",
+    author="Süleyman Semih Demir",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+)
+
+##pypi-AgEIcHlwaS5vcmcCJDRkNmIyYTAzLTZmMTEtNDM1Zi1iZGEzLTI2ZTIxZDViZTk2MgACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgYebeMY5eKSNjCneN8X5wOmSS4cxUc0GuUOlUObmur6c
