30c3c3dbdf311b0b14404a6aa4365a63386c202f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:22 +0000

    Add package biasedrandom/0.2/biasedrandom-0.2.tar.gz

---
+++ packages/biasedrandom/biasedrandom-0.2.tar.gz/biasedrandom-0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+  name = 'biasedrandom',
+  packages = ['biasedrandom'],
+  version = '0.2',
+  license='MIT',
+  description = 'A python library for doing biased random based on random module',
+  author = 'Caleb M',
+  author_email = 'bersekware@gmail.com',
+  url = 'https://github.com/Berserkware/biasedrandom',
+  download_url = 'https://github.com/Berserkware/biasedrandom/archive/refs/tags/v_02.tar.gz',
+  keywords = ['biased', 'random', 'python', 'biasedrandom'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
