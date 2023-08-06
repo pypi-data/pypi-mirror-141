3050ab5ec716e06cafcd232234c8a3e6c8a217e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:05 +0000

    Add package plantfem/21.10.1.10/plantfem-21.10.1.10-py3-none-any.whl

---
+++ packages/plantfem/plantfem-21.10.1.10-py3-none-any.whl/src/plantfem.py
@@ -0,0 +1,19 @@
+# prior to this, 
+# please install plantfem
+import os
+import sys
+
+from python.SoybeanClass.SoybeanClass import Soybean
+from python.LightClass.LightClass import Light
+
+class plantfem:
+
+    # create a Light object
+    def Light(self,config):
+        return Light(light_angle=90.0,light_direction=180.0)
+
+    # create a Soybean object
+    def Soybean(self,config):
+        return Soybean(config)
+
+
