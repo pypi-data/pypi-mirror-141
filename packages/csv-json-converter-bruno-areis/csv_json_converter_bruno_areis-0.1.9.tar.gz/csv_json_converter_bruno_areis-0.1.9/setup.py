d6acf5e74b0fc5452f1e39446cbdf1173b17b211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:55 +0000

    Add package csv-json-converter-bruno-areis/0.1.9/csv_json_converter_bruno_areis-0.1.9.tar.gz

---
+++ packages/csv-json-converter-bruno-areis/csv_json_converter_bruno_areis-0.1.9.tar.gz/csv_json_converter_bruno_areis-0.1.9/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['csv_json_converter_bruno_areis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['csv_json_converter = '
+                     'csv_json_converter_bruno_areis.converter:converter']}
+
+setup_kwargs = {
+    'name': 'csv-json-converter-bruno-areis',
+    'version': '0.1.9',
+    'description': 'Convert csv to json or json to csv',
+    'long_description': '',
+    'author': 'Bruno Reis',
+    'author_email': 'brunoalvesbhx@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
