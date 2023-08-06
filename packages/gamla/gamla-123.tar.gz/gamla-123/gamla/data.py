d80004d2bc9bf3caac21bd17f1cd833bd23c9213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package gamla/123/gamla-123.tar.gz

---
+++ packages/gamla/gamla-123.tar.gz/gamla-123/gamla/data.py
@@ -0,0 +1,149 @@
+import dataclasses
+import itertools
+import json
+from typing import Any, Callable, Collection, Dict, Text, Tuple
+
+import dataclasses_json
+
+from gamla import currying, functional, functional_generic, operator
+from gamla.optimized import sync
+
+
+def _immutable(self, *args, **kws):
+    raise TypeError("cannot change object - object is immutable")
+
+
+class frozendict(dict):  # noqa: N801
+    def __init__(self, *args, **kwargs):
+        self.__setattr__ = _immutable
+        super(frozendict, self).__init__(*args, **kwargs)
+
+    def __hash__(self):
+        return hash(tuple(self.items()))
+
+    # TODO(nitzo): Disabled since we need to be able to un-serialize with dill/pickle.
+    # __setitem__ = _immutable
+    __delitem__ = _immutable
+    pop = _immutable
+    popitem = _immutable
+    clear = _immutable
+    update = _immutable
+    setdefault = _immutable
+
+
+def get_encode_config():
+    """Display dataclass field as a tuple of JSON strings."""
+    return dataclasses.field(
+        metadata=dataclasses_json.config(
+            encoder=lambda lst: sorted(lst, key=json.dumps, reverse=False),
+        ),
+    )
+
+
+def _freeze_nonterminal(v):
+    if isinstance(v, Dict):
+        return frozendict(v)
+    return tuple(v)
+
+
+#: Freeze recursively a dictionary.
+#:
+#: >>> freeze_deep({"1": {"2": "345", "some-string": ["hello"]}})
+#: data.frozendict(
+#:  {"1": data.frozendict({"2": "345", "some-string": ("hello",)})},
+#: )
+freeze_deep = functional_generic.map_dict(_freeze_nonterminal, operator.identity)
+
+
+@currying.curry
+def tuple_of_tuples_to_csv(
+    tuple_of_tuples: Tuple[Tuple[Any], ...],
+    separator: Text = "\t",
+) -> Text:
+    """Return a CSV formatted string given a tuple of tuples. Each element is separated by the character "separator" (default is \t).
+
+    >>> tuple_of_tuples_to_csv((("name", "age"), ("David", "23"), ("Itay", "26")))
+    'name\\tage\\nDavid\\t23\\nItay\\t26'
+    >>> tuple_of_tuples_to_csv((("name", "age"), ("David", "23"), ("Itay", "26")), " ")
+    'name age\\nDavid 23\\nItay 26'
+    """
+    return functional_generic.pipe(
+        tuple_of_tuples,
+        functional_generic.curried_map(
+            functional_generic.compose_left(
+                functional_generic.curried_map(str),
+                tuple,
+                separator.join,
+            ),
+        ),
+        "\n".join,
+    )
+
+
+class Enum(frozenset):
+    def __getattr__(self, name):
+        if name in self:
+            return name
+        raise AttributeError
+
+
+def _do_on_positions(f, predicate: Callable[[int], bool]):
+    return sync.compose_left(
+        enumerate,
+        sync.map(
+            sync.ternary(
+                sync.compose_left(
+                    operator.head,
+                    predicate,
+                ),
+                sync.compose_left(operator.second, f),
+                operator.second,
+            ),
+        ),
+    )
+
+
+def explode(*positions: Collection[int]):
+    """Flattens a non homogeneous iterable.
+
+    For an iterable where some positions are iterable and some are not,
+    "explodes" the iterable, so that each element appears in a single row, and duplicates the non iterable.
+
+    >>> functional_generic.pipe(
+    ...     ["x", ["y1", "y2", "y3"], "z"],
+    ...     data.explode(1),
+    ...     tuple,
+    ... )
+    (
+        ("x", "y1", "z"),
+        ("x", "y2", "z"),
+        ("x", "y3", "z"),
+    )
+    """
+    return sync.compose_left(
+        _do_on_positions(
+            functional.wrap_tuple,
+            sync.complement(operator.contains(positions)),
+        ),
+        sync.star(itertools.product),
+    )
+
+
+def transform_if_not_none(transform: Callable, value):
+    """
+    Apply a function on a given value if it's not None. Else, return the None value.
+
+    >>> transform_if_not_none(
+    ...     functional_generic.when(operator.is_instance, lambda x: x.casefold())),
+    ...     "Some Title"
+    ... )
+    'some title'
+    >>> transform_if_not_none(
+    ...     functional_generic.when(operator.is_instance, lambda x: x.casefold())),
+    ...     None
+    ... )
+
+    """
+    if value is not None:
+        return transform(value)
+    return value
---
+++ packages/gamla/gamla-123.tar.gz/gamla-123/gamla/data_test.py
@@ -0,0 +1,79 @@
+import pickle
+
+from gamla import data, functional_generic, operator
+
+
+def test_freeze_deep():
+    original = {"1": {"2": "345", "some-string": ["hello"]}}
+    frozen = data.freeze_deep(original)
+    # Check values are intact.
+    assert frozen == data.frozendict(
+        {"1": data.frozendict({"2": "345", "some-string": ("hello",)})},
+    )
+    # Check hashability.
+    {data.freeze_deep(original)}
+
+
+def test_freeze_deep_idempotent():
+    assert data.freeze_deep(data.freeze_deep({"a": 1})) == data.freeze_deep({"a": 1})
+
+
+def test_frozendict_serializable():
+    fd = data.frozendict({"a": "something", "b": 1})
+    fd_str = pickle.dumps(fd)
+
+    fd_clone = pickle.loads(fd_str)
+
+    assert fd == fd_clone
+
+
+def test_tuple_of_tuples_to_csv():
+    assert (
+        data.tuple_of_tuples_to_csv((("name", "age"), ("David", "23"), ("Itay", "26")))
+        == "name\tage\nDavid\t23\nItay\t26"
+    )
+
+
+def test_tuple_of_tuples_to_csv_custom_separator():
+    assert (
+        data.tuple_of_tuples_to_csv(
+            (("name", "age"), ("David", "23"), ("Itay", "26")),
+            " ",
+        )
+        == "name age\nDavid 23\nItay 26"
+    )
+
+
+def test_explode():
+    assert functional_generic.pipe(
+        [
+            "x",
+            [
+                "y1",
+                "y2",
+                "y3",
+            ],
+            "z",
+        ],
+        data.explode(1),
+        tuple,
+    ) == (
+        ("x", "y1", "z"),
+        ("x", "y2", "z"),
+        ("x", "y3", "z"),
+    )
+
+
+def test_transform_if_not_none():
+    assert (
+        data.transform_if_not_none(
+            functional_generic.compose_left(
+                functional_generic.when(
+                    operator.is_instance(str),
+                    lambda x: x.casefold(),
+                ),
+            ),
+            "Some Title",
+        )
+        == "some title"
+    )
