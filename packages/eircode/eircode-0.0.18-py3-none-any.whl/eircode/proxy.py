23958064213052b9341ee4a6407809358dca0db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:52 +0000

    Add package eircode/0.0.18/eircode-0.0.18-py3-none-any.whl

---
+++ packages/eircode/eircode-0.0.18-py3-none-any.whl/eircode/proxy.py
@@ -0,0 +1,60 @@
+import urllib
+import urllib.parse
+
+import requests
+from requests_ip_rotator import ApiGateway
+
+
+class Proxy():
+
+    def __init__(self, skip_setup=False, site=None):
+        self.gateway = None
+        self.session = None
+
+        self.site = site if site is not None else 'https://api-finder.eircode.ie'
+
+        self._is_setup = False
+
+        if not skip_setup:
+            self.setup()
+
+    def setup(self, force=False):
+        if not force and self._is_setup:
+            return
+
+        self.gateway = ApiGateway(
+            self.site,
+            regions=['eu-west-1']
+        )
+        self.gateway.shutdown()
+        self.gateway.start(force=True)
+
+        self.session = requests.Session()
+        self.session.mount(
+            self.site,
+            self.gateway
+        )
+
+        self._is_setup = True
+
+    def get(self, url, params={}):
+        try:
+            response = self.session.get(
+                url + '?' + urllib.parse.urlencode(params)
+            )
+
+            if self.site == 'https://api-finder.eircode.ie':
+                if response.json().get('error', {}).get('code', None) == 403:
+                    raise Exception()
+            return response
+        except:
+            print('Resetting proxy')
+            self.shutdown()
+            self.setup(force=True)
+            return self.get(url, params=params)
+
+    def shutdown(self):
+        self.gateway.shutdown()
+
+
+proxy = Proxy(skip_setup=True)
