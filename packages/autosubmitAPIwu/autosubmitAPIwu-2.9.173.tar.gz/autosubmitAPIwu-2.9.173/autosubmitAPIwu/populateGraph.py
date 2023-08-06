633b4a42fdd4f094376607b24609bb40deeaaade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:47 +0000

    Add package autosubmitAPIwu/2.9.173/autosubmitAPIwu-2.9.173.tar.gz

---
+++ packages/autosubmitAPIwu/autosubmitAPIwu-2.9.173.tar.gz/autosubmitAPIwu-2.9.173/autosubmitAPIwu/populateGraph.py
@@ -0,0 +1,12 @@
+import autosubmitAPIwu.workers.process_graph_drawings as ProcessGraph
+
+
+def main():
+    """
+    Process and positions graph drawing.
+    """
+    ProcessGraph.process_active_graphs()
+
+
+if __name__ == "__main__":
+    main()
