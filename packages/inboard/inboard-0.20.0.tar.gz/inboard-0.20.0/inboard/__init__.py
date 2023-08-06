4e68a8a6ede372b5c6a82055469dc3dc67636bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:58 +0000

    Add package inboard/0.20.0/inboard-0.20.0.tar.gz

---
+++ packages/inboard/inboard-0.20.0.tar.gz/inboard-0.20.0/inboard/__init__.py
@@ -0,0 +1,23 @@
+"""inboard
+
+Docker images and utilities to power your Python APIs and help you ship faster.
+
+https://github.com/br3ndonland/inboard
+"""
+# FastAPI and Starlette are optional, and will raise ImportErrors if not installed.
+try:
+    from .app.utilities_fastapi import basic_auth as fastapi_basic_auth
+except ImportError:  # pragma: no cover
+    pass
+try:
+    from .app.utilities_starlette import BasicAuth as StarletteBasicAuth
+except ImportError:  # pragma: no cover
+    pass
+from .logging_conf import LOGGING_CONFIG, configure_logging
+
+__all__ = (
+    "LOGGING_CONFIG",
+    "StarletteBasicAuth",
+    "configure_logging",
+    "fastapi_basic_auth",
+)
