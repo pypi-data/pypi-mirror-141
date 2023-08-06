bf62deabb6e856f6d1c2bd72bfc48a6605b91ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:44 +0000

    Add package dw-core/0.0.3/dw_core-0.0.3-py3-none-any.whl

---
+++ packages/dw-core/dw_core-0.0.3-py3-none-any.whl/dw_core/__about__.py
@@ -0,0 +1,25 @@
+# Inspired by:
+# https://github.com/pypa/pipfile/blob/master/pipfile/__about__.py
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "dw-core"
+__summary__ = "Core library for DeskWatch"
+__uri__ = "https://github.com/DeskWatch/dw-core"
+
+__version__ = "0.4.2"
+
+__author__ = "Kohinoor Bharti"
+__email__ = "kohinoorbharti7@gmail.com"
+
+__license__ = "MPL2"
+__copyright__ = "Copyright 2021 %s" % __author__
