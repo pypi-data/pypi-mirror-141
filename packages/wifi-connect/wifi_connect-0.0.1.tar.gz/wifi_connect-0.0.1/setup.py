9ac56d0edc0d646d504f4be5193b9b1653b66327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:01 +0000

    Add package wifi-connect/0.0.1/wifi_connect-0.0.1.tar.gz

---
+++ packages/wifi-connect/wifi_connect-0.0.1.tar.gz/wifi_connect-0.0.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wifi_connect",
+    version="0.0.1",
+    author="Xairi Valdivia",
+    author_email="xairi.valdivia@gmail.com",
+    description="List and connect to wifi networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Prometeo136/wifi-cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    scripts=["bin/wifi-connect"],
+)
