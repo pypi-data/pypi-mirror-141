31ce523c6604cc13b91e6ad8397a008cb7faab37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:31 +0000

    Add package cs453-hw1-elif-ozer/3.0.0/cs453-hw1-elif-ozer-3.0.0.tar.gz

---
+++ packages/cs453-hw1-elif-ozer/cs453-hw1-elif-ozer-3.0.0.tar.gz/cs453-hw1-elif-ozer-3.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    #test comment
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="cs453-hw1-elif-ozer",
+    author="Elif Ozer",  # TODO: Write your name
+    version="3.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
