99e9e52c842309c4daca1d9dbcb0248664c698e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:44 +0000

    Add package vmklib/0.7.7/vmklib-0.7.7.tar.gz

---
+++ packages/vmklib/vmklib-0.7.7.tar.gz/vmklib-0.7.7/vmklib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.13.4
+# hash=aa49b15bb4cee1a47c9f767dbc57482e
+# =====================================
+
+"""
+vmklib - Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
+PKG_NAME = "vmklib"
+VERSION = "0.7.7"
---
+++ packages/vmklib/vmklib-0.7.7.tar.gz/vmklib-0.7.7/vmklib/data/grip.mk
@@ -0,0 +1,24 @@
+GRIP_PREFIX := grip-
+
+.PHONY: $(GRIP_PREFIX)check-env $(GRIP_PREFIX)render
+
+GRIP_ENV            := $($(PROJ)_DIR)/.grip.env
+SECRETHUB_GRIP_PATH ?= $(USER)/github/tokens/basic-api-token
+$(GRIP_ENV):
+	@rm -f $@
+	@echo "export GRIP_TOKEN=`secrethub read $(SECRETHUB_GRIP_PATH)`" >> $@
+	+@echo "wrote '$@'"
+
+$(GRIP_PREFIX)check-env: | $(GRIP_ENV)
+ifndef GRIP_TOKEN
+	$(error GRIP_TOKEN not set, run 'source $(GRIP_ENV)')
+endif
+
+GRIP_PORT := 0.0.0.0:8000
+GRIP_FILE := README.md
+$(GRIP_PREFIX)render: $(GRIP_PREFIX)check-env | $(VENV_CONC)
+	@$(PYTHON_BIN)/grip \
+		--pass $(GRIP_TOKEN) \
+		--title=$(PROJ) \
+		$($(PROJ)_DIR)/$(GRIP_FILE) \
+		$(GRIP_PORT)
