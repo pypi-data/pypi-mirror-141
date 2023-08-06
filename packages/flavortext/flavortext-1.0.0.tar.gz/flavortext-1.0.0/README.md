11f446396c320e9cbb965a9d1fe9d4ea3dfa40db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:24 +0000

    Add package flavortext/1.0.0/flavortext-1.0.0.tar.gz

---
+++ packages/flavortext/flavortext-1.0.0.tar.gz/flavortext-1.0.0/README.md
@@ -0,0 +1,17 @@
+# flavortext.py
+I shamelessly yoinked this from [ZachFreedman's Singularitron](https://github.com/ZackFreedman/Singularitron) project, and rewrote it in Python.
+
+sue me.
+
+## How to use:
+import the module:
+`from flavortext import flavoricious`
+
+The flavoricious function can take 6 arguments:
+
+`yourConstrVerbs`: A list, verbs that are constructive such as 'Building' or 'Assembling'.
+`yourDestrVerbs`: A list, verbs that are destructive such as 'Destroying' or 'Breaking'.
+*Note for both of these args: Make sure to remove any sort of ending like 'ing' from the words you provide.
+yourNouns = None,
+length = 30,
+delay = 0.05`
---
+++ packages/flavortext/flavortext-1.0.0.tar.gz/flavortext-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="flavortext",
+    version="1.0.0",
+    description="Add a touch of pizzaz to your python projects.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheMingus/flavortext.py",
+    author="Mingus",
+    license="MIT",
+    classifiers=["License :: OSI Approved :: MIT License"],
+    packages=["flavortext"],
+    include_package_data=True,
+)
