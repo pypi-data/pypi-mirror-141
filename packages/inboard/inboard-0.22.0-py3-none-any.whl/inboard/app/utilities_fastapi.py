3b95b23709060e037a0c398cbafacb1458e66fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:42 +0000

    Add package inboard/0.22.0/inboard-0.22.0-py3-none-any.whl

---
+++ packages/inboard/inboard-0.22.0-py3-none-any.whl/inboard/app/utilities_fastapi.py
@@ -0,0 +1,42 @@
+import os
+import secrets
+from typing import Optional
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
+
+
+async def basic_auth(credentials: HTTPBasicCredentials = Depends(HTTPBasic())) -> str:
+    """Authenticate a FastAPI request with HTTP Basic auth."""
+    basic_auth_username = os.getenv("BASIC_AUTH_USERNAME")
+    basic_auth_password = os.getenv("BASIC_AUTH_PASSWORD")
+    if not (basic_auth_username and basic_auth_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Server HTTP Basic auth credentials not set",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    correct_username = secrets.compare_digest(credentials.username, basic_auth_username)
+    correct_password = secrets.compare_digest(credentials.password, basic_auth_password)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="HTTP Basic auth credentials not correct",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+class GetRoot(BaseModel):
+    Hello: str = "World"
+
+
+class GetStatus(BaseModel):
+    application: str
+    status: str
+    message: Optional[str]
+
+
+class GetUser(BaseModel):
+    username: str
