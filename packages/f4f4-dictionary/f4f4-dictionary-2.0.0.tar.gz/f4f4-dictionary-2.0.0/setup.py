5ead055d8163b1dd4213f5a7aa4bd05b865a12f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:41 +0000

    Add package f4f4-dictionary/2.0.0/f4f4-dictionary-2.0.0.tar.gz

---
+++ packages/f4f4-dictionary/f4f4-dictionary-2.0.0.tar.gz/f4f4-dictionary-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="f4f4-dictionary",
+    author="Barış Tiftik",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
