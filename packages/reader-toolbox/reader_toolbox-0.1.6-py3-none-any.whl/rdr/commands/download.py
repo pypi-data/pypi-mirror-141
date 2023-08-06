cad45a26c8dc127553eba62a87142506c34f7d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:45 +0000

    Add package reader-toolbox/0.1.6/reader_toolbox-0.1.6-py3-none-any.whl

---
+++ packages/reader-toolbox/reader_toolbox-0.1.6-py3-none-any.whl/rdr/commands/download.py
@@ -0,0 +1,59 @@
+
+# download - given the name of a remote carrel, cache it locally
+
+# require
+from rdr import *
+
+# harvest
+@click.command( options_metavar='[<options>]' )
+@click.argument( 'carrel', metavar='<carrel>' )
+def download( carrel ) :
+
+	"""Cache <carrel> from the public library of study carrels.
+
+	A collection of about 3,000 pre-created study carrels is available at http://library.distantreader.org. Given <carrel>, this function will download the remote carrel and save it locally.
+
+	Example: rdr download homer
+
+	See also:
+	
+	\b
+	  rdr catalog --help
+	  rdr set --help"""
+			
+	# configure
+	ZIPFILE = 'study-carrel.zip'
+	CARRELS = 'carrels'
+
+	# require
+	from requests import get
+	from tempfile import TemporaryFile
+	from zipfile  import ZipFile
+
+	# initialize
+	localLibrary  = configuration( 'localLibrary' )
+
+	# get the remote zip file; needs error checking
+	click.echo( "\n  INFO: Downloading remote study carrel... ", err=True )
+	response = get( REMOTELIBRARY + '/' + CARRELS + '/' + carrel + '/' + ZIPFILE )
+	
+	# initialize a temporary file and write to it
+	click.echo( "  INFO: Saving study carrel... ", err=True )
+	handle = TemporaryFile()
+	handle.write( response.content )
+	
+	# unzip the temporary file and close it, which also deletes it
+	click.echo( "  INFO: Unziping study carrel... ", err=True )
+	with ZipFile( handle, 'r' ) as zip : zip.extractall( str( localLibrary ) )
+	handle.close()
+
+	# done
+	click.echo( ( '''  INFO: Done. You may now any of the RDR commands. For example:
+
+	* rdr info %s
+	* rdr bib %s
+	* rdr cluster %s
+	* rdr ngrams %s -s 2 -c
+	* rdr wrd %s -c
+	* rdr tm %s
+''' ) % ( carrel, carrel, carrel, carrel, carrel, carrel, ), err=True )
