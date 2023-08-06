d931873a9d4fa6d99b8733bf94d370b5d6e67be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:59 +0000

    Add package puredb/2022.3.9.16.17/puredb-2022.3.9.16.17.tar.gz

---
+++ packages/puredb/puredb-2022.3.9.16.17.tar.gz/puredb-2022.3.9.16.17/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='puredb',
+    version='2022.03.09.16.17',
+    description='封装自用的leveldb',
+    long_description=long_description,
+    author='chenzongwei',
+    author_email='17695480342@163.com',
+    py_modules=['puredb'],
+    install_requires=['leveldb','loguru'],
+    python_requires='>=3',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    requires=[]
+)
+
