8c6f26273e91ddfd87c4749c8fcb30fb15f22ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:52 +0000

    Add package plot2html/0.0.8/plot2html-0.0.8.tar.gz

---
+++ packages/plot2html/plot2html-0.0.8.tar.gz/plot2html-0.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+DESCRIPTION = 'Creates a static .html file with charts using Google Charts API'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plot2html",
+    version="0.0.8",
+    author="Rafael Rodrigues Troiani",
+    author_email="rafael.r.troiani@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    url='https://github.com/rafatro/plot2html',
+    install_requires=['pandas','datetime'],
+    keywords=['python', 'Google Charts', 'html'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ]
+)
