5173baae6d9e9563a98175fd0e812325df8fe095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:53 +0000

    Add package django-socio-grpc/0.11.28/django-socio-grpc-0.11.28.tar.gz

---
+++ packages/django-socio-grpc/django-socio-grpc-0.11.28.tar.gz/django-socio-grpc-0.11.28/django_socio_grpc/tests/fakeapp/services/foreign_model_service.py
@@ -0,0 +1,19 @@
+from django_socio_grpc import generics, mixins
+from fakeapp.models import ForeignModel
+from fakeapp.serializers import (
+    ForeignModelRetrieveCustomProtoSerializer,
+    ForeignModelSerializer,
+)
+
+
+class ForeignModelService(
+    generics.GenericService, mixins.AsyncListModelMixin, mixins.AsyncRetrieveModelMixin
+):
+    queryset = ForeignModel.objects.all().order_by("uuid")
+    serializer_class = ForeignModelSerializer
+    lookup_field = "name"
+
+    def get_serializer_class(self):
+        if self.action == "Retrieve":
+            return ForeignModelRetrieveCustomProtoSerializer
+        return super().get_serializer_class()
