73ce3f8e81c2e7b5d5e74535e864a36eb38f75ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:09 +0000

    Add package quick-sqlite/1.0/quick_sqlite-1.0.tar.gz

---
+++ packages/quick-sqlite/quick_sqlite-1.0.tar.gz/quick_sqlite-1.0/README.md
@@ -0,0 +1,17 @@
+# quick_sqlite
+
+It is a fast package that will ease you to work with sqlite3.
+
+## Installation
+
+```
+pip install quick_sqlite
+```
+
+## Usage
+
+```
+from quick_sqlite import QuickSqlite
+sqle = QuickSqlite('example')
+sqle.create_table('employes', 'name, email')
+```
---
+++ packages/quick-sqlite/quick_sqlite-1.0.tar.gz/quick_sqlite-1.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='quick_sqlite',
+    packages=['quick_sqlite'],
+    version='1.0',
+    license='MIT',
+    description='This library can you help you use sqlite3 much easier and faster.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Spark Drago',
+    author_email='huzaifa.farooq05@gmail.com',
+    url='https://github.com/SparkDrago05/quick_sqlite',
+    download_url='https://github.com/SparkDrago05/quick_sqlite/archive/refs/tags/v1.0.tar.gz',
+    keywords=['sqlite', 'sqlite3', 'quick'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
