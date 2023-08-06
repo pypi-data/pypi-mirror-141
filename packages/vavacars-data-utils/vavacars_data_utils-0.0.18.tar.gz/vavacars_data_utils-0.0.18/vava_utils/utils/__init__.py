0a1607e5a4bde68a82ac532270c4367e7065217d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:30 +0000

    Add package vavacars-data-utils/0.0.18/vavacars_data_utils-0.0.18.tar.gz

---
+++ packages/vavacars-data-utils/vavacars_data_utils-0.0.18.tar.gz/vavacars_data_utils-0.0.18/vava_utils/utils/__init__.py
@@ -0,0 +1,7 @@
+from .comparisons import get_best_match
+from .callables import return_on_failure
+from .strings import extract_json_objects
+
+__all__ = [
+    "get_best_match", "return_on_failure", "extract_json_objects",
+]
