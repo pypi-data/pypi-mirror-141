d00ea67da49cfd4b374fe987a84e931189d7e692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:21 +0000

    Add package immuta-audit-export/0.1.0/immuta_audit_export-0.1.0.tar.gz

---
+++ packages/immuta-audit-export/immuta_audit_export-0.1.0.tar.gz/immuta_audit_export-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['immuta_audit_export', 'immuta_audit_export.s3']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.21.11,<2.0.0']
+
+setup_kwargs = {
+    'name': 'immuta-audit-export',
+    'version': '0.1.0',
+    'description': 'Scaffold for serverless audit exporting',
+    'long_description': None,
+    'author': 'LukeBoyer',
+    'author_email': 'boyer.l@husky.neu.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
