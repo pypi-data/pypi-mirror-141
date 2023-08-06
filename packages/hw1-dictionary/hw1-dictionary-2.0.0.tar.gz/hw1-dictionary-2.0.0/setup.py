60d0f6af0a7fda88a8d41982bef33c091a6c9cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:45 +0000

    Add package hw1-dictionary/2.0.0/hw1-dictionary-2.0.0.tar.gz

---
+++ packages/hw1-dictionary/hw1-dictionary-2.0.0.tar.gz/hw1-dictionary-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hw1-dictionary",
+    author="Funda Tan",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
