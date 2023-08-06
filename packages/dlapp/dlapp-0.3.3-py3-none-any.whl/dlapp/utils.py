6da714d8be1376787fc5b2986d04db83094fd449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:42 +0000

    Add package dlapp/0.3.3/dlapp-0.3.3-py3-none-any.whl

---
+++ packages/dlapp/dlapp-0.3.3-py3-none-any.whl/dlapp/utils.py
@@ -0,0 +1,131 @@
+"""Module containing the logic for utilities."""
+
+import re
+from collections import OrderedDict
+from dlapp.argumenthelper import validate_argument_type
+
+
+class UtilsError(Exception):
+    """Use to capture utility error."""
+
+
+class RegexConversionError(UtilsError):
+    """Use to capture regular expression conversion error."""
+
+
+class Printer:
+    """A printer class.
+
+    Methods
+    Printer.print(data, header='', footer='', failure_msg='', print_func=None) -> None
+    """
+    @classmethod
+    def print(cls, data, header='', footer='', failure_msg='', print_func=None):
+        """Decorate data by organizing header, data, footer, and failure_msg
+
+        Parameters
+        ----------
+        data (str, list): a text or a list of text.
+        header (str): a header text.  Default is empty.
+        footer (str): a footer text.  Default is empty.
+        failure_msg (str): a failure message.  Default is empty.
+        print_func (function): a print function.  Default is None.
+        """
+        headers = str(header).splitlines()
+        footers = str(footer).splitlines()
+        data = data if isinstance(data, (list, tuple)) else [data]
+        lst = []
+        result = []
+        for item in data:
+            lst.extend(str(item).splitlines())
+        width = max(len(str(i)) for i in lst + headers + footers)
+        result.append('+-{}-+'.format('-' * width))
+        if header:
+            for item in headers:
+                result.append('| {} |'.format(item.ljust(width)))
+            result.append('+-{}-+'.format('-' * width))
+
+        for item in lst:
+            result.append('| {} |'.format(item.ljust(width)))
+        result.append('+-{}-+'.format('-' * width))
+
+        if footer:
+            for item in footers:
+                result.append('| {} |'.format(item.ljust(width)))
+            result.append('+-{}-+'.format('-' * width))
+
+        if failure_msg:
+            result.append(failure_msg)
+
+        print_func = print_func if callable(print_func) else print
+        print_func('\n'.join(result))
+
+
+def convert_wildcard_to_regex(pattern, closed=False):
+    """Convert a wildcard pattern to a regex pattern.
+
+    Parameters
+    ----------
+    pattern (str): a wildcard pattern.
+    closed (bool): will prepend ^ symbol and append $ symbol to pattern
+            if set to True
+    Returns
+    -------
+    str: a regular express pattern.
+
+    Notes
+    -----
+    Wildcard support:
+        ? (question mark): this can represent any single character.
+        * (asterisk): this can represent any number of characters
+            (including zero, in other words, zero or more characters).
+        [] (square brackets): specifies a range.
+        [!] : match any that not specifies in a range.
+
+    """
+    validate_argument_type(str, pattern=pattern)
+    regex_pattern = ''
+    try:
+        regex_pattern = pattern.replace('.', r'\.')
+        regex_pattern = regex_pattern.replace('+', r'\+')
+        regex_pattern = regex_pattern.replace('?', '_replacetodot_')
+        regex_pattern = regex_pattern.replace('*', '_replacetodotasterisk_')
+        regex_pattern = regex_pattern.replace('_replacetodot_', '.')
+        regex_pattern = regex_pattern.replace('_replacetodotasterisk_', '.*')
+        regex_pattern = regex_pattern.replace('[!', '[^')
+        regex_pattern = '^{}$'.format(regex_pattern) if closed else regex_pattern
+        re.compile(regex_pattern)
+        return regex_pattern
+    except Exception as ex:
+        fmt = 'Failed to convert wildcard({!r}) to regex({!r})\n{}'
+        raise RegexConversionError(fmt.format(pattern, regex_pattern, ex))
+
+
+def foreach(data, choice='keys'):
+    """"a set-like object providing a view on D's keys/values/items
+
+    Parameters
+    ----------
+    data (Any): data
+    choice (str): keys|values|items.  Default is keys.
+
+    Returns
+    -------
+    dict_keys or odict_keys if choice is keys
+    dict_values or odict_values if choice is values
+    dict_items or odict_items if choice is items
+    """
+    if isinstance(data, dict):
+        node = data
+    elif isinstance(data, (list, tuple)):
+        total = len(data)
+        node = OrderedDict(zip(range(total), data))
+    else:
+        node = dict()
+
+    if choice == 'keys':
+        return node.keys()
+    elif choice == 'values':
+        return node.values()
+    else:
+        return node.items()
