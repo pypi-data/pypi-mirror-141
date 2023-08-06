428b6466bda5dd1981f5b0e1dd5a2ba9a1f8f883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:59 +0000

    Add package azure-search-documents/11.3.0b8/azure_search_documents-11.3.0b8-py3-none-any.whl

---
+++ packages/azure-search-documents/azure_search_documents-11.3.0b8-py3-none-any.whl/azure/search/documents/_api_versions.py
@@ -0,0 +1,15 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+from enum import Enum
+
+
+class ApiVersion(str, Enum):
+    #: this is the default version
+    V2020_06_30 = "2020-06-30"
+    V2021_04_30_preview = "2021-04-30-Preview"
+
+
+DEFAULT_VERSION = ApiVersion.V2021_04_30_preview
