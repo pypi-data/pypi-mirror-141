2d08cea2926bfe1fc55a8e182e6eae4753ca8036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:25 +0000

    Add package blueboard/0.1.1/blueboard-0.1.1.tar.gz

---
+++ packages/blueboard/blueboard-0.1.1.tar.gz/blueboard-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='blueboard',
+      version='0.1.1',
+      description='Python Package',
+      packages = ['package'],
+      install_requires = [],
+      zip_safe = False,
+      )
