a8d654341f1be0cc6ba960979e71376ff295e7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:02 +0000

    Add package jacobi/0.2.1/jacobi-0.2.1.tar.gz

---
+++ packages/jacobi/jacobi-0.2.1.tar.gz/jacobi-0.2.1/src/jacobi/__init__.py
@@ -0,0 +1,7 @@
+"""Jacobi
+
+Fast numerical derivatives for real analytic functions with arbitrary round-off error.
+"""
+
+from .core import jacobi, propagate  # noqa
+from ._version import version as __version__  # noqa
