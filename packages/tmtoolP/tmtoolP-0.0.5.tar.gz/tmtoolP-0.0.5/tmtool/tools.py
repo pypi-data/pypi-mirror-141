6b0b9c7deebc4af412feb8c4fd1b97656be983f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:08 +0000

    Add package tmtoolP/0.0.5/tmtoolP-0.0.5.tar.gz

---
+++ packages/tmtoolP/tmtoolP-0.0.5.tar.gz/tmtoolP-0.0.5/tmtool/tools.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/3/9 1:39 下午
+# @Author : chendb
+# @Description :
+
+from .util import send_mail_util, send_robot_msg_util, logger_util
+
+class Tool(object):
+
+    def __init__(self):
+        self.mail_from_user = ''    # 邮件发送者账号
+        self.mail_from_user_pwd = ''    # 邮件发送者密码
+        self.mail_from_user_host = ''    # 邮件发送者host
+        self.qy_wechat_token = ''  # 企业微信机器人token
+
+    def send_mail_msg(self, to_user, subject, content):
+        send_mail_util(self.mail_from_user, self.mail_from_user_pwd, self.mail_from_user_host, to_user, subject, content)
+
+    def send_qy_wechat_msg(self, msg, at_all=None):
+        # 发送企业微信webhook
+        if at_all is None:
+            at_all = []
+        send_robot_msg_util(msg, 'qyWechat', qy_wechat_token=self.qy_wechat_token, at_all=at_all)
+
+    @staticmethod
+    def logger(msg):
+        logger_util(msg)
+
+if __name__ == '__main__':
+
+    tool = Tool()
+    # tool.qy_wechat_token = '7fdda192-cfcb-4eb5-87a5-b341574562d5'
+    # tool.send_qy_wechat_msg('线上配置走查', at_all=['@all'])
+    tool.logger('日志信息！！！')
+
