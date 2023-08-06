d9ce5972d76db1ca834490af4e6bd0cc674493c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:28 +0000

    Add package modsim/0.2.0/modsim-0.2.0.tar.gz

---
+++ packages/modsim/modsim-0.2.0.tar.gz/modsim-0.2.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['modsim']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['paho-mqtt>=1.5.1,<2.0.0',
+ 'pip>=21.2,<22.0',
+ 'pymodbus[twisted]>=2.5.2,<3.0.0']
+
+extras_require = \
+{':python_version < "3.8"': ['importlib-metadata>=1.0,<2.0']}
+
+entry_points = \
+{'console_scripts': ['modsim = modsim:app']}
+
+setup_kwargs = {
+    'name': 'modsim',
+    'version': '0.2.0',
+    'description': 'A Modbus TCP Device Simulator',
+    'long_description': '# ModSim - A Simple Modbus TCP Device Simulator\n\n## Quick Start\n\nA docker image has been provided for user to directly run the program, \n\n  ```bash\n  docker run -p 5020:5020 helloysd/modsim\n  ```\n',
+    'author': 'Ying Shaodong',
+    'author_email': 'helloysd@foxmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/gavinying/modsim',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
