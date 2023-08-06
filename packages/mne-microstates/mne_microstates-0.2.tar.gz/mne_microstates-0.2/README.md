8489e15ca3225038be1917ba717e516bc2b44634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:29 +0000

    Add package mne-microstates/0.2/mne_microstates-0.2.tar.gz

---
+++ packages/mne-microstates/mne_microstates-0.2.tar.gz/mne_microstates-0.2/README.md
@@ -0,0 +1,45 @@
+# Microstate analysis for use with MNE-Python
+
+A small module that works with MNE-Python to perform microstate analysis in EEG
+and MEG data.
+
+To learn more about microstate analysis, read the paper:
+
+Pascual-Marqui, R. D., Michel, C. M., & Lehmann, D. (1995).  Segmentation of
+brain electrical activity into microstates: model estimation and validation.
+IEEE Transactions on Biomedical Engineering.
+https://ieeexplore.ieee.org/document/391164
+
+## Usage
+
+```python
+import mne
+import microstates
+
+# Load MNE sample dataset
+from mne.datasets import sample
+fname = sample.data_path() + '/MEG/sample/sample_audvis_filt.fif'
+raw = mne.io.read_raw_fif(fname, preload=True)
+
+# Always use an average EEG reference when doing microstate analysis
+raw.set_eeg_reference('average')
+
+# Highpass filter the data a little bit
+raw.filter(0.2, None)
+
+# Selecting the sensor types to use in the analysis. In this example, we
+# use only EEG channels
+raw.pick_types(meg=False, eeg=True)
+
+# Segment the data into 6 microstates
+maps, segmentation = microstates.segment(raw.get_data(), n_states=6)
+
+# Plot the topographic maps of the found microstates
+microstates.plot_maps(maps, raw.info)
+
+# Plot the segmentation of the first 500 samples
+microstates.plot_segmentation(segmentation[:500], raw.get_data()[:, :500], raw.times[:500])
+```
+
+## Author
+Marijn van Vliet <w.m.vanvliet@gmail.com>
---
+++ packages/mne-microstates/mne_microstates-0.2.tar.gz/mne_microstates-0.2/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+from setuptools import setup
+import codecs
+import os
+
+# get the version (don't import, so dependencies are not needed)
+version = None
+with open('microstates.py') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None:
+    raise RuntimeError('Could not determine version')
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(
+        name='mne_microstates',
+        maintainer='Marijn van Vliet',
+        maintainer_email='w.m.vanvliet@gmail.com',
+        description=('Code for microstate analysis, in combination with '
+                     'MNE-Python.'),
+        license='BSD-3',
+        url='https://github.com/wmvanvliet/mne_microstates',
+        version=version,
+        long_description=codecs.open('README.md', encoding='utf8').read(),
+        long_description_content_type='text/markdown',
+        classifiers=['Intended Audience :: Science/Research',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved',
+                     'Programming Language :: Python',
+                     'Topic :: Software Development',
+                     'Topic :: Scientific/Engineering',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS'],
+        platforms='any',
+        py_modules=['microstates'],
+        install_requires=['numpy', 'scipy', 'mne'],
+    )
