34309ab6691c93a10d736235070d61f05ae5e29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:05 +0000

    Add package sdputils/1.0.4.10/sdputils-1.0.4.10.tar.gz

---
+++ packages/sdputils/sdputils-1.0.4.10.tar.gz/sdputils-1.0.4.10/sdputils/sdputils.py
@@ -0,0 +1,91 @@
+from sqlalchemy import create_engine
+from pyhive import presto 
+from unidecode import unidecode
+
+import pandas as pd
+from exceptions import WrongFileType, InvalidIdCol
+
+class SdpUtils:
+
+    def __init__(self, host:str, port:int, username:str) -> None:
+        _engine_str = f'presto://{host}:{port}/hive'
+        self.engine = create_engine(_engine_str)
+        self.cursor = presto.connect(host, port, username)
+
+    def get_csv_name(self, path):
+        file = path.split('/')[-1]
+        
+        if not file.endswith('csv'):
+            raise WrongFileType
+
+        table_name = file.split('.')[0].lower()
+        table_name = table_name.replace(" ", "_")
+        return table_name
+
+    def csv_to_raw(self, path, chunksize=700, separator=';', verbose=False, check_duplicates=False, id_col=None):
+        '''
+        Upload a CSV to a raw table.
+        Args:
+            path: Path to csv file;
+        '''
+        
+        table_name = self.get_csv_name(path=path)
+        if check_duplicates and id_col == None:
+            raise InvalidIdCol
+
+        df_generator = pd.read_csv(path, sep=separator, header=0, skipinitialspace=True, keep_default_na=True, infer_datetime_format=True, keep_date_col=True, encoding='utf-8', chunksize=chunksize, verbose=False)
+        
+        if verbose:
+            print(f'Uploading {table_name} to raw zone..')
+            up_idx = 1
+            print(f'Uploading:    ', end='\r')
+
+        for df in df_generator:
+
+            cols = df.columns.str.lower().str.replace(' ', "_", regex=False)\
+                                        .str.replace(',','', regex=False)\
+                                        .str.replace('.','', regex=False)
+            
+            df.columns = [unidecode(col) for col in cols]
+                
+            df = df.astype(str)
+            
+            if check_duplicates:
+                df = self.get_new_entities(df, id_col, f"hive.raw.{table_name}")
+            
+            df.to_sql(table_name, con=self.engine, schema='raw', if_exists='append', index=False, method='multi')
+            
+            if verbose:
+                if up_idx < 4:
+                    up_str = '.' * up_idx
+                    print(f'Uploading: {up_str}', end='\r')
+                    up_idx += 1
+                else:
+                    print(f'Uploading:    ', end='\r')
+                    up_idx = 1
+        if verbose:
+            print('Finished upload!')
+
+    def get_new_entities(self, df:pd.DataFrame, id_col:str, table:str, chunksize:int=700):
+        '''
+        Get only new entities from a dataframe (df) according to the table and id_col specified.
+        Args:
+            df: dataframe to be analyzed;
+            id_col: column that matches the dataframe and table;
+            table: database table name.
+        Returns:
+            A pd.DataFrame containing only unique rows.
+        '''
+
+        # Get all values in the column defined as id in the df
+        ids = tuple(df[id_col].array)
+        
+        try:
+            # Get all values that matches id between the dataframa and the table. 
+            for chunk in pd.read_sql_query(f'''select {id_col} from {table} where {id_col} in {ids}''', con=self.engine, chunksize=chunksize):
+                # Remove the matched values from the df
+                mask = df[id_col].isin(chunk[id_col])
+                df = df.loc[~mask]
+        except:
+            pass
+        return df
---
+++ packages/sdputils/sdputils-1.0.4.10.tar.gz/sdputils-1.0.4.10/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="sdputils",
+    version="1.0.4.10",
+    author="Lucas S. Maximiano",
+    description="Utils' functions to use inside the Semantix Data Platform for data transformation.",
+    install_requires=[
+          'pandas==1.2.4',
+          'sqlalchemy==1.4.9',
+          'pyhive==0.6.5',
+          'unidecode==1.3.3'
+      ],
+    py_modules=['sdputils'],
+    packages=setuptools.find_packages('.')
+)
