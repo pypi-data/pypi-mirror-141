ee62f5140c8ffb0851d4fd9f48587379a3096cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:31 +0000

    Add package pydude/0.4.0/pydude-0.4.0.tar.gz

---
+++ packages/pydude/pydude-0.4.0.tar.gz/pydude-0.4.0/dude/rule.py
@@ -0,0 +1,53 @@
+from typing import Callable, NamedTuple, Optional, Tuple
+
+
+class Selector(NamedTuple):
+    selector: Optional[str] = None
+    css: Optional[str] = None
+    xpath: Optional[str] = None
+    text: Optional[str] = None
+    regex: Optional[str] = None
+
+    def to_str(self, with_type: bool = False) -> str:
+        if not with_type or self.selector:
+            selector = self.selector or self.css or self.xpath or self.text or self.regex
+            assert selector is not None
+            return selector
+        if self.css:
+            return f"css={self.css}"
+        elif self.xpath:
+            return f"xpath={self.xpath}"
+        elif self.text:
+            return f"text={self.text}"
+        return f"text=/{self.regex}/i"  # NOTE: Playwright support only
+
+    def __bool__(self) -> bool:
+        return (self.selector or self.css or self.xpath or self.text or self.regex) is not None
+
+    def __str__(self) -> str:
+        return self.selector or self.css or self.xpath or self.text or self.regex or ""
+
+
+class Rule(NamedTuple):
+    group: Selector
+    selector: Selector
+    url_pattern: str
+    handler: Callable
+    setup: bool
+    navigate: bool
+    priority: int
+
+
+def rule_sorter(rule: Rule) -> Tuple[str, Selector, Selector]:
+    return rule.url_pattern, rule.group, rule.selector
+
+
+def rule_grouper(rule: Rule) -> Tuple[str, Selector]:
+    return rule.url_pattern, rule.group
+
+
+def rule_filter(setup: bool = False, navigate: bool = False) -> Callable:
+    def wrapper(rule: Rule) -> bool:
+        return rule.setup is setup and rule.navigate is navigate
+
+    return wrapper
---
+++ packages/pydude/pydude-0.4.0.tar.gz/pydude-0.4.0/dude/scraper.py
@@ -0,0 +1,68 @@
+import logging
+from typing import Optional, Sequence
+
+from playwright import sync_api
+
+from .base import ScraperBase
+from .beautifulsoup import BeautifulSoupScraper
+from .playwright import PlaywrightScraper
+
+logger = logging.getLogger(__name__)
+
+
+class Scraper(ScraperBase):
+    """
+    Convenience class to easily use the available decorators.
+    """
+
+    def run(
+        self,
+        urls: Sequence[str],
+        pages: int = 1,
+        proxy: Optional[sync_api.ProxySettings] = None,
+        output: Optional[str] = None,
+        format: str = "json",
+        # extra args
+        parser: str = "playwright",
+        headless: bool = True,
+        browser_type: str = "chromium",
+    ) -> None:
+        """
+        Convenience method to handle switching between different types of parsers.
+
+        :param urls: List of website URLs.
+        :param pages: Maximum number of pages to crawl before exiting (default=1). This is only used when a navigate handler is defined. # noqa
+        :param proxy: Proxy settings. (see https://playwright.dev/python/docs/api/class-apirequest#api-request-new-context-option-proxy)  # noqa
+        :param output: Output file. If not provided, prints in the terminal.
+        :param format: Output file format. If not provided, uses the extension of the output file or defaults to json.
+
+        :param parser: Parser type ["playwright" (default) or "bs4"]
+        :param headless: Enables headless browser. (default=True)
+        :param browser_type: Playwright supported browser types ("chromium", "webkit" or "firefox").
+        """
+        logger.info("Scraper started...")
+
+        if not self.scraper:
+            if parser == "bs4":
+                self.scraper = BeautifulSoupScraper(
+                    rules=self.rules,
+                    groups=self.groups,
+                    save_rules=self.save_rules,
+                    has_async=self.has_async,
+                )
+            else:
+                self.scraper = PlaywrightScraper(
+                    rules=self.rules,
+                    groups=self.groups,
+                    save_rules=self.save_rules,
+                    has_async=self.has_async,
+                )
+
+        self.scraper.run(
+            urls=urls,
+            pages=pages,
+            proxy=proxy,
+            output=output,
+            format=format,
+            **{"headless": headless, "browser_type": browser_type},
+        )
