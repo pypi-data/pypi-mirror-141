d388d7c1b17902a102c758e093d0f449447fe225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:32 +0000

    Add package make-vcard/0.1.0/make_vcard-0.1.0.tar.gz

---
+++ packages/make-vcard/make_vcard-0.1.0.tar.gz/make_vcard-0.1.0/make_vcard/__main__.py
@@ -0,0 +1,4 @@
+from make_vcard import command
+
+if __name__ == "__main__":
+    command.cmd()
---
+++ packages/make-vcard/make_vcard-0.1.0.tar.gz/make_vcard-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setuptools.setup(
+    name="make_vcard",
+    version="0.1.0",
+    author="antianshi",
+    author_email="948258209@qq.com",
+    description="用于以execl文件生成.vcf通讯录导入文件",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/antianshi/make_vcard", 
+    packages=setuptools.find_packages(), 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[ 
+        'openpyxl >= 3.0.9',
+    ],
+)
