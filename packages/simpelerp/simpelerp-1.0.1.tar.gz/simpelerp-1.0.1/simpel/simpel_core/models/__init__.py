967e12bd979b1e9a1b93177fed4f341f07c8465c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:09 +0000

    Add package simpelerp/1.0.1/simpelerp-1.0.1.tar.gz

---
+++ packages/simpelerp/simpelerp-1.0.1.tar.gz/simpelerp-1.0.1/simpel/simpel_core/models/__init__.py
@@ -0,0 +1,9 @@
+from .fields import *  # NOQA
+from .mixins import *  # NOQA
+from .models import *  # NOQA
+from .models import GeneralSetting
+from .registries import *  # NOQA
+from .registries import register_setting  # NOQA
+from .templates import *  # NOQA
+
+register_setting(GeneralSetting)
