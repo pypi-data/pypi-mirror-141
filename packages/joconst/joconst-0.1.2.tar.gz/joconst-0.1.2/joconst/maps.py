6342049936db2d6aa22a7a1a1f42fa2bac0d66ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:57 +0000

    Add package joconst/0.1.2/joconst-0.1.2.tar.gz

---
+++ packages/joconst/joconst-0.1.2.tar.gz/joconst-0.1.2/joconst/maps.py
@@ -0,0 +1,9 @@
+from joconst.constant import TdxMarket, Exchange
+
+TDX_JONPY_MARKET_MAP = {
+    TdxMarket.DCE: Exchange.DCE,
+    TdxMarket.SGE: Exchange.SGE,
+    TdxMarket.CFFEX: Exchange.CFFEX,
+    TdxMarket.SHFE: Exchange.SHFE,
+    TdxMarket.CZCE: Exchange.CZCE,
+}
