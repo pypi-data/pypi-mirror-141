918ab05fc95d952605b93a86126a109dd6c0bb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:40 +0000

    Add package UiLibrary/1.0.6/UiLibrary-1.0.6-py3-none-any.whl

---
+++ packages/UiLibrary/UiLibrary-1.0.6-py3-none-any.whl/UiLibrary/test.py
@@ -0,0 +1,15 @@
+from UiLibrary import screen 
+from UiLibrary import addButtom
+from UiLibrary import addNumBox
+from UiLibrary import addlable
+from UiLibrary import textboxoutput
+from UiLibrary import mainsloop
+
+screen()
+addButtom("grhg4f",textboxoutput)
+addNumBox()
+addlable()
+help(addlable)
+help(addNumBox)
+help(addButtom)
+mainsloop()
