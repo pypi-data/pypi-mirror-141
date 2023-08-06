a614b03ea463767fe633ec58a74cc0d06f6afa24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:22 +0000

    Add package mooss-serialize/0.0.1/mooss_serialize-0.0.1-py3-none-any.whl

---
+++ packages/mooss-serialize/mooss_serialize-0.0.1-py3-none-any.whl/mooss/serialize/__version__.py
@@ -0,0 +1,12 @@
+# Constants
+VERSION_MAJOR = 0
+"""Current major version number of the application"""
+
+VERSION_MINOR = 0
+"""Current minor version number of the application"""
+
+VERSION_PATCH = 1
+"""Current patch version number of the application"""
+
+VERSION = "{}.{}.{}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+"""Current version of the application"""
