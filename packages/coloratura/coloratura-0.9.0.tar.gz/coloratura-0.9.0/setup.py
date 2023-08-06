26575c873fbe68ff6a3ed5728c88849792751d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:18 +0000

    Add package coloratura/0.9.0/coloratura-0.9.0.tar.gz

---
+++ packages/coloratura/coloratura-0.9.0.tar.gz/coloratura-0.9.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+NAME = 'coloratura'
+
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Environment :: Console'
+]
+
+setup(
+    name=NAME,
+    version='0.9.0',
+    description='Awesome cprint() function to colored terminal text. Supported full RGB!',
+    # long_description=open('README.md').read(),
+    keywords='color colour terminal text ansi windows colorama hue',
+    url='https://github.com/DawidKos/Coloratura',
+    author='pyblog.pl',
+    author_email='rewolucjazywieniowa@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+)
