3d8a8d7ec413b51ef6dbf7c29404e24310c64314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:26 +0000

    Add package damei/1.0.161/damei-1.0.161.tar.gz

---
+++ packages/damei/damei-1.0.161.tar.gz/damei-1.0.161/damei/__init__.py
@@ -0,0 +1,38 @@
+__name__ = "damei"
+
+import subprocess
+
+from damei import version
+
+__version__ = version.__version__
+__author__ = version.__author__
+
+# dmsystem
+from damei.dmsystem.system import current_system, system_lib_suffix
+
+# controls
+from damei.controls.color import ColorControl
+
+
+def colors(num):
+	return ColorControl(num).color
+
+
+# functions
+from damei.functions import general, torch_utils
+from damei.functions.sub_process import popen
+
+# post
+from damei.post import post
+
+# tools
+from damei.tools.tools import Tools
+
+# from damei.tools.tools import video2frames
+tools = Tools()
+
+from damei.tools.check_yolo import CheckYOLO
+
+# misc
+from damei.misc import misc
+from damei.misc.logger import getLogger
