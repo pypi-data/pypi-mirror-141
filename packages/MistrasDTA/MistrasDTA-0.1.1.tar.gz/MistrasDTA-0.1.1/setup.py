93115f755583145b8a91f451ad4c3df279212204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:51 +0000

    Add package MistrasDTA/0.1.1/MistrasDTA-0.1.1.tar.gz

---
+++ packages/MistrasDTA/MistrasDTA-0.1.1.tar.gz/MistrasDTA-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="MistrasDTA",
+    version="0.1.1",
+    description="Read AEWin acoustic emissions binary data files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dan Cogswell",
+    author_email="cogswell@mit.edu",
+    license="MIT",
+    url="https://github.com/d-cogswell/MistrasDTA",
+    py_modules=["MistrasDTA"],
+    install_requires=["numpy"]
+)
