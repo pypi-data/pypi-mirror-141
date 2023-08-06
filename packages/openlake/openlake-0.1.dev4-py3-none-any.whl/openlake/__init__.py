b13214809ad11ea013e2e16d3ab8c4eb3df6d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:11 +0000

    Add package openlake/0.1.dev4/openlake-0.1.dev4-py3-none-any.whl

---
+++ packages/openlake/openlake-0.1.dev4-py3-none-any.whl/openlake/__init__.py
@@ -0,0 +1,130 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Openlake ForML feed."""
+import types
+import typing
+
+import sqlalchemy
+from forml import io
+from forml.io import dsl, layout
+from forml.io.dsl import parser as parsmod
+from forml.lib.feed.reader.sql import alchemy
+from sqlalchemy import sql
+from sqlalchemy.engine import interfaces
+
+from openlake import provider
+from openlake.provider import kaggle, sklearn
+
+__version__ = '0.1.dev4'
+__author__ = 'ForML Authors'
+
+ORIGINS: typing.Iterable[provider.Origin] = {kaggle.Titanic(), sklearn.BreastCancer(), sklearn.Iris()}
+
+
+class _Tables(dsl.Source.Visitor):
+    """Visitor extracting dsl.Table instances from the given query."""
+
+    def __init__(self):
+        self._match: set[dsl.Table] = set()
+
+    @classmethod
+    def extract(cls, query: dsl.Query) -> frozenset[dsl.Table]:
+        """Frontend method for extracting tables from the given query."""
+        return cls()(query)
+
+    def __call__(self, query: dsl.Query) -> frozenset[dsl.Table]:
+        """Apply this visitor to the given query.
+
+        Args:
+            query: Query to dissect.
+
+        Returns:
+            Set of dsl.Table instances.
+        """
+        query.accept(self)
+        return frozenset(self._match)
+
+    def visit_table(self, source: dsl.Table) -> None:
+        self._match.add(source)
+
+
+class Local(io.Feed[sql.Selectable, sql.ColumnElement]):
+    """Openlake feed."""
+
+    class Reader(alchemy.Reader):
+        """Extending the SQLAlchemy reader."""
+
+        class Backend(interfaces.Connectable):
+            """Serializable in-memory SQLite connection."""
+
+            def __init__(self):
+                self._engine: typing.Optional[interfaces.Connectable] = sqlalchemy.create_engine('sqlite://')
+
+            def __repr__(self):
+                return 'OpenLakeLocalBackend'
+
+            def __reduce__(self):
+                return self.__class__, tuple()
+
+            def connect(self, **kwargs):
+                return self._engine.connect(**kwargs)
+
+            def execute(self, object_, *multiparams, **params):
+                return self._engine.execute(object_, *multiparams, **params)
+
+            def scalar(self, object_, *multiparams, **params):
+                return self._engine.scalar(object_, *multiparams, **params)
+
+            # pylint: disable=protected-access
+            def _run_visitor(self, visitorcallable, element, **kwargs):
+                return self._engine._run_visitor(visitorcallable, element, **kwargs)
+
+            def _execute_clauseelement(self, elem, multiparams=None, params=None):
+                return self._engine._execute_clauseelement(elem, multiparams, params)
+
+            def __getattr__(self, item):
+                return getattr(self._engine, item)
+
+        def __init__(
+            self,
+            sources: typing.Mapping[dsl.Source, parsmod.Source],
+            features: typing.Mapping[dsl.Feature, parsmod.Feature],
+            origins: typing.Iterable[provider.Origin],
+            **kwargs,
+        ):
+            self._loaded: set[dsl.Queryable] = set()
+            self._origins: dict[dsl.Queryable, provider.Origin] = {o.source: o for o in origins}
+            super().__init__(sources, features, self.Backend(), **kwargs)
+
+        def __call__(self, query: dsl.Query, request: typing.Optional[io.Request] = None) -> layout.Tabular:
+            tables = _Tables.extract(query)
+            for origin in (self._origins[t] for t in tables if t not in self._loaded and not self._loaded.add(t)):
+                origin().to_sql(origin.name, self._kwargs['con'], index=False)
+            return super().__call__(query, request)
+
+    def __init__(self, *origins: provider.Origin, **readerkw):
+        if not origins:
+            origins = ORIGINS
+        self._sources: typing.Mapping[dsl.Source, sql.Selectable] = types.MappingProxyType(
+            {o.source: sqlalchemy.table(o.name) for o in origins}
+        )
+        super().__init__(origins=frozenset(origins), **readerkw)
+
+    @property
+    def sources(self) -> typing.Mapping[dsl.Source, sql.Selectable]:
+        return self._sources
---
+++ packages/openlake/openlake-0.1.dev4-py3-none-any.whl/openlake/provider/__init__.py
@@ -0,0 +1,110 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+Openlake providers.
+"""
+import abc
+import pathlib
+import types
+import typing
+
+import forml
+import pandas
+from forml.io import dsl
+
+from openlake import cache
+
+Format = typing.TypeVar('Format')
+
+
+class Origin(typing.Generic[Format], metaclass=abc.ABCMeta):
+    """Openlake origin base class."""
+
+    def __hash__(self):
+        return hash(self.source)
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and other.source == self.source
+
+    def __call__(
+        self,
+        columns: typing.Optional[typing.Iterable[dsl.Feature]] = None,
+        predicate: typing.Optional[dsl.Feature] = None,
+    ) -> pandas.DataFrame:
+        frame = self._load(columns, predicate)
+        expected = {f.name for f in self.source.features}
+        assert set(frame.columns) == expected, f'Column name mismatch: {expected.symmetric_difference(frame.columns)}'
+        return frame.astype({f.name: f.kind.__native__ for f in self.source.features})
+
+    @property
+    def _cachedir(self) -> pathlib.Path:
+        """Root directory for this origin cache."""
+        return cache.DIR / self.__class__.__qualname__.lower()
+
+    def _load(
+        self, columns: typing.Optional[typing.Iterable[dsl.Feature]], predicate: typing.Optional[dsl.Feature]
+    ) -> pandas.DataFrame:
+        """Content loader with caching capabilities."""
+        return cache.dataframe(
+            repr(tuple(columns or [])) + repr(predicate),
+            lambda: self.parse(self.fetch(columns, predicate), columns, predicate),
+            self._cachedir,
+        )
+
+    @property
+    def name(self) -> str:
+        """Name to be used for internal (unique) referencing.
+
+        Must be a valid identifier -> [_a-zA-Z][_a-zA-Z0-9]*
+        """
+        return repr(self.source)
+
+    @property
+    @abc.abstractmethod
+    def source(self) -> dsl.Queryable:
+        """The source query this origin provides."""
+
+    @abc.abstractmethod
+    def fetch(
+        self, columns: typing.Optional[typing.Iterable[dsl.Feature]], predicate: typing.Optional[dsl.Feature]
+    ) -> Format:
+        """Fetch the content and return a file object."""
+
+    @abc.abstractmethod
+    def parse(
+        self,
+        content: Format,
+        columns: typing.Optional[typing.Iterable[dsl.Feature]],
+        predicate: typing.Optional[dsl.Feature],
+    ) -> pandas.DataFrame:
+        """Load the origin dataset."""
+
+
+class Unavailable(types.ModuleType):
+    """Placeholder for missing provider functionality that raises upon access."""
+
+    class Error(forml.MissingError):
+        """Custom exception for indicating access to an unavailable provider."""
+
+    def __init__(self, name: str, error: Exception):
+        super().__init__(name)
+        self.__error__: Exception = error
+
+    def __getattr__(self, item: str):
+        raise self.Error(
+            f'Provider {self.__name__} not available when accessing {item}: {self.__error__}.'
+        ) from self.__error__
