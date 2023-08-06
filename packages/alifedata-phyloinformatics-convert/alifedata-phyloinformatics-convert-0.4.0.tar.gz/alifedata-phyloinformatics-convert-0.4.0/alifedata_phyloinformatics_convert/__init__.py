b94b26c6c28fb6acef1b04496e7d0132e44ea7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:07 +0000

    Add package alifedata-phyloinformatics-convert/0.4.0/alifedata-phyloinformatics-convert-0.4.0.tar.gz

---
+++ packages/alifedata-phyloinformatics-convert/alifedata-phyloinformatics-convert-0.4.0.tar.gz/alifedata-phyloinformatics-convert-0.4.0/alifedata_phyloinformatics_convert/__init__.py
@@ -0,0 +1,26 @@
+"""Top-level package for alifedata-phyloinformatics-convert."""
+
+__author__ = """Matthew Andres Moreno"""
+__email__ = 'm.more500@gmail.com'
+__version__ = '0.4.0'
+
+from .alife_dataframe_to_dendropy_tree import alife_dataframe_to_dendropy_tree
+from .alife_dataframe_to_dendropy_trees \
+    import alife_dataframe_to_dendropy_trees
+from .biopython_tree_to_alife_dataframe \
+    import biopython_tree_to_alife_dataframe
+from .dendropy_tree_to_alife_dataframe import dendropy_tree_to_alife_dataframe
+from .dendropy_tree_to_scipy_linkage_matrix \
+    import dendropy_tree_to_scipy_linkage_matrix
+from .scipy_linkage_matrix_to_dendropy_tree \
+    import scipy_linkage_matrix_to_dendropy_tree
+
+# adapted from https://stackoverflow.com/a/31079085
+__all__ = [
+    'alife_dataframe_to_dendropy_tree',
+    'alife_dataframe_to_dendropy_trees',
+    'biopython_tree_to_alife_dataframe',
+    'dendropy_tree_to_alife_dataframe',
+    'dendropy_tree_to_scipy_linkage_matrix',
+    'scipy_linkage_matrix_to_dendropy_tree',
+]
