a6857cacd5db838dd0fd69e74dae16a9dc3e02a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:21:42 +0000

    Add package aslibtry/0.0.2/aslibtry-0.0.2.tar.gz

---
+++ packages/aslibtry/aslibtry-0.0.2.tar.gz/aslibtry-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='aslibtry',
+  version='0.0.2',
+  description='A very basic calculator',
+  long_description=open('readme.txt').read() + '\n\n' + open('changelog.txt').read(),
+  url='',  
+  author=' Aslı Nur ',
+  author_email='aslinurt85@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
