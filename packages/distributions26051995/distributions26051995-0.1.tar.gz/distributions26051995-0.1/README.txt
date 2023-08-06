5ee5dc01cd624ecec9210b1f7ca705812da86109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:43 +0000

    Add package distributions26051995/0.1/distributions26051995-0.1.tar.gz

---
+++ packages/distributions26051995/distributions26051995-0.1.tar.gz/distributions26051995-0.1/README.txt
@@ -0,0 +1,9 @@
+# dsnd-distributions package
+
+Summary of the package
+
+# Files
+
+Explanition of files in the package
+
+# Installation
---
+++ packages/distributions26051995/distributions26051995-0.1.tar.gz/distributions26051995-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions26051995',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Davi Misturini',
+      author_email = 'misturinidavi@gmail.com',
+      license = 'MIT',
+      zip_safe=False)
