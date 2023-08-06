e2bd81311c9d424b5e3038e9ed4dde09bb5eea4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:58 +0000

    Add package origin-platform-utils/0.6.4.dev23/origin_platform_utils-0.6.4.dev23.tar.gz

---
+++ packages/origin-platform-utils/origin_platform_utils-0.6.4.dev23.tar.gz/origin_platform_utils-0.6.4.dev23/src/origin/api/endpoint.py
@@ -0,0 +1,42 @@
+from abc import abstractmethod
+from typing import Optional, Any, Type
+from inspect import getfullargspec
+from functools import cached_property
+
+
+class Endpoint(object):
+    """Represents a single HTTP API endpoint."""
+
+    # Request and response schemas (dataclasses or None)
+    Request = None
+    Response = None
+
+    @abstractmethod
+    def handle_request(self, **kwargs) -> Optional[Any]:
+        """Handle a HTTP request."""
+        raise NotImplementedError
+
+    @property
+    def request_schema(self) -> Optional[Type[Any]]:
+        """Handle a request schema."""
+        return self.Request
+
+    @cached_property
+    def requires_context(self) -> bool:
+        """
+        Check if the handle_request() requires a Context.
+
+        Returns True if handle_request() requires a Context passed as argument.
+        """
+        return 'context' in getfullargspec(self.handle_request)[0]
+
+    @cached_property
+    def should_parse_request_data(self) -> bool:
+        """
+        Check if the handle_request() requires an instance.
+
+        Returns True if handle_request() requires an instance of self.Request
+        passed as argument.
+        """
+        return (self.request_schema is not None
+                and 'request' in getfullargspec(self.handle_request)[0])
