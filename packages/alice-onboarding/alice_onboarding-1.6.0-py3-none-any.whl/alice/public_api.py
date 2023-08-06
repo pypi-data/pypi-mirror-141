a7e71d429084e5a3adb3bcfbce65512a481e0f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:12 +0000

    Add package alice-onboarding/1.6.0/alice_onboarding-1.6.0-py3-none-any.whl

---
+++ packages/alice-onboarding/alice_onboarding-1.6.0-py3-none-any.whl/alice/public_api.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+# Copyright (C) 2019+ Alice, Vigo, Spain
+
+"""Public API of Alice Onboarding Python SDK"""
+
+# Modules
+from alice.webhooks.webhook import Webhook
+from alice.webhooks.webhooks import Webhooks
+from alice.webhooks.webhooks_client import WebhooksClient
+
+modules = []
+
+from alice.auth.auth import Auth
+from alice.auth.auth_client import AuthClient
+from alice.config import Config
+from alice.onboarding.enums.decision import Decision
+from alice.onboarding.models.device_info import DeviceInfo
+from alice.onboarding.models.user_info import UserInfo
+
+# Classes
+from alice.onboarding.onboarding import Onboarding
+from alice.onboarding.onboarding_client import OnboardingClient
+from alice.sandbox.sandbox import Sandbox
+from alice.sandbox.sandbox_client import SandboxClient
+
+classes = [
+    "Onboarding",
+    "OnboardingClient",
+    "UserInfo",
+    "DeviceInfo",
+    "Auth",
+    "AuthClient",
+    "Sandbox",
+    "SandboxClient",
+    "Config",
+    "Webhooks",
+    "WebhooksClient",
+    "Webhook",
+    "Decision",
+]
+
+
+# Errors
+from alice.onboarding.onboarding_errors import OnboardingError
+from alice.sandbox.sandbox_errors import SandboxError
+
+errors = ["OnboardingError", "SandboxError"]
+
+__all__ = modules + classes + errors
