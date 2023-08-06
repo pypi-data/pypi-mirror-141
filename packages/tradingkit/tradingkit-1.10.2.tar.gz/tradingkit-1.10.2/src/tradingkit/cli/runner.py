a6624fc65df3923bed6474c284c2445cf59479f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:10 +0000

    Add package tradingkit/1.10.2/tradingkit-1.10.2.tar.gz

---
+++ packages/tradingkit/tradingkit-1.10.2.tar.gz/tradingkit-1.10.2/src/tradingkit/cli/runner.py
@@ -0,0 +1,37 @@
+from tradingkit.exchange.testex import TestEX
+
+
+class Runner:
+
+    @staticmethod
+    def run(feeder, exchange, plotter, strategy, bridge, feeder_adapters=None, optimize=False, plot=False):
+        if feeder_adapters is None:
+            feeder_adapters = []
+
+        chain = feeder
+        for adapter in feeder_adapters:
+            chain.register(adapter)
+            chain = adapter
+
+        bridge.register(strategy)
+        if plot:
+            bridge.register(plotter)
+            strategy.register(plotter)
+
+        if isinstance(exchange, TestEX):
+            chain.register(exchange)
+            chain = exchange
+        chain.register(bridge)
+        feeder.feed()
+        if optimize:
+            result = strategy.finish(False)
+        else:
+            result = strategy.finish()
+            print("Trading results")
+            for info in result:
+                print("%15s: %10.2f" % (info, result[info]))
+
+        if plot:
+            plotter.plot()
+
+        return result
