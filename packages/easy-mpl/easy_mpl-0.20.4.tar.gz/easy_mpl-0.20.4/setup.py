b145d1410cb5cfe7f62c96836e187a3d6fcd6462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:11 +0000

    Add package easy-mpl/0.20.4/easy_mpl-0.20.4.tar.gz

---
+++ packages/easy-mpl/easy_mpl-0.20.4.tar.gz/easy_mpl-0.20.4/setup.py
@@ -0,0 +1,42 @@
+
+from setuptools import setup
+
+setup(
+
+    name='easy_mpl',
+
+    version="0.20.4",
+
+    description='one stop shop for matplotlib plots',
+    long_description="",
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/Sara-Iftikhar/easy_mpl',
+
+    author='Sara Iftikhar',
+    author_email='sara.rwpk@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Natural Language :: English',
+
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    packages=['easy_mpl'],
+
+    install_requires=['numpy', 'matplotlib', 'pandas'],
+)
