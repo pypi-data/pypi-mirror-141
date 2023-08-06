9bfb2e2f60a58eb0f37a1440e898ef0b6fee4bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:11 +0000

    Add package TheengsGateway/0.1.3/TheengsGateway-0.1.3.tar.gz

---
+++ packages/TheengsGateway/TheengsGateway-0.1.3.tar.gz/TheengsGateway-0.1.3/TheengsDecoder/src/devices/Miband_json.h
@@ -0,0 +1,24 @@
+const char* _Miband_json = "{\"brand\":\"Xiaomi\",\"model\":\"Miband\",\"model_id\":\"MiBand\",\"condition\":[\"uuid\",\"contain\",\"fee0\"],\"properties\":{\"steps\":{\"decoder\":[\"value_from_hex_data\",\"servicedata\",0,4,true,false]}}}";
+/*R""""(
+{
+   "brand":"Xiaomi",
+   "model":"Miband",
+   "model_id":"MiBand",
+   "condition":["uuid", "contain", "fee0"],
+   "properties":{
+      "steps":{
+         "decoder":["value_from_hex_data", "servicedata", 0, 4, true, false]
+      }
+   }
+})"""";*/
+
+const char* _Miband_json_props = "{\"properties\":{\"steps\":{\"unit\":\"int\",\"name\":\"step-count\"}}}";
+/*R""""(
+{
+   "properties":{
+      "steps":{
+         "unit":"int",
+         "name":"step-count"
+      }
+   }
+})"""";*/
