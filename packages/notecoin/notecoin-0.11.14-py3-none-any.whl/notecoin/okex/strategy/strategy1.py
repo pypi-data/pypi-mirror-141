f7d80d2035bfbb2bddd455ac3ca65479a0719945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:28 +0000

    Add package notecoin/0.11.14/notecoin-0.11.14-py3-none-any.whl

---
+++ packages/notecoin/notecoin-0.11.14-py3-none-any.whl/notecoin/okex/strategy/strategy1.py
@@ -0,0 +1,43 @@
+from flask_classful import FlaskView
+from notecoin.okex.strategy.domain import OkexCoin
+from notecoin.okex.strategy.utils import account_api, market_api
+
+
+class Strategy1(FlaskView):
+    def __init__(self):
+        self.usdt = 0
+        self.coin_map = {}
+
+    def load_account(self):
+        data = account_api.get_account().data[0]
+        self.coin_map = {}
+        for detail in data['details']:
+            if detail['ccy'] == 'USDT':
+                self.usdt = detail['availBal']
+                continue
+            coin = OkexCoin.instance_by_account(detail)
+            if coin.money > 1:
+                self.coin_map[coin.coin_id] = coin
+
+    def update_price(self):
+        data = market_api.get_tickers('SPOT').data
+        data_map = dict([(cin['instId'], cin['last']) for cin in data])
+        for coin in self.coin_map.values():
+            coin.price = float(data_map[coin.coin_id])
+
+    def to_json(self):
+        return {
+            "res": self.usdt,
+            "coins": [coin.to_json() for coin in self.coin_map.values()]
+        }
+
+    def update(self):
+        self.load_account()
+        self.update_price()
+        for coin in self.coin_map.values():
+            coin.watch()
+        return self.to_json()
+
+
+str = Strategy1()
+str.update()
