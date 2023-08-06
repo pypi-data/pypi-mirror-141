a3e12c2ee2666f7e8a1c3a8ffad0dae451d00d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:21 +0000

    Add package hypatie/2.10.2/hypatie-2.10.2-py3-none-any.whl

---
+++ packages/hypatie/hypatie-2.10.2-py3-none-any.whl/hypatie/__init__.py
@@ -0,0 +1,10 @@
+from .horizons import Vector, Observer, download
+from .animation import Body, play, play2d
+from .plots import star_chart, plot_xyz, plot_altaz, plot_radec, Telescope
+from .transform import radec_to_altaz, altaz_to_radec, radec_to_cartesian, to_xy_plane, rotating_coords
+from .simbad import bright_objects, get_objects, explore_region, search_region, search_sky, explore_sky
+from .catalogues import available_catalogues, Catalogue
+from .cosmology import CosModel, Planck18
+from .time import datetime_to_jd, jd_to_datetime
+
+__version__ = "2.10.2"
