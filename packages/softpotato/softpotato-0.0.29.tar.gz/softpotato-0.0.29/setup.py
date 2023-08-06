3e0bc407f8177b097237912d6cf2002020008597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package softpotato/0.0.29/softpotato-0.0.29.tar.gz

---
+++ packages/softpotato/softpotato-0.0.29.tar.gz/softpotato-0.0.29/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='softpotato',
+    version='0.0.29',
+    license='GPL3',
+    author='Oliver Rodriguez',
+    author_email='oliver.rdz@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    url='https://github.com/oliverrdz/softpotato_py',
+    keywords='Electrochemistry',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+)
