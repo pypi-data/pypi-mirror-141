01c2adf5ed786161ff03a779c2422b916957758e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:06 +0000

    Add package acconeer-exptool/4.0.0b1/acconeer_exptool-4.0.0b1-py3-none-any.whl

---
+++ packages/acconeer-exptool/acconeer_exptool-4.0.0b1-py3-none-any.whl/acconeer/exptool/__init__.py
@@ -0,0 +1,11 @@
+SDK_VERSION = "2.10.0"
+
+from . import a111, utils
+from .pg_process import PGProccessDiedException, PGProcess
+from .structs import configbase
+
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
