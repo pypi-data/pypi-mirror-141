0fcccf2d441bcfd9c49f2df38a5a506282b24fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:34 +0000

    Add package aa-memberaudit/1.10.0/aa_memberaudit-1.10.0-py3-none-any.whl

---
+++ packages/aa-memberaudit/aa_memberaudit-1.10.0-py3-none-any.whl/memberaudit/tests/testdata/create_eveuniverse.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from eveuniverse.models import EveUniverseEntityModel
+from eveuniverse.tools.testdata import ModelSpec, create_testdata
+
+from . import eveuniverse_test_data_filename
+
+
+class CreateEveUniverseTestData(TestCase):
+    def test_create_testdata(self):
+        testdata_spec = [
+            ModelSpec("EveAncestry", ids=[11]),
+            ModelSpec("EveBloodline", ids=[1]),
+            ModelSpec("EveFaction", ids=[500001]),
+            ModelSpec("EveRace", ids=[1]),
+            ModelSpec("EveSolarSystem", ids=[30000142, 30004984, 30001161, 30002537]),
+            ModelSpec(
+                "EveType",
+                ids=[
+                    2,
+                    5,
+                    23,
+                    603,
+                    1376,
+                    16159,
+                    20185,
+                    24311,
+                    24312,
+                    24313,
+                    24314,
+                    35832,
+                    35835,
+                    52678,
+                ],
+            ),
+            ModelSpec(
+                "EveType",
+                ids=[
+                    19540,
+                    19551,
+                    19553,
+                ],
+                enabled_sections=[EveUniverseEntityModel.LOAD_DOGMAS],
+            ),
+        ]
+        create_testdata(testdata_spec, eveuniverse_test_data_filename())
