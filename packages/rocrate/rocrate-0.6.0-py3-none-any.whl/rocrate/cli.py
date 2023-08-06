c91aa8a57fc67823e5687ff66792c11c779bab18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:35 +0000

    Add package rocrate/0.6.0/rocrate-0.6.0-py3-none-any.whl

---
+++ packages/rocrate/rocrate-0.6.0-py3-none-any.whl/rocrate/cli.py
@@ -0,0 +1,154 @@
+# Copyright 2019-2022 The University of Manchester, UK
+# Copyright 2020-2022 Vlaams Instituut voor Biotechnologie (VIB), BE
+# Copyright 2020-2022 Barcelona Supercomputing Center (BSC), ES
+# Copyright 2020-2022 Center for Advanced Studies, Research and Development in Sardinia (CRS4), IT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+
+import click
+from .rocrate import ROCrate
+from .model.computerlanguage import LANG_MAP
+from .model.testservice import SERVICE_MAP
+from .model.softwareapplication import APP_MAP
+from .utils import is_url
+
+
+LANG_CHOICES = list(LANG_MAP)
+SERVICE_CHOICES = list(SERVICE_MAP)
+ENGINE_CHOICES = list(APP_MAP)
+
+
+def add_hash(id_):
+    if id_ is None or id_.startswith("#") or is_url(id_):
+        return id_
+    return "#" + id_
+
+
+class State:
+    pass
+
+
+class CSVParamType(click.ParamType):
+    name = "csv"
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, (list, tuple, set, frozenset)):
+            return value
+        try:
+            return value.split(",") if value else []
+        except AttributeError:
+            self.fail(f"{value!r} is not splittable", param, ctx)
+
+
+CSV = CSVParamType()
+
+
+@click.group()
+@click.option('-c', '--crate-dir', type=click.Path())
+@click.pass_context
+def cli(ctx, crate_dir):
+    ctx.obj = state = State()
+    state.crate_dir = os.getcwd() if not crate_dir else os.path.abspath(crate_dir)
+
+
+@cli.command()
+@click.option('--gen-preview', is_flag=True)
+@click.option('-e', '--exclude', type=CSV)
+@click.pass_obj
+def init(state, gen_preview, exclude):
+    crate = ROCrate(state.crate_dir, init=True, gen_preview=gen_preview, exclude=exclude)
+    crate.metadata.write(state.crate_dir)
+    if crate.preview:
+        crate.preview.write(state.crate_dir)
+
+
+@cli.group()
+@click.pass_obj
+def add(state):
+    state.crate = ROCrate(state.crate_dir, init=False, gen_preview=False)
+
+
+@add.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-l', '--language', type=click.Choice(LANG_CHOICES), default="cwl")
+@click.pass_obj
+def workflow(state, path, language):
+    source = Path(path).resolve(strict=True)
+    try:
+        dest_path = source.relative_to(state.crate_dir)
+    except ValueError:
+        # For now, only support marking an existing file as a workflow
+        raise ValueError(f"{source} is not in the crate dir {state.crate_dir}")
+    # TODO: add command options for main and gen_cwl
+    state.crate.add_workflow(source, dest_path, main=True, lang=language, gen_cwl=False)
+    state.crate.metadata.write(state.crate_dir)
+
+
+@add.command(name="test-suite")
+@click.option('-i', '--identifier')
+@click.option('-n', '--name')
+@click.option('-m', '--main-entity')
+@click.pass_obj
+def suite(state, identifier, name, main_entity):
+    suite_ = state.crate.add_test_suite(identifier=add_hash(identifier), name=name, main_entity=main_entity)
+    state.crate.metadata.write(state.crate_dir)
+    print(suite_.id)
+
+
+@add.command(name="test-instance")
+@click.argument('suite')
+@click.argument('url')
+@click.option('-r', '--resource', default="")
+@click.option('-s', '--service', type=click.Choice(SERVICE_CHOICES), default="jenkins")
+@click.option('-i', '--identifier')
+@click.option('-n', '--name')
+@click.pass_obj
+def instance(state, suite, url, resource, service, identifier, name):
+    instance_ = state.crate.add_test_instance(
+        add_hash(suite), url, resource=resource, service=service,
+        identifier=add_hash(identifier), name=name
+    )
+    state.crate.metadata.write(state.crate_dir)
+    print(instance_.id)
+
+
+@add.command(name="test-definition")
+@click.argument('suite')
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-e', '--engine', type=click.Choice(ENGINE_CHOICES), default="planemo")
+@click.option('-v', '--engine-version')
+@click.pass_obj
+def definition(state, suite, path, engine, engine_version):
+    source = Path(path).resolve(strict=True)
+    try:
+        dest_path = source.relative_to(state.crate_dir)
+    except ValueError:
+        # For now, only support marking an existing file as a test definition
+        raise ValueError(f"{source} is not in the crate dir {state.crate_dir}")
+    state.crate.add_test_definition(suite, source=source, dest_path=dest_path, engine=engine, engine_version=engine_version)
+    state.crate.metadata.write(state.crate_dir)
+
+
+@cli.command()
+@click.argument('dst', type=click.Path(writable=True))
+@click.pass_obj
+def write_zip(state, dst):
+    crate = ROCrate(state.crate_dir, init=False, gen_preview=False)
+    crate.write_zip(dst)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/rocrate/rocrate-0.6.0-py3-none-any.whl/test/test_cli.py
@@ -0,0 +1,202 @@
+# Copyright 2019-2022 The University of Manchester, UK
+# Copyright 2020-2022 Vlaams Instituut voor Biotechnologie (VIB), BE
+# Copyright 2020-2022 Barcelona Supercomputing Center (BSC), ES
+# Copyright 2020-2022 Center for Advanced Studies, Research and Development in Sardinia (CRS4), IT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import pytest
+from click.testing import CliRunner
+
+from rocrate.cli import cli
+from rocrate.model.file import File
+from rocrate.model.metadata import TESTING_EXTRA_TERMS
+from rocrate.rocrate import ROCrate
+
+
+@pytest.mark.parametrize("gen_preview,cwd", [(False, False), (False, True), (True, False), (True, True)])
+def test_cli_init(test_data_dir, helpers, monkeypatch, cwd, gen_preview):
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    metadata_path = crate_dir / helpers.METADATA_FILE_NAME
+    preview_path = crate_dir / helpers.PREVIEW_FILE_NAME
+    metadata_path.unlink()
+
+    runner = CliRunner()
+    args = []
+    if cwd:
+        monkeypatch.chdir(str(crate_dir))
+    else:
+        args.extend(["-c", str(crate_dir)])
+    args.append("init")
+    if gen_preview:
+        args.append("--gen-preview")
+    result = runner.invoke(cli, args)
+    assert result.exit_code == 0
+    assert metadata_path.is_file()
+    if gen_preview:
+        assert preview_path.is_file()
+    else:
+        assert not preview_path.exists()
+
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert "sort-and-change-case.ga" in json_entities
+    assert json_entities["sort-and-change-case.ga"]["@type"] == "File"
+
+
+def test_cli_init_exclude(test_data_dir, helpers):
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    (crate_dir / helpers.METADATA_FILE_NAME).unlink()
+    exclude = "test,README.md"
+    runner = CliRunner()
+    args = ["-c", str(crate_dir), "init", "-e", exclude]
+    assert runner.invoke(cli, args).exit_code == 0
+    crate = ROCrate(crate_dir)
+    for p in "LICENSE", "sort-and-change-case.ga":
+        assert isinstance(crate.dereference(p), File)
+    for p in exclude.split(",") + ["test/"]:
+        assert not crate.dereference(p)
+    for e in crate.data_entities:
+        assert not(e.id.startswith("test"))
+
+
+@pytest.mark.parametrize("cwd", [False, True])
+def test_cli_add_workflow(test_data_dir, helpers, monkeypatch, cwd):
+    # init
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    runner = CliRunner()
+    assert runner.invoke(cli, ["-c", str(crate_dir), "init"]).exit_code == 0
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert "sort-and-change-case.ga" in json_entities
+    assert json_entities["sort-and-change-case.ga"]["@type"] == "File"
+    # add
+    wf_path = crate_dir / "sort-and-change-case.ga"
+    args = []
+    if cwd:
+        monkeypatch.chdir(str(crate_dir))
+        wf_path = wf_path.relative_to(crate_dir)
+    else:
+        args.extend(["-c", str(crate_dir)])
+    for lang in "cwl", "galaxy":
+        extra_args = ["add", "workflow", "-l", lang, str(wf_path)]
+        result = runner.invoke(cli, args + extra_args)
+        assert result.exit_code == 0
+        json_entities = helpers.read_json_entities(crate_dir)
+        helpers.check_wf_crate(json_entities, wf_path.name)
+        assert "sort-and-change-case.ga" in json_entities
+        lang_id = f"https://w3id.org/workflowhub/workflow-ro-crate#{lang}"
+        assert lang_id in json_entities
+        assert json_entities["sort-and-change-case.ga"]["programmingLanguage"]["@id"] == lang_id
+
+
+@pytest.mark.parametrize("cwd", [False, True])
+def test_cli_add_test_metadata(test_data_dir, helpers, monkeypatch, cwd):
+    # init
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    runner = CliRunner()
+    assert runner.invoke(cli, ["-c", str(crate_dir), "init"]).exit_code == 0
+    json_entities = helpers.read_json_entities(crate_dir)
+    def_id = "test/test1/sort-and-change-case-test.yml"
+    assert def_id in json_entities
+    assert json_entities[def_id]["@type"] == "File"
+    # add workflow
+    wf_path = crate_dir / "sort-and-change-case.ga"
+    assert runner.invoke(cli, ["-c", str(crate_dir), "add", "workflow", "-l", "galaxy", str(wf_path)]).exit_code == 0
+    # add test suite
+    result = runner.invoke(cli, ["-c", str(crate_dir), "add", "test-suite"])
+    assert result.exit_code == 0
+    suite_id = result.output.strip()
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert suite_id in json_entities
+    # add test instance
+    result = runner.invoke(cli, ["-c", str(crate_dir), "add", "test-instance", suite_id, "http://example.com", "-r", "jobs"])
+    assert result.exit_code == 0
+    instance_id = result.output.strip()
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert instance_id in json_entities
+    # add test definition
+    def_path = crate_dir / def_id
+    args = []
+    if cwd:
+        monkeypatch.chdir(str(crate_dir))
+        def_path = def_path.relative_to(crate_dir)
+    else:
+        args.extend(["-c", str(crate_dir)])
+    extra_args = ["add", "test-definition", "-e", "planemo", "-v", ">=0.70", suite_id, str(def_path)]
+    result = runner.invoke(cli, args + extra_args)
+    assert result.exit_code == 0
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert def_id in json_entities
+    assert set(json_entities[def_id]["@type"]) == {"File", "TestDefinition"}
+    # check extra terms
+    metadata_path = crate_dir / helpers.METADATA_FILE_NAME
+    with open(metadata_path, "rt") as f:
+        json_data = json.load(f)
+    assert "@context" in json_data
+    context = json_data["@context"]
+    assert isinstance(context, list)
+    assert len(context) > 1
+    extra_terms = context[1]
+    assert set(TESTING_EXTRA_TERMS.items()).issubset(extra_terms.items())
+
+
+@pytest.mark.parametrize("hash_", [False, True])
+def test_cli_add_test_metadata_explicit_ids(test_data_dir, helpers, monkeypatch, hash_):
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    runner = CliRunner()
+    assert runner.invoke(cli, ["-c", str(crate_dir), "init"]).exit_code == 0
+    wf_path = crate_dir / "sort-and-change-case.ga"
+    assert runner.invoke(cli, ["-c", str(crate_dir), "add", "workflow", "-l", "galaxy", str(wf_path)]).exit_code == 0
+    suite_id = "#foo"
+    cli_suite_id = suite_id if hash_ else suite_id[1:]
+    result = runner.invoke(cli, ["-c", str(crate_dir), "add", "test-suite", "-i", cli_suite_id])
+    assert result.exit_code == 0
+    assert result.output.strip() == suite_id
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert suite_id in json_entities
+    instance_id = "#bar"
+    cli_instance_id = instance_id if hash_ else instance_id[1:]
+    result = runner.invoke(
+        cli, ["-c", str(crate_dir), "add", "test-instance", cli_suite_id,
+              "http://example.com", "-r", "jobs", "-i", cli_instance_id]
+    )
+    assert result.exit_code == 0
+    assert result.output.strip() == instance_id
+    json_entities = helpers.read_json_entities(crate_dir)
+    assert instance_id in json_entities
+
+
+@pytest.mark.parametrize("cwd", [False, True])
+def test_cli_write_zip(test_data_dir, monkeypatch, cwd):
+    crate_dir = test_data_dir / "ro-crate-galaxy-sortchangecase"
+    runner = CliRunner()
+    assert runner.invoke(cli, ["-c", str(crate_dir), "init"]).exit_code == 0
+    wf_path = crate_dir / "sort-and-change-case.ga"
+    args = ["-c", str(crate_dir), "add", "workflow", str(wf_path)]
+    assert runner.invoke(cli, args).exit_code == 0
+
+    output_zip_path = test_data_dir / "test-zip-archive.zip"
+    args = []
+    if cwd:
+        monkeypatch.chdir(str(crate_dir))
+    else:
+        args.extend(["-c", str(crate_dir)])
+    args.append("write-zip")
+    args.append(str(output_zip_path))
+
+    result = runner.invoke(cli, args)
+    assert result.exit_code == 0
+    assert output_zip_path.is_file()
+
+    crate = ROCrate(output_zip_path)
+    assert crate.mainEntity is not None
