a1cbd8d451b9d268c2fc71bd63a6420d8bc8361d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:55 +0000

    Add package Beautifly-B/0.1.2.3.2/Beautifly_B-0.1.2.3.2.tar.gz

---
+++ packages/Beautifly-B/Beautifly_B-0.1.2.3.2.tar.gz/Beautifly_B-0.1.2.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='Beautifly_B',
+    version='0.1.2.3.2',  # Development release
+    description='Beautifly_B team B packages',
+    url='https://github.com/azmidri/Beautifly_B',
+    author='IE TeamB',
+    author_email='azmidri@student.ie.edu',
+    license='MIT',
+    packages=['Beautifly_B'],
+    keywords="pandas data-science data-analysis python eda",
+    install_requires=[
+          'bokeh>=2.4.2','holoviews>=1.14.8'],
+    python_requires='>=3',
+    zip_safe=False)
