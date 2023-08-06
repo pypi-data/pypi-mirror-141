a2aaa2b7409697b98f291fa9747bc274f47fb4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:21:51 +0000

    Add package bizchat/5.0.0/bizchat-5.0.0.tar.gz

---
+++ packages/bizchat/bizchat-5.0.0.tar.gz/bizchat-5.0.0/bizchat/__init__.py
@@ -0,0 +1,43 @@
+
+from socket import*
+import socket
+
+
+def server():
+    #server=socket.socket()
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    print(hostname, local_ip)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = input("enter address  ")
+    port = int(input("Enter Port : "))
+    server.bind((address, port))
+    server.listen()
+    print("server listening...")
+    connection, address = server.accept()
+    print("connected to client...")
+
+    while True:
+        data = input("server : ")
+        connection.send(bytes(data, 'utf-8'))
+        recdata = connection.recv(1024).decode()
+        print('client : ', recdata)
+
+
+
+
+def client():
+    client = socket()
+    address = input("enter address : ")
+    port = int(input("Enter Port : "))
+    client.connect((address, port))
+    print("connected to server..")
+    
+    while True:
+        recdata = client.recv(1024).decode()
+        print('server : ', recdata)
+        data = input("client: ")
+        client.send(bytes(data, 'utf-8'))
+
+
+
