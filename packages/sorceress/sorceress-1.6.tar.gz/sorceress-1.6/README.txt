ef493b586e54e8bb978f238af886cbc95e8d4af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:00 +0000

    Add package sorceress/1.6/sorceress-1.6.tar.gz

---
+++ packages/sorceress/sorceress-1.6.tar.gz/sorceress-1.6/README.txt
@@ -0,0 +1,5 @@
+Python Package For Creating Optical Illusions
+
+Author: Enes Altun
+
+instructions: https://github.com/altunenes/sorceress
---
+++ packages/sorceress/sorceress-1.6.tar.gz/sorceress-1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='sorceress',
+    version='1.6',
+    description='Optical Illusions with Python',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/emportent/sorceress',
+    author='Enes Altun',
+    author_email='enesaltun2@gmail.com',
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Optical-Illusion,illusions,optical',
+    packages=['sorceress'],
+    install_requires=['opencv-python',"numpy","colour-science","imageio","matplotlib","Pillow",],
+extras_require = {
+    'dev': [
+        'imageio',
+        'pytest',
+        'pytest-cov',
+        'flake8',
+    ]
+}
+)
