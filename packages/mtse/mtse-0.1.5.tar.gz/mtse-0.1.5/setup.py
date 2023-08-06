3cbfcc047f1ce2fe5f68be195f3e03149b6dc72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package mtse/0.1.5/mtse-0.1.5.tar.gz

---
+++ packages/mtse/mtse-0.1.5.tar.gz/mtse-0.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'Multi Time Series Encoders'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="mtse",
+    version='0.1.5',
+    author="FractalySyn (Corentin Lobet)",
+    author_email="<corentin.lobet@etu.unistra.fr>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['torch>=1.7', 'numpy>=1.19', 'pandas>=1.3', 'matplotlib>=3.4', 'setuptools'],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    license='Apache 2.0',
+    url="https://github.com/FractalySyn/mtse",
+    python_requires=">=3.7",
+    include_package_data=True,
+    package_data={'': ['data/*']}
+)
+
+
