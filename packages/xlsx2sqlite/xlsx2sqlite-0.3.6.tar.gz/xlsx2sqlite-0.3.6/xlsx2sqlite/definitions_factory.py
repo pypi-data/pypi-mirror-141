3412c9633460bb4a405a267e2deaa79534217359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package xlsx2sqlite/0.3.6/xlsx2sqlite-0.3.6.tar.gz

---
+++ packages/xlsx2sqlite/xlsx2sqlite-0.3.6.tar.gz/xlsx2sqlite-0.3.6/xlsx2sqlite/definitions_factory.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+import xlsx2sqlite.constraint_factory as constraint
+import xlsx2sqlite.field_factory as field
+
+
+class Definitions:
+    """Definitions generator for Sqlite3 tables."""
+
+    COMMA_DELIM = ","
+
+    def __init__(
+        self,
+        name=None,
+        headers=None,
+        row=None,
+        model=None,
+    ):
+        try:
+            if headers is None or row is None:
+                raise TypeError
+            self.tablename = name
+            self.headers = headers
+            self.row = row
+            self.unique_keys = model["unique"]
+            self.pk = model["primary_key"]
+            self.not_null = model["not_null"]
+            self.table_constraints = []
+            # map types to column names
+            self._fields = dict(
+                zip(
+                    self.headers,
+                    [{"type": self.test_type(v)} for v in self.row],
+                )
+            )
+            if self.pk is None:
+                """If a table has a single column primary key and the declared type of that column
+                is "INTEGER" and the table is not a WITHOUT ROWID table, then the column is known
+                as an INTEGER PRIMARY KEY. The column become an alias for the rowid column.
+                If an INSERT statement attempts to insert a NULL value into a rowid or integer
+                primary key column, the system chooses an integer value to use as the rowid
+                automatically.
+                """
+                self.primary_key = field.create_field("PrimaryKey", "id", "INTEGER")
+            elif isinstance(self.pk, list):
+                if any(set(self.pk) & set(headers)):
+                    for key in self.pk:
+                        if key in set(self._fields):
+                            pass
+                        else:
+                            self.primary_key = field.create_field(
+                                "NotNullField", key, "INTEGER"
+                            )
+                    # support for composite primary key
+                    self.table_constraints.append(
+                        constraint.create_table_constraint(
+                            clause="PrimaryKey", columns=self.pk
+                        )
+                    )
+                else:
+                    # set a custom name for the row_id alias
+                    self.primary_key = field.create_field(
+                        "PrimaryKey", self.pk[0], "INTEGER"
+                    )
+            if self.unique_keys and isinstance(self.unique_keys, list):
+                self.table_constraints.append(
+                    constraint.create_table_constraint(
+                        clause="Unique", columns=self.unique_keys
+                    )
+                )
+        except TypeError:
+            print("Must declare headers and row.")
+            return
+
+    @staticmethod
+    def test_type(value):
+        """Detect the type of an object and return a Sqlite3 type affinity.
+
+        :param value: An instance to check for affinity.
+        :returns: The name of the Sqlite3 affinity type.
+        :rtype: str
+        """
+        if isinstance(value, str):
+            return "TEXT"
+        elif isinstance(value, int):
+            return "INTEGER"
+        elif isinstance(value, float):
+            return "REAL"
+        elif isinstance(value, datetime):
+            return "TIMESTAMP"
+        elif value is None:
+            return "BLOB"
+        else:
+            return "TEXT"
+
+    def get_fields(self):
+        """Get the list of fields for using as columns definitions.
+        This method doesn't return the primary key in the list of fields.
+
+        :returns: A list of Field instances describing the columns of the database.
+        :rtype: list
+        """
+
+        def column_constraint(key):
+            if pk is not None and key in pk:
+                # workaround primary key field bug in sqlite, using NOT NULL column constraint
+                return "NotNullField"
+            else:
+                if not_null is not None and key in not_null:
+                    return "NotNullField"
+                else:
+                    return "Field"
+
+        pk = None if self.pk is None else set(self.pk)
+        not_null = None if self.not_null is None else set(self.not_null)
+        fields = [
+            field.create_field(column_constraint(k), k, v["type"])
+            for k, v in self._fields.items()
+        ]
+        return fields
+
+    def prepare_sql(self):
+        """Returns the complete sql definition as a string.
+
+        :returns: A string representing the SQL definitions for the CREATE TABLE statement.
+        :rtype: str
+        """
+        fields = self.get_fields()
+
+        if hasattr(self, "primary_key"):
+            fields.insert(0, self.primary_key)
+        sql_strings = [field.to_sql() for field in fields]
+        if any(self.table_constraints):
+            [
+                sql_strings.append(costraint.to_sql())
+                for costraint in self.table_constraints
+            ]
+        return self.COMMA_DELIM.join(sql_strings)
+
+    def get_labels(self):
+        labels = [field.label() for field in self.get_fields()]
+        return self.COMMA_DELIM.join(labels)
---
+++ packages/xlsx2sqlite/xlsx2sqlite-0.3.6.tar.gz/xlsx2sqlite-0.3.6/xlsx2sqlite/field_factory.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+__all__ = ["create_field"]
+
+
+COMMA_DELIM = ","
+SPACE_DELIM = " "
+
+
+def create_field(type_of: str, field_name: str, field_type: str) -> object:
+    """Field factory
+
+    :param type_of: The type to assign to the database table column, choose between:
+
+                    - Field: generic field without a column level constraint
+                    - Unique: field with a UNIQUE clause
+                    - PrimaryKey: field with PRIMARY KEY column level constraint
+                    - NotNullField: set a NOT NULL column constraint on the database column
+
+    :type type_of: str
+    :param field_name: Label for the column of the database table.
+    :type field_name: str
+    :param field_type: Column type as a string defined in sqlite column types.
+    :type field_type: str
+    :returns: An instance of a Field object with a to_sql() method that returns an
+              appropriate SQL string.
+    :rtype: object
+    """
+
+    fields = {
+        "Field": Field,
+        "Unique": UniqueField,
+        "PrimaryKey": PrimaryKey,
+        "NotNullField": NotNullField,
+    }
+    return fields[type_of](field_name, field_type)
+
+
+class Field:
+    """Represents a database field."""
+
+    def __init__(self, field_name=None, field_type=None):
+        self.field_name = field_name
+        self.field_type = field_type
+
+    def __repr__(self):
+        return f"<Field {self.field_name}, type={self.field_type}>"
+
+    def label(self):
+        """A form of the string with the leading and trailing characters removed.
+
+        :returns: A form of the string with the leading and trailing characters removed.
+        :rtype: str
+        """
+        backtick = "`"
+        label = f"{backtick}{self.field_name.strip()}{backtick}"
+        return label
+
+    def to_sql(self):
+        """Return a suitable string for using in the database.
+
+        :returns: A capitalized form of the string with the leading
+                  and trailing characters removed.
+        :rtype: str
+        """
+        return SPACE_DELIM.join((self.label(), self.field_type))
+
+
+class AddDefinitionMixin:
+    """Render the definition field added in the subclass.
+
+    Main class must contain a label method that returns a string.
+    """
+
+    def __repr__(self):
+        return f"<Field {self.field_name}, type={self.field_type} definition={self.definition}>"
+
+    def to_sql(self):
+        """Return a suitable string for using in the database.
+
+        :returns: A capitalized form of the string with the leading
+                  and trailing characters removed.
+        :rtype: str
+        """
+        return SPACE_DELIM.join((self.label(), self.field_type, self.definition))
+
+
+class PrimaryKey(AddDefinitionMixin, Field):
+    """Represents the primary key for a database table."""
+
+    def __init__(self, field_name, field_type):
+        super().__init__(field_name, field_type)
+        self.definition = "NOT NULL PRIMARY KEY"
+
+
+class UniqueField(AddDefinitionMixin, Field):
+    """Represents a field with a UNIQUE clause."""
+
+    def __init__(self, field_name, field_type):
+        super().__init__(field_name, field_type)
+        self.definition = "UNIQUE"
+
+
+class NotNullField(AddDefinitionMixin, Field):
+    """A field with the NOT NULL clause."""
+
+    def __init__(self, field_name, field_type):
+        super().__init__(field_name, field_type)
+        self.definition = "NOT NULL"
