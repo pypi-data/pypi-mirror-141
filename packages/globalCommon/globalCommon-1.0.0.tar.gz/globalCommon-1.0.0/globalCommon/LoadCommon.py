f4fbeea58a3513c50aa2d020071475d4cb258fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:37 +0000

    Add package globalCommon/1.0.0/globalCommon-1.0.0.tar.gz

---
+++ packages/globalCommon/globalCommon-1.0.0.tar.gz/globalCommon-1.0.0/globalCommon/LoadCommon.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+
+"""
+	自动引入上级的Common组件
+"""
+
+
+path=''
+flag=False
+# 循环向上级目录查找Common
+for i in range(10):
+	tempStr='/'.join(['..' for i in range(i)])
+	path=os.path.abspath(os.path.join(os.getcwd(),tempStr))
+	if os.path.exists(path+'/Common') and os.path.exists(path+'/Common/__init__.py'):
+		flag=True
+		break
+
+
+# 正常找到Common
+if flag:
+	sys.path.append(path+r'\Common')
+
+else:
+	raise Exception("Can't find Common!")
