fe040dec1cd7bbdfc5a1ec85349964c666391863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:11 +0000

    Add package pyfrench/0.0.1/pyfrench-0.0.1-py3-none-any.whl

---
+++ packages/pyfrench/pyfrench-0.0.1-py3-none-any.whl/tests/console_test.py
@@ -0,0 +1,7 @@
+from module.pyfrench import *
+
+erreur('CECI N\'EST PAS UNE ERREUR')
+attention('CECI N\'EST PAS UN WARN')
+succes('CECI N\'EST PAS UN SUCCÈS')
+
+afficher(couleur(77, 77, 77, 'CECI EST UN TEST DE COULEUR'))
