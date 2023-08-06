62266c26db12dd4795b1b35e377c4b2cbc3a100e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:05 +0000

    Add package MR-Package/1.1.7/MR_Package-1.1.7.tar.gz

---
+++ packages/MR-Package/MR_Package-1.1.7.tar.gz/MR_Package-1.1.7/src/SimpleMySQL/__init__.py
@@ -0,0 +1,2 @@
+print("Loading")
+print("SimpleMySQL Package by Manuel Raffl 2022")
---
+++ packages/MR-Package/MR_Package-1.1.7.tar.gz/MR_Package-1.1.7/src/SimpleMySQL/connect.py
@@ -0,0 +1,59 @@
+import mysql.connector
+
+print_stat = False
+DB = ""
+cursor = ""
+
+def DB(user_password="", hostname="localhost", username="root"):
+    print("try to connect")
+    try:
+        Database = mysql.connector.connect(
+            host = hostname,
+            user = username,
+            password = user_password
+        )
+    except:
+        if print_stat:
+            print("Could not connect to Database")
+        return "unable to connect"
+
+    if print_stat:
+        print("Connection established")
+    DB = Database
+    return Database
+
+def new_cursor():
+    global cursor
+    try:
+        __cursor = DB.cursor()
+    except:
+        if print_stat:
+            print("Error while creating cursor on DB: " + str(DB))
+        if DB == "":
+            if print_stat:
+                print("no database set")
+        return "Error while creating cursor on DB: " + str(DB)
+        
+
+    if print_stat:
+            print("Succesfully created cursor on DB: " + str(DB))
+    cursor = __cursor
+    return __cursor
+
+def new_DB(Name):
+    global cursor
+    command = "CREATE DATABASE " + str(Name)
+    try:
+        cursor.execute(command)
+    except:
+        if print_stat:
+            print("Error while creating new DB: " + str(Name))
+        if cursor == "":
+            if print_stat:
+                print("no cursor set")
+                new_cursor()
+                new_DB(Name)
+        return "Error while creating new DB: " + str(Name)
+    if print_stat:
+        print("Suzccesfully created new database: " + str(Name))
+    return
