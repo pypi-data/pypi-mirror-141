d6f655fe48af18102374c4dbd7e68df2d352b3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:25 +0000

    Add package moxfield/0.3.1/moxfield-0.3.1-py3-none-any.whl

---
+++ packages/moxfield/moxfield-0.3.1-py3-none-any.whl/moxfield/search.py
@@ -0,0 +1,21 @@
+class MoxfieldSearchAPI:
+
+    def search_single(self, query):
+        pageNum = 1
+        params = {"q": query, "page": pageNum}
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.5'
+        }
+        r = self.session.get('https://api.moxfield.com/v2/cards/search', params=params, headers=headers)
+        r.raise_for_status()
+        resp = r.json()
+
+        if 'code' in resp and resp['code'] == 'not_found':
+            return None
+
+        return resp['data'][0]
+
+    def __init__(self, session):
+        self.session = session
+        pass
