6cf98fecefe335a6c2bcc2e1b4513f4be7b3a50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:21 +0000

    Add package CardDeckPy/1.0.1/CardDeckPy-1.0.1.tar.gz

---
+++ packages/CardDeckPy/CardDeckPy-1.0.1.tar.gz/CardDeckPy-1.0.1/PyCardDeck/deck.py
@@ -0,0 +1,51 @@
+from random import shuffle as s
+from typing import List
+from .card import Card
+
+
+class Deck:
+    def __init__(self, cards: List, shuffled: bool = True):
+        self.deck = cards
+
+        if shuffled:
+            self.shuffle()
+        self.get_deck()
+    def get_deck(self):
+        return self.deck
+    def shuffle(self):
+        s(self.deck)
+    def draw(self):
+        return self.deck.pop(0)
+
+    def __len__(self):
+        return len(self.deck)
+
+    def draw_n_cards(self, amount):
+        drawn_cards = []
+        for _ in range(amount):
+            drawn_cards.append(self.draw())
+        return drawn_cards
+
+    def fill(self):
+        word_nums = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "jack",
+            "queen",
+            "king",
+            "ace",
+        ]
+        self.deck.clear()
+        for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+            for num in range(2, 15):
+                self.deck.append(Card(num, word_nums[num - 2], suit))
+    def __repr__(self):
+        return f'{self.deck}'
