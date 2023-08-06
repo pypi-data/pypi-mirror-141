be25c509e85e1ea06ea8b8d4e92a0daf9c322a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:12 +0000

    Add package PyMyORM/1.3.2/PyMyORM-1.3.2-py3-none-any.whl

---
+++ packages/PyMyORM/PyMyORM-1.3.2-py3-none-any.whl/tests/my_where.py
@@ -0,0 +1,17 @@
+from pymyorm.database import Database
+from config import db
+from models.user import User
+
+
+if __name__ == '__main__':
+
+    Database.connect(**db)
+
+    name = 'jack'
+    status = 1
+    where = "name='%s' or status=%s" % (name, status)
+    # where2 = f"name='{name}' or status={status}"
+    print(where)
+    one = User.find().where("name='%s' or status=%s" % (name, status)).one()
+    # one = User.find().where(name=name, status=status).one()
+    print(one)
