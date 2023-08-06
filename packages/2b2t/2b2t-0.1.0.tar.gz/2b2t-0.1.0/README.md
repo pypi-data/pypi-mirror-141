10a35fc6724374fc23fe10c4d4c9e85bda8eb330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:24 +0000

    Add package 2b2t/0.1.0/2b2t-0.1.0.tar.gz

---
+++ packages/2b2t/2b2t-0.1.0.tar.gz/2b2t-0.1.0/README.md
@@ -0,0 +1,3 @@
+# bbtt: A 2b2t toolbox
+
+This is a simple commandline utility which polls *2b2t*'s server status and print it.
---
+++ packages/2b2t/2b2t-0.1.0.tar.gz/2b2t-0.1.0/bbtt/__main__.py
@@ -0,0 +1,44 @@
+import mcstatus
+import beepy
+import threading
+
+import time
+import enum
+
+
+class Status(enum.Enum):
+    OFFLINE = 0
+    ONLINE = 1
+
+
+class Beeper(threading.Thread):
+    def run(self) -> None:
+        for _ in range(5):
+            beepy.beep(sound='error')
+
+    @staticmethod
+    def beep():
+        Beeper().start()
+
+
+def main():
+    server = mcstatus.MinecraftServer('connect.2b2t.org')
+    last_status = Status.ONLINE
+    while True:
+        try:
+            status = server.status()
+        except IOError:
+            print('status: offline')
+            last_status = Status.OFFLINE
+            time.sleep(0.5)
+            continue
+
+        if last_status == Status.OFFLINE:
+            Beeper.beep()
+        last_status = Status.ONLINE
+        print(f'status: online players: {status.players.online} latency: {status.latency:.2f}')
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    main()
