5a7965e3004f7beb593c65f9a61f208b7ed8960c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:53 +0000

    Add package PPK/1.1/PPK-1.1.tar.gz

---
+++ packages/PPK/PPK-1.1.tar.gz/PPK-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='PPK',
+      version="1.01",
+      packages=['PPK', 'PPK.GeoIP', 'PPK.Tools', 'PPK.Exceptions'],
+      url='https://github.com/MHProDev/PyRoxy',
+      license='MIT',
+      author="FFPS",
+      install_requires=[
+          "maxminddb>=2.2.0", "requests>=2.27.1", "yarl>=1.7.2",
+          "pysocks>=1.7.1"
+      ],
+      include_package_data=True,
+      package_data={
+          'PPK.GeoIP': ['Sqlite/*.txt', "Sqlite/GeoLite2-Country.mmdb"],
+          '': ["LICENSE.md"]
+      })
