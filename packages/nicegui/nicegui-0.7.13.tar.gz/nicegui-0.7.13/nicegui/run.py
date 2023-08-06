d70da2a67c1304d93eebad0b283ce33951074d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:49 +0000

    Add package nicegui/0.7.13/nicegui-0.7.13.tar.gz

---
+++ packages/nicegui/nicegui-0.7.13.tar.gz/nicegui-0.7.13/nicegui/run.py
@@ -0,0 +1,32 @@
+from typing import Awaitable, Callable, Optional, Union
+import inspect
+import sys
+import webbrowser
+import uvicorn
+from . import globals
+
+if not globals.config.interactive and globals.config.reload and not inspect.stack()[-2].filename.endswith('spawn.py'):
+
+    if globals.config.show:
+        webbrowser.open(f'http://{globals.config.host}:{globals.config.port}/')
+    uvicorn.run('nicegui:app', host=globals.config.host, port=globals.config.port, lifespan='on', reload=True,
+                log_level=globals.config.uvicorn_logging_level)
+    sys.exit()
+
+def run(self, *,
+        host: str = '0.0.0.0',
+        port: int = 8080,
+        title: str = 'NiceGUI',
+        favicon: str = 'favicon.ico',
+        dark: Optional[bool] = False,
+        reload: bool = True,
+        show: bool = True,
+        on_connect: Optional[Union[Callable, Awaitable]] = None,
+        uvicorn_logging_level: str = 'warning',
+        main_page_classes: str = 'q-ma-md column items-start',
+        ):
+
+    if globals.config.interactive or reload == False:  # NOTE: if reload == True we already started uvicorn above
+        if show:
+            webbrowser.open(f'http://{host if host != "0.0.0.0" else "127.0.0.1"}:{port}/')
+        uvicorn.run(globals.app, host=host, port=port, lifespan='on', log_level=uvicorn_logging_level)
