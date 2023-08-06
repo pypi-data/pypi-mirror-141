a8759ff1f638d1b25e1da521af60269b51b4b10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:13 +0000

    Add package alapakam-generics/0.0.1/alapakam_generics-0.0.1-py3-none-any.whl

---
+++ packages/alapakam-generics/alapakam_generics-0.0.1-py3-none-any.whl/alpakam_generics/generic.py
@@ -0,0 +1,14 @@
+def addition(a=None, b=None):
+    return a + b
+
+
+def multiplication(a=None, b=None):
+    return a * b
+
+
+def division(a=None, b=None):
+    return a % b
+
+
+def reverse_the_string(name=None):
+    return name[::-1]
