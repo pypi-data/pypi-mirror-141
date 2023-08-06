ca3d49e3a07f6496749362479ea40d81509505b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:31 +0000

    Add package furkan-baskaya-453-hw1/2.0.1/furkan-baskaya-453-hw1-2.0.1.tar.gz

---
+++ packages/furkan-baskaya-453-hw1/furkan-baskaya-453-hw1-2.0.1.tar.gz/furkan-baskaya-453-hw1-2.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="furkan-baskaya-453-hw1",
+    author="Furkan Başkaya",
+    version="2.0.1",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
