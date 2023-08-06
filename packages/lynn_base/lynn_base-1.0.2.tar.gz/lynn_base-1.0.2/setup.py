f075848a102c476d36d8acf41227f9e1adc2c057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:34 +0000

    Add package lynn_base/1.0.2/lynn_base-1.0.2.tar.gz

---
+++ packages/lynn_base/lynn_base-1.0.2.tar.gz/lynn_base-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lynn_base',
+    version='1.0.2',
+    py_modules=['lynn_base'],
+    author='lynn',
+    author_email='yinyin2568@163.com',
+    url='https://gitee.com/lynn2568',
+    description='a simple object of 【json/yaml/excel/log/mysql/redis/ssh/mongodb】',
+    classifiers=[
+        # 'base_json :: Python'
+    ],
+    install_requires=[],
+)
