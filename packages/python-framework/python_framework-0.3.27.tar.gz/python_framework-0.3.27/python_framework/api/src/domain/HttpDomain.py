c7cac504d67914ef110d7d8f60961578eb450cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:18 +0000

    Add package python-framework/0.3.27/python_framework-0.3.27.tar.gz

---
+++ packages/python-framework/python_framework-0.3.27.tar.gz/python_framework-0.3.27/python_framework/api/src/domain/HttpDomain.py
@@ -0,0 +1,17 @@
+
+
+
+class HeaderKey:
+    CONTENT_TYPE = 'Content-Type'
+
+
+class Verb:
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    PATCH = 'PATCH'
+    OPTIONS = 'OPTIONS'
+
+CONTROLLER_CONTEXT = 'ControllerRequest'
+CLIENT_CONTEXT = 'ClientRequest'
