d263eebdc1bfb1cb484298584d2b368437026a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package dqdatasdk/2.9.45.1/dqdatasdk-2.9.45.1.tar.gz

---
+++ packages/dqdatasdk/dqdatasdk-2.9.45.1.tar.gz/dqdatasdk-2.9.45.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    version = "2.9.45.1"
+
+    setup(
+        name='dqdatasdk',
+        version=version,
+        author='digquant',
+        author_email='it@digquant.com',
+        description='dqdatasdk api system',
+        keywords='dqdatasdk',
+        license='LICENSE',
+        packages=find_packages(),
+        package_dir={'': '.'},
+        install_requires=[
+            'rqdatac>=' + version + ',' + '<=' + version,
+            ],
+        dependency_links=[
+
+        ],
+        url='',
+
+    )
