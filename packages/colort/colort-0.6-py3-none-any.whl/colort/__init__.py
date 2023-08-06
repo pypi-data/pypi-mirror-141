75af71f8353e60b41957b0a8d119de02d6587ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:41 +0000

    Add package colort/0.6/colort-0.6-py3-none-any.whl

---
+++ packages/colort/colort-0.6-py3-none-any.whl/colort/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author:    thepoy
+# @Email:     thepoy@163.com
+# @File Name: __init__.py
+# @Created:   2021-05-27 09:34:57
+# @Modified:  2022-03-09 11:15:45
+
+from colort.colort import DisplayStyle
+
+__version__ = "0.6"
+
+display_style = DisplayStyle()
