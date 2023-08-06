3f19ec9dd80d4a80c9a8b54c94f71f2a9262710f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:29 +0000

    Add package OpenFisca-UK/0.14.1/OpenFisca_UK-0.14.1-py3-none-any.whl

---
+++ packages/OpenFisca-UK/OpenFisca_UK-0.14.1-py3-none-any.whl/openfisca_uk/tests/microsimulation/test_statistics.py
@@ -0,0 +1,176 @@
+from typing import Union
+import yaml
+from pathlib import Path
+from openfisca_uk import Microsimulation
+from openfisca_uk_data import FRSEnhanced
+import pytest
+
+with open(Path(__file__).parent / "statistics.yaml") as f:
+    statistics = yaml.load(f, Loader=yaml.SafeLoader)
+
+sim = Microsimulation(dataset=FRSEnhanced, year=2019)
+variables = sim.simulation.tax_benefit_system.variables
+parameters = sim.simulation.tax_benefit_system.parameters
+
+
+def get_openfisca_uk_aggregate(variable: str, year: int) -> float:
+    return sim.calc(variable, period=year, map_to="household").sum()
+
+
+def get_openfisca_uk_caseload(variable: str, year: int) -> float:
+    values = sim.map_to(
+        sim.calc(variable, period=year).values > 0,
+        variables[variable].entity.key,
+        "household",
+    )
+    return (sim.calc("household_weight", period=year).values * values).sum()
+
+
+class StatisticTest:
+    def __init__(self, variable: str, statistic: str, year: int, **kwargs):
+        self.variable = variable
+        self.year = year
+        self.statistic = statistic
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @property
+    def openfisca_uk(self):
+        if self.statistic == "aggregate":
+            return get_openfisca_uk_aggregate(self.variable, self.year)
+        elif self.statistic == "caseload":
+            return get_openfisca_uk_caseload(self.variable, self.year)
+        raise ValueError(f"Unknown statistic: {self.statistic}")
+
+    def stored_result(self, source: str):
+        try:
+            if self.statistic == "aggregate":
+                return statistics[self.variable][source]["aggregate"][
+                    self.year
+                ]
+            elif self.statistic == "caseload":
+                return statistics[self.variable][source]["count"][self.year]
+        except:
+            # Not overridden - use statistics parameter
+            if source == "official":
+                return parameters.calibration.children[
+                    {
+                        "aggregate": "program_aggregates",
+                        "caseload": "program_caseloads",
+                    }[self.statistic]
+                ].children[self.variable](f"{self.year}-01-01")
+            elif source == "ukmod":
+                raise ValueError(
+                    f"UKMOD values not given for {self.variable} {self.statistic} in {self.year}"
+                )
+        raise ValueError(f"Unknown statistic: {self.statistic}")
+
+    @property
+    def ukmod(self):
+        return self.stored_result("ukmod")
+
+    @property
+    def official(self):
+        return self.stored_result("official")
+
+    @property
+    def variable_label(self):
+        return variables[self.variable].label
+
+    def test(self) -> bool:
+        raise NotImplementedError("Test not specified")
+
+    def describe(self) -> str:
+        raise NotImplementedError("Description not specified")
+
+    def __repr__(self):
+        return self.describe()
+
+
+class CloserThanUKMOD(StatisticTest):
+    def test(self) -> bool:
+        openfisca_uk_error = abs(self.openfisca_uk - self.official)
+        ukmod_error = abs(self.ukmod - self.official)
+        return openfisca_uk_error <= ukmod_error, locals()
+
+    def describe(self):
+        return f"OpenFisca-UK {self.variable_label} {self.statistic} at least as close to the official aggregate as UKMOD in {self.year}"
+
+
+class AbsoluteErrorLessThan(StatisticTest):
+    def test(self):
+        openfisca_uk = self.openfisca_uk
+        official = self.official
+        openfisca_uk_error = abs(openfisca_uk - official)
+        return openfisca_uk_error < self.max_error, locals()
+
+    def describe(self):
+        return f"OpenFisca-UK {self.variable_label} {self.statistic} error is less than {self.max_error:,} in {self.year}"
+
+
+class RelativeErrorLessThan(StatisticTest):
+    def test(self):
+        openfisca_uk = self.openfisca_uk
+        official = self.official
+        absolute_error = abs(openfisca_uk - official)
+        relative_error = absolute_error / official
+        return relative_error < self.max_error, locals()
+
+    def describe(self):
+        return f"OpenFisca-UK {self.variable_label} {self.statistic} error is less than {self.max_error:.1%} in {self.year}"
+
+
+tests = []
+
+for variable in statistics:
+    variable_tests = statistics[variable]["test"]
+    test_names = list(
+        map(
+            lambda item: item
+            if isinstance(item, str)
+            else list(item.keys())[0],
+            variable_tests,
+        )
+    )
+    dict_tests = {
+        list(item.keys())[0]: item
+        for item in variable_tests
+        if isinstance(item, dict)
+    }
+    for statistic in ("aggregate", "caseload"):
+        if f"{statistic}_closer_than_ukmod" in test_names:
+            if statistic in statistics[variable]["ukmod"]:
+                for year in statistics[variable]["ukmod"][statistic]:
+                    tests += [CloserThanUKMOD(variable, statistic, year)]
+        if f"{statistic}_error_less_than" in test_names:
+            for year in range(2019, 2023):
+                tests += [
+                    AbsoluteErrorLessThan(
+                        variable,
+                        statistic,
+                        year,
+                        max_error=dict_tests[
+                            f"absolute_{statistic}_error_less_than"
+                        ][f"absolute_{statistic}_error_less_than"],
+                    )
+                ]
+        if f"relative_{statistic}_error_less_than" in test_names:
+            for year in range(2019, 2023):
+                tests += [
+                    RelativeErrorLessThan(
+                        variable,
+                        statistic,
+                        year,
+                        max_error=dict_tests[
+                            f"relative_{statistic}_error_less_than"
+                        ][f"relative_{statistic}_error_less_than"],
+                    )
+                ]
+
+
+@pytest.mark.parametrize("test", tests, ids=str)
+def test_statistics(test):
+    passed, variables = test.test()
+    assert passed, "Test failed: \n\n" + "\n".join(
+        [f"{key}: {value}" for key, value in variables.items()]
+    )
---
+++ packages/OpenFisca-UK/OpenFisca_UK-0.14.1-py3-none-any.whl/openfisca_uk/tools/simulation.py
@@ -0,0 +1,156 @@
+import logging
+from openfisca_uk import CountryTaxBenefitSystem
+from openfisca_uk.entities import entities
+import numpy as np
+import warnings
+from openfisca_uk.entities import *
+import numpy as np
+import warnings
+from openfisca_uk.initial_setup import set_default
+from openfisca_uk.tools.parameters import backdate_parameters
+from openfisca_uk.reforms.benefit_takeup import apply_takeup_rates
+from openfisca_tools import ReformType
+from openfisca_uk_data import DATASETS, SynthFRS
+from openfisca_tools.microsimulation import (
+    Microsimulation as GeneralMicrosimulation,
+)
+from openfisca_tools.hypothetical import IndividualSim as GeneralIndividualSim
+import yaml
+from pathlib import Path
+import h5py
+import pandas as pd
+
+
+with open(Path(__file__).parent / "datasets.yml") as f:
+    datasets = yaml.safe_load(f)
+    DEFAULT_DATASET = list(
+        filter(lambda ds: ds.name == datasets["default"], DATASETS)
+    )[0]
+
+
+warnings.filterwarnings("ignore")
+
+np.random.seed(0)
+
+
+class Microsimulation(GeneralMicrosimulation):
+    tax_benefit_system = CountryTaxBenefitSystem
+    entities = entities
+    default_dataset = DEFAULT_DATASET
+    post_reform = backdate_parameters()
+
+    def __init__(
+        self,
+        reform: ReformType = (),
+        dataset: type = None,
+        year: int = None,
+        duplicate_records: bool = False,
+        adjust_weights: bool = True,
+    ):
+        if adjust_weights:
+            duplicate_records = True
+        if dataset is None:
+            dataset = self.default_dataset
+        else:
+            dataset = dataset
+        if year is None:
+            year = self.default_year or max(dataset.years)
+        else:
+            year = year
+
+        # Check if dataset is available
+
+        if year not in dataset.years:
+            download = input(
+                f"\nYear {year} not available in dataset {dataset.name}: \n\t* Download the dataset [y]\n\t* Use the synthetic FRS (and set default) [n]\n\nChoice: "
+            )
+            if download == "y":
+                dataset.download(year)
+            else:
+                set_default(SynthFRS)
+                dataset = SynthFRS
+                if year not in dataset.years:
+                    logging.info(
+                        f"Year {year} synthetic FRS not stored, downloading..."
+                    )
+                    dataset.download(year)
+
+        if (dataset.name == "frs_enhanced") and duplicate_records:
+            data = dataset.load(year)
+            num_duplications = 2
+
+            def duplicate(key, values):
+                if "_id" in key:
+                    return np.concatenate(
+                        tuple([values] * num_duplications)
+                    ) * 10 + np.repeat(
+                        list(range(1, 1 + num_duplications)), len(values)
+                    )
+                elif "_weight" in key:
+                    return (
+                        np.concatenate(tuple([values] * num_duplications))
+                        / duplicate_records
+                    )
+                else:
+                    return np.concatenate(tuple([values] * num_duplications))
+
+            class ProxyDataObject(dict):
+                def close(self):
+                    return None
+
+            df = ProxyDataObject(
+                {
+                    key: duplicate(key, dataset.load(year, key))
+                    for key in data.keys()
+                }
+            )
+
+            df["claims_legacy_benefits"] = np.repeat(
+                [True, False], len(df["benunit_id"]) / 2
+            )
+
+            class ProxyDataset:
+                def load(year):
+                    return df
+
+                def close():
+                    return None
+
+                years = dataset.years
+                name = "frs_enhanced_duplicated"
+
+            dataset = ProxyDataset
+
+        if not adjust_weights:
+            reform = (reform, apply_takeup_rates())
+
+        super().__init__(reform=reform, dataset=dataset, year=year)
+
+        if (
+            ("frs_enhanced" in dataset.name)
+            and adjust_weights
+            and year >= 2019
+        ):
+            weight_file = (
+                Path(__file__).parent.parent / "calibration" / "frs_weights.h5"
+            )
+            if not weight_file.exists():
+                raise FileNotFoundError("Weight adjustment file not found.")
+            with h5py.File(weight_file, "r") as f:
+                for year in f.keys():
+                    self.simulation.set_input(
+                        "household_weight", year, np.array(f[year])
+                    )
+
+                    self.simulation.set_input(
+                        "person_weight",
+                        year,
+                        self.calc(
+                            "household_weight", period=year, map_to="person"
+                        ).values,
+                    )
+
+
+class IndividualSim(GeneralIndividualSim):
+    tax_benefit_system = CountryTaxBenefitSystem
+    post_reform = backdate_parameters()
