cd98b2bcb2dd55042ab16e30a386286865c864ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:07 +0000

    Add package AdyanUtils/0.1.6/AdyanUtils-0.1.6.tar.gz

---
+++ packages/AdyanUtils/AdyanUtils-0.1.6.tar.gz/AdyanUtils-0.1.6/Utils/connect/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/26 11:24
+# @Author  : Adyan
+# @File    : __init__.py.py
+
+
+from Utils import package
+
+pk_lst = [
+    'pymongo==3.6.0',
+    'pika==1.2.0',
+    'redis==4.1.4',
+]
+package(pk_lst)
+
+from .mongo_conn import MongoPerson
+from .rabbit_conn import RabbitClient, MonitorRabbit
+from .redis_conn import ReidsClient
