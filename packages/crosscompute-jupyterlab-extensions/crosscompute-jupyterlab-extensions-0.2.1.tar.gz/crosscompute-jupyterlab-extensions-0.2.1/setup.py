f93a3e04b17ce7a45ab693e386fb129f02af51e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:00 +0000

    Add package crosscompute-jupyterlab-extensions/0.2.1/crosscompute-jupyterlab-extensions-0.2.1.tar.gz

---
+++ packages/crosscompute-jupyterlab-extensions/crosscompute-jupyterlab-extensions-0.2.1.tar.gz/crosscompute-jupyterlab-extensions-0.2.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='CrossCompute Inc.',
+    author_email='support@crosscompute.com',
+    description='CrossCompute Extensions for JupyterLab',
+    install_requires=['jupyterlab-crosscompute'],
+    long_description='Use `jupyterlab-crosscompute <https://pypi.python.org/pypi/jupyterlab-crosscompute/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='crosscompute-jupyterlab-extensions',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/jupyterlab-crosscompute/',
+    version='0.2.1',
+    zip_safe=False,
+)
