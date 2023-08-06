b04c47ffa6a5e79ae919a974e6693ad62e7d2425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:12 +0000

    Add package wkrtry/1.2/wkrtry-1.2.tar.gz

---
+++ packages/wkrtry/wkrtry-1.2.tar.gz/wkrtry-1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="wkrtry",
+    version="1.2",
+    description="这是第三个对外发布的模块",
+    author="wxl",
+    author_email="2017178507@qq.com",
+    py_modules=["wkrtry.666"],
+)
