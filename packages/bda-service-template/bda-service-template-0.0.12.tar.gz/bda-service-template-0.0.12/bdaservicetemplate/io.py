aa4d20ffe04c18a69a676edf67d8d4530476c03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:06 +0000

    Add package bda-service-template/0.0.12/bda-service-template-0.0.12.tar.gz

---
+++ packages/bda-service-template/bda-service-template-0.0.12.tar.gz/bda-service-template-0.0.12/bdaservicetemplate/io.py
@@ -0,0 +1,55 @@
+import os
+
+class IO:
+    @staticmethod
+    def dataset_to_dataset(func):
+        
+        def wrapper(*args):
+            self = args[0]
+            dataset = self.get_dataset()
+            resulting_dataset = func(self, dataset)
+            self.save_dataset(resulting_dataset)
+
+        return wrapper
+
+
+    @staticmethod
+    def dataset_to_model(func):
+        
+        def wrapper(*args):
+            self = args[0]
+            dataset = self.get_dataset()
+            model_path = func(self, dataset)
+            self.upload_model(model_path)
+
+        return wrapper
+
+
+    @staticmethod
+    def dataset_and_model_to_dataset(func):
+        
+        def wrapper(*args):
+            self = args[0]
+            dataset = self.get_dataset()
+            model_path = os.path.join(".", "model")
+            self.download_model(path=model_path)
+            resulting_dataset = func(self, dataset, model_path)
+            self.save_dataset(resulting_dataset)
+
+        return wrapper
+
+
+    @staticmethod
+    def dataset_and_model_to_dataset_and_model(func):
+        
+        def wrapper(*args):
+            self = args[0]
+            dataset = self.get_dataset()
+            model_path = os.path.join(".", "model")
+            self.download_model(path=model_path)
+            resulting_dataset, resulting_model = func(self, dataset, model_path)
+            self.save_dataset(resulting_dataset)
+            self.upload_model(resulting_model)
+
+        return wrapper
+
