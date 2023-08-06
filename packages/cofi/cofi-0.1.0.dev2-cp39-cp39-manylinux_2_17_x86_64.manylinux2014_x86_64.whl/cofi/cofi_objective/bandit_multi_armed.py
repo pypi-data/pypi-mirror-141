545e9547d1481c48ccc43f504e318ba1933d423f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package cofi/0.1.0.dev2/cofi-0.1.0.dev2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cofi/cofi-0.1.0.dev2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cofi/cofi_objective/bandit_multi_armed.py
@@ -0,0 +1,30 @@
+from typing import Callable, Union
+from numbers import Number
+
+import numpy as np
+
+from ..base_objective import BaseObjective
+from ..model_params import Model
+
+
+class MultiArmedBandit(BaseObjective):
+    """The Multi-armed Bandit Model (bandit or MAB for short).
+
+    The problem aims to maximize the sum of the collected rewards, given a set of
+    distributions :math:`B = \{R_1, ..., R_k\}` where each distribution is associated 
+    with the rewards delivered by one of the :math:`K` levers. 
+
+    """
+
+    def __init__(
+        self, n_bandits: int, pull_bandit: Callable[[int], Union[bool, Number]]
+    ):
+        # self.n_bandits = n_bandits
+        # self.pull_bandit = pull_bandit
+        pass
+
+    def misfit(self, model: Union[Model, np.ndarray]):
+        pass
+
+    def propose(self,):
+        pass
