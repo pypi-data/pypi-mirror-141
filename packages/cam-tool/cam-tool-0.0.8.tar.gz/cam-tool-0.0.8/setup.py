4f0a80d4ec311f65c94f06b950f377ff435b3aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:43 +0000

    Add package cam-tool/0.0.8/cam-tool-0.0.8.tar.gz

---
+++ packages/cam-tool/cam-tool-0.0.8.tar.gz/cam-tool-0.0.8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cam-tool',
+    version='0.0.8',
+    description='Cloud Assignment Manager Tool',
+    url='https://github.com/fuzihaofzh/cam-tool',
+    author='',
+    author_email='',
+    license='',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='Cloud Assignment Manager',
+    packages=find_packages(),
+    install_requires=[
+          'fire', 'pyyaml', 'redis', 'tabulate'
+    ],
+    entry_points={
+          'console_scripts': [
+              'cam = cam.cam:main'
+          ]
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
