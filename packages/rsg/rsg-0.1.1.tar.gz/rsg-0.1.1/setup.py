895d72b21ec42ca4d0e19737f7859cd5572f0555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:55 +0000

    Add package rsg/0.1.1/rsg-0.1.1.tar.gz

---
+++ packages/rsg/rsg-0.1.1.tar.gz/rsg-0.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.readlines()
+
+with open("requirements_dev.txt") as requirements_file:
+    requirements_dev = requirements_file.readlines()
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+keywords = [
+    "rng",
+    "random",
+    "structure",
+    "generator",
+    "rsg",
+    "deep",
+    "nested",
+    "composite",
+    "tree",
+    "recursive",
+]
+
+setup(
+    # Info
+    author="Luca Bonfiglioli",
+    author_email="Luca.Bonfiglioli@gmail.com",
+    name="rsg",
+    description="Random Structure Generator",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="GNU General Public License v3.0",
+    version="0.1.1",
+    keywords=keywords,
+    url="https://github.com/lucabonfiglioli/rsg",
+    # Requirements
+    python_requires=">=3.9",
+    install_requires=requirements,
+    # Tests
+    test_suite="tests",
+    test_requires=requirements_dev,
+    # Packaging
+    packages=find_packages(include=["rsg", "rsg.*"]),
+    include_package_data=True,
+)
