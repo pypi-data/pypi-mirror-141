886dd0b18d94eae047dc100d8e9b708e0636f91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:08 +0000

    Add package je-api-testka/0.0.49/je_api_testka-0.0.49-py3-none-any.whl

---
+++ packages/je-api-testka/je_api_testka-0.0.49-py3-none-any.whl/je_api_testka/utils/get_data_strcture/get_api_data.py
@@ -0,0 +1,18 @@
+from requests.utils import dict_from_cookiejar
+
+
+def get_api_response_data(response):
+    return {
+        "status_code": response.status_code,
+        "text": response.text,
+        "content": response.content,
+        "headers": response.headers,
+        "history": response.history,
+        "encoding": response.encoding,
+        "cookies": dict_from_cookiejar(response.cookies),
+        "elapsed": response.elapsed,
+        "request_time_sec": response.elapsed.total_seconds(),
+        "request_method": response.request.method,
+        "request_url": response.request.url,
+        "request_body": response.request.body
+    }
