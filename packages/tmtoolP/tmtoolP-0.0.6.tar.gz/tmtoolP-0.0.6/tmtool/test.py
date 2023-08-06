b5e2de5765b19b0e56d72efc014fd5ad94c36519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package tmtoolP/0.0.6/tmtoolP-0.0.6.tar.gz

---
+++ packages/tmtoolP/tmtoolP-0.0.6.tar.gz/tmtoolP-0.0.6/tmtool/test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/3/10 11:14 上午
+# @Author : chendb
+# @Description : 测试
+
+from tmtool.tools import Tool
+
+tool = Tool()
+tool.logger('日志信息!!!')
+
+tool.qy_wechat_token = '7fdda192-cfcb-4eb5-87a5-b341574562d5'
+# @所有人
+tool.send_qy_wechat_msg('线上配置走查', at_all=['@all'])
+# # @指定单人
+# tool.send_qy_wechat_msg('线上配置走查', at_all=['18600967174'])
+# # @指定多人
+# tool.send_qy_wechat_msg('线上配置走查', at_all=['15210205078', '18701105997', '13821910557', '13426002048'])
+
+
