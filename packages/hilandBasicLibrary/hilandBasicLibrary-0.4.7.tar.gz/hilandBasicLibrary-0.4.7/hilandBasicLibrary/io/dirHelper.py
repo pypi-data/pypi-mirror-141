1125338e6b9349d785784da19a7aff793f770de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:27 +0000

    Add package hilandBasicLibrary/0.4.7/hilandBasicLibrary-0.4.7.tar.gz

---
+++ packages/hilandBasicLibrary/hilandBasicLibrary-0.4.7.tar.gz/hilandBasicLibrary-0.4.7/hilandBasicLibrary/io/dirHelper.py
@@ -0,0 +1,44 @@
+"""
+ * @file   : dirHelper.py
+ * @time   : 12:28
+ * @date   : 2022/3/10
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+import os
+
+from hilandBasicLibrary.io.pathHelper import PathHelper
+
+
+class DirHelper:
+    @classmethod
+    def get_files(cls, dir_path, include_sub_dir=True):
+        """
+        获取某目录下的文件名称
+        :param dir_path:
+        :param include_sub_dir: 是否包含子目录（缺省是True，包含）
+        :return:
+        """
+        result = []
+        cls.__list_dir(dir_path, result, include_sub_dir)
+        return result
+
+    @classmethod
+    def __list_dir(cls, path, list_name, include_sub_dir=True):  # 传入存储的list
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            if os.path.isdir(file_path):
+                if include_sub_dir is True:
+                    cls.__list_dir(file_path, list_name)
+            else:
+                list_name.append(file_path)
+
+    @staticmethod
+    def ensure_exist(path):
+        """
+        取保目录存在，如果不存在就创建
+        :param path:
+        :return:
+        """
+        PathHelper.ensure_exist(path)
