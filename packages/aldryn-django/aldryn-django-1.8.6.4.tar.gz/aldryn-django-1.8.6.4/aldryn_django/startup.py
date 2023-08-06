14c85e404fdb0c97315ee31ce9bd3cdb0b76a475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:30 +0000

    Add package aldryn-django/1.8.6.4/aldryn-django-1.8.6.4.tar.gz

---
+++ packages/aldryn-django/aldryn-django-1.8.6.4.tar.gz/aldryn-django-1.8.6.4/aldryn_django/startup.py
@@ -0,0 +1,30 @@
+import os
+
+import django.core.management
+
+from getenv import env
+
+
+def manage(path):
+    _setup(path=path)
+    utility = django.core.management.ManagementUtility(None)
+    utility.execute()
+
+
+def wsgi(path):
+    _setup(path=path)
+    from django.core.wsgi import get_wsgi_application
+    from django.conf import settings
+    from dj_static import Cling, MediaCling
+    app = get_wsgi_application()
+    if not settings.STATIC_URL_IS_ON_OTHER_DOMAIN:
+        app = Cling(app)
+    if not settings.MEDIA_URL_IS_ON_OTHER_DOMAIN:
+        app = MediaCling(app)
+    return app
+
+
+def _setup(path):
+    os.environ['DJANGO_SETTINGS_MODULE'] = env(
+        'DJANGO_SETTINGS_MODULE', 'settings'
+    )
