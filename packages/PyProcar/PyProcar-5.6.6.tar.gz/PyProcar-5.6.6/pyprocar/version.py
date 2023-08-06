51eee6b3c253d7befd4e5c8eb6eef895ad2915be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:39 +0000

    Add package PyProcar/5.6.6/PyProcar-5.6.6.tar.gz

---
+++ packages/PyProcar/PyProcar-5.6.6.tar.gz/PyProcar-5.6.6/pyprocar/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM PYPROCAR SETUP.PY.
+name = 'PyProcar'
+version = '5.6.6'
+description = 'A Python library for electronic structure pre/post-processing. '
+url = 'https://github.com/romerogroup/pyprocar'
+author = 'Francisco Muñoz, Aldo Romero, Sobhit Singh, Uthpala Herath, Pedram Tavadze, Eric Bousquet, Xu He, Reese Boucher, Logan Lang, Freddy Farah'
+email = 'fvmunoz@gmail.com, alromero@mail.wvu.edu, smsingh@mix.wvu.edu, ukh0001@mix.wvu.edu, petavazohi@mix.wvu.edu, eric.bousquet@uliege.be, mailhexu@gmail.com'
+status = 'development'
+copyright = 'Copyright 2021'
+date = 'March 6th, 2022'
