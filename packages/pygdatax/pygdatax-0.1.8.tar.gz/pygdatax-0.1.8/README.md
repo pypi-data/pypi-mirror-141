c9717f6a2a006c3790babb91ca81f8c2d8fd14f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:12 +0000

    Add package pygdatax/0.1.8/pygdatax-0.1.8.tar.gz

---
+++ packages/pygdatax/pygdatax-0.1.8.tar.gz/pygdatax-0.1.8/README.md
@@ -0,0 +1,34 @@
+# PYGDATAX
+Python libray for Small Angle Neutron (SANS) and X-ray (SAXS) scattering data treatment and visualization.
+This package can be used for the following instruments : 
+ - Xeuss 2.0 SAXS instrument
+ - SANS_LLB instrument installed at the Paul Scherrer Institut (PSI)
+## Installation
+To install pygdatax, run:
+```
+pip install pygdatax
+```
+Depending on your OS and python distribution, you may need to install PyQt5 and Ipython
+```
+pip install pyqt5 Ipython
+```
+## Upgrade Version
+pygdatax package is up to now often upgraded. In order to get the last version type the following command in your
+ terminal or acaconda prompt
+```
+pip install --upgrade pygdatax
+```
+## Launch Graphical User Interface for Xeuss data treatment and visualization
+To launch the GUI, run in you terminal (or conda prompt):
+````
+pygdatax_gui
+````
+This GUI can also be used to visualize nexus files from SANS_LLB.
+
+![alt text](pygdatax_screenshot_2.png)
+
+
+
+## Quick Overview
+
+### EDF viewer
---
+++ packages/pygdatax/pygdatax-0.1.8.tar.gz/pygdatax-0.1.8/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+# import os
+
+# with open('./requirements.txt', 'r') as fid:
+#     text = fid.readline()
+#     required_packages = text.splitlines()
+
+# required_packages = [
+#     'numpy',
+#     'silx',
+#     'fabio~=0.12.0',
+#     'matplotlib~=3.4.2',
+#     'pyFAI~=0.20.0',
+#     'nexusformat~=0.6.1',
+#     'Ipython',
+#     'openpyxl~=3.0.7'
+# ]
+
+required_packages = [
+    'silx',
+    'matplotlib',
+    'scipy',
+    'pyFAI',
+    'nexusformat',
+    'openpyxl',
+    'pyyaml',
+    'ipython'
+]
+
+setup(
+    name='pygdatax',
+    version='0.1.8',
+    url='',
+    license='',
+    author='achennev',
+    author_email='alexis.chenneviere@cea.fr',
+    description='Python Based Progamme for SAXS data treatment on XEUSS diffractometer',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires='>=3.6',
+    install_requires=required_packages,
+    include_package_data=True,
+    entry_points={'console_scripts': {'pygdatax_gui=pygdatax.gui.gui_xeuss:main',
+                                      'pygdatax_log=pygdatax.gui.gui_log:main'}}
+)
+# packages=[find_packages(where="src"])
+# package_dir = {"": "src"},
