7175edaab1ee281c824a1631bfadd5b003c318ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:52 +0000

    Add package rocrateValidator/0.0.8/rocrateValidator-0.0.8.tar.gz

---
+++ packages/rocrateValidator/rocrateValidator-0.0.8.tar.gz/rocrateValidator-0.0.8/setup.py
@@ -0,0 +1,12 @@
+import setuptools 
+
+setuptools.setup(name = 'rocrateValidator',
+	version = '0.0.8', 
+	description = 'This is python library for ro-crate validator',
+	package=["src", "src.validator"],
+	package_data = {"validator": ["validator/workflow_extension.txt", "validator/check_list.txt"]},
+  	include_package_data=True,
+	packages=setuptools.find_packages(),
+	url = "https://github.com/ResearchObject/ro-crate-validator-py",
+	zip_safe = False
+	)
