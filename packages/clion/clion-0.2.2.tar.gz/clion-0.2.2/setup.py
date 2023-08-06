8970d4541d345a1d087b22f6ac6a59f17144e245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:59 +0000

    Add package clion/0.2.2/clion-0.2.2.tar.gz

---
+++ packages/clion/clion-0.2.2.tar.gz/clion-0.2.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['clion']
+setup_kwargs = {
+    'name': 'clion',
+    'version': '0.2.2',
+    'description': 'Minimalistic library for building CLI applications',
+    'long_description': None,
+    'author': 'Yevhen Shymotiuk',
+    'author_email': 'yshym@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/yshym/clion',
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
