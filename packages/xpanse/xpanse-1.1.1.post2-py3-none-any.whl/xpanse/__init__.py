6f48ba78c8b68a6bb7609ef6b3680e4bab464f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:17 +0000

    Add package xpanse/1.1.1.post2/xpanse-1.1.1.post2-py3-none-any.whl

---
+++ packages/xpanse/xpanse-1.1.1.post2-py3-none-any.whl/xpanse/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.1.1-post-2"
+__author__ = "Palo Alto Cortex Xpanse"
+__license__ = "ISC"
+__maintainer__ = "Palo Alto Cortex Xpanse"
+__email__ = "help@expanseinc.com"
+__status__ = "Production"
