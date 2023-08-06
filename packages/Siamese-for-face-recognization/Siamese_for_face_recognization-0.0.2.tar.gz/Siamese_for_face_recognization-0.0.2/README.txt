800b5a927e3db821f837e827860b369617f7fb6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:27 +0000

    Add package Siamese-for-face-recognization/0.0.2/Siamese_for_face_recognization-0.0.2.tar.gz

---
+++ packages/Siamese-for-face-recognization/Siamese_for_face_recognization-0.0.2.tar.gz/Siamese_for_face_recognization-0.0.2/README.txt
@@ -0,0 +1,6 @@
+Siamese for face recognization
+This package contains only the models used for face recognization by siamese neural network 
+
+See  `Siamese_for_face_recognization <'https://github.com/xalphonseinbaraj/Siamese_for_face_recognization'>`
+for more information.
+
---
+++ packages/Siamese-for-face-recognization/Siamese_for_face_recognization-0.0.2.tar.gz/Siamese_for_face_recognization-0.0.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+	]
+
+setup(
+    name='Siamese_for_face_recognization',
+    version='0.0.2',
+    description="Models used for face_recognition by siamese neural network.",
+    long_description=readme,
+	long_description_content_type="text/markdown",
+    author="AlphonseInbarajXavier",
+    author_email='xalphonse@gmail.com',
+    url='https://github.com/xalphonseinbaraj/Siamese_for_face_recognization',
+    packages=[
+        'Siamese_for_face_recognization',
+    ],
+    package_dir={'Siamese_for_face_recognization': 'Siamese_for_face_recognization'},
+    package_data={
+        'Siamese_for_face_recognization': ['models/*.dat'],
+		'Siamese_for_face_recognization': ['models/*.xml'],
+		'Siamese_for_face_recognization': ['models/*.h5'],
+		
+    },
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='Siamese_for_face_recognization',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
