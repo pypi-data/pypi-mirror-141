ddae1f626b15d42b0a694166cb145b41582cf2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:22 +0000

    Add package photoarchiver/0.2/photoarchiver-0.2.tar.gz

---
+++ packages/photoarchiver/photoarchiver-0.2.tar.gz/photoarchiver-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='photoarchiver',
+      version='0.2',
+      description='Archive your photos and files using file timestamps and geolocation.',
+      url='http://github.com/baigo/photoarchiver',
+      author='Martín Baigorria Alonso',
+      author_email='martinbaigorria@gmail.com',
+      license='MIT',
+      packages=['photoarchiver', 'photoarchiver.helpers'],
+      install_requires=[
+          'exifread',
+          'reverse_geocoder',
+          'tqdm'
+      ],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'photoarchiver = photoarchiver.__main__:main',
+          ]
+      },
+      long_description=long_description,
+      long_description_content_type='text/markdown')
