d83899c6ffe631902859f453e25fe1b82ed60737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:01 +0000

    Add package aryan/0.0.3/aryan-0.0.3.tar.gz

---
+++ packages/aryan/aryan-0.0.3.tar.gz/aryan-0.0.3/main/aryan/a.py
@@ -0,0 +1,30 @@
+import asyncio
+from main import Mirai, MiraiSession, Bot, BotConfiguration
+from main import GlobalEventChannel, EventPriority, ConcurrencyKind, ListeningStatus
+from main import GroupMessage, FriendMessage
+
+
+
+app = Mirai(
+    MiraiSession(
+        verify_key="verifyKey",
+        host="localhost:8080",
+    ),
+    loop=asyncio.get_event_loop(),
+    bots=[
+        Bot(BotConfiguration(account=1375075223)),
+        # Bot(BotConfiguration(account=552282813))
+    ]
+)
+
+
+async def main(event: FriendMessage):
+    # bot = app.getBot(1375075223)
+    # await app.sendFriendMessage(bot, event.sender, "hello")
+    await event.quoteReply("hello world")
+
+GlobalEventChannel.INSTANCE.subscribeAlways(FriendMessage, main)
+
+GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessage, lambda event: event.quoteReply("我爱你"))
+
+app.launch_blocking()
