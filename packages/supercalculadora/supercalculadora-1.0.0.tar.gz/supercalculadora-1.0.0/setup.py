e0d174367d43c7912ee1877ca7205a6f67db7efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:59 +0000

    Add package supercalculadora/1.0.0/supercalculadora-1.0.0.tar.gz

---
+++ packages/supercalculadora/supercalculadora-1.0.0.tar.gz/supercalculadora-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="supercalculadora",
+    version="1.0.0",
+    description="Read the latest Real Python tutorials",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["supercalculadora"],
+    include_package_data=True,
+    install_requires=["feedparser", "html2text"],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
