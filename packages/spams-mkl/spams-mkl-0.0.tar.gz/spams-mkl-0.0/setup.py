ffbd8e894a5781341f363f730376668df56dbb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:17 +0000

    Add package spams-mkl/0.0/spams-mkl-0.0.tar.gz

---
+++ packages/spams-mkl/spams-mkl-0.0.tar.gz/spams-mkl-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Julien Mairal',
+    author_email='spams.dev@inria.fr',
+    description='Python interface for SPAMS',
+    install_requires=['spams'],
+    long_description='Use `spams <https://pypi.python.org/pypi/spams/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='spams-mkl',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/spams/',
+    version='0.0',
+    zip_safe=False,
+)
