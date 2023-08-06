b8d3b906ee9c620691b0bba127241d263798d40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package CI-CD-Homework-Dictionary-dgndds/2.0.2/CI-CD-Homework-Dictionary-dgndds-2.0.2.tar.gz

---
+++ packages/CI-CD-Homework-Dictionary-dgndds/CI-CD-Homework-Dictionary-dgndds-2.0.2.tar.gz/CI-CD-Homework-Dictionary-dgndds-2.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="CI-CD-Homework-Dictionary-dgndds",
+    author="Muhammed Doğancan Yılmazoğlu",  # TODO: Write your name
+    version="2.0.2",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+)
