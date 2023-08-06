b64e1560356101d8db815fcbfb6d16ab9803b266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package symbiont-io.pytest-assembly/2.0.4.dev3/symbiont_io.pytest_assembly-2.0.4.dev3-py3-none-any.whl

---
+++ packages/symbiont-io.pytest-assembly/symbiont_io.pytest_assembly-2.0.4.dev3-py3-none-any.whl/_assembly/__init__.py
@@ -0,0 +1,9 @@
+"""Not configurable constants"""
+import logging
+logger = logging.getLogger(__name__)
+
+APP_VERSION = '2.0.4.dev3'
+APP_NAME = 'pytest-assembly'
+APP_DESCRIPTION = 'Contracts Virtual Machine and SDK'
+APP_AUTHOR = 'Symbiont.io, Inc.'
+
