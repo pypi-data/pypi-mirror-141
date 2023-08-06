eed4d195f5ca762bbb2247b30de4ca27b7dd1ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:17 +0000

    Add package pygl-nf/16.9/pygl_nf-16.9.tar.gz

---
+++ packages/pygl-nf/pygl_nf-16.9.tar.gz/pygl_nf-16.9/pygl_nf/__init__.py
@@ -0,0 +1,3 @@
+import colorama
+print(colorama.Fore.BLUE+'GRPgraph version - (16.9)'+colorama.Fore.RESET)
+print(colorama.Fore.CYAN+colorama.Style.BRIGHT+'My Github --- ( https://github.com/dedisvoin/GRPgraph-versions )'+colorama.Fore.RESET,colorama.Style.RESET_ALL)
