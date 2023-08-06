dcd1507ba05e0218e11bd25d87efbfa28c26c759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:22 +0000

    Add package ex3package/0.0.1/ex3package-0.0.1.tar.gz

---
+++ packages/ex3package/ex3package-0.0.1.tar.gz/ex3package-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'ex3'
+LONG_DESCRIPTION = 'A package ex3'
+
+# Setting up
+setup(
+    name="ex3package",
+    version=VERSION,
+    author="Hanh",
+    author_email="<tnh210302@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
