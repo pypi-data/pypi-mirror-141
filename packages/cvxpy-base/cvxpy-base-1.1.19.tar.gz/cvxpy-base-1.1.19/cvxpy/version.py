46d6c143419e4eb5218f3af1a132909ff9e4fa02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:00 +0000

    Add package cvxpy-base/1.1.19/cvxpy-base-1.1.19.tar.gz

---
+++ packages/cvxpy-base/cvxpy-base-1.1.19.tar.gz/cvxpy-base-1.1.19/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.1.19'
+version = '1.1.19'
+full_version = '1.1.19'
+git_revision = '43b6523'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
