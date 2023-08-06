70206d502ccfda65a10c8d4ce0d8a7b5a10f256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:37 +0000

    Add package pollination-streamlit-io/0.15.2/pollination-streamlit-io-0.15.2.tar.gz

---
+++ packages/pollination-streamlit-io/pollination-streamlit-io-0.15.2.tar.gz/pollination-streamlit-io-0.15.2/pollination_streamlit_io/button/get/precache-manifest.29128a99ba3ded1009c42c5dee46ba27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe455fdcb9b50da4886e08f7eac907d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "796fe7ded6db107dcf9d",
+    "url": "./static/css/main.3ccb58d0.chunk.css"
+  },
+  {
+    "revision": "c0ee6d7a5133b2df426b",
+    "url": "./static/js/2.dc22e782.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.dc22e782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796fe7ded6db107dcf9d",
+    "url": "./static/js/main.b26eb8dd.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
