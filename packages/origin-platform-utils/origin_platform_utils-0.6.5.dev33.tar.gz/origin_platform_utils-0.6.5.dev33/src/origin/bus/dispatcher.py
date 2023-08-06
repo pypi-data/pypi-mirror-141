be2e20c286525b2e679958dfd5ab161d92725514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:31 +0000

    Add package origin-platform-utils/0.6.5.dev33/origin_platform_utils-0.6.5.dev33.tar.gz

---
+++ packages/origin-platform-utils/origin_platform_utils-0.6.5.dev33.tar.gz/origin_platform_utils-0.6.5.dev33/src/origin/bus/dispatcher.py
@@ -0,0 +1,22 @@
+from typing import Dict, Type
+
+from .broker import Message, TMessageHandler
+
+
+class MessageDispatcher(Dict[Type[Message], TMessageHandler]):
+    """
+    Message handler to dispatch incoming messages.
+
+    A message handler that dispatches incoming messages to the appropriate
+    handler. Each message type can have a single handler associated.
+    """
+
+    def __call__(self, msg: Message):
+        """TODO."""
+        message_type = type(msg)
+        if message_type in self:
+            print('DISPATCH: %s' % msg)
+            handler = self[message_type]
+            handler(msg)
+        else:
+            print('IGNORING: %s' % message_type)
