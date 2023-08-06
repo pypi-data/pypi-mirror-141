a5e29158911a970b3511edba324fa8584648a533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package bizchat/6.1.0/bizchat-6.1.0.tar.gz

---
+++ packages/bizchat/bizchat-6.1.0.tar.gz/bizchat-6.1.0/setup.py
@@ -0,0 +1,33 @@
+from unicodedata import name
+from setuptools import setup, find_packages
+
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    
+]
+
+setup(
+    name='bizchat',
+    version='6.1.0',
+    description='import this library to use chat from client to  server directly',
+    long_description_content_type="text/markdown",
+    long_description='README.txt',
+    #long_description=open('README.txt').read() + '\n\n',
+    url='',
+    author='mayur', 
+    author_email='mayurdhameliya98@gmai.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='server client,chatapp',
+    packages=find_packages(),
+    install_requires=['']
+    
+)
+
+
+
