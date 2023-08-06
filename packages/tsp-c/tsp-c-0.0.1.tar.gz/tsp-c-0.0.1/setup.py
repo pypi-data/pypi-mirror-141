dfad62ee2d8a7841c37993ceb8649016a74ce69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:34 +0000

    Add package tsp-c/0.0.1/tsp-c-0.0.1.tar.gz

---
+++ packages/tsp-c/tsp-c-0.0.1.tar.gz/tsp-c-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tsp-c",
+    version="0.0.1",
+    author="Udom Janjarassuk",
+    author_email="kjudom@gmail.com",
+    description="A wrapper for c++ to solve the Traveling Salesman Problem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kjudom/tsp-c",
+    project_urls={
+        "Bug Tracker": "https://github.com/kjudom/tsp-c",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
