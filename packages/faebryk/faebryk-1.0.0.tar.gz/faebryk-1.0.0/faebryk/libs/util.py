2b27dd3914b362e5ccc3e9f1b79423e25ab88021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:32 +0000

    Add package faebryk/1.0.0/faebryk-1.0.0.tar.gz

---
+++ packages/faebryk/faebryk-1.0.0.tar.gz/faebryk-1.0.0/faebryk/libs/util.py
@@ -0,0 +1,40 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+class lazy:
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __str__(self):
+        return str(self.expr())
+
+    def __repr__(self):
+        return repr(self.expr())
+
+def kw2dict(**kw):
+    return dict(kw)
+
+class hashable_dict:
+    def __init__(self, obj: dict):
+        self.obj = obj
+
+    def __hash__(self):
+        return hash(sum(map(hash, self.obj.items())))
+
+    def __repr__(self):
+        return "{}({})".format(type(self), repr(self.obj))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+
+def unique(it, key):
+    seen = []
+    out = []
+    for i in it:
+        v = key(i)
+        if v in seen:
+            continue
+        seen.append(v)
+        out.append(i)
+    return out
---
+++ packages/faebryk/faebryk-1.0.0.tar.gz/faebryk-1.0.0/faebryk/version.py
@@ -0,0 +1,5 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+def version():
+    return "1.0.0"
