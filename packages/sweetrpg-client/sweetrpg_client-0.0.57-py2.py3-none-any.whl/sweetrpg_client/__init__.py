3514e7934e58ee6441829042897e9bee096b99a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:44 +0000

    Add package sweetrpg-client/0.0.57/sweetrpg_client-0.0.57-py2.py3-none-any.whl

---
+++ packages/sweetrpg-client/sweetrpg_client-0.0.57-py2.py3-none-any.whl/sweetrpg_client/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Metadata.
+"""
+
+__title__ = "sweetrpg-client"
+__description__ = "SweetRPG SDK"
+__url__ = "https://github.com/sweetrpg/sdk"
+__version__ = "0.0.57"
+__build__ = 0x000001
+__author_email__ = "dm@sweetrpg.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 SweetRPG"
+__cake__ = "\u2728 \U0001f370 \u2728"
