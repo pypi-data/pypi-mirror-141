47011c23842f5ea85c48f6284e9e9a78c2a2fb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:11 +0000

    Add package LiteLogger/0.1.0/LiteLogger-0.1.0-py2.py3-none-any.whl

---
+++ packages/LiteLogger/LiteLogger-0.1.0-py2.py3-none-any.whl/LiteLogger/__version__.py
@@ -0,0 +1,25 @@
+# Part of the LiteLogger python client
+# Copyright (C) 2022- LiteLogger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+__title__ = 'LiteLogger'
+__description__ = 'The official LiteLogger client for Python'
+__url__ = 'https://litelogger.com/'
+__version__ = '0.1.00'
+__author__ = 'Marc Leonard'
+__author_email__ = 'contact@litelogger.com'
+__license__ = 'GNU GPLv3'
+__copyright__ = 'Copyright (C) 2022 - LiteLogger'
