562b0e5f02329a263532babdd07c982cebfedd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:40 +0000

    Add package DNBC4-test/1.0.4/DNBC4_test-1.0.4-py3-none-any.whl

---
+++ packages/DNBC4-test/DNBC4_test-1.0.4-py3-none-any.whl/DNBC4tools/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+_version = "1.0.4"
+
+_pipelist = [
+    "data",
+    'count',
+    'analysis',
+    #'report',
+    #'clean',
+    #'mkref',
+    #'run',
+]
+_root_dir = os.path.abspath(os.path.dirname(__file__))
