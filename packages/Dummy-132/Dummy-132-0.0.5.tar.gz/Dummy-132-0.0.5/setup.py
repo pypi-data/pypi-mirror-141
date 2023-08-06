cfb7bc46b81940dcdab1e55a7a27ffbaab94b05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:21 +0000

    Add package Dummy-132/0.0.5/Dummy-132-0.0.5.tar.gz

---
+++ packages/Dummy-132/Dummy-132-0.0.5.tar.gz/Dummy-132-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from versioning import updateVersion
+from setuptools import setup, find_packages
+
+
+def getAndUpdateVersion():
+    version = updateVersion()
+    return version
+
+setup(
+    name='Dummy-132',
+    version=getAndUpdateVersion(),
+    license='MIT',
+    author="uniquename21",
+    author_email='uniquename21@yahoo.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='dummy project'
+)
