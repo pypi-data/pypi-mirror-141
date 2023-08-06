6764893d191224d8ab1b484788f523b8d7a1b164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:21 +0000

    Add package yandex-conqueror/0.6/yandex_conqueror-0.6-py3-none-any.whl

---
+++ packages/yandex-conqueror/yandex_conqueror-0.6-py3-none-any.whl/conqueror/endpoints/view_business.py
@@ -0,0 +1,59 @@
+from conqueror.app import app
+from conqueror.cassandra import session
+from conqueror.utils import as_json
+
+
+@app.route('/v1/view-businesses/<string:sector>')
+@as_json
+def get_businesses(sector: str):
+    """
+    Get a list of Russian businesses to post reviews for
+    ---
+    parameters:
+    - in: path
+      name: sector
+      schema:
+        type: string
+      description: Name of Russian city in Latin alphabet
+      required: true
+    ---
+    responses:
+        '200':
+            description: OK
+            content:
+                application/json:
+                    schema:
+                        type: array
+                        items:
+                            type: string
+                            description: Business ID of the object
+    """
+    resp = session.execute('SELECT businessId FROM businesses WHERE sector=%s', (sector,))
+    results = []
+    for datum in resp:
+        results.append(datum[0])
+    return results
+
+
+@app.route('/v1/view-businesses')
+@as_json
+def get_businesses_all():
+    """
+    Get a list of all Russian businesses to post reviews for
+    ---
+    responses:
+        '200':
+            description: OK
+            content:
+                application/json:
+                    schema:
+                        type: array
+                        items:
+                            type: string
+                            description: Business ID of the object
+    """
+    resp = session.execute('SELECT businessId FROM businesses')
+    results = []
+    for datum in resp:
+        results.append(datum[0])
+    return results
---
+++ packages/yandex-conqueror/yandex_conqueror-0.6-py3-none-any.whl/conqueror/scraper/__main__.py
@@ -0,0 +1,7 @@
+import time
+
+from conqueror.scraper.orders_obtainer import OrdersObtainer
+from .pulse_coordinator import run
+
+if __name__ == '__main__':
+    run()
