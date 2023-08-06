b96b19200486e8823dd6815ee95e65b82f881d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package hpo-uq/1.3.5/hpo-uq-1.3.5.tar.gz

---
+++ packages/hpo-uq/hpo-uq-1.3.5.tar.gz/hpo-uq-1.3.5/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+import pkg_resources
+from setuptools import setup,find_packages
+from glob import glob
+    
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+with open('README.rst', 'r') as f:
+    longdesc = f.read()
+
+setup(
+    name="hpo-uq",
+    version='1.3.5',
+    description="Hyperparameter Optimization Tool using Surrogate Modeling and Uncertainty Quantification.",
+    long_description=longdesc,
+    long_description_content_type='text/x-rst',
+    scripts = glob('bin/*'),
+    author="Vincent Dumont",
+    author_email="vincentdumont11@gmail.com",
+    maintainer="Vincent Dumont",
+    maintainer_email="vincentdumont11@gmail.com",
+    url="https://hpo-uq.gitlab.io/hyppo",
+    license_files = ('LICENSE.txt',),
+    packages=find_packages(include=('hyppo*',)),
+    project_urls={
+        "Source Code": "https://gitlab.com/hpo-uq/hyppo",
+    },
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        "License :: Other/Proprietary License",
+        'Natural Language :: English',
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+
+)
