a4464565f720cd50165f3266229b6c62dcb9bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:44 +0000

    Add package sweetrpg-client/0.0.56/sweetrpg-client-0.0.56.tar.gz

---
+++ packages/sweetrpg-client/sweetrpg-client-0.0.56.tar.gz/sweetrpg-client-0.0.56/src/sweetrpg_client/types/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Types.
+"""
+
+CONTRIBUTION = 'contribution'
+PERSON = 'person'
+PUBLISHER = 'publisher'
+REVIEW = 'review'
+STUDIO = 'studio'
+SYSTEM = 'system'
+VOLUME = 'volume'
