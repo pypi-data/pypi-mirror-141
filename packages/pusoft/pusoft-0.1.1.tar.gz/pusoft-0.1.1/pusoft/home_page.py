5acf41810fcc1a33ead4614436be50769eda4d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:03 +0000

    Add package pusoft/0.1.1/pusoft-0.1.1.tar.gz

---
+++ packages/pusoft/pusoft-0.1.1.tar.gz/pusoft-0.1.1/pusoft/home_page.py
@@ -0,0 +1,9 @@
+import time
+from colorama import init, Fore, Back, Style
+
+init()
+init(autoreset=True)
+
+def cmd():
+    print(Fore.MAGENTA + Back.CYAN + "WELCOME TO PUSOFT!\n\nBy Pumpkin_1001\n")
+    print(Fore.WHITE + Back.MAGENTA + "You can enter [pusoft] in CMD to view the home page;\n              [pudatabase] to use the tool;\n              [pupm] to view PUSOFT Package Manager.")
