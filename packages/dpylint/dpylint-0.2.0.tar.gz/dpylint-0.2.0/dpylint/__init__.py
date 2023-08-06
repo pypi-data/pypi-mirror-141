9ff533771cbe803f164e50065f91bdb12b9ce82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:47 +0000

    Add package dpylint/0.2.0/dpylint-0.2.0.tar.gz

---
+++ packages/dpylint/dpylint-0.2.0.tar.gz/dpylint-0.2.0/dpylint/__init__.py
@@ -0,0 +1,18 @@
+"""Discord.py Linter"""
+from .checkers import HasPermissionsChecker, EventsChecker, NamingConventionChecker
+
+
+def register(linter):
+    """Registers the linters to pylint"""
+    linter.register_checker(HasPermissionsChecker(linter))
+    linter.register_checker(EventsChecker(linter))
+    linter.register_checker(NamingConventionChecker(linter))
+
+__name__    = "dpylint"
+__title__   =  __name__
+__license__ = "MIT"
+__version__ = "0.2.0"
+__author__  = "Arian Mollik Wasi"
+__github__  = "https://github.com/wasi-master/dpylint"
+
+__all__ = ("register",)
