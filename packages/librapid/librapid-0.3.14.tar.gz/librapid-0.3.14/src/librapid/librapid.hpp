60138a7bf14cf58d2b6401653fcd027869d760e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:16 +0000

    Add package librapid/0.3.14/librapid-0.3.14.tar.gz

---
+++ packages/librapid/librapid-0.3.14.tar.gz/librapid-0.3.14/src/librapid/librapid.hpp
@@ -0,0 +1,18 @@
+#ifndef LIBRAPID_INCLUDE
+#define LIBRAPID_INCLUDE
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#include <librapid/config.hpp>
+#include <librapid/VERSION.hpp>
+
+#include <librapid/utils/utils.hpp>
+#include <librapid/test/librapid_test.hpp>
+#include <librapid/stringmethods/format_number.hpp>
+#include <librapid/math/rapid_math.hpp>
+#include <librapid/autocast/autocast.hpp>
+#include <librapid/array/multiarray.hpp>
+
+#endif // LIBRAPID_INCLUDE
---
+++ packages/librapid/librapid-0.3.14.tar.gz/librapid-0.3.14/src/librapid/test/librapid_test.hpp
@@ -0,0 +1,16 @@
+#ifndef LIBRAPID_TEST
+#define LIBRAPID_TEST
+
+#include <librapid/config.hpp>
+
+namespace librapid
+{
+	namespace test
+	{
+		int testLibrapid(int x);
+
+		void streamTest();
+	}
+}
+
+#endif // LIBRAPID_TEST
