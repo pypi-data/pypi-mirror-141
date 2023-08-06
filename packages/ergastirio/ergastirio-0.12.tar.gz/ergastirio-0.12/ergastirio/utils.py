9a870084f5acb4df12b4712afbadac6ab5dab3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:13 +0000

    Add package ergastirio/0.12/ergastirio-0.12.tar.gz

---
+++ packages/ergastirio/ergastirio-0.12.tar.gz/ergastirio-0.12/ergastirio/utils.py
@@ -0,0 +1,23 @@
+def disable_widget(widgets):
+    for widget in widgets:
+        if widget:
+            widget.setEnabled(False)   
+
+def enable_widget(widgets):
+    for widget in widgets:
+        if widget:
+            widget.setEnabled(True)  
+
+def convert_fraction_to_float(fraction):
+    #code adapted from https://stackoverflow.com/questions/1806278/convert-fraction-to-float
+    try:
+        return float(fraction)
+    except ValueError:
+        num, denom = fraction.split('/')
+        try:
+            leading, num = num.split(' ')
+            whole = float(leading)
+        except ValueError:
+            whole = 0
+        frac = float(num) / float(denom)
+        return whole - frac if whole < 0 else whole + frac
