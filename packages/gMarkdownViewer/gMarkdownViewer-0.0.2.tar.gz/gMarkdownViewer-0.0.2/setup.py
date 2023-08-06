4e3d36fd2e5764021f854e585d260c2e66e187c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:48 +0000

    Add package gMarkdownViewer/0.0.2/gMarkdownViewer-0.0.2.tar.gz

---
+++ packages/gMarkdownViewer/gMarkdownViewer-0.0.2.tar.gz/gMarkdownViewer-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Author: Ben Knisley [benknisley@gmail.com]
+Date: 22 February, 2022
+"""
+from setuptools import setup
+
+setup(
+    name = "gMarkdownViewer",
+    version = "0.0.2",
+    author = "Ben Knisley",
+    author_email = "benknisley@gmail.com",
+    description = "An application for viewing Markdown files.",
+    url = "https://git.benknisley.com/benknisley/MarkdownViewer",
+    license = "MIT",
+    keywords = "markdown",
+    install_requires=['Markdown==3.3.4', 'PyGObject==3.40.1'],
+    packages=["MarkdownViewer",],
+    long_description="A simple Markdown viewer, made with Python, GTK, and Webkit.",
+    
+    ## Set up console scripts files
+    entry_points = {
+        'console_scripts': [
+            'MarkdownViewer = MarkdownViewer:main',
+        ],              
+    },
+
+    ## Include aux package files (in MarkdownViewer folder)
+    include_package_data=True,
+    package_data={'MarkdownViewer': ['highlight_js.html', 'place_holder.md']},
+
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+    ],
+)
