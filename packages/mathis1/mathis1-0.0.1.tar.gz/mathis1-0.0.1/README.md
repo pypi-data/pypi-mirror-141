2d1383bc4a9e7872458ba3a3368e1e7489c51211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package mathis1/0.0.1/mathis1-0.0.1.tar.gz

---
+++ packages/mathis1/mathis1-0.0.1.tar.gz/mathis1-0.0.1/README.md
@@ -0,0 +1,21 @@
+HOW TO USE THIS PACKAGE
+
+
+from example_package import mathis
+
+mathis.youtube()
+mathis.instagram()
+mathis.facebook()
+mathis.linkedin()
+
+
+Source code
+
+You can check the latest sources with the command:
+https://github.com/mathismathis/mathis_auto_login
+
+
+License
+mathis is licensed under the MIT license. The full license text can be found here.
+
+
---
+++ packages/mathis1/mathis1-0.0.1.tar.gz/mathis1-0.0.1/src/example_package/mathis.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+url='https://www.instagram.com/maestro_mathis/?hl=en'
+url1='https://www.linkedin.com/in/mathis-t-794254222/'
+url2='https://www.youtube.com/channel/UChAqVuW3nJCWdbbJOYEP00Q'
+url3='https://www.youtube.com/channel/UChAqVuW3nJCWdbbJOYEP00Q'
+
+
+
+def instagram():
+    webbrowser.open(url)
+def linkedin():
+    webbrowser.open(url1)
+def youtube():
+    webbrowser.open(url2)
+def facebook():
+    webbrowser.open(url3)
