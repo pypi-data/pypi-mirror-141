db5278032213d7b64b65c560fcbf7f95f58841ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:09 +0000

    Add package notecoin/0.12.20/notecoin-0.12.20-py3.9.egg

---
+++ packages/notecoin/notecoin-0.12.20-py3.9.egg/notecoin/server/notecoin_server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from notecoin.okex.server.const import account_account, market_tickers
+from 
+
+
+seller = AutoSeller()
+
+
+app = FastAPI()
+app.include_router(account_account)
+app.include_router(market_tickers)
+
+# uvicorn notecoin:app --host '0.0.0.0' --port 8444 --reload
