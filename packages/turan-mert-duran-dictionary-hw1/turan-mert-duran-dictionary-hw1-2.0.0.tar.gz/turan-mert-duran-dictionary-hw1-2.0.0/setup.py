50ab93f12b25ec118a1191ad651edbbae81acc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package turan-mert-duran-dictionary-hw1/2.0.0/turan-mert-duran-dictionary-hw1-2.0.0.tar.gz

---
+++ packages/turan-mert-duran-dictionary-hw1/turan-mert-duran-dictionary-hw1-2.0.0.tar.gz/turan-mert-duran-dictionary-hw1-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="turan-mert-duran-dictionary-hw1",
+    author="Turan Mert Duran",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
