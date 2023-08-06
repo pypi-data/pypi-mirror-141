585659e4ef5d55078d45c4893a24d3dc25489385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:58 +0000

    Add package BlueLvRenHello/0.1.0/BlueLvRenHello-0.1.0.tar.gz

---
+++ packages/BlueLvRenHello/BlueLvRenHello-0.1.0.tar.gz/BlueLvRenHello-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:setup.py
+# author:PigKnight
+# datetime:2022/3/5 9:25
+# software: PyCharm
+"""
+this is function description
+"""
+# import module your need
+import setuptools
+
+setuptools.setup()
---
+++ packages/BlueLvRenHello/BlueLvRenHello-0.1.0.tar.gz/BlueLvRenHello-0.1.0/src/BlueLvRenHello/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:hello.py
+# author:PigKnight
+# datetime:2022/3/4 16:42
+# software: PyCharm
+"""
+this is function description
+"""
+
+
+# import module your need
+def sayHello():
+    print("Hello,this is BlueLvRen!")
