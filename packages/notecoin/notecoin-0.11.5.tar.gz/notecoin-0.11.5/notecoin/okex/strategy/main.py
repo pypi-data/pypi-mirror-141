3fe45f940844e9741413c26f371de9cae555f205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package notecoin/0.11.5/notecoin-0.11.5.tar.gz

---
+++ packages/notecoin/notecoin-0.11.5.tar.gz/notecoin-0.11.5/notecoin/okex/strategy/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from notecoin.okex.strategy.strategy1 import Strategy1
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'success'
+
+
+Strategy1.register(app, route_base='/')
+
+
+app.run(host='0.0.0.0', port=8444)
