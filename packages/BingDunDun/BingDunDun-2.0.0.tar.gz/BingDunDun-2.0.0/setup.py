0de27cb00bd8c12f17ad1a301952493dd637bf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:49 +0000

    Add package BingDunDun/2.0.0/BingDunDun-2.0.0.tar.gz

---
+++ packages/BingDunDun/BingDunDun-2.0.0.tar.gz/BingDunDun-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+     name="BingDunDun",  #pypi中的名称，pip或者easy_install安装时使用的名称
+     version="2.0.0",
+     author="Eno",
+     author_email="",
+     description=("这是一个用海龟画图画出冰墩墩的程序代码"),
+     packages=['upload'],  # 需要打包的目录列表
+)
