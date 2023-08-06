2e07ceb71d21ca6f2823edec839a36184847ea7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:29 +0000

    Add package fluigent-sdk/0.0.1/fluigent_sdk-0.0.1-py3-none-any.whl

---
+++ packages/fluigent-sdk/fluigent_sdk-0.0.1-py3-none-any.whl/fluigent_sdk/__init__.py
@@ -0,0 +1,7 @@
+raise NotImplementedError(
+"""
+This is only a placeholder package that I uploaded to PyPI to prevent bad evil people
+from registering it. You should get fluigent_sdk from https://github.com/Fluigent/fgt-SDK/.
+If you are an employee of Fluigent and would like to take control of this PyPI package, please
+get into contact.
+""")
