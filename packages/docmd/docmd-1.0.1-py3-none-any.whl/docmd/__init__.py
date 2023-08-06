97eee7c9d874853593c54de205f89259f62984c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:14 +0000

    Add package docmd/1.0.1/docmd-1.0.1-py3-none-any.whl

---
+++ packages/docmd/docmd-1.0.1-py3-none-any.whl/docmd/__init__.py
@@ -0,0 +1,17 @@
+"""
+Generate api-style github markdown-files from python docstrings.
+
+Generate one file:
+
+```
+docmd my_module > README.md
+```
+
+Generate a whole folder full of documentation:
+
+```
+docmd my_module -out docs -url https://github.com/atakamallc/docmd/blob/master
+```
+"""
+
+from .docmd import DocMd
