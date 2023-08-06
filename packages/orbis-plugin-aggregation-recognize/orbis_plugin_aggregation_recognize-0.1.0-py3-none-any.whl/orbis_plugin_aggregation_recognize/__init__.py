74370c661ed17f1f3e130951993e32b3eaf66ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:32 +0000

    Add package orbis-plugin-aggregation-recognize/0.1.0/orbis_plugin_aggregation_recognize-0.1.0-py3-none-any.whl

---
+++ packages/orbis-plugin-aggregation-recognize/orbis_plugin_aggregation_recognize-0.1.0-py3-none-any.whl/orbis_plugin_aggregation_recognize/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_aggregation_recognize"
+__version__ = "0.1.0"
+__author__ = "Albert Weichselbraun"
+__year__ = "2022"
+__description__ = "The spotlight aggregation plugin for Orbis"
+__license__ = "GPL3"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_aggregation_recognize"
+__type__ = "plugin"
+
+from .main import Main
