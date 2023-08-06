a84edb80ab7b835ebd68abe1fc928fce190838d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:09 +0000

    Add package pyq3serverlist/0.1.9/pyq3serverlist-0.1.9-py3-none-any.whl

---
+++ packages/pyq3serverlist/pyq3serverlist-0.1.9-py3-none-any.whl/pyq3serverlist/__init__.py
@@ -0,0 +1,15 @@
+from .connection import Connection
+from .exceptions import PyQ3SLError, PyQ3SLTimeoutError
+from .principalserver import PrincipalServer
+from .server import Server
+
+"""
+pyq3serverlist.
+
+Simple Python library for querying Quake 3 based principal servers and their game servers.
+"""
+
+__version__ = '0.1.9'
+__author__ = 'cetteup'
+__credits__ = 'https://github.com/jacklul'
+__all__ = ['Connection', 'PrincipalServer', 'Server', 'PyQ3SLError', 'PyQ3SLTimeoutError']
