0c83d57bca2eff04374d77ae30e82ecf8dd73a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:41 +0000

    Add package PTokenQuery/0.0.3/PTokenQuery-0.0.3-py3-none-any.whl

---
+++ packages/PTokenQuery/PTokenQuery-0.0.3-py3-none-any.whl/p_token_query/config/__init__.py
@@ -0,0 +1,9 @@
+from p_token_query.config.global_config import AbiConfig
+
+
+class _GlobalConfig:
+    def __init__(self):
+        self.AbiConfig = AbiConfig
+
+
+GlobalInfoConfig = _GlobalConfig()
