cf30ea8fa4c5335134e11d69f26a5bf6a87c006a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:46 +0000

    Add package nester66666/1.2.0/nester66666-1.2.0.tar.gz

---
+++ packages/nester66666/nester66666-1.2.0.tar.gz/nester66666-1.2.0/nester.py
@@ -0,0 +1,12 @@
+"""这是neser.py模块，提供了一个名为print_lol()的函数，这个函数的作用是打印列表，其中
+有可能包含（也可能不包含）嵌套列表"""
+def print_lol(the_list,level=0):
+        """这个函数取一个位置参数，名为the_list，这可以是任何python列表，所指定的
+           列表中的每个数据项会递归的输出到屏幕上，各数据项各占一行"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level+1)
+                else:
+                        for tab_stop in range(level):
+                                print("\t",end="")
+                        print(each_item)
---
+++ packages/nester66666/nester66666-1.2.0.tar.gz/nester66666-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+        name        = 'nester66666',
+        version     = '1.2.0',
+        py_modules  = ['nester'],
+        author      = 'chrisZ',
+        author_email = 'jzhangc2131@163.com',
+        description = 'A simple printer of lists',
+    )
