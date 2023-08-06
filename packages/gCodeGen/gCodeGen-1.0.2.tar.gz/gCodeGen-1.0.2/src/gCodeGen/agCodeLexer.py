d1d7de01e2368e374f29ffea17227173ee29779e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:40 +0000

    Add package gCodeGen/1.0.2/gCodeGen-1.0.2.tar.gz

---
+++ packages/gCodeGen/gCodeGen-1.0.2.tar.gz/gCodeGen-1.0.2/src/gCodeGen/agCodeLexer.py
@@ -0,0 +1,175 @@
+# Generated from C:/Users/dennis.schwebius/PycharmProjects/advGcode\agCode.g4 by ANTLR 4.9.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2$")
+        buf.write("\u00d9\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+        buf.write("\t\36\4\37\t\37\4 \t \4!\t!\4\"\t\"\4#\t#\3\2\3\2\3\2")
+        buf.write("\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\5\3\5\3\6\3\6\3")
+        buf.write("\6\3\6\3\7\3\7\3\7\3\b\3\b\3\b\3\b\3\b\3\t\3\t\3\t\3\t")
+        buf.write("\3\n\3\n\3\13\3\13\3\13\3\13\3\13\3\13\3\f\3\f\3\r\3\r")
+        buf.write("\3\16\3\16\3\17\3\17\3\20\3\20\3\21\3\21\3\22\3\22\3\22")
+        buf.write("\3\23\3\23\3\24\3\24\3\24\3\25\3\25\3\25\3\26\3\26\3\27")
+        buf.write("\3\27\3\30\3\30\3\30\3\30\5\30\u008d\n\30\3\31\3\31\3")
+        buf.write("\32\3\32\3\32\3\32\5\32\u0095\n\32\3\33\3\33\3\34\6\34")
+        buf.write("\u009a\n\34\r\34\16\34\u009b\3\34\3\34\6\34\u00a0\n\34")
+        buf.write("\r\34\16\34\u00a1\5\34\u00a4\n\34\3\35\6\35\u00a7\n\35")
+        buf.write("\r\35\16\35\u00a8\3\36\3\36\3\36\3\36\3\36\3\36\3\36\3")
+        buf.write("\36\3\36\5\36\u00b4\n\36\3\37\3\37\7\37\u00b8\n\37\f\37")
+        buf.write("\16\37\u00bb\13\37\3\37\3\37\3 \3 \3 \3 \3 \3 \3 \3 \5")
+        buf.write(" \u00c7\n \3!\3!\3\"\3\"\3\"\3\"\3#\3#\7#\u00d1\n#\f#")
+        buf.write("\16#\u00d4\13#\3#\3#\3#\3#\4\u00b9\u00d2\2$\3\3\5\4\7")
+        buf.write("\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16\33\17")
+        buf.write("\35\20\37\21!\22#\23%\24\'\25)\26+\27-\30/\31\61\32\63")
+        buf.write("\33\65\34\67\359\36;\37= ?!A\"C#E$\3\2\6\5\2HHJJZ\\\3")
+        buf.write("\2\62;\4\2aac|\5\2\13\f\17\17\"\"\2\u00e5\2\3\3\2\2\2")
+        buf.write("\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r")
+        buf.write("\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3")
+        buf.write("\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2")
+        buf.write("\2\2\2\37\3\2\2\2\2!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\2\'")
+        buf.write("\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2\2-\3\2\2\2\2/\3\2\2\2\2")
+        buf.write("\61\3\2\2\2\2\63\3\2\2\2\2\65\3\2\2\2\2\67\3\2\2\2\29")
+        buf.write("\3\2\2\2\2;\3\2\2\2\2=\3\2\2\2\2?\3\2\2\2\2A\3\2\2\2\2")
+        buf.write("C\3\2\2\2\2E\3\2\2\2\3G\3\2\2\2\5J\3\2\2\2\7L\3\2\2\2")
+        buf.write("\tS\3\2\2\2\13U\3\2\2\2\rY\3\2\2\2\17\\\3\2\2\2\21a\3")
+        buf.write("\2\2\2\23e\3\2\2\2\25g\3\2\2\2\27m\3\2\2\2\31o\3\2\2\2")
+        buf.write("\33q\3\2\2\2\35s\3\2\2\2\37u\3\2\2\2!w\3\2\2\2#y\3\2\2")
+        buf.write("\2%|\3\2\2\2\'~\3\2\2\2)\u0081\3\2\2\2+\u0084\3\2\2\2")
+        buf.write("-\u0086\3\2\2\2/\u008c\3\2\2\2\61\u008e\3\2\2\2\63\u0094")
+        buf.write("\3\2\2\2\65\u0096\3\2\2\2\67\u0099\3\2\2\29\u00a6\3\2")
+        buf.write("\2\2;\u00b3\3\2\2\2=\u00b5\3\2\2\2?\u00c6\3\2\2\2A\u00c8")
+        buf.write("\3\2\2\2C\u00ca\3\2\2\2E\u00ce\3\2\2\2GH\7I\2\2HI\7\66")
+        buf.write("\2\2I\4\3\2\2\2JK\7V\2\2K\6\3\2\2\2LM\7t\2\2MN\7g\2\2")
+        buf.write("NO\7r\2\2OP\7g\2\2PQ\7c\2\2QR\7v\2\2R\b\3\2\2\2ST\7<\2")
+        buf.write("\2T\n\3\2\2\2UV\7g\2\2VW\7p\2\2WX\7f\2\2X\f\3\2\2\2YZ")
+        buf.write("\7k\2\2Z[\7h\2\2[\16\3\2\2\2\\]\7g\2\2]^\7n\2\2^_\7u\2")
+        buf.write("\2_`\7g\2\2`\20\3\2\2\2ab\7x\2\2bc\7c\2\2cd\7t\2\2d\22")
+        buf.write("\3\2\2\2ef\7?\2\2f\24\3\2\2\2gh\7r\2\2hi\7t\2\2ij\7k\2")
+        buf.write("\2jk\7p\2\2kl\7v\2\2l\26\3\2\2\2mn\7*\2\2n\30\3\2\2\2")
+        buf.write("op\7.\2\2p\32\3\2\2\2qr\7+\2\2r\34\3\2\2\2st\7,\2\2t\36")
+        buf.write("\3\2\2\2uv\7\61\2\2v \3\2\2\2wx\7-\2\2x\"\3\2\2\2yz\7")
+        buf.write("@\2\2z{\7?\2\2{$\3\2\2\2|}\7@\2\2}&\3\2\2\2~\177\7?\2")
+        buf.write("\2\177\u0080\7?\2\2\u0080(\3\2\2\2\u0081\u0082\7>\2\2")
+        buf.write("\u0082\u0083\7?\2\2\u0083*\3\2\2\2\u0084\u0085\7>\2\2")
+        buf.write("\u0085,\3\2\2\2\u0086\u0087\7#\2\2\u0087.\3\2\2\2\u0088")
+        buf.write("\u0089\7I\2\2\u0089\u008d\7\62\2\2\u008a\u008b\7I\2\2")
+        buf.write("\u008b\u008d\7\63\2\2\u008c\u0088\3\2\2\2\u008c\u008a")
+        buf.write("\3\2\2\2\u008d\60\3\2\2\2\u008e\u008f\t\2\2\2\u008f\62")
+        buf.write("\3\2\2\2\u0090\u0091\7I\2\2\u0091\u0095\7\64\2\2\u0092")
+        buf.write("\u0093\7I\2\2\u0093\u0095\7\65\2\2\u0094\u0090\3\2\2\2")
+        buf.write("\u0094\u0092\3\2\2\2\u0095\64\3\2\2\2\u0096\u0097\4LM")
+        buf.write("\2\u0097\66\3\2\2\2\u0098\u009a\t\3\2\2\u0099\u0098\3")
+        buf.write("\2\2\2\u009a\u009b\3\2\2\2\u009b\u0099\3\2\2\2\u009b\u009c")
+        buf.write("\3\2\2\2\u009c\u00a3\3\2\2\2\u009d\u009f\7\60\2\2\u009e")
+        buf.write("\u00a0\t\3\2\2\u009f\u009e\3\2\2\2\u00a0\u00a1\3\2\2\2")
+        buf.write("\u00a1\u009f\3\2\2\2\u00a1\u00a2\3\2\2\2\u00a2\u00a4\3")
+        buf.write("\2\2\2\u00a3\u009d\3\2\2\2\u00a3\u00a4\3\2\2\2\u00a48")
+        buf.write("\3\2\2\2\u00a5\u00a7\t\4\2\2\u00a6\u00a5\3\2\2\2\u00a7")
+        buf.write("\u00a8\3\2\2\2\u00a8\u00a6\3\2\2\2\u00a8\u00a9\3\2\2\2")
+        buf.write("\u00a9:\3\2\2\2\u00aa\u00ab\7V\2\2\u00ab\u00ac\7t\2\2")
+        buf.write("\u00ac\u00ad\7w\2\2\u00ad\u00b4\7g\2\2\u00ae\u00af\7H")
+        buf.write("\2\2\u00af\u00b0\7c\2\2\u00b0\u00b1\7n\2\2\u00b1\u00b2")
+        buf.write("\7u\2\2\u00b2\u00b4\7g\2\2\u00b3\u00aa\3\2\2\2\u00b3\u00ae")
+        buf.write("\3\2\2\2\u00b4<\3\2\2\2\u00b5\u00b9\7$\2\2\u00b6\u00b8")
+        buf.write("\13\2\2\2\u00b7\u00b6\3\2\2\2\u00b8\u00bb\3\2\2\2\u00b9")
+        buf.write("\u00ba\3\2\2\2\u00b9\u00b7\3\2\2\2\u00ba\u00bc\3\2\2\2")
+        buf.write("\u00bb\u00b9\3\2\2\2\u00bc\u00bd\7$\2\2\u00bd>\3\2\2\2")
+        buf.write("\u00be\u00c7\7@\2\2\u00bf\u00c0\7@\2\2\u00c0\u00c7\7?")
+        buf.write("\2\2\u00c1\u00c2\7?\2\2\u00c2\u00c7\7?\2\2\u00c3\u00c4")
+        buf.write("\7>\2\2\u00c4\u00c7\7?\2\2\u00c5\u00c7\7>\2\2\u00c6\u00be")
+        buf.write("\3\2\2\2\u00c6\u00bf\3\2\2\2\u00c6\u00c1\3\2\2\2\u00c6")
+        buf.write("\u00c3\3\2\2\2\u00c6\u00c5\3\2\2\2\u00c7@\3\2\2\2\u00c8")
+        buf.write("\u00c9\7/\2\2\u00c9B\3\2\2\2\u00ca\u00cb\t\5\2\2\u00cb")
+        buf.write("\u00cc\3\2\2\2\u00cc\u00cd\b\"\2\2\u00cdD\3\2\2\2\u00ce")
+        buf.write("\u00d2\7%\2\2\u00cf\u00d1\13\2\2\2\u00d0\u00cf\3\2\2\2")
+        buf.write("\u00d1\u00d4\3\2\2\2\u00d2\u00d3\3\2\2\2\u00d2\u00d0\3")
+        buf.write("\2\2\2\u00d3\u00d5\3\2\2\2\u00d4\u00d2\3\2\2\2\u00d5\u00d6")
+        buf.write("\7\f\2\2\u00d6\u00d7\3\2\2\2\u00d7\u00d8\b#\2\2\u00d8")
+        buf.write("F\3\2\2\2\r\2\u008c\u0094\u009b\u00a1\u00a3\u00a8\u00b3")
+        buf.write("\u00b9\u00c6\u00d2\3\b\2\2")
+        return buf.getvalue()
+
+
+class agCodeLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    T__8 = 9
+    T__9 = 10
+    T__10 = 11
+    T__11 = 12
+    T__12 = 13
+    T__13 = 14
+    T__14 = 15
+    T__15 = 16
+    T__16 = 17
+    T__17 = 18
+    T__18 = 19
+    T__19 = 20
+    T__20 = 21
+    T__21 = 22
+    G01 = 23
+    ML = 24
+    G23 = 25
+    CL = 26
+    NUM = 27
+    ID = 28
+    BOOL = 29
+    TEXT = 30
+    COMPAR = 31
+    MINUS = 32
+    WHITESPACE = 33
+    COMMENT = 34
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'G4'", "'T'", "'repeat'", "':'", "'end'", "'if'", "'else'", 
+            "'var'", "'='", "'print'", "'('", "','", "')'", "'*'", "'/'", 
+            "'+'", "'>='", "'>'", "'=='", "'<='", "'<'", "'!'", "'-'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "G01", "ML", "G23", "CL", "NUM", "ID", "BOOL", "TEXT", "COMPAR", 
+            "MINUS", "WHITESPACE", "COMMENT" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "T__8", "T__9", "T__10", "T__11", "T__12", "T__13", 
+                  "T__14", "T__15", "T__16", "T__17", "T__18", "T__19", 
+                  "T__20", "T__21", "G01", "ML", "G23", "CL", "NUM", "ID", 
+                  "BOOL", "TEXT", "COMPAR", "MINUS", "WHITESPACE", "COMMENT" ]
+
+    grammarFileName = "agCode.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
