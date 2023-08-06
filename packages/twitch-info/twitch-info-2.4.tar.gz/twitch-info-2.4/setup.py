fdfc80895f255ad7fc32a4c06e92f80c584cf2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:13 +0000

    Add package twitch-info/2.4/twitch-info-2.4.tar.gz

---
+++ packages/twitch-info/twitch-info-2.4.tar.gz/twitch-info-2.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '2.4'
+DESCRIPTION = 'Get information about a twitch channels and streams'
+
+# Setting up
+setup(
+    name="twitch-info",
+    version=VERSION,
+    author="Feytus",
+    author_email="feytusinho@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'stream', 'twitch', 'channel', 'information', 'info'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
