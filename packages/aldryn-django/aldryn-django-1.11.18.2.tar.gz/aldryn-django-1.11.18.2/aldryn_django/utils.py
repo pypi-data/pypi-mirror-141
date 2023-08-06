9924692b523f041fd1dc9be1f2bfe1a1cae570bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:21 +0000

    Add package aldryn-django/1.11.18.2/aldryn-django-1.11.18.2.tar.gz

---
+++ packages/aldryn-django/aldryn-django-1.11.18.2.tar.gz/aldryn-django-1.11.18.2/aldryn_django/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns as django_i18n_patterns
+
+
+def i18n_patterns(*args):
+    """
+    We want to avoid having to change anything in the projects
+    root urls.py when new versions of Django are released.
+    """
+    use_prefix = getattr(settings, 'PREFIX_DEFAULT_LANGUAGE', True)
+    return django_i18n_patterns(*args, prefix_default_language=use_prefix)
