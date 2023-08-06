be3045597fb0b16402f3830d53e1d8863cc1ef8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:22 +0000

    Add package hcai-datasets-nightly/0.1.1.dev202203101745/hcai_datasets_nightly-0.1.1.dev202203101745-py3-none-any.whl

---
+++ packages/hcai-datasets-nightly/hcai_datasets_nightly-0.1.1.dev202203101745-py3-none-any.whl/hcai_dataset_utils/bridge_tf.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+from hcai_dataset_utils.dataset_iterable import DatasetIterable
+
+
+class BridgeTensorflow:
+    """
+    Provides a helper function to make a tf Dataset directly from a DatasetIterable
+    """
+
+    TYPE_MAPPING = {
+        np.str: tf.string,
+        np.int: tf.int32,
+        np.int32: tf.int32,
+        np.int64: tf.int64,
+        np.float: tf.float32,
+        np.float32: tf.float32,
+        np.float64: tf.float32,
+    }
+
+    @staticmethod
+    def make(ds: DatasetIterable):
+        iter = ds.__iter__
+
+        info = ds.get_output_info()
+        output_types = {
+            **{k: BridgeTensorflow.TYPE_MAPPING[info[k]["dtype"]] for k in info.keys()}
+        }
+
+        return tf.data.Dataset.from_generator(iter, output_types=output_types)
