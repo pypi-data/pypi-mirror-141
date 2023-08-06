a28754ffc77983dcaf4aca564159203b2879e56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:12 +0000

    Add package hplib/0.0.4/hplib-0.0.4.tar.gz

---
+++ packages/hplib/hplib-0.0.4.tar.gz/hplib-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(name='hplib',
+version='0.0.4',
+description='Database and code to simulate heat pumps',
+long_description=long_description,
+long_description_content_type='text/markdown',
+py_modules=['hplib'],
+package_dir={'':'src'},
+package_data={'': ['hplib-database.py']}
+)
