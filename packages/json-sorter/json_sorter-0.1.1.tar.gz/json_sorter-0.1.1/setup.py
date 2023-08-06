87cfaa547b77467e2d82b912250609682c92b060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:52 +0000

    Add package json-sorter/0.1.1/json_sorter-0.1.1.tar.gz

---
+++ packages/json-sorter/json_sorter-0.1.1.tar.gz/json_sorter-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(name='json_sorter',
+version='0.1.1',
+description='Simple package to Sort Json Content',
+license='MIT',
+url='https://github.com/jasonfedorowich/jsonsort',
+author='Jason',
+install_requires=[],
+author_email='jason.fedorowich@gmail.com',
+python_requires='>=3',
+packages=setuptools.find_packages(),
+zip_safe=False,
+long_description=open('README.md', 'r').read(),
+long_description_content_type="")
