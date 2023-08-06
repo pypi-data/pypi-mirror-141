ee65b9c765d353113a9912b7b1cdfa5fee3b2da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:20 +0000

    Add package duang222/6.0/duang222-6.0.tar.gz

---
+++ packages/duang222/duang222-6.0.tar.gz/duang222-6.0/duang.py
@@ -0,0 +1,1 @@
+print('duang222 test')
---
+++ packages/duang222/duang222-6.0.tar.gz/duang222-6.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    version=6.0,
+    name='duang222',
+    description="test.",
+    author='iamduang',
+    author_email='xx@xxx.com',
+    url='https://github.com/canonical/subiquity',
+    license="AGPLv3+",
+    packages=find_packages(),
+    py_modules=['duang'],
+)
