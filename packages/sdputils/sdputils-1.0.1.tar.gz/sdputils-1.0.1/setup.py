7c98a6c30ac64f42222a103d53e9a2f66d33bacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:42 +0000

    Add package sdputils/1.0.1/sdputils-1.0.1.tar.gz

---
+++ packages/sdputils/sdputils-1.0.1.tar.gz/sdputils-1.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="sdputils",
+    version="1.0.1",
+    author="Lucas S. Maximiano",
+    description="Utils' functions to use inside the Semantix Data Platform for data transformation.",
+    install_requires=[
+          'pandas==1.2.4',
+      ]
+
+)
