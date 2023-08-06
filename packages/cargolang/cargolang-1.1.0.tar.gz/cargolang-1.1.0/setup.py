b3ee4dc92b4e424bd0a7da83f874397aac73b20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:40 +0000

    Add package cargolang/1.1.0/cargolang-1.1.0.tar.gz

---
+++ packages/cargolang/cargolang-1.1.0.tar.gz/cargolang-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cargolang',
+    author='Cargo',
+    version='1.1.0',
+    packages=['shell', 'cargo'],
+    install_requires=['click', 'osascript', 'KvK'],
+    entry_points='''
+    [console_script]
+    cargo=shell:cargo
+    '''
+)
