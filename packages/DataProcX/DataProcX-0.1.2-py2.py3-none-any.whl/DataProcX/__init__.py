9fa7466cc2b3339eb8f7fdb5512e3fcf481fffa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:42 +0000

    Add package DataProcX/0.1.2/DataProcX-0.1.2-py2.py3-none-any.whl

---
+++ packages/DataProcX/DataProcX-0.1.2-py2.py3-none-any.whl/DataProcX/__init__.py
@@ -0,0 +1,11 @@
+'Text label processing framework.'
+
+__author__ = 'Lethon'
+__version__ = '0.1.2'
+__all__ = ["Parser", "Data"]
+
+import DataProc.parser
+import DataProc.data
+
+Parser = DataProc.parser.Parser
+Data = DataProc.data.Data
