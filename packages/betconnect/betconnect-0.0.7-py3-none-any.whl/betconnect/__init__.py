a42b7f537716c6b5430c03b542301e1b9786372b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:44 +0000

    Add package betconnect/0.0.7/betconnect-0.0.7-py3-none-any.whl

---
+++ packages/betconnect/betconnect-0.0.7-py3-none-any.whl/betconnect/__init__.py
@@ -0,0 +1,26 @@
+from .exceptions import (
+    InValidPersonalisedProductionURL,
+    UnknownBetConnectEnvironment,
+    LoginMissingTokenInResponse,
+    FailedLogin,
+    MinOddException,
+    UnexpectedResponseStatusCode,
+    GamStopException,
+    MissingUserPerferences,
+    APIError,
+    BetRequestIDFormatException,
+    BetRequestIDStakeSizeException,
+    BetRequestInvalidCustomerOrderRefFormatException,
+    BetRequestInvalidCustomerStrategyRefFormatException,
+)
+
+from .enums import (
+    Environment,
+    BetStatus,
+    BetSide,
+    BetRequestStatus,
+    MarketStatus,
+    TradingStatus,
+)
+
+from .apiclient import APIClient
