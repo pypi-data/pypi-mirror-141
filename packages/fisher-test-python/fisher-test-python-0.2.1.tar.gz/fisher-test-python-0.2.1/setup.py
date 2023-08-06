0d3f767452100cfa90045be801acab2f5ca2ee98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:47 +0000

    Add package fisher-test-python/0.2.1/fisher-test-python-0.2.1.tar.gz

---
+++ packages/fisher-test-python/fisher-test-python-0.2.1.tar.gz/fisher-test-python-0.2.1/setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import site
+
+from setuptools import (
+    setup,
+    find_packages,
+    Extension
+)
+from setuptools.command.install import install
+
+
+PACKAGE = "fisher"
+
+
+class AfterInstall(install):
+
+  def build_c(self, path, filename):
+    obj_path = os.path.join(path, PACKAGE, "src", f"{filename}.so")
+    c_path = os.path.join(path, PACKAGE, "src", f"{filename}.c")
+    ret = os.system(
+        f"gcc -o {obj_path} -shared -fPIC {c_path}"
+    )
+    if ret != 0:
+      raise Exception("gcc build failed.")
+
+  def run(self):
+    install.run(self)
+
+    installed_path = ""
+    if "--user" in sys.argv:
+      installed_path = site.getusersitepackages()
+    else:
+      site_path_list = site.getsitepackages()
+      for site_path in site_path_list:
+        if (os.path.exists(os.path.join(site_path, PACKAGE))):
+          installed_path = site_path
+
+    self.build_c(installed_path, "fisher")
+    self.build_c(installed_path, "asa159")
+
+
+setup(
+    install_requires=[
+        "numpy",
+        "scipy"
+    ],
+    test_suite="tests",
+    tests_require=["pytest"],
+    packages=find_packages(exclude="tests"),
+    package_data={
+      PACKAGE: ["src/*.c"]
+    },
+    cmdclass={"install": AfterInstall},
+)
