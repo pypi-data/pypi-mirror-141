fe806999a0bc0a66da35aa376a2697ff1a469571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:17 +0000

    Add package jajbjcjd/0.0.8/jajbjcjd-0.0.8.tar.gz

---
+++ packages/jajbjcjd/jajbjcjd-0.0.8.tar.gz/jajbjcjd-0.0.8/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name="jajbjcjd",
+    version="0.0.8",
+    author="XHLin",
+    author_email="xhaughearl@gmail.com",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    install_requires = [
+
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["jajbjcjd"],
+    python_requires=">=3.6",
+)
