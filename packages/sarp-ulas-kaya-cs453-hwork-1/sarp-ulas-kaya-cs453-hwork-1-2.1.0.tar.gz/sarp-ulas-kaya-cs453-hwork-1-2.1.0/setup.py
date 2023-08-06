80c5c09d06b885bac16a00103e95a60e03b70871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:57 +0000

    Add package sarp-ulas-kaya-cs453-hwork-1/2.1.0/sarp-ulas-kaya-cs453-hwork-1-2.1.0.tar.gz

---
+++ packages/sarp-ulas-kaya-cs453-hwork-1/sarp-ulas-kaya-cs453-hwork-1-2.1.0.tar.gz/sarp-ulas-kaya-cs453-hwork-1-2.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="sarp-ulas-kaya-cs453-hwork-1",
+    author="Sarp Ulas Kaya",
+    version="2.1.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
