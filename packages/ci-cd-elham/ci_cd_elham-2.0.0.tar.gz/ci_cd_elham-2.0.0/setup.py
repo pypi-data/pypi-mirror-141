50030b296ce850785c2568ad09e2dca3e45470da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package ci-cd-elham/2.0.0/ci_cd_elham-2.0.0.tar.gz

---
+++ packages/ci-cd-elham/ci_cd_elham-2.0.0.tar.gz/ci_cd_elham-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ci_cd_elham",
+    author="Elham Amin",  # TODO: Write your name
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
