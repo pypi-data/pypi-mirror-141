b04ddac6f0b1f48052dd02997e53f2f5d4f9265b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:26:38 +0000

    Add package neuraldon/0.1.1/neuraldon-0.1.1.tar.gz

---
+++ packages/neuraldon/neuraldon-0.1.1.tar.gz/neuraldon-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='neuraldon',
+      version='0.1.1',
+      description='Python Package',
+      packages = ['package'],
+      install_requires = [],
+      zip_safe = False,
+      )
