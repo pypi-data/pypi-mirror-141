0e50e1e97222397fb4b98ca1aa4181ff5e4d51a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:32 +0000

    Add package aplicacion-ejemplo-kc/0.0.1/aplicacion-ejemplo-kc-0.0.1.tar.gz

---
+++ packages/aplicacion-ejemplo-kc/aplicacion-ejemplo-kc-0.0.1.tar.gz/aplicacion-ejemplo-kc-0.0.1/src/aplicacion_ejemplo/ejemplo.py
@@ -0,0 +1,13 @@
+import coloredlogs, logging
+
+logger = logging.getLogger(__name__)
+
+coloredlogs.install(level='DEBUG', logger=logger) # Explicar niveles de LOG, pros y contras
+
+logger.info("Programa python de ejemplo")
+
+def add_one(number):
+    return number + 1
+
+# def add_two(number):
+#     return number + 2
