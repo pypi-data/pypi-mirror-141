3680f522affff4362cbb7b8e8a408a0c5d086add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:59 +0000

    Add package pylists/0.0.1.4/pylists-0.0.1.4.tar.gz

---
+++ packages/pylists/pylists-0.0.1.4.tar.gz/pylists-0.0.1.4/pylists/__init__.py
@@ -0,0 +1,43 @@
+C_G_ = [
+    "@DogeUserBot",
+    "@DogeSup",
+]
+
+
+GR__PS = [
+    -1001310554327, # @DogeSup
+    -1001062690377, # @DogeSohbet
+    -1001760595751, # @DogeStatus
+]
+
+
+M_ST_RS = [
+    827754695, # @Mutlcc
+    1692479574, # @TeleDoge
+    1016037515, # @Doblesny
+    1644193846, # @Yigih
+    931341953, # @hesenazor
+    1827098294, # Luci
+    1769015061, # @Ayiak ayLak
+    2034602789, # @atayist Yakup
+]
+
+
+G_YS = [
+    1422746074, # @erdewbey
+    1097068650, # @bberc
+    1729277877, # @syntaxerr
+    1687646994, # @berce
+    1014656491, # @artikkullanilmiyor
+    1310308056, # @ixelizm
+    1044658315, # @ber4tbey
+    1527722982, # @FVREED
+    1797867165, # @sherlock_exe
+    979515849, # @TheMiri
+    1849828581, # @TheOksigen
+    1108583389, # @HusnuEhedov
+    877938637, # @ValiyevAli
+    1858376867, # @nasca - nisa
+    1631262264, # @zehrz - Zehra
+    875593379, # @kalkisimege
+]
---
+++ packages/pylists/pylists-0.0.1.4.tar.gz/pylists-0.0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="pylists",
+    version="0.0.1.4",
+    url="https://github.com/DOG-E",
+    author="DOG-E",
+    author_email="DogeUserBot@gmail.com",
+    description=("Lists"),
+    packages=["pylists"],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ],
+)
