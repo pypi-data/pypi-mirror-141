cae12918cb35e9d5f685559e6d046bb75c09e710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:22 +0000

    Add package immuta-audit-export/0.1.10/immuta_audit_export-0.1.10-py3-none-any.whl

---
+++ packages/immuta-audit-export/immuta_audit_export-0.1.10-py3-none-any.whl/immuta_audit_export/export_all.py
@@ -0,0 +1,13 @@
+from .tenant_config.get_tenant_configs import get_tenant_configs
+from .common.call_export_record import call_export_record
+
+def export_all(tenant, record):
+	configs = get_tenant_configs(tenant)
+	for key, config in configs:
+		call_export_record(key, config, record)
+
+
+		
+
+
+
