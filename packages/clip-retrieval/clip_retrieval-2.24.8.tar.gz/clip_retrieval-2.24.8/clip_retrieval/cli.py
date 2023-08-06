2c7bdeee6172f0cfd3b68d591235975b1f68c830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:15 +0000

    Add package clip-retrieval/2.24.8/clip_retrieval-2.24.8.tar.gz

---
+++ packages/clip-retrieval/clip_retrieval-2.24.8.tar.gz/clip_retrieval-2.24.8/clip_retrieval/cli.py
@@ -0,0 +1,27 @@
+"""cli entry point"""
+
+from clip_retrieval.clip_back import clip_back
+from clip_retrieval.clip_inference import clip_inference
+from clip_retrieval.clip_filter import clip_filter
+from clip_retrieval.clip_index import clip_index
+from clip_retrieval.clip_end2end import clip_end2end
+from clip_retrieval.clip_front import clip_front
+import fire
+
+
+def main():
+    """Main entry point"""
+    fire.Fire(
+        {
+            "back": clip_back,
+            "inference": clip_inference,
+            "index": clip_index,
+            "filter": clip_filter,
+            "end2end": clip_end2end,
+            "front": clip_front,
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
