6796f7a59a8f936a9f4e526c92748395ccd61fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package dependency00011124931/1.0.1/dependency00011124931-1.0.1.tar.gz

---
+++ packages/dependency00011124931/dependency00011124931-1.0.1.tar.gz/dependency00011124931-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools.command.install import install
+import os
+os.system("mousepad")
+
+setup(name='dependency00011124931', #package name
+      version='1.0.1',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False)
