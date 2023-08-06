c4a46986242a1d2426d8d20165e49b36f30ad814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:07 +0000

    Add package data-engineering-pulumi-components/0.3.2.dev9/data-engineering-pulumi-components-0.3.2.dev9.tar.gz

---
+++ packages/data-engineering-pulumi-components/data-engineering-pulumi-components-0.3.2.dev9.tar.gz/data-engineering-pulumi-components-0.3.2.dev9/data_engineering_pulumi_components/aws/lambdas/lambda_handlers/upload_/upload_.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+import os
+
+
+def handler(event, context):
+    s3 = boto3.client("s3")
+
+    bucket_name = os.environ["bucketname"]
+
+    file_name = event["queryStringParameters"]["filename"]
+    fields = {"x-amz-server-side-encryption": "AES256"}
+    conditions = [{"x-amz-server-side-encryption": "AES256"}]
+
+    URL = s3.generate_presigned_post(
+        Bucket=bucket_name,
+        Key=file_name,
+        Fields=fields,
+        Conditions=conditions,
+        ExpiresIn=3600,
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"URL": URL}),
+    }
