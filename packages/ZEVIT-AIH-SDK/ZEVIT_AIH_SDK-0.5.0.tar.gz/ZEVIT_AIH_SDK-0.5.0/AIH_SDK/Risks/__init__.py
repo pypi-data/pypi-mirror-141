77ee565e5d357a91f1b45f50f89c5c84dcc8bbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:13 +0000

    Add package ZEVIT-AIH-SDK/0.5.0/ZEVIT_AIH_SDK-0.5.0.tar.gz

---
+++ packages/ZEVIT-AIH-SDK/ZEVIT_AIH_SDK-0.5.0.tar.gz/ZEVIT_AIH_SDK-0.5.0/AIH_SDK/Risks/__init__.py
@@ -0,0 +1,9 @@
+from .Mitigation import Mitigation
+from .RiskAssessmentResult import RiskAssessmentResult
+from .Risk import Risk
+
+__all__ = [
+    'Mitigation',
+    'Risk',
+    'RiskAssessmentResult',
+]
