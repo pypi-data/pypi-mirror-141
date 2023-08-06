f2764eda6276c14ba2479a2e2da6f83395fa46d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:43 +0000

    Add package create-pyproj/0.2.9/create-pyproj-0.2.9.tar.gz

---
+++ packages/create-pyproj/create-pyproj-0.2.9.tar.gz/create-pyproj-0.2.9/test/test_main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+# import pytest
+from src.create_pyproj.createfile import copyTemplates, createFiles
+
+DIR = Path(__file__).parent.absolute()
+
+projectname = 'z-test-proj'
+cli = False
+python_version = '3.8'
+
+TEST_DIR = DIR.parent / projectname
+PROJ_DIR = TEST_DIR / 'src' / projectname.replace('-', '_')
+
+
+def test_copy_templates():
+    assert not TEST_DIR.exists(), f"{TEST_DIR} should not exist"
+    copyTemplates(projectname, cli)
+    assert (PROJ_DIR / '_config' / 'logging.yaml').exists(), 'logging.yaml not copied to '
+
+
+def test_create_files():
+    createFiles(projectname, cli, python_version)
+    assert (TEST_DIR / '.gitlab-ci.yml').exists(), f'gitlab-ci.yml not copied to {PROJ_DIR}'
