9a1f18f0090066f15f26cc01c8aea8a497b78241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:49 +0000

    Add package sackeylib/0.0.11/sackeylib-0.0.11.tar.gz

---
+++ packages/sackeylib/sackeylib-0.0.11.tar.gz/sackeylib-0.0.11/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='sackeylib',
+    version='0.0.11',
+    description='easy to use Python, recode str_time*,added readTxt',
+    author='Yuanshaohua',
+    zip_safe=False,
+    author_email='739115136@qq.com',
+    url='https://github.com/Sackey-yuan/nvm',
+    packages=('sackeylib',)
+)
