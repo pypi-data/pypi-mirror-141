d07e9cedf6aed3809e2c58242db2ad107514a80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:36 +0000

    Add package kolibri-ml/1.0.59/kolibri_ml-1.0.59-py3-none-any.whl

---
+++ packages/kolibri-ml/kolibri_ml-1.0.59-py3-none-any.whl/kolibri/__init__.py
@@ -0,0 +1,9 @@
+from kolibri.config import register_all
+from kolibri.config import ModelConfig
+from kolibri.model_loader import ModelLoader
+from kolibri.model_trainer import ModelTrainer
+from kolibri.task.text.intents import domains
+from kolibri.backend.models import get_available_models
+from kolibri.version import __version__
+
+register_all()
