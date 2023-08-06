08ec2d9d6233832367209813a174c3ee21a8a6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:50 +0000

    Add package aws-managers/0.17/aws-managers-0.17.tar.gz

---
+++ packages/aws-managers/aws-managers-0.17.tar.gz/aws-managers-0.17/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='aws-managers',
+    packages=find_packages(),
+    version='0.017',
+    license='MIT',
+    description='Wrappers around boto3 and sagemaker',
+    author='Vahndi Minah',
+    url='https://github.com/vahndi/aws-managers',
+    keywords=['boto3', 'sagemaker'],
+    install_requires=[
+        'awswrangler',
+        'botocore',
+        'boto3',
+        'jinja2',
+        'numpy',
+        'pandas',
+        'sagemaker',
+        'tqdm'
+    ],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+    ]
+)
