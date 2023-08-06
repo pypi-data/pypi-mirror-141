57007298369afe81bf633cd7c746de5d49e505b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:54 +0000

    Add package idem/18.1.3/idem-18.1.3.tar.gz

---
+++ packages/idem/idem-18.1.3.tar.gz/idem-18.1.3/idem/idem/get.py
@@ -0,0 +1,24 @@
+"""
+This file contains routines to get sls files from references
+"""
+
+
+async def ref(hub, name, sls):
+    """
+    Cache the given file from the named reference point
+    """
+    if hub.idem.RUNS[name]["params_processing"]:
+        sources = hub.idem.RUNS[name]["param_sources"]
+    else:
+        sources = hub.idem.RUNS[name]["sls_sources"]
+
+    for source in sources:
+        proto = source[: source.index(":")]
+        path = sls.replace(".", "/")
+        locs = [f"{path}.sls", f"{path}/init.sls"]
+        for loc in locs:
+            cfn = await hub.pop.ref.last(f"idem.sls.{proto}.cache")(
+                hub.idem.RUNS[name]["cache_dir"], source, loc
+            )
+            if cfn:
+                return cfn
