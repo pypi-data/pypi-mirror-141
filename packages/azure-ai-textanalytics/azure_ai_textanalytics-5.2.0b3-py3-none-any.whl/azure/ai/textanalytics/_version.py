901aee34ccef2564b67bae0c79a0c8f5a91faf45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:51 +0000

    Add package azure-ai-textanalytics/5.2.0b3/azure_ai_textanalytics-5.2.0b3-py3-none-any.whl

---
+++ packages/azure-ai-textanalytics/azure_ai_textanalytics-5.2.0b3-py3-none-any.whl/azure/ai/textanalytics/_version.py
@@ -0,0 +1,7 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+VERSION = "5.2.0b3"
+DEFAULT_API_VERSION = "v3.2-preview.2"
