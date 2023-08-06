f7f76244ca53f2277e33cd334996943d69d02456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:42 +0000

    Add package owlshell/0.0.4/owlshell-0.0.4.tar.gz

---
+++ packages/owlshell/owlshell-0.0.4.tar.gz/owlshell-0.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name="owlshell",
+    version="0.0.4",
+    author="Sovenok-Hacker",
+    author_email="artemka.hvsotov@yandex.ru",
+    description="A tool to use the OS functions and SEE AN ASCII ART OWL!",
+    url="https://github.com/Sovenok-Hacker/owlshell",
+    python_requires=">=3.6"
+)
