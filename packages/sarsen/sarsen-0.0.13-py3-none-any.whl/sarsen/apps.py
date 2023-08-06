7616c340a8af16581de1cbe595c951832999e4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:30 +0000

    Add package sarsen/0.0.13/sarsen-0.0.13-py3-none-any.whl

---
+++ packages/sarsen/sarsen-0.0.13-py3-none-any.whl/sarsen/apps.py
@@ -0,0 +1,134 @@
+import typing as T
+
+import numpy as np
+import xarray as xr
+import xarray_sentinel
+
+from . import geocoding, orbit, scene
+
+
+def mosaic_slc_iw(image: xr.DataArray, crop: int = 90) -> xr.DataArray:
+    bursts = []
+    for i in range(image.attrs["number_of_bursts"]):
+        burst = xarray_sentinel.crop_burst_dataset(image, burst_index=i)
+        bursts.append(burst.isel(azimuth_time=slice(crop, -crop)))
+    return xr.concat(bursts, dim="azimuth_time")  # type: ignore
+
+
+def simulate_acquisition(
+    position_ecef: xr.DataArray,
+    dem_raster: xr.DataArray,
+) -> xr.Dataset:
+
+    print("pre-process DEM")
+
+    dem_ecef = scene.convert_to_dem_ecef(dem_raster)
+
+    print("interpolate orbit")
+
+    orbit_interpolator = orbit.OrbitPolyfitIterpolator.from_position(position_ecef)
+    position_ecef = orbit_interpolator.position()
+    velocity_ecef = orbit_interpolator.velocity()
+
+    print("geocode")
+
+    acquisition = geocoding.backward_geocode(dem_ecef, position_ecef, velocity_ecef)
+
+    return acquisition
+
+
+def interpolate_measurement(
+    image: xr.DataArray,
+    multilook: T.Optional[T.Tuple[int, int]] = None,
+    interp_method: str = "nearest",
+    **interp_kwargs: T.Any,
+) -> xr.DataArray:
+    if multilook:
+        image = image.rolling(
+            azimuth_time=multilook[0], slant_range_time=multilook[1]
+        ).mean()
+
+    geocoded = image.interp(method=interp_method, **interp_kwargs)
+
+    return geocoded
+
+
+def backward_geocode_sentinel1(
+    product_urlpath: str,
+    measurement_group: str,
+    dem_urlpath: str,
+    orbit_group: T.Optional[str] = None,
+    calibration_group: T.Optional[str] = None,
+    output_urlpath: str = "GRD.tif",
+    correct_radiometry: bool = False,
+    interp_method: str = "nearest",
+    multilook: T.Optional[T.Tuple[int, int]] = None,
+    **kwargs: T.Any,
+) -> None:
+    orbit_group = orbit_group or f"{measurement_group}/orbit"
+    calibration_group = calibration_group or f"{measurement_group}/calibration"
+
+    print("open data")
+
+    measurement_ds = xr.open_dataset(product_urlpath, engine="sentinel-1", group=measurement_group, **kwargs)  # type: ignore
+    measurement = measurement_ds.measurement
+
+    dem_raster = scene.open_dem_raster(dem_urlpath)
+
+    orbit_ecef = xr.open_dataset(product_urlpath, engine="sentinel-1", group=orbit_group, **kwargs)  # type: ignore
+    position_ecef = orbit_ecef.position
+    calibration = xr.open_dataset(product_urlpath, engine="sentinel-1", group=calibration_group, **kwargs)  # type: ignore
+    beta_nought_lut = calibration.beta_naught
+
+    print("simulate acquisition")
+
+    acquisition = simulate_acquisition(position_ecef, dem_raster)
+
+    print("calibrate radiometry")
+
+    beta_nought = xarray_sentinel.calibrate_intensity(measurement, beta_nought_lut)
+
+    print("interpolate image")
+
+    if measurement_ds.attrs["sar:product_type"] == "GRD":
+        coordinate_conversion = xr.open_dataset(
+            product_urlpath,
+            engine="sentinel-1",
+            group=f"{measurement_group}/coordinate_conversion",
+            **kwargs,
+        )  # type: ignore
+        ground_range = xarray_sentinel.slant_range_time_to_ground_range(
+            acquisition.azimuth_time,
+            acquisition.aslant_range_time,
+            coordinate_conversion,
+        )
+        interp_kwargs = {"ground_range": ground_range}
+    elif measurement_ds.attrs["sar:product_type"] == "SLC":
+        interp_kwargs = {"slant_range_time": acquisition.slant_range_time}
+        if measurement_ds.attrs["sar:instrument_mode"] == "IW":
+            beta_nought = mosaic_slc_iw(beta_nought)
+    else:
+        raise ValueError(
+            f"unsupported sar:product_type {measurement_ds.attrs['sar:product_type']}"
+        )
+
+    geocoded = interpolate_measurement(
+        beta_nought,
+        multilook=multilook,
+        azimuth_time=acquisition.azimuth_time,
+        method=interp_method,
+        **interp_kwargs,
+    )
+
+    print("save output")
+
+    geocoded.rio.set_crs(dem_raster.rio.crs)
+    geocoded.rio.to_raster(
+        output_urlpath,
+        dtype=np.float32,
+        tiled=True,
+        blockxsize=512,
+        blockysize=512,
+        compress="ZSTD",
+        num_threads="ALL_CPUS",
+    )
---
+++ packages/sarsen/sarsen-0.0.13-py3-none-any.whl/sarsen/scene.py
@@ -0,0 +1,66 @@
+import typing as T
+
+import numpy as np
+import xarray as xr
+from rasterio import warp
+
+# Earth-Centered, Earth-Fixed Coordinate Reference System used to express satellite orbit
+# https://en.wikipedia.org/wiki/Earth-centered,_Earth-fixed_coordinate_system
+# https://spatialreference.org/ref/epsg/wgs-84-2/
+ECEF_CRS = "EPSG:4978"
+
+
+def open_dem_raster(dem_urlpath: str, **kwargs: T.Any) -> xr.DataArray:
+    dem_raster = xr.open_dataarray(dem_urlpath, engine="rasterio", **kwargs)  # type: ignore
+    if dem_raster.y.diff("y").values[0] < 0:
+        dem_raster = dem_raster.isel(y=slice(None, None, -1))
+    return dem_raster.squeeze(drop=True)  # type: ignore
+
+
+def convert_to_dem_3d(dem_raster: xr.DataArray, dim: str = "axis") -> xr.DataArray:
+    _, dem_raster_x = xr.broadcast(dem_raster, dem_raster.x)  # type: ignore
+    dem_3d = xr.concat(
+        [dem_raster_x, dem_raster.y, dem_raster], dim=dim, coords="minimal"
+    ).chunk({dim: None})
+    dem_3d = dem_3d.assign_coords({dim: [0, 1, 2]})  # type: ignore
+    return dem_3d.rename("dem_3d")
+
+
+def transform_dem_3d(
+    dem_3d: xr.DataArray,
+    target_crs: str = ECEF_CRS,
+    source_crs: T.Optional[str] = None,
+    dim: str = "axis",
+) -> xr.DataArray:
+    if source_crs is None:
+        source_crs = dem_3d.rio.crs
+    x, y, z = warp.transform(
+        source_crs,
+        target_crs,
+        dem_3d.sel({dim: 0}).values.flat,
+        dem_3d.sel({dim: 1}).values.flat,
+        dem_3d.sel({dim: 2}).values.flat,
+    )
+    dem_3d_crs: xr.DataArray = xr.zeros_like(dem_3d)
+    shape = dem_3d_crs.loc[{dim: 0}].shape
+    dem_3d_crs.loc[{dim: 0}] = np.reshape(x, shape)
+    dem_3d_crs.loc[{dim: 1}] = np.reshape(y, shape)
+    dem_3d_crs.loc[{dim: 2}] = np.reshape(z, shape)
+    return dem_3d_crs
+
+
+def convert_to_dem_ecef(dem_raster: xr.DataArray) -> xr.DataArray:
+    dem_3d = convert_to_dem_3d(dem_raster)
+    return transform_dem_3d(dem_3d, target_crs=ECEF_CRS)
+
+
+def compute_diff_normal(
+    dem_ecef: xr.DataArray,
+    spatial_dims: T.Tuple[str, str] = ("x", "y"),
+    axis_dim: str = "axis",
+) -> xr.DataArray:
+    x, y = spatial_dims
+    x_diff = dem_ecef.shift({x: -1}) - dem_ecef.shift({x: 1})
+    y_diff = dem_ecef.shift({y: -1}) - dem_ecef.shift({y: 1})
+    up = xr.cross(x_diff, y_diff, dim=axis_dim)
+    return up / xr.dot(up, up, dims=axis_dim) ** 0.5  # type: ignore
