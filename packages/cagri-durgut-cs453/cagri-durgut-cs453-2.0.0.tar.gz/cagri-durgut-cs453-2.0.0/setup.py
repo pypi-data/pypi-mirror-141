996ac463cf1781043fc66f375c8a2afcdbd5787e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:51 +0000

    Add package cagri-durgut-cs453/2.0.0/cagri-durgut-cs453-2.0.0.tar.gz

---
+++ packages/cagri-durgut-cs453/cagri-durgut-cs453-2.0.0.tar.gz/cagri-durgut-cs453-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="cagri-durgut-cs453",
+    author="Çağrı Durgut",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
