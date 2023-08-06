d5fb25b75377b5e4c4e5420eddddab799f3f377b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:38 +0000

    Add package fABBA/0.9.1/fABBA-0.9.1.tar.gz

---
+++ packages/fABBA/fABBA-0.9.1.tar.gz/fABBA-0.9.1/fABBA/__init__.py
@@ -0,0 +1,21 @@
+try:
+    import numpy, scipy
+    if scipy.__version__ != '1.8.0':
+        from .separate.aggregation_cm import *
+    else:
+        from .separate.aggregation_c import *
+        
+    if numpy.__version__ >= '1.22.0':
+        from .extmod.chainApproximation_cm import *
+    else:
+        from .separate.aggregation_c import *
+        from .extmod.chainApproximation_c import *
+        
+except (ModuleNotFoundError, ValueError):
+    from .chainApproximation import *
+    from .separate.aggregation import *
+    warnings.warn("This installation is not using Cython.")
+
+from .load_datasets import load_images
+from .symbolic_representation import *
+from .digitization import *
