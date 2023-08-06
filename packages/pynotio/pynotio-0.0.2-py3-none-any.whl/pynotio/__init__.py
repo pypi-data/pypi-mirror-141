d63bca8c190a8428239f07f4a452c2dfac9f0252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:58 +0000

    Add package pynotio/0.0.2/pynotio-0.0.2-py3-none-any.whl

---
+++ packages/pynotio/pynotio-0.0.2-py3-none-any.whl/pynotio/__init__.py
@@ -0,0 +1,9 @@
+"""
+Pynotio is designed to be a really simple Python wrapper around the official Notion API.
+
+Ruslan Abkadirov [https://blog.ruslanabk.ru/]
+rusabk@yandex.ru
+# License: Apache License 2.0
+"""
+__author__ = 'Ruslan Abkadirov'
+__version__ = "0.0.2"
---
+++ packages/pynotio/pynotio-0.0.2-py3-none-any.whl/pynotio/pagelist.py
@@ -0,0 +1,7 @@
+from Notion.page import Page
+
+class Pagelist:
+    def __init__(self, pagelist):
+        self.data = []
+
+    
