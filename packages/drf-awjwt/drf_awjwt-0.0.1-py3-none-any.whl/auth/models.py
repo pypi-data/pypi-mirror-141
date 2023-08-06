43d5955a795519f7754d5c7d5e52d70f09ded03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:35 +0000

    Add package drf-awjwt/0.0.1/drf_awjwt-0.0.1-py3-none-any.whl

---
+++ packages/drf-awjwt/drf_awjwt-0.0.1-py3-none-any.whl/auth/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+import jwt
+
+from datetime import datetime, timedelta
+from django.conf import settings
+
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+from django.utils.translation import gettext_lazy as _
+
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """Create and return a `User` with an email, username and password."""
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser):
+    username    = None
+
+    first_name = models.CharField(max_length=255,null=True,blank=True)
+    last_name = models.CharField(max_length=255,null=True,blank=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email   
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    def __str__(self):
+        return self.email
+
+
+    def get_full_name(self):
+        return str(self.first_name + self.last_name)
+
+    def get_short_name(self):
+        return self.first_name
---
+++ packages/drf-awjwt/drf_awjwt-0.0.1-py3-none-any.whl/auth/serializers.py
@@ -0,0 +1,100 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model,authenticate
+from django.contrib.auth.password_validation import validate_password
+
+
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('id','first_name','last_name','email')
+        extra_kwargs = {'email': {'read_only': True}}
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = UserModel
+        fields = ('email','password')
+
+    def validate(self,data):
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+        
+        if email is None:
+            raise serializers.ValidationError(
+                'An email address is required to log in.'
+            )
+
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        user = authenticate(email=email, password=password)
+        
+
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+
+        return user
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = UserModel
+        fields = ('first_name','last_name','email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True,required=True)
+    password = serializers.CharField(write_only=True,required=True)
+    password2 = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = UserModel
+        fields = ('old_password', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password":"Password fields didn't match."})
+        return attrs
+    
+    def validate_password(self,value):
+        validate_password(value)
+        return value
+    
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return value
+    
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
