038336f3ddeb5dcbae9f1cdc56826ce1351ff050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:49 +0000

    Add package raumdeuter/0.0.1/raumdeuter-0.0.1.tar.gz

---
+++ packages/raumdeuter/raumdeuter-0.0.1.tar.gz/raumdeuter-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+  
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+  
+long_description = 'Raumdeuter runs your sequence of bash commands defined custom json files. \
+    This files can be created on the online platform airlocks.xyz!'
+  
+setup(
+        name ='raumdeuter',
+        version ='0.0.1',
+        author ='Roman Botnari',
+        author_email ='romanbotnari@me.com',
+        url ='',
+        description ='.',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='Apache License, Version 2.0',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'raumd = main.raumd:main'
+            ]
+        },
+        classifiers =[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+        keywords ='',
+        install_requires = 'requirements.txt',
+        zip_safe = False,
+        package_data={
+            "main": ["raumd.conf","sequence.json"],
+        }
+)
