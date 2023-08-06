3acac496e8919f6a991cfb3f17ec7b5d7a7970cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:33 +0000

    Add package graph-theory/2022.3.9.54615/graph-theory-2022.3.9.54615.tar.gz

---
+++ packages/graph-theory/graph-theory-2022.3.9.54615.tar.gz/graph-theory-2022.3.9.54615/setup.py
@@ -0,0 +1,59 @@
+"""
+graph-theory
+"""
+build_tag = "5f1ab7d5047312cd2c4eda6ce6a98d123a90b9a81ea484eb2741e3a29891144a"
+from setuptools import setup
+from pathlib import Path
+
+
+folder = Path(__file__).parent
+file = "README.md"
+readme = folder / file
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+keywords = list({
+    'complex-networks', 'discrete mathematics', 'graph', 'Graph Theory', 'graph-algorithms', 'graph-analysis',
+    'graph-generation', 'graph-theory', 'graph-visualization', 'graphs', 'math', 'Mathematics', 'maths',
+    'minimum-spanning-trees', 'network', 'Networks', 'optimization', 'python', 'shortest-path', 'tsp', 'tsp-solver',
+    'assignment problem', 'flow-problem', 'hash', 'graph-hash', 'random graph', 'search', 'cycle', 'path',
+    'component', 'components', 'adjacency', 'matrix', 'all pairs shortest path', 'finite state machine', 'fsm',
+    'traffic-jam', 'traffic-jam-solver', 'solver', 'hill-climbing', 'simple', 'simple-path', 'critical', 'path',
+    'method', 'critical-path', 'minimize', 'minimise', 'optimize', 'optimise', 'merkle', 'tree', 'merkle-tree',
+    'hash-tree'
+})
+
+keywords.sort(key=lambda x: x.lower())
+
+
+setup(
+    name="graph-theory",
+    version="2022.3.9.54615",
+    url="https://github.com/root-11/graph-theory",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A graph library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=keywords,
+    packages=["graph"],
+    include_package_data=True,
+    data_files=[(".", ["LICENSE", "README.md"])],
+    platforms="any",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
+
+
