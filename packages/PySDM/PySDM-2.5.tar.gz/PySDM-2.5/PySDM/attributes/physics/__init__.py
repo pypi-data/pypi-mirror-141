1f1b17ef693e1f5e15182a93d9f79a6faadf9bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:32 +0000

    Add package PySDM/2.5/PySDM-2.5.tar.gz

---
+++ packages/PySDM/PySDM-2.5.tar.gz/PySDM-2.5/PySDM/attributes/physics/__init__.py
@@ -0,0 +1,13 @@
+"""
+attributes carrying information on particle physical properties
+"""
+from .critical_supersaturation import CriticalSupersaturation
+from .critical_volume import CriticalVolume
+from .dry_radius import DryRadius
+from .dry_volume import DryVolume
+from .heat import Heat
+from .multiplicities import Multiplicities
+from .radius import Radius
+from .temperature import Temperature
+from .terminal_velocity import TerminalVelocity
+from .volume import Volume
