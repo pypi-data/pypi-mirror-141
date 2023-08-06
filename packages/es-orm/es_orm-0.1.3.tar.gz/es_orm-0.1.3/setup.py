9d0ceb867a04c8063ceb374462285c2a461e697b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:43 +0000

    Add package es-orm/0.1.3/es_orm-0.1.3.tar.gz

---
+++ packages/es-orm/es_orm-0.1.3.tar.gz/es_orm-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['es_orm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['elasticsearch==7.10.0']
+
+setup_kwargs = {
+    'name': 'es-orm',
+    'version': '0.1.3',
+    'description': 'easy es helper',
+    'long_description': None,
+    'author': 'iulmt',
+    'author_email': '1817556010@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
