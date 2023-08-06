e932c0289e66f0467f052f995c16f84cf0fb44c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:48 +0000

    Add package gMarkdownViewer/0.0.2/gMarkdownViewer-0.0.2-py3-none-any.whl

---
+++ packages/gMarkdownViewer/gMarkdownViewer-0.0.2-py3-none-any.whl/MarkdownViewer/__init__.py
@@ -0,0 +1,10 @@
+"""
+Author: Ben Knisley [benknisley@gmail.com]
+Created: 19 February
+"""
+
+## Allow MarkdownViewer application be ran from main function
+from .MarkdownViewer import main
+
+## Allow MarkdownViewWidget to be used anywhere
+from .MarkdownViewer import MarkdownViewWidget
