e391e9821cfb2c33b699b92b6b0659c7f9ad09c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:42 +0000

    Add package Shpresa/1.0.2/Shpresa-1.0.2.tar.gz

---
+++ packages/Shpresa/Shpresa-1.0.2.tar.gz/Shpresa-1.0.2/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup,find_packages
+
+setup(name='Shpresa',
+      version='1.0.2',
+      description='Python Distribution Utilities',
+      author='SHPRESA TUP',
+      author_email='SHPRESA@python.net',
+      packages=find_packages(),
+      url='https://www.python.org/sigs/distutils-sig/',
+      include_package_data=True,
+     )
