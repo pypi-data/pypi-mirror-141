ee9c8d302d2662729eb06918b6172ec756587456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:50 +0000

    Add package reseval/0.0.1/reseval-0.0.1-py3-none-any.whl

---
+++ packages/reseval/reseval-0.0.1-py3-none-any.whl/reseval/assets/client/src/components/Video.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+import '../css/components.css';
+
+export default function Video({ src, reference, onEnded }) {
+    /* Create an HTML video element */
+    // Reload video when src changes
+    useEffect(() => {
+        if (reference.current) {
+            reference.current.pause();
+            reference.current.load();
+        }
+    }, [reference, src]);
+
+    // Render
+    return (
+        <video
+            controls
+            ref={reference}
+            style={{ minWidth: '150px', maxWidth: '750px' }}
+            onEnded={() => typeof (onEnded) !== 'undefined' && onEnded()}
+        >
+            <source src={src} type='video/mp4'/>
+        </video>
+    );
+};
