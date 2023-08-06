f666a70c2f4de340f68f9ab97f928677f4f072bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:02 +0000

    Add package SeisLog/0.1.2/SeisLog-0.1.2-py3-none-any.whl

---
+++ packages/SeisLog/SeisLog-0.1.2-py3-none-any.whl/SeisLog/__init__.py
@@ -0,0 +1,8 @@
+from .SeisLog import prep_data
+from .SeisLog import trace_extraction
+from .SeisLog import plot_extractions
+from .SeisLog import depth_conversion
+
+__version__ = "0.1.2"
+__author__ = 'Samara Omar'
+__credits__ = ''
