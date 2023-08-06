9c2f216de3e1b067774edc1ea5ca60398b88bbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:17 +0000

    Add package dahua/1.0.0/dahua-1.0.0.tar.gz

---
+++ packages/dahua/dahua-1.0.0.tar.gz/dahua-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dahua',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/dongyi0412/dahua.git',
+    license='MIT',
+    author='dongyi',
+    author_email='1193272767@qq.com',
+    description='大华的记录'
+)
