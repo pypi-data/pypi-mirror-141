23b02310d1177f447502bc338b20ed87647f0bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:12 +0000

    Add package embedding-encoder/0.0.4/embedding_encoder-0.0.4-py3-none-any.whl

---
+++ packages/embedding-encoder/embedding_encoder-0.0.4-py3-none-any.whl/embedding_encoder/utils/__init__.py
@@ -0,0 +1,4 @@
+from embedding_encoder.utils.compose import ColumnTransformerWithNames
+from embedding_encoder.utils.custom_ordinal import OrdinalEncoderStart1
+
+__all__ = ["ColumnTransformerWithNames", "OrdinalEncoderStart1"]
