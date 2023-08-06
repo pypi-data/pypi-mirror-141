e1f98ed5bbc80a79fa6eb3034a384f1c8bc3d7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:48 +0000

    Add package jsonism/1.0.8/jsonism-1.0.8-py3-none-any.whl

---
+++ packages/jsonism/jsonism-1.0.8-py3-none-any.whl/jsonism/checker.py
@@ -0,0 +1,71 @@
+import logging
+
+
+def validate(input: any, schema: any):
+    """ Validate a top-level element. JSON supports lists, dicts and raw
+    elements at the top level.
+
+    :param input: JSON to be validated, as Python list, dict or base type
+    :param schema: JSON schema, as Python list, dict or base type
+    :return: True or False
+    """
+    if schema in [int, str, bool, float]:
+        return _validate_generic(input, schema)
+    if type(schema) == dict:
+        return _validate_dict(input, schema)
+    elif type(schema) == list:
+        return _validate_list(input, schema)
+    else:
+        raise NotImplementedError("Unknown type found in schema")
+
+
+def _validate_generic(input, schema):
+    """ Validate a base type.
+
+    :param input: A base value to be validated
+    :param schema: A base type to validate the input against
+    :return: True or False
+    """
+    if type(input) != schema:
+        return False
+    else:
+        return True
+
+
+def _validate_dict(input, schema):
+    """ Validate a dictionary.
+
+    :param input: The dictionary to be validated
+    :param schema: The schema against which to validate, as a dictionary
+    :return: True or False
+    """
+    for key, value in schema.items():
+        if type(value) in [dict, list]:
+            if type(input.get(key)) == type(value):
+                if not validate(input.get(key), value):
+                    return False
+            else:
+                logging.warning(f"Schema field '{key}': Expected {str(dict)}, got {str(type(input.get(key)))}")
+                return False
+        elif key in input:
+            if type(input.get(key)) != value:
+                logging.warning(f"Schema field '{key}': Expected {str(value)}, got {str(type(input.get(key)))}")
+                return False
+        else:
+            logging.warning(f"Schema field '{key}' not found in input.")
+            return False
+    return True
+
+
+def _validate_list(input, schema):
+    """ Validate a list.
+
+    :param input: The list to be validated
+    :param schema: The schema against which to validate, as a list
+    :return: True or False
+    """
+    list_item_type = schema[0]
+    for item in input:
+        if not validate(item, list_item_type):
+            return False
+    return True
---
+++ packages/jsonism/jsonism-1.0.8-py3-none-any.whl/tests/test_check.py
@@ -0,0 +1,240 @@
+import unittest
+
+from jsonism.checker import validate
+
+
+class TestCheck(unittest.TestCase):
+
+    def test_basic_dictionary(self):
+        schema = {
+            "Bob": str,
+            "Lucy": int,
+            "Bert": bool
+        }
+        input = {
+            "Bob": "Is Bob",
+            "Lucy": 13,
+            "Bert": True
+        }
+        self.assertTrue(validate(input, schema))
+
+    def test_unsupported_type(self):
+        schema = "JUST A STRING"
+        input = {
+            "Bob": "Is Bob",
+        }
+        with self.assertRaises(NotImplementedError):
+            validate(input, schema)
+
+    def test_nested_dictionaries(self):
+        schema = {
+            "Bob": str,
+            "Lucy": int,
+            "Bert": {
+                "age": int,
+                "size": str
+            }
+        }
+        input = {
+            "Bob": "Is Bob",
+            "Lucy": 13,
+            "Bert": {
+                "age": 13,
+                "size": "Medium"
+            }
+        }
+        self.assertTrue(validate(input, schema))
+
+    def test_nested_dictionaries_with_fault(self):
+        schema = {
+            "Bob": str,
+            "Lucy": int,
+            "Bert": {
+                "age": int,
+                "size": str
+            }
+        }
+        input = {
+            "Bob": "Is Bob",
+            "Lucy": 13,
+            "Bert": {
+                "age": "12",
+                "size": "Medium"
+            }
+        }
+        self.assertFalse(validate(input, schema))
+
+    def test_lists(self):
+        input = ["Bob", "Alice", "John"]
+        schema = [str]
+        self.assertTrue(validate(input, schema))
+
+    def test_lists_invalid_value(self):
+        input = ["Bob", "Alice", 12]
+        schema = [str]
+        self.assertFalse(validate(input, schema))
+
+    def test_nested_lists(self):
+        input = {
+            "item": ["Bob", "Bob", "Bob"]
+        }
+        schema = {
+            "item": [str]
+        }
+        self.assertTrue(validate(input, schema))
+
+    def test_nested_lists_invalid_value(self):
+        input = {
+            "item": ["Bob", "Frank", True]
+        }
+        schema = {
+            "item": [str]
+        }
+        self.assertFalse(validate(input, schema))
+
+    def test_multiple_nesting(self):
+        input = {"David": [{"Bob": 23, "Jane": True}]}
+        schema = {"David": [{"Bob": int, "Jane": bool}]}
+        self.assertTrue(validate(input, schema))
+
+    def test_multiple_nesting_multiple_repeats(self):
+        input = {"Usernames": [{"username": "Bob", "age": 23}, {"username": "Bill", "age": 98}]}
+        schema = {"Usernames": [{"username": str, "age": int}]}
+        self.assertTrue(validate(input, schema))
+
+    def test_multiple_nesting_multiple_repeats_with_fault(self):
+        input = {"David": [{"Bob": 23, "Jane": True}, {"Bob": 19, "Jane": 23}]}
+        schema = {"David": [{"Bob": int, "Jane": bool}]}
+        self.assertFalse(validate(input, schema))
+
+    def test_string(self):
+        input = "David"
+        schema = str
+        self.assertTrue(validate(input, schema))
+
+    def test_int(self):
+        input = 14
+        schema = int
+        self.assertTrue(validate(input, schema))
+
+    def test_float(self):
+        input = 14.3
+        schema = float
+        self.assertTrue(validate(input, schema))
+
+    def test_missing_element(self):
+        schema = {
+            "Bob": str,
+            "Lucy": int,
+            "Bert": bool
+        }
+        input = {
+            "Bob": "Is Bob",
+            "Lucy": 13,
+        }
+        self.assertFalse(validate(input, schema))
+
+    def test_extra_element(self):
+        schema = {
+            "Bob": str,
+            "Lucy": int,
+            "Bert": bool
+        }
+        input = {
+            "Bob": "Is Bob",
+            "Lucy": 13,
+            "Bert": True,
+            "Colin": 21
+        }
+        self.assertTrue(validate(input, schema))
+
+    def test_longer_json(self):
+        _testobject = {
+            "family": {
+                "available": True,
+                "state": "passed",
+                "members": [
+                    {
+                        "state": "ready",
+                        "status": "alive",
+                        "id": 1,
+                        "permission": True,
+                        "colour": "None",
+                        "uuid": "221edec0-e5b8-4a34-8eb6-87f97dab04b6",
+                        "year": 1990,
+                        "attributes": [
+                            {
+                                "name": "rex",
+                                "value": "no",
+                            },
+                            {
+                                "name": "banded",
+                                "value": "no"
+                            }
+                        ],
+                        "sex": "MALE"
+                    }
+                ]
+            }
+        }
+        _testschema = {
+            "family": {
+                "available": bool,
+                "state": str,
+                "members": [
+                    {
+                        "state": str,
+                        "status": str,
+                        "id": int,
+                        "permission": bool,
+                        "colour": str,
+                        "uuid": str,
+                        "year": int,
+                        "attributes": [
+                            {
+                                "name": str,
+                                "value": str,
+                            }
+                        ],
+                        "sex": str
+                    }
+                ]
+            }
+        }
+        self.assertTrue(validate(_testobject, _testschema))
+
+    def test_repeated_dict_list(self):
+        _testobject = {
+            "family": {
+                "members": [
+                    {
+                        "attributes": [
+                            {
+                                "name": "rex",
+                                "value": "no",
+                            }
+                        ],
+                        "sex": "MALE"
+                    }
+                ]
+            }
+        }
+        _testschema = {
+            "family": {
+                "members": [
+                    {
+                        "attributes": [
+                            {
+                                "name": str,
+                                "value": str,
+                            }
+                        ],
+                        "sex": str
+                    }
+                ]
+            }
+        }
+        self.assertTrue(validate(_testobject, _testschema))
+
+if __name__ == '__main__':
+    unittest.main()
