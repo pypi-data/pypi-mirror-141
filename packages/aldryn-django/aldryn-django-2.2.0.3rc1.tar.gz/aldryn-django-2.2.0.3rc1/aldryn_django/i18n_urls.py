794036856142a7c440eb4a0b8a3943d328159d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:29 +0000

    Add package aldryn-django/2.2.0.3rc1/aldryn-django-2.2.0.3rc1.tar.gz

---
+++ packages/aldryn-django/aldryn-django-2.2.0.3rc1.tar.gz/aldryn-django-2.2.0.3rc1/aldryn_django/i18n_urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.urls import re_path
+
+
+if settings.ALDRYN_DJANGO_ENABLE_GIS:
+    from django.contrib.gis import admin
+else:
+    from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+]
