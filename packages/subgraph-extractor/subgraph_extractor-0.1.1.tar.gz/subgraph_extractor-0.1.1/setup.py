e2b5d0fa704069807a1857a6b8c5fcc29d885d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:14 +0000

    Add package subgraph-extractor/0.1.1/subgraph_extractor-0.1.1.tar.gz

---
+++ packages/subgraph-extractor/subgraph_extractor-0.1.1.tar.gz/subgraph_extractor-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name="subgraph_extractor",
+    version="0.1.1",
+    description="Pull data from graph-node databases into parquet files",
+    url="http://github.com/cardstack/subgraph-extractor",
+    author="Ian Calvert",
+    author_email="ian.calvert@cardstack.com",
+    license="None",
+    install_requires=[
+        "click",
+        "pandas",
+        "pyyaml",
+        "sqlalchemy",
+        "psycopg2-binary",
+        "pyarrow",
+        "simple-term-menu",
+        "tqdm",
+        "cloudpathlib[all]",
+        "deepdiff",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "isort",
+            "pytest-pep8",
+            "pytest-cov",
+            "pytest-postgresql",
+            "hypothesis",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "subgraph_extractor=subgraph_extractor.cli:main",
+            "subgraph_config_generator=subgraph_extractor.cli:config_generator",
+        ],
+    },
+    packages=["subgraph_extractor"],
+    zip_safe=False,
+)
