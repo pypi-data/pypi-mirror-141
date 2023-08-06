00f3f806715ecbd87a1e194a35b97515300b0689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:16 +0000

    Add package predicthq/1.13.1/predicthq-1.13.1-py2.py3-none-any.whl

---
+++ packages/predicthq/predicthq-1.13.1-py2.py3-none-any.whl/predicthq/endpoints/v1/events/endpoint.py
@@ -0,0 +1,53 @@
+from predicthq.endpoints.base import UserBaseEndpoint
+from predicthq.endpoints.decorators import accepts, returns
+from .schemas import (
+    SearchParams,
+    EventResultSet,
+    CountResultSet,
+    CalendarParams,
+    CalendarResultSet,
+    ImpactParams,
+    ImpactResultSet,
+)
+
+
+class EventsEndpoint(UserBaseEndpoint):
+    @accepts(SearchParams)
+    @returns(EventResultSet)
+    def search(self, **params):
+        verify_ssl = params.pop("config.verify_ssl", True)
+        return self.client.get(
+            self.build_url("v1", "events"),
+            params=params,
+            verify=verify_ssl,
+        )
+
+    @accepts(SearchParams)
+    @returns(CountResultSet)
+    def count(self, **params):
+        verify_ssl = params.pop("config.verify_ssl", True)
+        return self.client.get(
+            self.build_url("v1", "events/count"),
+            params=params,
+            verify=verify_ssl,
+        )
+
+    @accepts(CalendarParams)
+    @returns(CalendarResultSet)
+    def calendar(self, **params):
+        verify_ssl = params.pop("config.verify_ssl", True)
+        return self.client.get(
+            self.build_url("v1", "events/calendar"),
+            params=params,
+            verify=verify_ssl,
+        )
+
+    @accepts(ImpactParams)
+    @returns(ImpactResultSet)
+    def impact(self, **params):
+        verify_ssl = params.pop("config.verify_ssl", True)
+        return self.client.get(
+            self.build_url("v1", "events/impact"),
+            params=params,
+            verify=verify_ssl,
+        )
