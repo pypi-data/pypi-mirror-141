7805bc02497e0666c1449f97bc4bf5835ad591fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:11 +0000

    Add package HFODetector/0.0.4/HFODetector-0.0.4.tar.gz

---
+++ packages/HFODetector/HFODetector-0.0.4.tar.gz/HFODetector-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HFODetector",
+    version="0.0.4",
+    author="Taylor Chung",
+    author_email="taylorchung@ucla.edu",
+    description="Package for detecing HFOs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={ },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'mne',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pandas'
+    ],
+    include_package_data=True,
+    python_requires=">=3.6",
+)
