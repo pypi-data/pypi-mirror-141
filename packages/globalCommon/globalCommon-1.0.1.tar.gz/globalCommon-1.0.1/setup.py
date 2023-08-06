4152aa500a063b376b758908fbf5dab2d8cdd61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:23 +0000

    Add package globalCommon/1.0.1/globalCommon-1.0.1.tar.gz

---
+++ packages/globalCommon/globalCommon-1.0.1.tar.gz/globalCommon-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='globalCommon',
+    version="1.0.1",
+    packages=["globalCommon"],
+    author='kitsune',
+    author_email='kitsune@gmail.com',
+    description=''
+)
