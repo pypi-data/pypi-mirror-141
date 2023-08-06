29f309e25e8042e82ee5605bb38e4f66d30d7eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:15 +0000

    Add package plantfem/21.10.1.32/plantfem-21.10.1.32.tar.gz

---
+++ packages/plantfem/plantfem-21.10.1.32.tar.gz/plantfem-21.10.1.32/setup.py
@@ -0,0 +1,26 @@
+"""Minimal setup file for tasks project."""
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='plantfem',
+    version='21.10.1.32',
+    license='MIT',
+    description='Module Experiment',
+    author='kazulagi',
+    author_email='hogehoge@gmail.com',
+    url='https://plantfem.org',
+    #packages=find_packages(include=['src','src.*']),
+    packages=[''],
+    include_package_data=True,
+    #package_dir={'': 'src'},
+    install_requires=[
+        'numpy>=1.14.5',
+        'matplotlib>=2.2.0',
+    ],
+    data_files=['soy.json','soyleaf.json','soystem.json','soyroot.json']
+    #entry_points={
+    #    'console_scripts':['plantfem = src.plantfem:plantfem']
+    #}
+)
+
