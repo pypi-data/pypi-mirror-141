488cd3c20fa58a6fc57c30dbbfaf9b1562a54b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package chatushka/0.7.2rc15/chatushka-0.7.2rc15-py3-none-any.whl

---
+++ packages/chatushka/chatushka-0.7.2rc15-py3-none-any.whl/chatushka/bot/matchers/__init__.py
@@ -0,0 +1,13 @@
+from chatushka.bot.matchers.admin import admin_matcher
+from chatushka.bot.matchers.bobuk_jokes import jokes_matcher
+from chatushka.bot.matchers.eight_ball import eight_ball_matcher
+from chatushka.bot.matchers.helpers import helpers_matcher
+from chatushka.bot.matchers.suicide import suicide_matcher
+
+__all__ = (
+    "admin_matcher",
+    "jokes_matcher",
+    "eight_ball_matcher",
+    "helpers_matcher",
+    "suicide_matcher",
+)
