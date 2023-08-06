b2eb4c52aa0fbdbbfaca836a18f413842080d1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:03 +0000

    Add package sqlschm/0.1.0/sqlschm-0.1.0.tar.gz

---
+++ packages/sqlschm/sqlschm-0.1.0.tar.gz/sqlschm-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sqlschm']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['corpus = scripts:main']}
+
+setup_kwargs = {
+    'name': 'sqlschm',
+    'version': '0.1.0',
+    'description': 'A SQLite Schema parser',
+    'long_description': None,
+    'author': 'Victorien Elvinger',
+    'author_email': 'victorien.elvinger@inria.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/coast-team/sqlschm',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/sqlschm/sqlschm-0.1.0.tar.gz/sqlschm-0.1.0/sqlschm/__init__.py
@@ -0,0 +1,5 @@
+# type: ignore
+from .generator import generate_schema
+from .parser import parse_schema
+from .sql import *
+from . import lexer, tok
