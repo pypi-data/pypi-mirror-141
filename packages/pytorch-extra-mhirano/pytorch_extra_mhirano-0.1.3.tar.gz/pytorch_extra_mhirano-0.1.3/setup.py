fd7cae385181ba15ecccedebd2402fa5b8e32b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:34 +0000

    Add package pytorch-extra-mhirano/0.1.3/pytorch_extra_mhirano-0.1.3.tar.gz

---
+++ packages/pytorch-extra-mhirano/pytorch_extra_mhirano-0.1.3.tar.gz/pytorch_extra_mhirano-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytorch_extra_mhirano', 'pytorch_extra_mhirano.nn']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch>=1.9.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pytorch-extra-mhirano',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Masanori HIRANO',
+    'author_email': 'masa.hirano.1996@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
