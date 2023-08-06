509f3e5ec9cd39b26ff254145b27bf1dabf90410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:39 +0000

    Add package robotframework-Authentication/1.0.4/robotframework-Authentication-1.0.4.tar.gz

---
+++ packages/robotframework-Authentication/robotframework-Authentication-1.0.4.tar.gz/robotframework-Authentication-1.0.4/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="robotframework-Authentication",
+    version="1.0.4",
+    description="Permet de se connecter à un portail.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    author="ADSN GROUP",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["Authentication"],
+    include_package_data=True,
+)
