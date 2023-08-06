7922a74b295328da03afe1acd2dd336a9422a2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:04 +0000

    Add package fastrand-fields/0.1.2/fastrand_fields-0.1.2.tar.gz

---
+++ packages/fastrand-fields/fastrand_fields-0.1.2.tar.gz/fastrand_fields-0.1.2/README.md
@@ -0,0 +1,6 @@
+# Faster random fields
+
+Package to randomly generate fields such as funky usernames. 
+
+* Suitable for fast microservices (no I/O operations).
+* No external dependencies 
---
+++ packages/fastrand-fields/fastrand_fields-0.1.2.tar.gz/fastrand_fields-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastrand_fields']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'fastrand-fields',
+    'version': '0.1.2',
+    'description': 'Package to randomly generate fields such as funny usernames. Suitable for fast microservices.',
+    'long_description': None,
+    'author': 'sam',
+    'author_email': 'contact@justsam.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jsam/fastrand_fields',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
