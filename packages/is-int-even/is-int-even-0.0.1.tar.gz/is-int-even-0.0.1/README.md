24138ab8c2d2063ce6373c25acd5eb8b87e321b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:18 +0000

    Add package is-int-even/0.0.1/is-int-even-0.0.1.tar.gz

---
+++ packages/is-int-even/is-int-even-0.0.1.tar.gz/is-int-even-0.0.1/README.md
@@ -0,0 +1,20 @@
+# is-even
+Returns true if the number passed is even, returns false if it is not.
+
+Inspired by [samuelmarina/is-even](https://github.com/samuelmarina/is-even) and is the python version of it :)
+
+## Installing
+```sh
+pip install -U git+https://github.com/sifte/is-even.git
+```
+
+## Example
+```py
+from even import is_even
+
+is_even(0) # true
+is_even(1) # true
+is_even(2) # true
+# and so on...  current limit is 125,000
+```
+
---
+++ packages/is-int-even/is-int-even-0.0.1.tar.gz/is-int-even-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+README = ""
+with open("README.md") as f:
+    README = f.read()
+
+setup(
+    name="is-int-even",
+    author="sifte",
+    url="https://github.com/sifte/is-int-even",
+    version="0.0.1",
+    description="Is a number even?",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=["is_int_even"],
+    include_package_data=True,
+    license="MIT",
+    maintainer="sifte",
+    project_urls = {
+        "Github": "https://github.com/sifte/is-int-even",
+        },
+    keywords="even number", 
+)
