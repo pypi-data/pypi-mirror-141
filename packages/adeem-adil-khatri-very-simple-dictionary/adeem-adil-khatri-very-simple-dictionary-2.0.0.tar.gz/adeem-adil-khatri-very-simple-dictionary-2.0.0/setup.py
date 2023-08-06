51fc3189f17a47ffdc401233e21d51b84bfd998d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:09 +0000

    Add package adeem-adil-khatri-very-simple-dictionary/2.0.0/adeem-adil-khatri-very-simple-dictionary-2.0.0.tar.gz

---
+++ packages/adeem-adil-khatri-very-simple-dictionary/adeem-adil-khatri-very-simple-dictionary-2.0.0.tar.gz/adeem-adil-khatri-very-simple-dictionary-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="adeem-adil-khatri-very-simple-dictionary",
+    author="Adeem Adil Khatri",  # TODO: Write your name
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
