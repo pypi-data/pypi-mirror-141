910bbc150de847f72a8a47b56c0f0cad1866b5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:02 +0000

    Add package func-adl/3.0b5/func_adl-3.0b5-py3-none-any.whl

---
+++ packages/func-adl/func_adl-3.0b5-py3-none-any.whl/func_adl/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# Dataset accessors
+from .ast.meta_data import extract_metadata  # NOQA
+from .event_dataset import EventDataset, find_EventDataset
+
+# Extra LINQ-like functions
+from .functions import Range
+
+# Main streaming object
+from .object_stream import ObjectStream
+
+# Decorators to add extra functionally to the module
+from .type_based_replacement import (
+    func_adl_callable,
+    func_adl_callback,
+    func_adl_parameterized_call,
+    register_func_adl_os_collection,
+)
