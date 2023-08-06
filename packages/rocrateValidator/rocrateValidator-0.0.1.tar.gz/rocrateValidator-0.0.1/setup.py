604cde5c249c3ac199cf6b313ef750aeb77def5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:42 +0000

    Add package rocrateValidator/0.0.1/rocrateValidator-0.0.1.tar.gz

---
+++ packages/rocrateValidator/rocrateValidator-0.0.1.tar.gz/rocrateValidator-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'rocrateValidator',
+	version = '0.0.1', 
+	description = 'This is python library for ro-crate validator',
+	py_modules = ['ro-crate-validator-py'],
+	zip_safe = False
+	)
