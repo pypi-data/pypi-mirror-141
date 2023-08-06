b8aeb7411f3b75ab76d8ea9f2f901c43eb817467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:45 +0000

    Add package tritium-pipeline/0.4.0/tritium-pipeline-0.4.0.tar.gz

---
+++ packages/tritium-pipeline/tritium-pipeline-0.4.0.tar.gz/tritium-pipeline-0.4.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tritium_pipeline',
+ 'tritium_pipeline.algebra',
+ 'tritium_pipeline.algebra.types',
+ 'tritium_pipeline.distribution',
+ 'tritium_pipeline.log']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.0,<2.0.0', 'toolz>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'tritium-pipeline',
+    'version': '0.4.0',
+    'description': 'Log based job pipeline tool',
+    'long_description': None,
+    'author': 'voidpointercast',
+    'author_email': 'patrick.daniel.gress@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
