338001fb14a4a82fc6ab71eea8cdd498f9062e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:46 +0000

    Add package teaching-tools/0.1.1/teaching-tools-0.1.1.tar.gz

---
+++ packages/teaching-tools/teaching-tools-0.1.1.tar.gz/teaching-tools-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['teaching_tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipython>=8.1.1,<9.0.0',
+ 'ipywidgets>=7.6.5,<8.0.0',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.4.1,<2.0.0',
+ 'sklearn>=0.0,<0.1']
+
+setup_kwargs = {
+    'name': 'teaching-tools',
+    'version': '0.1.1',
+    'description': 'Teaching tools for the WQU Data Science Lab',
+    'long_description': None,
+    'author': 'Nicholas Cifuentes-Goodbody',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
