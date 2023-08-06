83e78d591d46763b0c71a04c643fd1c6b2be6a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:31 +0000

    Add package cegalprizm-scripting-server/1.0rc34970/cegalprizm_scripting_server-1.0rc34970-py2.py3-none-any.whl

---
+++ packages/cegalprizm-scripting-server/cegalprizm_scripting_server-1.0rc34970-py2.py3-none-any.whl/cegalprizm/scripting_server/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 Cegal AS
+# All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+
+__version__ = '1.0.0'
+__git_hash__ = 'cdbb4b9f'
+
+import logging
+logger = logging.getLogger(__name__)
