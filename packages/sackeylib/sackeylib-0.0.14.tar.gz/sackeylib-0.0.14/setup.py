803271f56b243fd2c04a5974ba97ac011c229400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:56 +0000

    Add package sackeylib/0.0.14/sackeylib-0.0.14.tar.gz

---
+++ packages/sackeylib/sackeylib-0.0.14.tar.gz/sackeylib-0.0.14/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='sackeylib',
+    version='0.0.14',
+    description='easy to use Python:\n update print_var,var_name',
+    author='Yuanshaohua',
+    zip_safe=False,
+    author_email='739115136@qq.com',
+    url='https://github.com/Sackey-yuan/nvm',
+    packages=('sackeylib',)
+)
