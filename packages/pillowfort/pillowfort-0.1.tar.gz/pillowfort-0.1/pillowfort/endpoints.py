8c1411c45f682285597a536272df4b66e58a0345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:31 +0000

    Add package pillowfort/0.1/pillowfort-0.1.tar.gz

---
+++ packages/pillowfort/pillowfort-0.1.tar.gz/pillowfort-0.1/pillowfort/endpoints.py
@@ -0,0 +1,9 @@
+from pillowfort import Endpoint
+
+
+class BlackHoleEndpoint(Endpoint):
+    URL = '/<url:.*>'
+    NAME = 'Black Hole'
+
+    def handle_request(self, request):
+        return '[No Response]'
