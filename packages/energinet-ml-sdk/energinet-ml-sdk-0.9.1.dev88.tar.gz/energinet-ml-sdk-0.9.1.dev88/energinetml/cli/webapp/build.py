d29f1875f3856fa071a8633b8ec08891a3beac04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:33 +0000

    Add package energinet-ml-sdk/0.9.1.dev88/energinet-ml-sdk-0.9.1.dev88.tar.gz

---
+++ packages/energinet-ml-sdk/energinet-ml-sdk-0.9.1.dev88.tar.gz/energinet-ml-sdk-0.9.1.dev88/energinetml/cli/webapp/build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""[summary]
+"""
+import click
+
+from energinetml.cli.utils import discover_project
+from energinetml.core.docker import build_webapp_docker_image
+from energinetml.core.project import WebAppProject
+
+
+@click.command()
+@click.option(
+    "--tag",
+    "-t",
+    required=True,
+    help="Name and optionally a tag in the ‘name:tag’ format",
+)
+@discover_project(WebAppProject)
+def build(tag: str, project: WebAppProject) -> None:
+    """Build web app Docker image.
+
+    Args:
+        tag (str): [description]
+        project (WebAppProject): [description]
+    """
+    build_webapp_docker_image(project=project, tag=tag)
