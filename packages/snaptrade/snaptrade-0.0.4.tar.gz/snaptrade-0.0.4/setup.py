11a9c5b0aa2e692cb012c092d356faf23e3af222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:01 +0000

    Add package snaptrade/0.0.4/snaptrade-0.0.4.tar.gz

---
+++ packages/snaptrade/snaptrade-0.0.4.tar.gz/snaptrade-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="snaptrade",
+    version="0.0.4",
+    description="A Python implementation of SnapTrade API client library",
+    author="SnapTrade Dev Team",
+    author_email="support@snaptrade.com",
+    url="https://snaptrade.com",
+    packages=find_packages(),
+    package_data={"snaptrade": ["api_client/endpoints.json"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
