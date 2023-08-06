a06eca0e7d035b6ccc07ae0e74611c67f6d1f078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:31 +0000

    Add package royalnet-telethon/6.6.0/royalnet_telethon-6.6.0-py3-none-any.whl

---
+++ packages/royalnet-telethon/royalnet_telethon-6.6.0-py3-none-any.whl/royalnet_telethon/bullet/projectiles/_imports.py
@@ -0,0 +1,19 @@
+import royalnet.royaltyping as t
+
+import royalnet.engineer.bullet.casing as c
+import royalnet.engineer.bullet.projectiles as p
+import telethon as tt
+import telethon.tl.custom as tlc
+import async_property as ap
+import datetime
+
+
+__all__ = (
+    "t",
+    "c",
+    "p",
+    "tt",
+    "tlc",
+    "ap",
+    "datetime",
+)
