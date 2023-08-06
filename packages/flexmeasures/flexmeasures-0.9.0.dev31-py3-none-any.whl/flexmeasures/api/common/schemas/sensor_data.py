b649532336913f8dd30406a6b24ebd810082f7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:38 +0000

    Add package flexmeasures/0.9.0.dev31/flexmeasures-0.9.0.dev31-py3-none-any.whl

---
+++ packages/flexmeasures/flexmeasures-0.9.0.dev31-py3-none-any.whl/flexmeasures/api/common/schemas/sensor_data.py
@@ -0,0 +1,206 @@
+from datetime import timedelta
+from typing import List, Union
+
+from flask_login import current_user
+from marshmallow import fields, post_load, validates_schema, ValidationError
+from marshmallow.validate import Equal, OneOf
+from marshmallow_polyfield import PolyField
+from timely_beliefs import BeliefsDataFrame
+import pandas as pd
+
+from flexmeasures.data import ma
+from flexmeasures.data.models.data_sources import DataSource
+from flexmeasures.api.common.schemas.sensors import SensorField
+from flexmeasures.api.common.utils.api_utils import upsample_values
+from flexmeasures.data.schemas.times import AwareDateTimeField, DurationField
+from flexmeasures.utils.time_utils import server_now
+from flexmeasures.utils.unit_utils import (
+    convert_units,
+    units_are_convertible,
+)
+
+
+class SingleValueField(fields.Float):
+    """Field that both de-serializes and serializes a single value to a list of floats (length 1)."""
+
+    def _deserialize(self, value, attr, obj, **kwargs) -> List[float]:
+        return [self._validated(value)]
+
+    def _serialize(self, value, attr, data, **kwargs) -> List[float]:
+        return [self._validated(value)]
+
+
+def select_schema_to_ensure_list_of_floats(
+    values: Union[List[float], float], _
+) -> Union[fields.List, SingleValueField]:
+    """Allows both a single float and a list of floats. Always returns a list of floats.
+
+    Meant to improve user experience by not needing to make a list out of a single item, such that:
+
+        {
+            "values": [3.7]
+        }
+
+    can be written as:
+
+        {
+            "values": 3.7
+        }
+
+    Either will be de-serialized to [3.7].
+
+    Note that serialization always results in a list of floats.
+    This ensures that we are not requiring the same flexibility from users who are retrieving data.
+    """
+    if isinstance(values, list):
+        return fields.List(fields.Float)
+    else:
+        return SingleValueField()
+
+
+class SensorDataDescriptionSchema(ma.Schema):
+    """
+    Describing sensor data (i.e. in a GET request).
+
+    TODO: when we want to support other entity types with this
+          schema (assets/weather/markets or actuators), we'll need some re-design.
+    """
+
+    type = fields.Str(required=True, validate=Equal("GetSensorDataRequest"))
+    sensor = SensorField(required=True, entity_type="sensor", fm_scheme="fm1")
+    start = AwareDateTimeField(required=True, format="iso")
+    duration = DurationField(required=True)
+    horizon = DurationField(required=False)
+    prior = AwareDateTimeField(required=False, format="iso")
+    unit = fields.Str(required=True)
+
+    @validates_schema
+    def check_user_rights_against_sensor(self, data, **kwargs):
+        """If the user is a Prosumer and the sensor belongs to an asset
+        over which the Prosumer has no ownership, raise a ValidationError.
+        """
+        # todo: implement check once sensors can belong to an asset
+        #       https://github.com/FlexMeasures/flexmeasures/issues/155
+        pass
+
+    @validates_schema
+    def check_schema_unit_against_sensor_unit(self, data, **kwargs):
+        """Allows units compatible with that of the sensor.
+        For example, a sensor with W units allows data to be posted with units:
+        - W, kW, MW, etc. (i.e. units with different prefixes)
+        - J/s, Nm/s, etc. (i.e. units that can be converted using some multiplier)
+        - Wh, kWh, etc. (i.e. units that represent a stock delta, which knowing the duration can be converted to a flow)
+        For compatible units, the SensorDataSchema converts values to the sensor's unit.
+        """
+        posted_unit = data["unit"]
+        required_unit = data["sensor"].unit
+
+        if posted_unit != required_unit and not units_are_convertible(
+            posted_unit, required_unit
+        ):
+            raise ValidationError(
+                f"Required unit for this sensor is {data['sensor'].unit}, got incompatible unit: {data['unit']}"
+            )
+
+
+class SensorDataSchema(SensorDataDescriptionSchema):
+    """
+    This schema includes data, so it can be used for POST requests
+    or GET responses.
+
+    TODO: For the GET use case, look at api/common/validators.py::get_data_downsampling_allowed
+          (sets a resolution parameter which we can pass to the data collection function).
+    """
+
+    type = fields.Str(
+        validate=OneOf(["PostSensorDataRequest", "GetSensorDataResponse"])
+    )
+    values = PolyField(
+        deserialization_schema_selector=select_schema_to_ensure_list_of_floats,
+        serialization_schema_selector=select_schema_to_ensure_list_of_floats,
+        many=False,
+    )
+
+    @validates_schema
+    def check_resolution_compatibility_of_values(self, data, **kwargs):
+        inferred_resolution = data["duration"] / len(data["values"])
+        required_resolution = data["sensor"].event_resolution
+        # TODO: we don't yet have a good policy w.r.t. zero-resolution (direct measurement)
+        if required_resolution == timedelta(hours=0):
+            return
+        if inferred_resolution % required_resolution != timedelta(hours=0):
+            raise ValidationError(
+                f"Resolution of {inferred_resolution} is incompatible with the sensor's required resolution of {required_resolution}."
+            )
+
+    @post_load()
+    def possibly_convert_units(self, data, **kwargs):
+        """
+        Convert values if needed, to fit the sensor's unit.
+        Marshmallow runs this after validation.
+        """
+        data["values"] = convert_units(
+            data["values"],
+            from_unit=data["unit"],
+            to_unit=data["sensor"].unit,
+            event_resolution=data["sensor"].event_resolution,
+        )
+        return data
+
+    @post_load()
+    def possibly_upsample_values(self, data, **kwargs):
+        """
+        Upsample the data if needed, to fit to the sensor's resolution.
+        Marshmallow runs this after validation.
+        """
+        inferred_resolution = data["duration"] / len(data["values"])
+        required_resolution = data["sensor"].event_resolution
+
+        # TODO: we don't yet have a good policy w.r.t. zero-resolution (direct measurement)
+        if required_resolution == timedelta(hours=0):
+            return data
+
+        # we already know resolutions are compatible (see validation)
+        if inferred_resolution != required_resolution:
+            data["values"] = upsample_values(
+                data["values"],
+                from_resolution=inferred_resolution,
+                to_resolution=required_resolution,
+            )
+        return data
+
+    def load_bdf(sensor_data) -> BeliefsDataFrame:
+        """
+        Turn the de-serialized and validated data into a BeliefsDataFrame.
+        """
+        source = DataSource.query.filter(
+            DataSource.user_id == current_user.id
+        ).one_or_none()
+        if not source:
+            raise ValidationError(
+                f"User {current_user.id} is not an accepted data source."
+            )
+
+        num_values = len(sensor_data["values"])
+        event_resolution = sensor_data["duration"] / num_values
+        dt_index = pd.date_range(
+            sensor_data["start"],
+            periods=num_values,
+            freq=event_resolution,
+        )
+        s = pd.Series(sensor_data["values"], index=dt_index)
+
+        # Work out what the recording time should be
+        belief_timing = {}
+        if "prior" in sensor_data:
+            belief_timing["belief_time"] = sensor_data["prior"]
+        elif "horizon" in sensor_data:
+            belief_timing["belief_horizon"] = sensor_data["horizon"]
+        else:
+            belief_timing["belief_time"] = server_now()
+        return BeliefsDataFrame(
+            s,
+            source=source,
+            sensor=sensor_data["sensor"],
+            **belief_timing,
+        )
