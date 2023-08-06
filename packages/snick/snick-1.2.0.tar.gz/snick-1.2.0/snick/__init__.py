69f70f4c927d825820aa611ebfa008cc2a57b797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:54 +0000

    Add package snick/1.2.0/snick-1.2.0.tar.gz

---
+++ packages/snick/snick-1.2.0.tar.gz/snick-1.2.0/snick/__init__.py
@@ -0,0 +1,25 @@
+from snick.methods import (
+    dedent,
+    conjoin,
+    dedent_all,
+    enboxify,
+    indent,
+    indent_wrap,
+    pretty_format,
+    pretty_print,
+    strip_whitespace,
+    unwrap,
+)
+
+__all__ = [
+    "dedent",
+    "conjoin",
+    "dedent_all",
+    "enboxify",
+    "indent",
+    "indent_wrap",
+    "pretty_format",
+    "pretty_print",
+    "strip_whitespace",
+    "unwrap",
+]
