259c930a63aee61e7b0488ee3d328ac249ce228d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:46 +0000

    Add package autosubmit-api/1.0.0/autosubmit_api-1.0.0-py2-none-any.whl

---
+++ packages/autosubmit-api/autosubmit_api-1.0.0-py2-none-any.whl/autosubmit_api/components/jobs/test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import unittest
+from autosubmit_api.components.jobs.joblist_loader import JobListLoader
+
+class TestJobListLoader(unittest.TestCase):
+  def setUp(self):
+      pass
+      
+  def tearDown(self):
+      pass
+  
+  def test_load(self):    
+    loader = JobListLoader("a29z")
+    loader.load_jobs()
+    self.assertTrue(len(loader.jobs) > 0)
+    for job in loader.jobs:
+      job.do_print()  
+  
+  # def test_loader(self):
+  #   tree = 
+
+  # def test_load_out_err_files(self):
+    
+
+if __name__ == '__main__':
+  unittest.main()
+  
+
---
+++ packages/autosubmit-api/autosubmit_api-1.0.0-py2-none-any.whl/autosubmit_api/workers/populateComplete.py
@@ -0,0 +1,10 @@
+import autosubmit_api.workers.populate_times as PopulateTimes
+
+
+def main():
+    # Process queue and running times
+    PopulateTimes.process_completed_times()
+
+
+if __name__ == "__main__":
+    main()
