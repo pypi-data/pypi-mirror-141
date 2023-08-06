6002c7dadda408b1c9010bc04a783f436fdaa8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:20 +0000

    Add package CPT-GFFParser/1.2.1/CPT_GFFParser-1.2.1-py3-none-any.whl

---
+++ packages/CPT-GFFParser/CPT_GFFParser-1.2.1-py3-none-any.whl/CPT_GFFParser/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+  name='CPT_GFFParser',
+  version='1.2.0',
+  author='Anthony Criscione',
+  author_email='talkingbeaverthing@gmail.com',
+  packages=['CPT_GFFParser'],
+  #scripts=['bin/script1','bin/script2'],
+  url='https://pypi.org/project/CPT-GFFParser/',
+  license='LICENSE.txt',
+  description='A Biopython extension package for I/O of GFF files',
+  long_description=open('README.md').read(),
+  install_requires=[
+      "biopython>=1.79",
+  ],
+)
