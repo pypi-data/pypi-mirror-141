ac45c93800d3f788e0534fa7131fd21a7f8b4fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:01 +0000

    Add package solox/1.0.2/solox-1.0.2.tar.gz

---
+++ packages/solox/solox-1.0.2.tar.gz/solox-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="solox",
+    version="1.0.2",
+    author="Chen Hongqing",
+    author_email="",
+    description="Simple Test In SoloX !",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafa0128/SoloX",
+    packages=setuptools.find_packages(),
+    install_requires=['flask>=2.0.1'],
+    entry_points={
+        'console_scripts': [
+            'solox=solox.run:main'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
