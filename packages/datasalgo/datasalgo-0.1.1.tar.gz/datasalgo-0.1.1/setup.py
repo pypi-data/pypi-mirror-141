65563e113a1f1dd16fd955ffe715d32bc77cde60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:47 +0000

    Add package datasalgo/0.1.1/datasalgo-0.1.1.tar.gz

---
+++ packages/datasalgo/datasalgo-0.1.1.tar.gz/datasalgo-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='datasalgo',
+      version='0.1.1',
+      description='Data Structures and Algorithms package Package',
+      packages = ['datasalgo'],
+      install_requires = ["utilum"],
+      zip_safe = False,
+      )
