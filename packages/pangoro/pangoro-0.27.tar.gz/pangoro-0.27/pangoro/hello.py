3af069dbffa214baddb90d4fc067c6683d567363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:48 +0000

    Add package pangoro/0.27/pangoro-0.27.tar.gz

---
+++ packages/pangoro/pangoro-0.27.tar.gz/pangoro-0.27/pangoro/hello.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 00:38:56 2022
+
+@author: khalh
+"""
+
+
+def hi(txt):
+    '''
+    takes a text string and returns it as is 
+    Parameters
+    ----------
+    txt: str
+    
+    Returns
+    -------
+    str
+    text input is returned back to user
+    '''
+    return txt
+
+class MyDF():
+
+    def _constructor(self):
+        print('This actually works!')
+        return MyDF
+
+    def myhi(self):
+        print('hi there!')
