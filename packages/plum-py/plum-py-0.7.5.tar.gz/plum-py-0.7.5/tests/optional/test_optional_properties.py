37063f181eae5ffa3c47e9dc9adbe97a5f11c69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:17 +0000

    Add package plum-py/0.7.5/plum-py-0.7.5.tar.gz

---
+++ packages/plum-py/plum-py-0.7.5.tar.gz/plum-py-0.7.5/tests/optional/test_optional_properties.py
@@ -0,0 +1,27 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Copyright 2022 Daniel Mark Gass, see __about__.py for license information.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+"""Test OptionalX transform properties."""
+
+import pytest
+from plum.exceptions import SizeError
+from plum.littleendian import uint8
+from plum.optional import OptionalX
+
+
+class Test:
+
+    optional_x = OptionalX(fmt=uint8)
+
+    def test_default_name(self):
+        assert self.optional_x.name == "Optional[uint8]"
+
+    def test_fmt(self):
+        assert self.optional_x.fmt is uint8
+
+    def test_explicit_name(self):
+        assert OptionalX(fmt=uint8, name="Optional").name == "Optional"
+
+    def test_nbytes(self):
+        with pytest.raises(SizeError):
+            self.optional_x.nbytes  # pylint: disable=pointless-statement
