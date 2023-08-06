da3f4949ef4128ec0adc483237f5bcd6b2b8a518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:48 +0000

    Add package kirlent-docutils/0.1.1/kirlent_docutils-0.1.1-py3-none-any.whl

---
+++ packages/kirlent-docutils/kirlent_docutils-0.1.1-py3-none-any.whl/kirlent/docutils/html5.py
@@ -0,0 +1,143 @@
+# Copyright 2020-2022 H. Turgut Uyar <uyar@tekir.org>
+#
+# kirlent_docutils is released under the BSD license.
+# Read the included LICENSE.txt file for details.
+
+"""Custom HTML5 writer for docutils.
+
+This writer modifies the html5_polyglot writer in docutils.
+The differences are:
+
+- Uses "br" instead of line-block and line "div"s.
+- Uses no "p" in single paragraph list items and table entries.
+- Removes some docutils-specific classes.
+"""
+
+import re
+from functools import partial
+from pathlib import Path
+
+from docutils import frontend
+from docutils.writers.html5_polyglot import HTMLTranslator as HTML5Translator
+from docutils.writers.html5_polyglot import Writer as HTML5Writer
+
+
+class Writer(HTML5Writer):
+    """Writer for generating HTML5 output."""
+
+    supported = ("html", "html5")
+
+    default_stylesheet_dirs = [".", str(Path(__file__).parent)] + \
+        HTML5Writer.default_stylesheet_dirs[1:]
+
+    settings_default_overrides = {
+        "xml_declaration": False,
+        "compact_lists": False,
+        "compact_field_lists": False,
+        "cloak_email_addresses": False,
+    }
+
+    settings_spec = frontend.filter_settings_spec(
+        HTML5Writer.settings_spec,
+        "xml_declaration",
+        "no_xml_declaration",
+        "compact_lists",
+        "no_compact_lists",
+        "compact_field_lists",
+        "no_compact_field_lists",
+        "cloak_email_addresses",
+        stylesheet_dirs=(
+            'Comma-separated list of directories where stylesheets are found. '
+            'Used by --stylesheet-path when expanding relative path arguments. '  # noqa
+            '(default: "%s")' % ','.join(default_stylesheet_dirs),
+            ["--stylesheet-dirs"],
+            {
+                "metavar": "<dir[,dir,...]>",
+                "validator": frontend.validate_comma_separated_list,
+                "default": default_stylesheet_dirs,
+            }
+        ),
+    )
+
+    def __init__(self):
+        super().__init__()
+        self.translator_class = HTMLTranslator
+
+
+class HTMLTranslator(HTML5Translator):
+    """HTML5 translator for customizing generated output."""
+
+    _remove_xml = partial(re.sub, re.compile(r'\s*\bxml(ns|:\w+)="[^"]*"'), '')
+    _remove_type = partial(re.sub, re.compile(r'\s*\btype="[^"]*"'), '')
+
+    head_prefix_template = _remove_xml(HTML5Translator.head_prefix_template)
+    stylesheet_link = _remove_type(HTML5Translator.stylesheet_link)
+    embedded_stylesheet = _remove_type(HTML5Translator.embedded_stylesheet)
+
+    script = '<script>%s</script>\n'
+    script_external = '<script src="%s"></script>\n'
+    script_defer = '<script defer src="%s"></script>\n'
+
+    # no '<p>' under these if single paragraph
+    SIMPLE_BLOCKS = {"definition", "entry", "field_body", "list_item"}
+
+    UNWANTED_CLASSES = (
+        ({"container"}, "container"),
+        ({"container", "literal", "transition"}, "docutils"),
+        ({"entry"}, "head"),
+        ({"reference"}, "external"),
+        ({"reference"}, "internal"),
+        ({"reference"}, "reference"),
+    )
+
+    def starttag(self, node, *args, **kwargs):
+        # remove custom docutils classes
+        classes = kwargs.pop("CLASS", "").split()
+        classes.extend(kwargs.pop("class", "").split())
+        classes.extend(kwargs.pop("classes", []))
+        if len(classes) > 0:
+            for tagnames, classname in HTMLTranslator.UNWANTED_CLASSES:
+                if (node.tagname in tagnames) and (classname in classes):
+                    classes.remove(classname)
+            if len(classes) > 0:
+                kwargs["CLASS"] = " ".join(classes)
+
+        # add custom styles and properties, if any
+        custom = node.attributes.pop("_custom", {})
+        styles = node.attributes.pop("_styles", {})
+        if len(styles) > 0:
+            custom["style"] = " ".join(f"{k}: {v};" for k, v in styles.items())
+        kwargs.update(custom)
+        return super().starttag(node, *args, **kwargs)
+
+    def is_compactable(self, *args, **kwargs):
+        # suppress generation of "simple" classes for all elements
+        return False
+
+    def visit_paragraph(self, node):
+        # suppress '<p>' in single paragraph simple blocks
+        single_p = (len(node.parent.children) < 2) and \
+            (node.parent.tagname in HTMLTranslator.SIMPLE_BLOCKS)
+        if not single_p:
+            super().visit_paragraph(node)
+        node.attributes["_single_p"] = single_p
+
+    def depart_paragraph(self, node):
+        single_p = node.attributes.pop("_single_p", False)
+        if not single_p:
+            super().depart_paragraph(node)
+
+    def visit_line_block(self, node):
+        # suppress '<div class="line-block">'
+        pass
+
+    def depart_line_block(self, node):
+        pass
+
+    def visit_line(self, node):
+        # suppress '<div class="line">'
+        pass
+
+    def depart_line(self, node):
+        # add '<br/>' to end of line
+        self.body.append('<br/>\n')
---
+++ packages/kirlent-docutils/kirlent_docutils-0.1.1-py3-none-any.whl/kirlent/docutils/revealjs.py
@@ -0,0 +1,114 @@
+# Copyright 2022 H. Turgut Uyar <uyar@tekir.org>
+#
+# kirlent_docutils is released under the BSD license.
+# Read the included LICENSE.txt file for details.
+
+"""reveal.js writer for docutils."""
+
+from docutils import frontend
+
+from .slides import SlidesTranslator
+from .slides import Writer as SlidesWriter
+
+
+REVEAL_JS_URL = "https://cdn.jsdelivr.net/npm/reveal.js"
+
+REVEAL_JS_INIT = """
+  window.addEventListener('DOMContentLoaded', () => {
+      Reveal.initialize({
+          width: '%(width)d',
+          height: '%(height)d',
+          center: %(center)s,
+          transition: '%(transition)s'
+      });
+  }, false);
+"""
+
+
+class Writer(SlidesWriter):
+    """Writer for generating reveal.js output."""
+
+    default_stylesheets = ["minimal.css", "revealjs.css"]
+
+    default_transition = "none"
+    default_center_vertical = False
+
+    settings_spec = frontend.filter_settings_spec(
+        SlidesWriter.settings_spec,
+        stylesheet_path=(
+            'Comma separated list of stylesheet paths. '
+            'Relative paths are expanded if a matching file is found in '
+            'the --stylesheet-dirs. With --link-stylesheet, '
+            'the path is rewritten relative to the output HTML file. '
+            '(default: "%s")' % ','.join(default_stylesheets),
+            ["--stylesheet-path"],
+            {
+                "metavar": "<file[,file,...]>",
+                "overrides": "stylesheet",
+                "validator": frontend.validate_comma_separated_list,
+                "default": default_stylesheets,
+            }
+        ),
+    )
+
+    settings_spec = settings_spec + (
+        "RevealJS Writer Options",
+        "",
+        (
+            (
+                'Slide transition effect. (default: %s)' % default_transition,
+                ["--transition"],
+                {
+                    "default": default_transition,
+                }
+            ),
+            (
+                'Vertically center slides. (default: %s)' % (
+                    default_center_vertical,
+                ),
+                ["--center-vertical"],
+                {
+                    "default": default_center_vertical,
+                    "validator": frontend.validate_boolean,
+                }
+            ),
+        )
+    )
+
+    def __init__(self):
+        super().__init__()
+        self.translator_class = RevealJSTranslator
+
+
+class RevealJSTranslator(SlidesTranslator):
+    """Translator for generating reveal.js markup."""
+
+    script_revealjs = SlidesTranslator.script_defer % REVEAL_JS_URL
+    script_revealjs_init = SlidesTranslator.script % REVEAL_JS_INIT
+
+    substep_class = "fragment"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.center_vertical = self.document.settings.center_vertical
+        self.transition = self.document.settings.transition
+
+    def visit_document(self, node):
+        # add attributes for reveal.js
+        node.attributes["classes"].append("reveal")
+        super().visit_document(node)
+
+    def depart_document(self, node):
+        self.body_pre_docinfo.append('<div class="slides">\n')
+        self.body.append('</div>\n')
+        super().depart_document(node)
+
+        # add code for reveal.js
+        self.head.append(RevealJSTranslator.script_revealjs)
+        self.head.append(RevealJSTranslator.script_revealjs_init % {
+            "width": self.slide_width,
+            "height": self.slide_height,
+            "center": "true" if self.center_vertical else "false",
+            "transition": self.transition,
+        })
