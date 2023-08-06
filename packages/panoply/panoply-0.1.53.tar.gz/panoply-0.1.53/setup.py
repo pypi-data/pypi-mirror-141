c91629007c1dbd70180e3778bcec3a7aa6ebd3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package panoply/0.1.53/panoply-0.1.53.tar.gz

---
+++ packages/panoply/panoply-0.1.53.tar.gz/panoply-0.1.53/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['panoply']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['simple_term_menu>=1.4.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['panoply = panoply.panoply:main']}
+
+setup_kwargs = {
+    'name': 'panoply',
+    'version': '0.1.53',
+    'description': 'Panoply: save commands and reuse them',
+    'long_description': None,
+    'author': 'Jeremy Naccache',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jeremynac/panoply',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
