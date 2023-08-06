fd36be119ddf395be3a70c2b2c48f590d3a8c55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:05 +0000

    Add package jmlopez-m/0.7.1/jmlopez_m-0.7.1-py3-none-any.whl

---
+++ packages/jmlopez-m/jmlopez_m-0.7.1-py3-none-any.whl/m/cli/commands/__init__.py
@@ -0,0 +1,7 @@
+import inspect
+
+meta = {
+    'description': inspect.cleandoc('''
+        See the help for each of the following supported commands.
+    '''),
+}
---
+++ packages/jmlopez-m/jmlopez_m-0.7.1-py3-none-any.whl/m/cli/commands/ci/__init__.py
@@ -0,0 +1,9 @@
+import inspect
+
+meta = {
+    'help': 'continuous integration utilities',
+    'description': inspect.cleandoc('''
+        The following commands provide local support in the continuous
+        integration flow.
+    '''),
+}
