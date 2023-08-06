5f47f294deb35ec0bf709b77184b737d4ce6e577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:44 +0000

    Add package fba/0.0.12/fba-0.0.12.tar.gz

---
+++ packages/fba/fba-0.0.12.tar.gz/fba-0.0.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open(file='README.md', mode='r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='fba',
+    version='0.0.12',
+    author='JD',
+    author_email='t4mi27b3v@relay.firefox.com',
+    description='Tools for feature barcoding analysis',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jlduan/fba',
+    packages=find_packages(exclude=('docs', 'tests')),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    python_requires='>=3.6',
+    install_requires=[i.rstrip() for i in open(file='requirements.txt')],
+    entry_points={
+        'console_scripts': ['fba=fba.__main__:main'],
+    }
+)
