adabb4ebf501d78d7354675b6f8a28a3d10cf6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package immuta-audit-export/0.1.4/immuta_audit_export-0.1.4.tar.gz

---
+++ packages/immuta-audit-export/immuta_audit_export-0.1.4.tar.gz/immuta_audit_export-0.1.4/immuta_audit_export/s3_transform_record.py
@@ -0,0 +1,10 @@
+import json
+from datetime import datetime
+from s3_export_schema import S3ExportSchema
+
+def transform_record(input_record):
+	# make input into an s3_export_schema
+	timestamp = str(datetime.now())
+	key_suffix = "test_record.json"
+	key_name = f"{timestamp}{key_suffix}"
+	return S3ExportSchema(key_name, input_record.data)
