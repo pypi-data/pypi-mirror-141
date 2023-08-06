d36ca70be84aa98b562ec5c38f644595edf87bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:21:46 +0000

    Add package gravity/0.10.0/gravity-0.10.0-py3-none-any.whl

---
+++ packages/gravity/gravity-0.10.0-py3-none-any.whl/gravity/util/__init__.py
@@ -0,0 +1,70 @@
+"""
+"""
+import collections.abc
+import copy
+import os
+import sys
+
+import ruamel.yaml
+import yaml
+
+
+class AttributeDict(dict):
+    yaml_tag = u'tag:yaml.org,2002:map'
+
+    @classmethod
+    def loads(cls, s, *args, **kwargs):
+        return cls(yaml.safe_load(s, *args, **kwargs))
+
+    @classmethod
+    def to_yaml(cls, representer, node):
+        d = {}
+        for k in node.keys():
+            if not k.startswith("_"):
+                d[k] = node[k]
+        return representer.represent_mapping(cls.yaml_tag, d)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._yaml = ruamel.yaml.YAML()
+        self._yaml.register_class(self.__class__)
+
+    def __eq__(self, other):
+        return all([other[k] == v for k, v in self.items() if not k.startswith("_")])
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+    def dump(self, fp, *args, **kwargs):
+        self._yaml.dump(self, fp)
+
+
+def recursive_update(to_update, update_from):
+    """
+    Update values in `to_update` with values in `update_from`.
+
+    Does not mutate values in to_update, but returns a new dictionary.
+    """
+    d = copy.deepcopy(to_update)
+    for k, v in update_from.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = recursive_update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def which(file):
+    # http://stackoverflow.com/questions/5226958/which-equivalent-function-in-python
+    if os.path.exists(os.path.dirname(sys.executable) + "/" + file):
+        return os.path.dirname(sys.executable) + "/" + file
+    for path in os.environ["PATH"].split(":"):
+        if os.path.exists(path + "/" + file):
+            return path + "/" + file
+    return None
