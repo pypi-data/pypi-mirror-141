09ddcf53bf031ab9113cdca2b770b9fafcb37b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:50 +0000

    Add package twitch-info/2.4.1/twitch-info-2.4.1.tar.gz

---
+++ packages/twitch-info/twitch-info-2.4.1.tar.gz/twitch-info-2.4.1/twitch_info/twitch_info.py
@@ -0,0 +1,45 @@
+import requests
+
+
+def get_access_token(client_id: str, client_secret: str) -> str:
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
+
+    response = requests.post("https://id.twitch.tv/oauth2/token", params=params)
+    access_token = response.json()["access_token"]
+
+    return access_token
+
+
+def get_user_id(user_name: str, client_id: str, acces_token: str) -> int:
+    params = {
+        "login": user_name
+    }
+
+    headers = {
+        "Authorization": f"Bearer {acces_token}",
+        "Client-Id": client_id
+    }
+
+    response = requests.get("https://api.twitch.tv/helix/users", params=params, headers=headers)
+
+    return response.json()["data"][0]['id']
+
+
+def get_stream(user_id: int, client_id: str, acces_token: str) -> dict:
+    params = {
+        "user_id": user_id
+    }
+
+    headers = {
+        "Authorization": f"Bearer {acces_token}",
+        "Client-Id": client_id
+    }
+
+    response = requests.get("https://api.twitch.tv/helix/streams", params=params, headers=headers)
+    data = response.json()["data"]
+
+    return data[0]
