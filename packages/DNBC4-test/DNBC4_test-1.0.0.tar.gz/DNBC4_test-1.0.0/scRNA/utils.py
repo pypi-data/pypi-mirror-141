8e6d66c494ca69621ba67d615262a868977c61cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package DNBC4-test/1.0.0/DNBC4_test-1.0.0.tar.gz

---
+++ packages/DNBC4-test/DNBC4_test-1.0.0.tar.gz/DNBC4_test-1.0.0/scRNA/utils.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import resource
+import time
+import subprocess
+from collections import Counter, defaultdict
+from datetime import timedelta
+from functools import wraps
+import numpy as np
+import pandas as pd
+
+_version = "1.0.0"
+
+_pipelist = [
+    "data",
+    'count',
+    #'analysis',
+    #'report',
+    #'clean',
+    #'clean',
+    #'run',
+]
+_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../"))
+
+def str_mkdir(arg):
+    if not os.path.exists(arg):
+        os.system('mkdir -p %s'%arg)
+
+def change_env():
+    os.environ['PATH'] ="%s/miniconda3/envs/scRNA/bin"%_root_dir
+    os.environ['LD_LIBRARY_PATH'] ="%s/miniconda3/envs/scRNA/lib"%_root_dir
+
+def start_print_cmd(arg):
+    print(arg)
+    subprocess.check_call(arg,shell=True)
+
+
+
