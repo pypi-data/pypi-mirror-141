0a4bf9a81b15fef62de414e0da375b79286c2c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:29 +0000

    Add package svgreplicate/0.1.1/svgreplicate-0.1.1-py3-none-any.whl

---
+++ packages/svgreplicate/svgreplicate-0.1.1-py3-none-any.whl/svgreplicate/__main__.py
@@ -0,0 +1,51 @@
+import argparse
+import json
+from typing import Protocol
+
+from .svghandler import Replicas, SvgHandler
+
+
+class Args(Protocol):
+
+    filename: str
+    replications: str
+
+
+def get_args() -> Args:
+    parser = argparse.ArgumentParser(
+        description="Replicates and modifies SVG files in batch"
+    )
+    parser.add_argument(
+        "--filename", type=str, help="SVG file that will be used as the basis"
+    )
+    parser.add_argument(
+        "--replicas",
+        type=str,
+        help="JSON file with specification of replicas that will be created",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    with open(args.replicas) as f:
+        replicas: Replicas = json.load(f)
+
+    for replica in replicas:
+        svg_handler = SvgHandler()
+
+        # Load template
+        with open(args.filename) as f:
+            svg_handler.read(f)
+
+        # Modify stuff
+        svg_handler.modify(replica["modifications"])
+
+        # Save
+        with open(replica["filename"], "wb") as f:
+            svg_handler.write(f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/svgreplicate/svgreplicate-0.1.1-py3-none-any.whl/svgreplicate/svghandler.py
@@ -0,0 +1,99 @@
+import re
+import xml.etree.ElementTree as ET
+from io import BytesIO
+from typing import TextIO, TypedDict
+
+
+# Define types and interfaces
+class ElementModification(TypedDict):
+
+    id: str
+    style: dict[str, str]
+    text: str
+
+
+class Replica(TypedDict):
+
+    filename: str
+    modifications: list[ElementModification]
+
+
+Replicas = list[Replica]
+
+
+# Namespace
+ns = {"svg": "http://www.w3.org/2000/svg"}
+for prefix, uri in ns.items():
+    ET.register_namespace(prefix, uri)
+
+
+class SvgHandler:
+
+    _tree: ET.ElementTree
+
+    def read(self, file: TextIO) -> None:
+        """Reads SVG from TextIO"""
+        self._tree = ET.parse(file)
+
+    def modify(self, modifications: list[ElementModification]):
+        """Modify elements in SVG by passing list of dicts with shape ElementModification"""
+        for modification in modifications:
+            if "style" in modification:
+                self._modify_element_style(modification["id"], modification["style"])
+            if "text" in modification:
+                self._set_element_text(modification["id"], modification["text"])
+
+    def get_element_string(self, id: str) -> bytes:
+        """Converts element with given id to XML string"""
+        # TODO do we want this to be decoded?
+        return ET.tostring(self._get_element(id))
+
+    def write(self, file: BytesIO) -> None:
+        """Writes SVG to BytesIO"""
+        self._tree.write(file)
+
+    def _get_root(self) -> ET.Element:
+        try:
+            return self._tree.getroot()
+        except AttributeError:
+            raise Exception("You need to read an SVG file first")
+
+    # All this stuff could be taken to a separate class that wraps ET.Element
+    def _get_element(self, id: str) -> ET.Element:
+        if (element := self._get_root().find(f".//*[@id='{id}']")) is not None:
+            return element
+        else:
+            raise Exception(f'Element "{id}" not found')
+
+    def _set_element_text(self, id: str, text: str) -> None:
+        element = self._get_element(id)
+        element.text = text
+        # Remove tspan
+        if re.match(r"{.*}text", element.tag) and len(element) != 0:
+            self._remove_subelements_recursively(element, "svg:tspan")
+
+    def _modify_element_style(self, id: str, style: dict[str, str]) -> None:
+        element = self._get_element(id)
+        try:
+            current_style = self._marshal_style(element.attrib["style"])
+        except KeyError:
+            current_style = {}
+        style = {**current_style, **style}
+        element.set("style", self._unmarshal_style(style))
+
+    @staticmethod
+    def _remove_subelements_recursively(element: ET.Element, match: str) -> None:
+        while (subelement := element.find(match, ns)) is not None:
+            element.remove(subelement)
+
+    @staticmethod
+    def _marshal_style(style: str) -> dict[str, str]:
+        marshaled_style = {}
+        for attrib in style.split(";"):
+            key, value = tuple(attrib.split(":"))
+            marshaled_style[key] = value
+        return marshaled_style
+
+    @staticmethod
+    def _unmarshal_style(style: dict) -> str:
+        return ";".join([f"{key}:{val}" for key, val in style.items()])
