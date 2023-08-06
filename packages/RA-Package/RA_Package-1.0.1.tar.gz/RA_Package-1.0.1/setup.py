fcab1b44b5e0be40c98fa99a3e914a40624076e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:41 +0000

    Add package RA-Package/1.0.1/RA_Package-1.0.1.tar.gz

---
+++ packages/RA-Package/RA_Package-1.0.1.tar.gz/RA_Package-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="RA_Package",
+    version="1.0.1",
+    author="Manuel Alexander Christoph Raffl",
+    author_email="manuraffl003@gmail.com",
+    description="Backend Library for RA-Rechner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/manuraffl30/RA_Rechner",
+    project_urls={
+        "Bug Tracker": "https://github.com/manuraffl30/RA_Rechner/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),)
