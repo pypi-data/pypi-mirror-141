0ba2ed7b8062b240ae18812c21ac7bd9284773d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:41 +0000

    Add package espresso-config/0.1/espresso-config-0.1.tar.gz

---
+++ packages/espresso-config/espresso-config-0.1.tar.gz/espresso-config-0.1/src/espresso_config/registry.py
@@ -0,0 +1,37 @@
+from functools import singledispatchmethod
+import inspect
+from typing import Type, Callable, Generic, TypeVar, Union
+
+from espresso_config.node import ConfigNode
+
+CR = TypeVar('CR', bound='ConfigRegistry')
+RegistrableType = Union[Type[ConfigNode],Callable]
+
+
+class ConfigRegistry(Generic[CR]):
+    __registry__ = {}
+
+    @classmethod
+    def add(cls, config_node_cls: RegistrableType) -> ConfigNode:
+        is_node_config = (inspect.isclass(config_node_cls) and
+                          issubclass(config_node_cls, ConfigNode))
+        is_callable = isinstance(config_node_cls, Callable)
+
+        if not(is_node_config or is_callable):
+            msg = (f'`config_node_cls` must be a ConfigNode '
+                   f'class or callable fn, not `{type(config_node_cls)}`')
+            raise TypeError(msg)
+
+        name = config_node_cls.__name__
+
+        if name in cls.__registry__:
+            raise ValueError(f'A config of name "{name}" already exists')
+
+        cls.__registry__[name] = config_node_cls
+        return config_node_cls
+
+    @classmethod
+    def get(cls, name: str) -> ConfigNode:
+        if name not in cls.__registry__:
+            raise KeyError(f'`{name}` is not in the registry')
+        return cls.__registry__[name]
