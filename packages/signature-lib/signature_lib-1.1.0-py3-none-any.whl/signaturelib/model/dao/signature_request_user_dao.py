2958739c69bd1c989c41557da3d3cc3682b1c0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:24 +0000

    Add package signature-lib/1.1.0/signature_lib-1.1.0-py3-none-any.whl

---
+++ packages/signature-lib/signature_lib-1.1.0-py3-none-any.whl/signaturelib/model/dao/signature_request_user_dao.py
@@ -0,0 +1,117 @@
+
+from typing import List
+
+import requests
+from ..dto.model_dto import SignatureRequestUser
+
+class SignatureRequestUserDAO :
+
+    def __init__(self, baseUrl):
+        self.baseUrl = baseUrl
+    
+    def create(self, signature_request: SignatureRequestUser) -> SignatureRequestUser:
+        url = '/api/v1/signature_requests/'
+
+        data = signature_request.to_json()
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(self.baseUrl+url, data=data, headers=headers)
+
+        if response.status_code == 200 :
+            signature_request = SignatureRequestUser()
+            signature_request.from_json(response.json())
+            return signature_request
+        else :
+            print(response)
+            return None
+    
+    def get_all(self) -> List[SignatureRequestUser]:
+        url = '/api/v1/signature_requests/'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.baseUrl+url, headers=headers)
+
+        if response.status_code == 200 :
+            signature_requests = []
+            for signature_request in response.json() :
+                signature_request_dto = SignatureRequestUser()
+                signature_request_dto.from_json(signature_request)
+                signature_requests.append(signature_request_dto)
+            return signature_requests
+        else :
+            print(response)
+            return None
+    
+    def get_by_id(self, id) -> SignatureRequestUser:
+        url = '/api/v1/signature_requests/'+str(id)+'/'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.baseUrl+url, headers=headers)
+
+        if response.status_code == 200 :
+            signature_request = SignatureRequestUser()
+            signature_request.from_json(response.json())
+            return signature_request
+        else :
+            print(response)
+            return None
+        
+    def update(self, signature_request: SignatureRequestUser) -> SignatureRequestUser:
+        url = '/api/v1/signature_requests/'+str(signature_request.id)+'/'
+
+        data = signature_request.to_json()
+        headers = {'Content-Type': 'application/json'}
+        response = requests.put(self.baseUrl+url, data=data, headers=headers)
+
+        if response.status_code == 200 :
+            signature_request = SignatureRequestUser()
+            signature_request.from_json(response.json())
+            return signature_request
+        else :
+            print(response)
+            return None
+    
+    def delete(self, id) -> bool:
+        url = '/api/v1/signature_requests/'+str(id)+'/'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.delete(self.baseUrl+url, headers=headers)
+
+        if response.status_code == 200 :
+            return True
+        else :
+            print(response)
+            return False
+    
+
+    
+    def get_by_signature_request(self, signature_request_id) -> List[SignatureRequestUser]:
+        url = '/api/v1/signature_requests/signature_request/'+str(signature_request_id)+'/'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.baseUrl+url, headers=headers)
+
+        if response.status_code == 200 :
+            signature_requests = []
+            for signature_request in response.json() :
+                signature_request_dto = SignatureRequestUser()
+                signature_request_dto.from_json(signature_request)
+                signature_requests.append(signature_request_dto)
+            return signature_requests
+        else :
+            print(response)
+            return None
+    
+    def get_by_user_id(self, user_id):
+        url = '/api/v1/signature_requests/user/'+str(user_id)+'/'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.baseUrl+url, headers=headers)
+
+        if response.status_code == 200 :
+            signature_requests = []
+            for signature_request in response.json() :
+                signature_request_dto = SignatureRequestUser()
+                signature_request_dto.from_json(signature_request)
+                signature_requests.append(signature_request_dto)
+            return signature_requests
+        else :
+            print(response)
+            return None
+    
+ 
+      
