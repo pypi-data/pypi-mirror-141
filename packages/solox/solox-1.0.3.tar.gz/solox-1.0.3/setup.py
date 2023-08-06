53618f675d92f50f5097b3117fb5c13dde9becfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:02 +0000

    Add package solox/1.0.3/solox-1.0.3.tar.gz

---
+++ packages/solox/solox-1.0.3.tar.gz/solox-1.0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Licensed under MIT
+#
+import setuptools
+
+setuptools.setup(
+    install_requires=['flask>=2.0.1','requests'],
+    version='1.0.3',
+    packages=setuptools.find_namespace_packages(include=["solox", "solox.*"], ),
+    include_package_data=True
+)
