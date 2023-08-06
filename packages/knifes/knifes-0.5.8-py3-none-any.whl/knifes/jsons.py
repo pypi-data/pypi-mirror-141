ecfe6a7ea162cb2cd42a1180526bbef03f778422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:40 +0000

    Add package knifes/0.5.8/knifes-0.5.8-py3-none-any.whl

---
+++ packages/knifes/knifes-0.5.8-py3-none-any.whl/knifes/jsons.py
@@ -0,0 +1,23 @@
+import json
+import datetime
+from . import times
+import decimal
+import uuid
+
+
+# object encode
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return times.to_timestamp(o, unit='millisecond')
+        elif isinstance(o, (decimal.Decimal, uuid.UUID)):
+            return str(o)
+        elif isinstance(o, set):
+            return list(o)
+        else:
+            return o.__dict__
+
+
+# obj to json
+def to_json(obj):
+    return json.dumps(obj, cls=ObjectEncoder)
