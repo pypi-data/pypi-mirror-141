5dc565e7ce776c06986890d1f93994bd06680eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:40 +0000

    Add package very-simple-dictionary-by-ilke-v.0.4/2.0.0/very-simple-dictionary-by-ilke-v.0.4-2.0.0.tar.gz

---
+++ packages/very-simple-dictionary-by-ilke-v.0.4/very-simple-dictionary-by-ilke-v.0.4-2.0.0.tar.gz/very-simple-dictionary-by-ilke-v.0.4-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="very-simple-dictionary-by-ilke-v.0.4",
+    author="İlke Doğan",  # TODO: Write your name
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
