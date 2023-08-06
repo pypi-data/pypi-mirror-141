403f05754879971f46c20b61dfa5fa13930af636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:13 +0000

    Add package pyfrench/0.1.3a0/pyfrench-0.1.3a0.tar.gz

---
+++ packages/pyfrench/pyfrench-0.1.3a0.tar.gz/pyfrench-0.1.3a0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyfrench',
+    packages=find_packages(),
+    version='0.1.3a',
+    description='Une librairie qui traduit python en français',
+    long_description='Une librairie python de toute versions qui traduit ajoute les même fonctionnalités python sur le module en français.',
+    author='Artic#6377',
+    author_email="artic.admisoffi@gmail.com",
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='pyfrench',
+    classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+        ],
+)
