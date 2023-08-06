0061fc0b854f5474b28df137231b3b847d35092e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:39 +0000

    Add package towhee/0.5.0/towhee-0.5.0.tar.gz

---
+++ packages/towhee/towhee-0.5.0.tar.gz/towhee-0.5.0/setup.py
@@ -0,0 +1,58 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from typing import List
+from pathlib import Path
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+def test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('towhee/tests', pattern='test_*.py')
+    return test_suite
+
+
+def create_towhee_cache(dst: str):
+    if not Path(dst).is_dir():
+        Path(dst).mkdir(parents=True)
+
+
+def parse_requirements(file_name: str) -> List[str]:
+    with open(file_name) as f:
+        return [require.strip() for require in f if require.strip() and not require.startswith("#")]
+
+
+
+setup(
+    name="towhee",
+    version="0.5.0",
+    description="",
+    author="Towhee Team",
+    author_email="towhee-team@zilliz.com",
+    #use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=['setuptools_scm'],
+    url="https://github.com/towhee-io/towhee",
+    test_suite="setup.test_suite",
+    install_requires=parse_requirements('requirements.txt'),
+    extras_require={':python_version<"3.7"':'importlib-resources'},
+    tests_require=parse_requirements('test_requirements.txt'),
+    packages=find_packages(),
+    package_data={'towhee.tests.test_util': ['*.yaml']},
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    entry_points={
+        'console_scripts': ['towhee=towhee.command.cmdline:main'],
+    }
+)
---
+++ packages/towhee/towhee-0.5.0.tar.gz/towhee-0.5.0/towhee/doc/source/index.rst
@@ -0,0 +1,22 @@
+.. Towhee Trainer documentation master file, created by
+   sphinx-quickstart on Thu Mar  3 11:24:37 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Towhee Trainer's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   trainer/trainer
+   trainer/trainer.optimization
+   trainer/trainer.utils
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
