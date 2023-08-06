c82a47c301f216c460f0092f77b176095901ba9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:55 +0000

    Add package EDutilities/0.6/EDutilities-0.6.tar.gz

---
+++ packages/EDutilities/EDutilities-0.6.tar.gz/EDutilities-0.6/tests/test_viewers.py
@@ -0,0 +1,10 @@
+from utils import*
+from EDutils import viewers as vw;imp.reload(vw)
+from utils import pytest_util    ;imp.reload(pytest_util)
+
+@pytest_util.add_link(__file__)
+def test_show_exp():
+    v= vw.Base_Viewer('viewer',i=1,
+        pargs={'opt':''})
+    return v.fig,v.ax
+# test_show_exp()
