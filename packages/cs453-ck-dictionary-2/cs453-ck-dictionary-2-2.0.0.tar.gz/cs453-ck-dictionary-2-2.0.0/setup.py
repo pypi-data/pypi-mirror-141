7be63afa6017f07084d66be625b299bc5968d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:27 +0000

    Add package cs453-ck-dictionary-2/2.0.0/cs453-ck-dictionary-2-2.0.0.tar.gz

---
+++ packages/cs453-ck-dictionary-2/cs453-ck-dictionary-2-2.0.0.tar.gz/cs453-ck-dictionary-2-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="cs453-ck-dictionary-2",
+    author="Cankat Anday Kadim",
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+)
