29f0604f3e29925994cc042d6edf8714ca119df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package colrev-core/0.0.1/colrev_core-0.0.1.tar.gz

---
+++ packages/colrev-core/colrev_core-0.0.1.tar.gz/colrev_core-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='colrev_core',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Gerit Wagner',
+    author_email='gerit.wagner@hec.ca',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
