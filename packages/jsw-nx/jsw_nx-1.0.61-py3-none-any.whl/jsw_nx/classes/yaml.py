c6dc48314374e19fd7445134aef0151eef0e7af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:57 +0000

    Add package jsw-nx/1.0.61/jsw_nx-1.0.61-py3-none-any.whl

---
+++ packages/jsw-nx/jsw_nx-1.0.61-py3-none-any.whl/jsw_nx/classes/yaml.py
@@ -0,0 +1,11 @@
+import yaml
+
+
+class Yaml:
+    @classmethod
+    def stringify(cls, data):
+        return yaml.dump(data, allow_unicode=True, default_flow_style=False, sort_keys=False)
+
+    @classmethod
+    def parse(cls, data):
+        return yaml.safe_load(data)
