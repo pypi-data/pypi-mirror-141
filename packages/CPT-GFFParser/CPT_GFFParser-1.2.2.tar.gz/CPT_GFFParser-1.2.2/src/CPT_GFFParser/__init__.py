79ca0012ccebf63a5157c8adf9da678fb402e940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:29 +0000

    Add package CPT-GFFParser/1.2.2/CPT_GFFParser-1.2.2.tar.gz

---
+++ packages/CPT-GFFParser/CPT_GFFParser-1.2.2.tar.gz/CPT_GFFParser-1.2.2/src/CPT_GFFParser/__init__.py
@@ -0,0 +1,5 @@
+from .gffSeqFeature import gffSeqFeature
+from .gffSeqFeature import convertSeqFeat
+from .gffSeqFeature import convertSeqRec
+from .CPT_GFFParser import gffParse
+from .CPT_GFFParser import gffWrite
