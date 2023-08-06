dca61778d29270e80e2d6864614a9f216b42b671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:34 +0000

    Add package onatpostaci-cs453-hw1/6.1.9/onatpostaci-cs453-hw1-6.1.9.tar.gz

---
+++ packages/onatpostaci-cs453-hw1/onatpostaci-cs453-hw1-6.1.9.tar.gz/onatpostaci-cs453-hw1-6.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # TODO: Write a globally unique name which will be listed on PyPI
+    name="onatpostaci-cs453-hw1",
+    author="Ömer Onat Postacı",  # TODO: Write your name
+    version="6.1.9",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
