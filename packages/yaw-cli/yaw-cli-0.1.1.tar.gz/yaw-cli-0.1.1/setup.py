162c331d5b18ffb5422dd3c5356070eac7f05025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:14 +0000

    Add package yaw-cli/0.1.1/yaw-cli-0.1.1.tar.gz

---
+++ packages/yaw-cli/yaw-cli-0.1.1.tar.gz/yaw-cli-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='yaw-cli',
+    description='Query weather via cli',
+    author='dynamicfire',
+    author_email='plus@plusnow.me',
+    version='0.1.1',
+    py_modules=['yaw'],
+    install_requires=[
+        'Click',
+        'requests',
+        'Path'
+    ],
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            'yaw = yaw:main',
+        ],
+    },
+)
