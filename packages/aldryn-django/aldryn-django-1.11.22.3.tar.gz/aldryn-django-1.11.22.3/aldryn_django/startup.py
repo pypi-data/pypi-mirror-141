4e42d2eb7d01283872c19f4b0d927499f04edf03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:23 +0000

    Add package aldryn-django/1.11.22.3/aldryn-django-1.11.22.3.tar.gz

---
+++ packages/aldryn-django/aldryn-django-1.11.22.3.tar.gz/aldryn-django-1.11.22.3/aldryn_django/startup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+
+import django.core.management
+
+from getenv import env
+
+
+def manage(path):
+    _setup(path=path)
+    utility = django.core.management.ManagementUtility(None)
+    utility.execute()
+
+
+def wsgi(path):
+    _setup(path=path)
+    from django.core.wsgi import get_wsgi_application
+    from django.conf import settings
+    app = get_wsgi_application()
+    return app
+
+
+def setup(path):
+    _setup(path)
+    import django
+    django.setup()
+
+
+def _setup(path):
+    os.environ['DJANGO_SETTINGS_MODULE'] = env(
+        'DJANGO_SETTINGS_MODULE', 'settings'
+    )
