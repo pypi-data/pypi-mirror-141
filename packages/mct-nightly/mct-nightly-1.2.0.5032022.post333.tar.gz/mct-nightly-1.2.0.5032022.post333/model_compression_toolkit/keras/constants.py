165a2e7aaf2d462f5542cccf37389a9aced7df62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package mct-nightly/1.2.0.5032022.post333/mct-nightly-1.2.0.5032022.post333.tar.gz

---
+++ packages/mct-nightly/mct-nightly-1.2.0.5032022.post333.tar.gz/mct-nightly-1.2.0.5032022.post333/model_compression_toolkit/keras/constants.py
@@ -0,0 +1,54 @@
+# Copyright 2021 Sony Semiconductors Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+# Activation functions strings:
+SOFTMAX = 'softmax'
+SIGMOID = 'sigmoid'
+LINEAR = 'linear'
+IDENTITY = 'identity'
+TANH = 'tanh'
+SWISH = 'swish'
+RELU = 'relu'
+SELU = 'selu'
+GELU = 'gelu'
+
+# Layers attributes constants:
+PADDING = 'padding'
+STRIDES = 'strides'
+KERNEL_SIZE = 'kernel_size'
+LAYER_NAME = 'name'
+TRAINABLE = 'trainable'
+ACTIVATION = 'activation'
+USE_BIAS = 'use_bias'
+FILTERS = 'filters'
+PAD_VALID = 'valid'
+PAD_SAME = 'same'
+RELU_MAX_VALUE = 'max_value'
+THRESHOLD = 'threshold'
+NEGATIVE_SLOPE = 'negative_slope'
+CHANNELS_FORMAT = 'data_format'
+CHANNELS_FORMAT_FIRST = 'channels_first'
+CHANNELS_FORMAT_LAST = 'channels_last'
+
+# Layers variables names:
+KERNEL = 'kernel'
+DEPTHWISE_KERNEL = 'depthwise_kernel'
+BIAS = 'bias'
+GAMMA = 'gamma'
+BETA = 'beta'
+MOVING_MEAN = 'moving_mean'
+MOVING_VARIANCE = 'moving_variance'
+EPSILON = 'epsilon'
