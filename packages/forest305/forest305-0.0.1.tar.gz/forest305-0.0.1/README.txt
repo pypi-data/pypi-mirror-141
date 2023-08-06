d24e3da603b1f0c93398afc28238f6db92622a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:41 +0000

    Add package forest305/0.0.1/forest305-0.0.1.tar.gz

---
+++ packages/forest305/forest305-0.0.1.tar.gz/forest305-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a collecton of my functions.
---
+++ packages/forest305/forest305-0.0.1.tar.gz/forest305-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forest305',
+    version='0.0.1',
+    description='A collection of functions',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Jeff Song',
+    author_email='jeff.song0305@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires = ['']
+)
