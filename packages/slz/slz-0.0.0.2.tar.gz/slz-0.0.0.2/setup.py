f8e0ff64771136bfa3b2bcbe44cbbd6feeb2507a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:13 +0000

    Add package slz/0.0.0.2/slz-0.0.0.2.tar.gz

---
+++ packages/slz/slz-0.0.0.2.tar.gz/slz-0.0.0.2/setup.py
@@ -0,0 +1,54 @@
+import sys
+from os.path import dirname
+from os.path import abspath
+sys.path.append(dirname(abspath(__file__)))
+import monkeypatch_distutils
+
+from setuptools import setup
+try:
+	from pybind11.setup_helpers import Pybind11Extension
+except ImportError:
+	from setuptools import Extension as Pybind11Extension
+
+ext_modules = [
+    Pybind11Extension(
+        "slz",
+        ['src/pyslz.cpp', 'src/libslz/src/slz.c'],  # Sort source files for reproducibility
+        extra_compile_args=['-O2'],
+        extra_link_args=['-s'],
+    ),
+]
+
+setup(
+    name='slz',
+    description='a (light) binding for libslz',
+    long_description=open("README.md").read(),
+    version='0.0.0.2',
+    url='https://github.com/cielavenir/python-slz',
+    license='MIT',
+    author='cielavenir',
+    author_email='cielartisan@gmail.com',
+    setup_requires=["pybind11"],
+    ext_modules=ext_modules,
+    #cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    include_package_data=True,
+    # platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
+)
