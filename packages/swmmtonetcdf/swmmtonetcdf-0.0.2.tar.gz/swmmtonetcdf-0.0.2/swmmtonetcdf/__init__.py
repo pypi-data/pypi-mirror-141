534d0b4e76a33325a0c79d64020cf2b0ae796d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:08 +0000

    Add package swmmtonetcdf/0.0.2/swmmtonetcdf-0.0.2.tar.gz

---
+++ packages/swmmtonetcdf/swmmtonetcdf-0.0.2.tar.gz/swmmtonetcdf-0.0.2/swmmtonetcdf/__init__.py
@@ -0,0 +1,9 @@
+from swmmtonetcdf.swmmtonetcdf import *
+from swmmtonetcdf.__main__ import main
+
+VERSION_INFO = (0, 0, 2)
+
+__version__ = '.'.join(map(str, VERSION_INFO))
+__author__ = 'Caleb Buahin and Jenn Wu'
+__copyright__ = 'Copyright (c) 2022 Caleb A. Buahin'
+__licence__ = 'GNU GPLv3'
