546cc9f0f8f0c3ea49c29b2bb28a88349d5bc056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:17 +0000

    Add package ptflutter/1.0.1/ptflutter-1.0.1-py3-none-any.whl

---
+++ packages/ptflutter/ptflutter-1.0.1-py3-none-any.whl/ptflutter_templates/base_mobx/navigator.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:pt_flutter_architecture/pt_flutter_architecture.dart';
+import 'package:{{ package_name }}/generated/router.g.dart';
+
+class {{ name }}Navigator with AppRouter {
+  
+}
