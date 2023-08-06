1ffe4571cbaa25725d8f61b9562af6542f4b7b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:28 +0000

    Add package ci-cd-elhamin/2.0.1/ci_cd_elhamin-2.0.1.tar.gz

---
+++ packages/ci-cd-elhamin/ci_cd_elhamin-2.0.1.tar.gz/ci_cd_elhamin-2.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="ci_cd_elhamin",
+    author="Elham Amin",    
+    version="2.0.1",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
