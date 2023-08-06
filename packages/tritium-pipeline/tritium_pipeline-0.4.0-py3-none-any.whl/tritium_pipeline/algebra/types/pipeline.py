853f945f0369b7a629efe9de5cda0f4ee4c728a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:45 +0000

    Add package tritium-pipeline/0.4.0/tritium_pipeline-0.4.0-py3-none-any.whl

---
+++ packages/tritium-pipeline/tritium_pipeline-0.4.0-py3-none-any.whl/tritium_pipeline/algebra/types/pipeline.py
@@ -0,0 +1,51 @@
+from typing import Generic, Callable, Iterable
+from pydantic import BaseModel
+from tritium_pipeline.algebra.types.log import In, Out, Intermediate, LogWriter, no_log, LogEntry
+from tritium_pipeline.algebra.types.processor import connect_processors, Processor, create_processor, source_processor
+from tritium_pipeline.algebra.types.processor import JobDistribution, SerialDistribution
+
+
+class Pipeline(BaseModel, Generic[In, Out]):
+    write_log: LogWriter
+    processor: Processor[In, Out]
+    pipe: list[str]
+    distribution: JobDistribution[In, Out] = SerialDistribution()
+
+    class Config:
+        arbitrary_types_allowed: bool = True
+
+    def attach_process(
+        self,
+        endpoint: Processor[Out, Intermediate],
+        name: str | None = None
+    ) -> 'Pipeline[Out, Intermediate]':
+        return Pipeline(
+            write_log=self.write_log, processor=connect_processors(self.processor, endpoint, self.distribution),
+            pipe=self.pipe + [name if name else endpoint.__name__]
+        )
+
+    def attach(self, endpoint: Callable[[Out], Iterable[Intermediate]], name: str) -> 'Pipeline[Out, Intermediate]':
+        return self.attach_process(create_processor(endpoint, name), name)
+
+    def feed(self, item: In) -> Iterable[Out | Exception]:
+        return (
+            entry.data for entry in self.processor(LogEntry.from_value(item), self.write_log)
+        )
+
+    def inject(self, items: Iterable[In]) -> Iterable[Out | Exception]:
+        for item in items:
+            for result in self.processor(LogEntry.from_value(item), self.write_log):
+                yield result.data
+
+
+def create_pipeline(
+    write_log: LogWriter = no_log,
+    distribution: JobDistribution[In, In] | None = None
+) -> Pipeline[In, In]:
+    return Pipeline(
+       write_log=write_log, processor=source_processor, pipe=['_source'],
+       distribution=distribution if distribution else SerialDistribution()
+   )
+
+
+
