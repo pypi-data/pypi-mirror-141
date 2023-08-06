d9e09f5e6645ad1c114cf3864d45e6976b56c008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:19 +0000

    Add package cloudshell-cp-azure/3.5.0/cloudshell_cp_azure-3.5.0-py3-none-any.whl

---
+++ packages/cloudshell-cp-azure/cloudshell_cp_azure-3.5.0-py3-none-any.whl/cloudshell/cp/azure/exceptions.py
@@ -0,0 +1,34 @@
+class BaseAzureException(Exception):
+    pass
+
+
+class AzureTaskTimeoutException(BaseAzureException):
+    pass
+
+
+class InvalidAttrException(BaseAzureException):
+    pass
+
+
+class ResourceNotFoundException(BaseAzureException):
+    pass
+
+
+class MultipleResourceFoundException(BaseAzureException):
+    pass
+
+
+class NetworkNotFoundException(BaseAzureException):
+    pass
+
+
+class InvalidDiskTypeException(BaseAzureException):
+    pass
+
+
+class NoFreeDiskLunException(BaseAzureException):
+    pass
+
+
+class ReconfigureVMException(BaseAzureException):
+    pass
