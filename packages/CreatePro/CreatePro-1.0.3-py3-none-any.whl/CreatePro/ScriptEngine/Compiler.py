9a3540977f042c3c86494cadcc1cadb0c6d8f88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Add package CreatePro/1.0.3/CreatePro-1.0.3-py3-none-any.whl

---
+++ packages/CreatePro/CreatePro-1.0.3-py3-none-any.whl/CreatePro/ScriptEngine/Compiler.py
@@ -0,0 +1,68 @@
+"""
+Tree build
+"""
+import re
+
+
+class Tree:
+    def __init__(self, path):
+        super(Tree, self).__init__()
+        self.Tree = None
+        self.Dict = {}
+        self.state = None
+        self.Leval = 0
+        self.Describe = []
+        doing = []
+        final = []
+        with open(path) as data:
+            describe_line = data.readlines()
+        with open(path) as data:
+            self.Data = data.read()
+        for i in describe_line:
+            level = re.search('[^ ]', i).span()[0] // 4
+            if re.findall('~(.+?)~', i):
+                continue
+            elif level > self.Leval:
+                pre = doing[-1][0:2]
+                doing[-1] = pre + [True]
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+                self.Leval = level
+            elif level < self.Leval:
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+                final += doing
+                doing = []
+                self.Leval = 0
+            elif level == self.Leval:
+                doing.append([i.replace('\n', '').replace(' ', ''), level, False])
+            else:
+                raise Exception
+        self.Describe = final
+
+    def dict(self):
+        for i in self.Describe:
+            split = i[0].split(':')
+            self.Dict[split[0]] = i[1:3]
+
+    def tree(self, leveled=0, father='~', data=None):
+        doing = {}
+        final = {}
+        if data is None:
+            data = self.Dict
+        for k, v in data.items():
+            if leveled == v[0]:
+                if v[1]:
+                    final[k] = None
+                    doing = {}
+                    father = k
+                else:
+                    final[k] = re.findall('[ ]*' + k + ':(.+?)\n', self.Data)[0]
+            elif leveled > v[0]:
+                doing[k] = v
+            else:
+                doing[k] = v
+                final[father] = self.tree(leveled + 1, father, doing)
+        return final
+
+    def value(self):
+        self.Tree = self.tree()
+        return self.Tree
