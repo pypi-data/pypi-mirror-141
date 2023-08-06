b3f63444edb6eeec9761666009851c0f1a365adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:01 +0000

    Add package mhidayat-dsnd-probability/0.1/mhidayat_dsnd_probability-0.1.tar.gz

---
+++ packages/mhidayat-dsnd-probability/mhidayat_dsnd_probability-0.1.tar.gz/mhidayat_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mhidayat_dsnd_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mhidayat_dsnd_probability'],
+      author = 'Muhammad Hidayat',
+      author_email = 'mhidayatz86@gmail.com',
+      zip_safe=False)
