12be871de20c4cfe594b165b0882109693325be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:17 +0000

    Add package aisquared/0.1.2.dev4/aisquared-0.1.2.dev4.tar.gz

---
+++ packages/aisquared/aisquared-0.1.2.dev4.tar.gz/aisquared-0.1.2.dev4/aisquared/config/feedback/__init__.py
@@ -0,0 +1,5 @@
+from .SimpleFeedback import SimpleFeedback
+from .BinaryFeedback import BinaryFeedback
+from .MulticlassFeedback import MulticlassFeedback
+from .RegressionFeedback import RegressionFeedback
+from .ModelFeedback import ModelFeedback
