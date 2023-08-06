446123a7a50b1858e2cdbd57d75d2c09d7396cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:03 +0000

    Add package jionlp/1.3.53/jionlp-1.3.53-py2.py3-none-any.whl

---
+++ packages/jionlp/jionlp-1.3.53-py2.py3-none-any.whl/jionlp/algorithm/cws/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+# library: jionlp
+# author: dongrixinyu
+# license: Apache License 2.0
+# Email: dongrixinyu.89@163.com
+# github: https://github.com/dongrixinyu/JioNLP
+# description: Preprocessing tool for Chinese NLP
+
+
+from .cws_data_converter import tag2word, word2tag
+from .measure import F1
+
+f1 = F1()
