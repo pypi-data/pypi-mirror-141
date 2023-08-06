a99d97209423e972be76cfee32a3dc951ed46e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:26 +0000

    Add package qiwibyadam/0.0.1/qiwibyadam-0.0.1.tar.gz

---
+++ packages/qiwibyadam/qiwibyadam-0.0.1.tar.gz/qiwibyadam-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='qiwibyadam',
+      version='0.0.1',
+      description='fake:)',
+      packages=['qiwibyadam'],
+      author_email='adamisaev92341@gmail.com',
+      zip_safe=False)
