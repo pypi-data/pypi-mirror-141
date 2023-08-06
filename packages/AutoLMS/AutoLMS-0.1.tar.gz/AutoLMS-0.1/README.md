e9352a922b4f39e8b11e6d382c19d6a3e024a246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:48 +0000

    Add package AutoLMS/0.1/AutoLMS-0.1.tar.gz

---
+++ packages/AutoLMS/AutoLMS-0.1.tar.gz/AutoLMS-0.1/README.md
@@ -0,0 +1,72 @@
+# AutoLMS
+
+AutoLMS is a tool for automating LMS and get into class automatically on specified times.
+
+### Suported Universities
+
+- University of Tehran
+- Kharazmi University
+- Shahid Beheshti University
+
+## Install
+
+Clone the project:
+
+```sh
+$ git clone https://github.com/itsamirhn/AutoLMS
+$ cd AutoLMS
+```
+
+## Requirements
+
+You have to install these manually:
+
+- python3
+- Chrome
+- [ChromeDriver](https://chromedriver.chromium.org/downloads)
+
+Then install python requirements:
+
+```sh
+$ pip install -r requirements.txt
+```
+
+### How to download ChromeDriver?
+
+If you are new to ChromeDriver, follow instructions below:
+
+1) Go this [Address](https://chromedriver.chromium.org/downloads)
+2) Choose your Chrome corresponding version in the list
+    1) For example if you are using Chrome version 97.xx then choose
+       `ChromeDriver 97.xx`
+    2) Be careful, if you got error on run, it may because of difference between Chrome and ChromeDriver versions
+3) Select the file compatible with your system (Mac or Linux or Win)
+4) Unzip the downloaded file
+5) Put the `chromedriver` anywhere you want (It's just easier if you put it inside Cloned folder)
+6) On the setup of AutoLMS, type the path for `chromdriver` or just drag & drop the file into terminal
+
+## Configuration
+
+Just as easy as a command:
+
+```sh
+$ python main.py setup
+```
+
+And anytime you wanted to edit configs (e.g. add new session or course):
+
+```sh
+$ python main.py edit
+```
+
+## Run
+
+Finally, run the program for always:
+
+```sh
+$ python main.py run
+```
+
+## License
+
+AutoLMS is MIT licensed.
---
+++ packages/AutoLMS/AutoLMS-0.1.tar.gz/AutoLMS-0.1/autolms/main.py
@@ -0,0 +1,74 @@
+import datetime
+import time
+
+import fire
+import schedule
+
+import autolms.config as config
+from autolms.core import LMSDriver
+
+
+def check(chromedriver, username, password, url, course_id=None):
+    driver = LMSDriver(chromedriver, username, password, url)
+    if course_id:
+        driver.go_to_course_last_event(course_id)
+    else:
+        driver.go_to_last_event()
+
+
+def go(course_id=None):
+    cfg = config.get_config()
+    if not cfg:
+        print("Run `setup` command for config first.")
+        return
+    chromedriver = cfg["paths"]["chromedriver"]
+    username = cfg["credentials"]["username"]
+    password = cfg["credentials"]["password"]
+    url = cfg["credentials"]["url"]
+    try:
+        print("Checking...")
+        check(chromedriver, username, password, url, course_id)
+    except Exception as e:
+        print(e)
+        print("End")
+
+
+def setup():
+    config.setup()
+    print("You can use `run` command now.")
+
+
+def edit():
+    config.edit()
+    print("You can use `run` command now.")
+
+
+def run():
+    cfg = config.get_config()
+    if not cfg:
+        print("Run `setup` command for config first.")
+        return
+    for course in cfg["courses"]:
+        for session in course["sessions"]:
+            session_time = datetime.datetime.strptime(session["time"], "%H:%M") \
+                           - datetime.timedelta(seconds=int(cfg["options"]["rush"]))
+            rushed_time = session_time.strftime("%H:%M:%S")
+            getattr(schedule.every(), session["day"]).at(rushed_time).do(go, course_id=course["id"])
+            print("Add job for %s on %s at %s" % (course["name"], session["day"], rushed_time))
+    print("Running schedule...")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def main():
+    fire.Fire({
+        "go": go,
+        "run": run,
+        "setup": setup,
+        "edit": edit,
+    })
+
+
+if __name__ == "__main__":
+    main()
