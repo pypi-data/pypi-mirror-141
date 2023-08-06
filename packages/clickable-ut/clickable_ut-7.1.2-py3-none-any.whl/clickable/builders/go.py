44561211d1377e80696af4dc12c5a366ccf7456a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:49 +0000

    Add package clickable-ut/7.1.2/clickable_ut-7.1.2-py3-none-any.whl

---
+++ packages/clickable-ut/clickable_ut-7.1.2-py3-none-any.whl/clickable/builders/go.py
@@ -0,0 +1,20 @@
+from clickable.config.constants import Constants
+
+from .base import Builder
+
+
+class GoBuilder(Builder):
+    name = Constants.GO
+
+    def build(self):
+        command = [
+            '/usr/local/go/bin/go',
+            'build',
+            '-pkgdir', self.config.build_dir,
+            '-i',
+            '-o', self.config.install_dir,
+            '.',
+        ]
+
+        self.container.run_command(' '.join(command),
+                                   use_build_dir=False, cwd=self.config.src_dir)
