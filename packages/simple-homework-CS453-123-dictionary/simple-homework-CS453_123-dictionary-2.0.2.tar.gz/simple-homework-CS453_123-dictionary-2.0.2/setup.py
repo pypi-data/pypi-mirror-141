fade1822663a6dc6f24ffccee73fbad4b196e170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:12 +0000

    Add package simple-homework-CS453-123-dictionary/2.0.2/simple-homework-CS453_123-dictionary-2.0.2.tar.gz

---
+++ packages/simple-homework-CS453-123-dictionary/simple-homework-CS453_123-dictionary-2.0.2.tar.gz/simple-homework-CS453_123-dictionary-2.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="simple-homework-CS453_123-dictionary",
+    author="Emre Derman",
+    version="2.0.2",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
