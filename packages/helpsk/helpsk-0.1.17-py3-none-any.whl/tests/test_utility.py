0c7d6b444fd4084a5ce20d53b5b27a17264c4bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:10 +0000

    Add package helpsk/0.1.17/helpsk-0.1.17-py3-none-any.whl

---
+++ packages/helpsk/helpsk-0.1.17-py3-none-any.whl/tests/test_utility.py
@@ -0,0 +1,11 @@
+import unittest
+
+from helpsk import utility as hu
+
+
+# noinspection PyMethodMayBeStatic
+class TestValidation(unittest.TestCase):
+
+    def test_is_debugging(self):
+        # not sure how to test is_debugging is true, except manually
+        self.assertFalse(hu.is_debugging())
