dd8d42e9cda9be0b0558150a17574f3caa7573a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:58 +0000

    Add package jeff-distributions/0.1/jeff_distributions-0.1.tar.gz

---
+++ packages/jeff-distributions/jeff_distributions-0.1.tar.gz/jeff_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jeff_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jeff_distributions'],
+      author = 'Jeff Zhong',
+      author_email = 'jeffwzhong@gmail.com',
+      zip_safe=False)
