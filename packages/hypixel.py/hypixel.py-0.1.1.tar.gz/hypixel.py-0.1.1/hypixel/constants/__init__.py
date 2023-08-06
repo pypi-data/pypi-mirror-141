2e7a8fe0a2d7da06b0dc559cade97c119e5c728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package hypixel.py/0.1.1/hypixel.py-0.1.1.tar.gz

---
+++ packages/hypixel.py/hypixel.py-0.1.1.tar.gz/hypixel.py-0.1.1/hypixel/constants/__init__.py
@@ -0,0 +1,12 @@
+"""
+hypixel.constants
+~~~~~~~~~~~~~~~~~~~
+
+A module for storing constant data.
+
+(c) 2021-present duhby
+MIT License, see LICENSE for more details.
+"""
+
+from . import aliases
+from .game_types import *
