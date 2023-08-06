9c73925c06d414356ce1913d08446c896067b468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:26 +0000

    Add package set-math/0.0/set_math-0.0.tar.gz

---
+++ packages/set-math/set_math-0.0.tar.gz/set_math-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='set_math',
+      version='0.0',
+      description='calculation of algebraic sets',
+      packages=['C:/Users/Алексей/PycharmProjects/Set_math'],
+      author_email='ggammi1007@yandex.ru',
+      zip_safe=False)
